d239a2a4073ad8d0b7833284bc69d187fb86cc7f
noot <elizabethjbinks@gmail.com>
2019-02-19 12:39:14 -0500

    implement DecodeByteArray for arrays with length < 1 << 30

---
+++ codec/decode.go
@@ -0,0 +1,74 @@
+package codec
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+func DecodeInteger(b []byte) (int64, error) {
+	if len(b) == 1 {
+		return int64(b[0] >> 2), nil
+	} else if len(b) == 2 {
+		o := binary.LittleEndian.Uint16(b) >> 2
+		return int64(o), nil
+	} else if len(b) == 4 {
+		o := binary.LittleEndian.Uint32(b) >> 2
+		return int64(o), nil
+	} else {
+		topSixBits := (binary.LittleEndian.Uint16(b) & 0xff) >> 2
+		byteLen := topSixBits + 4
+
+		if byteLen == 4 {
+			return int64(binary.LittleEndian.Uint32(b[1 : byteLen+1])), nil
+		} else if byteLen > 4 && byteLen < 8 {
+			upperBytes := make([]byte, 8-byteLen)
+			upperBytes = append(b[5:byteLen+1], upperBytes...)
+			upper := int64(binary.LittleEndian.Uint32(upperBytes)) << 32
+			lower := int64(binary.LittleEndian.Uint32(b[1:5]))
+			return int64(upper + lower), nil
+		}
+		return int64(0), nil
+	}
+}
+
+func DecodeByteArray(b []byte) ([]byte, error) {
+	if b[0] & 0x03 == 0 { // encoding of length: 1 byte mode
+		length, err := DecodeInteger([]byte{b[0]})
+		if err != nil {
+			return nil, err
+		}
+
+		if length == 0 || length > 1 << 6 {
+			return nil, errors.New("Could not decode invalid byte array")
+		}
+
+		return b[1:length+1], nil
+	} else if b[0] & 0x03 == 1 { // encoding of length: 2 byte mode
+		// pass first two bytes of byte array to decode length
+		length, err := DecodeInteger(b[0:2]) 
+		if err != nil {
+			return nil, err
+		}
+
+		if length < 1 << 6 || length > 1 << 14 {
+			return nil, errors.New("Could not decode invalid byte array")
+		}
+
+		return b[2:length+2], nil
+	} else if b[0] & 0x03 == 2 { // encoding of length: 4 byte mode
+		// pass first four bytes of byte array to decode length
+		length, err := DecodeInteger(b[0:4]) 
+		if err != nil {
+			return nil, err
+		}
+
+		if length < 1 << 14 || length > 1 << 30 {
+			return nil, errors.New("Could not decode invalid byte array")
+		}
+
+		return b[4:length+4], nil
+	} else if b[0] & 0x03 == 3 { // encoding of length: big-integer mode
+
+	}
+	return []byte{}, nil
+}
---
+++ codec/decode_test.go
@@ -0,0 +1,64 @@
+package codec
+
+import (
+	"bytes"
+	"testing"
+)
+
+type decodeIntTest struct {
+	val    []byte
+	output int64
+	error  string
+}
+
+type decodeByteArrayTest struct {
+	val    []byte
+	output []byte
+	error  string
+}
+
+var decodeIntTests = []decodeIntTest{
+	// compact integers
+	{val: []byte{0x00}, output: int64(0)},
+	{val: []byte{0x04}, output: int64(1)},
+	{val: []byte{0xa8}, output: int64(42)},
+	{val: []byte{0x01, 0x01}, output: int64(64)},
+	{val: []byte{0x15, 0x01}, output: int64(69)},
+	{val: []byte{0xfd, 0xff}, output: int64(16383)},
+	{val: []byte{0x02, 0x00, 0x01, 0x00}, output: int64(16384)},
+	{val: []byte{0xfe, 0xff, 0xff, 0xff}, output: int64(1073741823)},
+	{val: []byte{0x03, 0x00, 0x00, 0x00, 0x40}, output: int64(1073741824)},
+	{val: []byte{0x03, 0xff, 0xff, 0xff, 0xff}, output: int64(1<<32 - 1)},
+	{val: []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x01}, output: int64(1 << 32)},
+}
+
+var decodeByteArrayTests = []decodeByteArrayTest{
+	// byte arrays
+	{val: []byte{0x04, 0x01}, output: []byte{0x01}},
+	{val: []byte{0x04, 0xff}, output: []byte{0xff}},
+	{val: []byte{0x08, 0x01, 0x01}, output: []byte{0x01, 0x01}},
+	{val: append([]byte{0x01, 0x01}, byteArrayLen64()...), output: byteArrayLen64()},
+	{val: append([]byte{0x02, 0x00, 0x01, 0x00}, byteArrayLen16384()...), output: byteArrayLen16384()},
+}
+
+func TestDecodeInts(t *testing.T) {
+	for _, test := range decodeIntTests {
+		output, err := DecodeInteger(test.val)
+		if err != nil {
+			t.Error(err)
+		} else if output != test.output {
+			t.Errorf("Fail: got %d expected %d", output, test.output)
+		} 
+	}
+}
+
+func TestDecodeByteArrays(t *testing.T) {
+	for _, test := range decodeByteArrayTests {
+		output, err := DecodeByteArray(test.val)
+		if err != nil {
+			t.Error(err)
+		} else if !bytes.Equal(output, test.output) {
+			t.Errorf("Fail: got %d expected %d", output, test.output)
+		} 
+	}
+}
