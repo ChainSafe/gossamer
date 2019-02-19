e607bdbcc4ebf4e6466623a94807ae0477c3c6ca
noot <elizabethjbinks@gmail.com>
2019-02-19 16:21:58 -0500

    fix gometalinter issues

--- codec/decode.go
+++ codec/decode.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 )
 
+// DecodeInteger accepts a byte array representing a SCALE encoded integer and performs SCALE decoding of the
+// int, then returns it
 func DecodeInteger(b []byte) (int64, error) {
 	if len(b) == 1 {
 		return int64(b[0] >> 2), nil
@@ -14,23 +16,26 @@ func DecodeInteger(b []byte) (int64, error) {
 	} else if len(b) == 4 {
 		o := binary.LittleEndian.Uint32(b) >> 2
 		return int64(o), nil
-	} else {
-		topSixBits := (binary.LittleEndian.Uint16(b) & 0xff) >> 2
-		byteLen := topSixBits + 4
+	} 
 
-		if byteLen == 4 {
-			return int64(binary.LittleEndian.Uint32(b[1 : byteLen+1])), nil
-		} else if byteLen > 4 && byteLen < 8 {
-			upperBytes := make([]byte, 8-byteLen)
-			upperBytes = append(b[5:byteLen+1], upperBytes...)
-			upper := int64(binary.LittleEndian.Uint32(upperBytes)) << 32
-			lower := int64(binary.LittleEndian.Uint32(b[1:5]))
-			return int64(upper + lower), nil
-		}
-		return int64(0), nil
+	topSixBits := (binary.LittleEndian.Uint16(b) & 0xff) >> 2
+	byteLen := topSixBits + 4
+
+	if byteLen == 4 {
+		return int64(binary.LittleEndian.Uint32(b[1 : byteLen+1])), nil
+	} else if byteLen > 4 && byteLen < 8 {
+		upperBytes := make([]byte, 8-byteLen)
+		upperBytes = append(b[5:byteLen+1], upperBytes...)
+		upper := int64(binary.LittleEndian.Uint32(upperBytes)) << 32
+		lower := int64(binary.LittleEndian.Uint32(b[1:5]))
+		return upper + lower, nil
 	}
+	
+	return int64(0), nil
 }
 
+// DecodeByteArray accepts a byte array representing a SCALE encoded byte array and performs SCALE decoding
+// of the byte array, then returns it.  If it is invalid, return nil and error
 func DecodeByteArray(b []byte) ([]byte, error) {
 	if b[0] & 0x03 == 0 { // encoding of length: 1 byte mode
 		length, err := DecodeInteger([]byte{b[0]})
@@ -87,6 +92,8 @@ func DecodeByteArray(b []byte) ([]byte, error) {
 	return nil, errors.New("could not decode invalid byte array")
 }
 
+// DecodeBool accepts a byte array representing a SCALE encoded bool and performs SCALE decoding
+// of the bool then returns it. if invalid, return false and an error
 func DecodeBool(b byte) (bool, error) {
 	if b == 1 {
 		return true, nil
