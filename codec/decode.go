731d7492d07acc7096234fc36f4b536369d2ceb2
noot <elizabethjbinks@gmail.com>
2019-02-20 12:06:58 -0500

    change DecodeByteArray to only have one return

--- codec/decode.go
+++ codec/decode.go
@@ -37,59 +37,51 @@ func DecodeInteger(b []byte) (int64, error) {
 // DecodeByteArray accepts a byte array representing a SCALE encoded byte array and performs SCALE decoding
 // of the byte array, then returns it.  If it is invalid, return nil and error
 func DecodeByteArray(b []byte) ([]byte, error) {
+	var o []byte
+	var err error 
+
 	if b[0] & 0x03 == 0 { // encoding of length: 1 byte mode
 		length, err := DecodeInteger([]byte{b[0]})
-		if err != nil {
-			return nil, err
+
+		if err == nil && (length == 0 || length > 1 << 6 || int64(len(b)) < length + 1) {
+			err = errors.New("could not decode invalid byte array")
 		}
 
-		if length == 0 || length > 1 << 6 || int64(len(b)) < length + 1 {
-			return nil, errors.New("could not decode invalid byte array")
-		}
-
-		return b[1:length+1], nil
+		o = b[1:length+1]
 	} else if b[0] & 0x03 == 1 { // encoding of length: 2 byte mode
 		// pass first two bytes of byte array to decode length
 		length, err := DecodeInteger(b[0:2]) 
-		if err != nil {
-			return nil, err
+
+		if err == nil && (length < 1 << 6 || length > 1 << 14 || int64(len(b)) < length + 2) { 
+			err = errors.New("could not decode invalid byte array")
 		}
 
-		if length < 1 << 6 || length > 1 << 14 || int64(len(b)) < length + 2 { 
-			return nil, errors.New("could not decode invalid byte array")
-		}
-
-		return b[2:length+2], nil
+		o = b[2:length+2]
 	} else if b[0] & 0x03 == 2 { // encoding of length: 4 byte mode
 		// pass first four bytes of byte array to decode length
 		length, err := DecodeInteger(b[0:4]) 
-		if err != nil {
-			return nil, err
-		}
 
-		if length < 1 << 14 || length > 1 << 30 || int64(len(b)) < length + 4 {
+		if err == nil && (length < 1 << 14 || length > 1 << 30 || int64(len(b)) < length + 4) {
 			return nil, errors.New("could not decode invalid byte array")
 		}
 
-		return b[4:length+4], nil
+		o = b[4:length+4]
 	} else if b[0] & 0x03 == 3 { // encoding of length: big-integer mode
 		length, err := DecodeInteger(b)
-		if err != nil {
-			return nil, err
-		}
 
 		// get the length of the encoded length
 		topSixBits := (binary.LittleEndian.Uint16(b) & 0xff) >> 2
 		byteLen := topSixBits + 4
 
-		if length < 1 << 30  || int64(len(b)) < length + int64(byteLen) {
-			return nil, errors.New("could not decode invalid byte array")
+		if err == nil && (length < 1 << 30  || int64(len(b)) < length + int64(byteLen)) {
+			err = errors.New("could not decode invalid byte array")
 		}
 
-		return b[int64(byteLen):length+int64(byteLen)], nil
+		o = b[int64(byteLen):length+int64(byteLen)]
 	}
 
-	return nil, errors.New("could not decode invalid byte array")
+	return o, err
+	//return nil, errors.New("could not decode invalid byte array")
 }
 
 // DecodeBool accepts a byte array representing a SCALE encoded bool and performs SCALE decoding
