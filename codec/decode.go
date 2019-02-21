447361cd6e099b504469aa56273b07c8f6f225ff
noot <elizabethjbinks@gmail.com>
2019-02-21 13:59:28 -0500

    implement DecodeTuple

--- codec/decode.go
+++ codec/decode.go
@@ -8,21 +8,19 @@ import (
 
 // DecodeInteger accepts a byte array representing a SCALE encoded integer and performs SCALE decoding of the
 // int, then returns it
-func DecodeInteger(b []byte) (int64, error) {
+func DecodeInteger(b []byte) (o int64, bytesDecoded int64, err error) {
 	mode := b[0] & 0x03
-	if mode == 0 {
-		return int64(b[0] >> 2), nil
-	} else if mode == 1 {
+	if mode == 0 { // 1 byte mode
+		return int64(b[0] >> 2), 1, nil
+	} else if mode == 1 { // 2 byte mode
 		o := binary.LittleEndian.Uint16(b) >> 2
-		return int64(o), nil
-	} else if mode == 2 {
+		return int64(o), 2, nil
+	} else if mode == 2 { // 4 byte mode
 		o := binary.LittleEndian.Uint32(b) >> 2
-		return int64(o), nil
+		return int64(o), 4, nil
 	}
 
-	var o int64
-	var err error
-
+	// >4 byte mode
 	topSixBits := (binary.LittleEndian.Uint16(b) & 0xff) >> 2
 	byteLen := topSixBits + 4
 
@@ -31,11 +29,13 @@ func DecodeInteger(b []byte) (int64, error) {
 	}
 
 	if err == nil {
+		bytesDecoded = int64(byteLen) + 1
 		if byteLen == 4 {
-			o = int64(binary.LittleEndian.Uint32(b[1 : byteLen+1]))
+			o = int64(binary.LittleEndian.Uint32(b[1 : bytesDecoded]))
 		} else if byteLen > 4 && byteLen < 8 {
+			// need to pad upper bytes with zeros so that we can perform binary decoding
 			upperBytes := make([]byte, 8-byteLen)
-			upperBytes = append(b[5:byteLen+1], upperBytes...)
+			upperBytes = append(b[5:bytesDecoded], upperBytes...)
 			upper := int64(binary.LittleEndian.Uint32(upperBytes)) << 32
 			lower := int64(binary.LittleEndian.Uint32(b[1:5]))
 			o = upper + lower
@@ -46,49 +46,49 @@ func DecodeInteger(b []byte) (int64, error) {
 		}
 	}
 
-	return o, err
+	return o, bytesDecoded, err
 }
 
 // DecodeByteArray accepts a byte array representing a SCALE encoded byte array and performs SCALE decoding
 // of the byte array, then returns it.  If it is invalid, return nil and error
-func DecodeByteArray(b []byte) ([]byte, error) {
-	var o []byte
-	var err error
-
+func DecodeByteArray(b []byte) (o []byte, bytesDecoded int64, err error) {
 	mode := b[0] & 0x03
 	if mode == 0 { // encoding of length: 1 byte mode
-		length, err := DecodeInteger([]byte{b[0]})
+		length, _, err := DecodeInteger([]byte{b[0]})
 		if err == nil {
 			if length == 0 || length > 1<<6 || int64(len(b)) < length+1 {
 				err = errors.New("could not decode invalid byte array")
 			} else {
 				o = b[1 : length+1]
+				bytesDecoded = length + 1
 			}
 		}
 	} else if mode == 1 { // encoding of length: 2 byte mode
 		// pass first two bytes of byte array to decode length
-		length, err := DecodeInteger(b[0:2])
+		length, _, err := DecodeInteger(b[0:2])
 
 		if err == nil {
 			if length < 1<<6 || length > 1<<14 || int64(len(b)) < length+2 {
 				err = errors.New("could not decode invalid byte array")
 			} else {
 				o = b[2 : length+2]
+				bytesDecoded = length + 2
 			}
 		}
 	} else if mode == 2 { // encoding of length: 4 byte mode
 		// pass first four bytes of byte array to decode length
-		length, err := DecodeInteger(b[0:4])
+		length, _, err := DecodeInteger(b[0:4])
 
 		if err == nil {
 			if length < 1<<14 || length > 1<<30 || int64(len(b)) < length+4 {
 				err = errors.New("could not decode invalid byte array")
 			} else {
 				o = b[4 : length+4]
+				bytesDecoded = length + 4
 			}
 		}
 	} else if mode == 3 { // encoding of length: big-integer mode
-		length, err := DecodeInteger(b)
+		length, _, err := DecodeInteger(b)
 
 		if err == nil {
 			// get the length of the encoded length
@@ -99,11 +99,12 @@ func DecodeByteArray(b []byte) ([]byte, error) {
 				err = errors.New("could not decode invalid byte array")
 			} else {
 				o = b[int64(byteLen)+1 : length+int64(byteLen)+1]
+				bytesDecoded = int64(byteLen) + length + 1
 			}
 		}
 	}
 
-	return o, err
+	return o, bytesDecoded, err
 }
 
 // DecodeBool accepts a byte array representing a SCALE encoded bool and performs SCALE decoding
@@ -115,34 +116,47 @@ func DecodeBool(b byte) (bool, error) {
 		return false, nil
 	}
 
-	return false, errors.New("cannot decode invalid boolean")
+	return false, errors.New("cannot decode invalid boolean") 
 }
 
 func DecodeTuple(b []byte, t interface{}) (interface{}, error) {
-	v := reflect.ValueOf(t)
+	v := reflect.ValueOf(t).Elem()
+	
+	var bytesDecoded int64 = 0
+	var byteLen int64
+	var err error
+	var o interface{}
 
-	output := make([]interface{}, v.NumField())
-	//bytesDecoded := 0
+	val := reflect.Indirect(reflect.ValueOf(t))
 
+	// iterate through each field in the struct
 	for i := 0; i < v.NumField(); i++ {
-		//fmt.Println(v.Field(i).Interface())
+		// get the field value at i
+		fieldValue := val.Field(i)
+
 		switch v.Field(i).Interface().(type) {
 		case []byte:
-			o, err := DecodeByteArray(b)
+			o, byteLen, err = DecodeByteArray(b[bytesDecoded:])
 			if err != nil {
 				return nil, err
 			}
 
-			output[i] = o
+			// get the pointer to the value and set the value
+			ptr := fieldValue.Addr().Interface().(*[]byte)
+			*ptr = o.([]byte)
 		case int64:
-			o, err := DecodeInteger(b)
+			o, byteLen, err = DecodeInteger(b[bytesDecoded:])
 			if err != nil {
 				return nil, err
 			}
 
-			output[i] = o
+			// get the pointer to the value and set the value
+			ptr := fieldValue.Addr().Interface().(*int64)
+			*ptr = o.(int64)
 		}
+
+		bytesDecoded = bytesDecoded + byteLen
 	}
 
-	return output, nil
+	return t, err
 }
--- codec/decode_test.go
+++ codec/decode_test.go
@@ -2,17 +2,20 @@ package codec
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 type decodeIntTest struct {
 	val    []byte
 	output int64
+	bytesDecoded int64
 }
 
 type decodeByteArrayTest struct {
 	val    []byte
 	output []byte
+	bytesDecoded int64 
 }
 
 type decodeBoolTest struct {
@@ -22,29 +25,29 @@ type decodeBoolTest struct {
 
 type decodeTupleTest struct {
 	val 	[]byte
-	t  	interface{}
+	t  		interface{}
 	output 	interface{}
 }
 
 var decodeIntTests = []decodeIntTest{
 	// compact integers
-	{val: []byte{0x00}, output: int64(0)},
-	{val: []byte{0x04}, output: int64(1)},
-	{val: []byte{0xa8}, output: int64(42)},
-	{val: []byte{0x01, 0x01}, output: int64(64)},
-	{val: []byte{0x15, 0x01}, output: int64(69)},
-	{val: []byte{0xfd, 0xff}, output: int64(16383)},
-	{val: []byte{0x02, 0x00, 0x01, 0x00}, output: int64(16384)},
-	{val: []byte{0xfe, 0xff, 0xff, 0xff}, output: int64(1073741823)},
-	{val: []byte{0x03, 0x00, 0x00, 0x00, 0x40}, output: int64(1073741824)},
-	{val: []byte{0x03, 0xff, 0xff, 0xff, 0xff}, output: int64(1<<32 - 1)},
-	{val: []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x01}, output: int64(1 << 32)},
+	{val: []byte{0x00}, output: int64(0), bytesDecoded: 1},
+	{val: []byte{0x04}, output: int64(1), bytesDecoded: 1},
+	{val: []byte{0xa8}, output: int64(42), bytesDecoded: 1},
+	{val: []byte{0x01, 0x01}, output: int64(64), bytesDecoded: 2},
+	{val: []byte{0x15, 0x01}, output: int64(69), bytesDecoded: 2},
+	{val: []byte{0xfd, 0xff}, output: int64(16383), bytesDecoded: 2},
+	{val: []byte{0x02, 0x00, 0x01, 0x00}, output: int64(16384), bytesDecoded: 4},
+	{val: []byte{0xfe, 0xff, 0xff, 0xff}, output: int64(1073741823), bytesDecoded: 4},
+	{val: []byte{0x03, 0x00, 0x00, 0x00, 0x40}, output: int64(1073741824), bytesDecoded: 5},
+	{val: []byte{0x03, 0xff, 0xff, 0xff, 0xff}, output: int64(1<<32 - 1), bytesDecoded: 5},
+	{val: []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x01}, output: int64(1 << 32), bytesDecoded: 6},
 }
 
 var decodeByteArrayTests = []decodeByteArrayTest{
 	// byte arrays
-	{val: []byte{0x04, 0x01}, output: []byte{0x01}},
-	{val: []byte{0x04, 0xff}, output: []byte{0xff}},
+	{val: []byte{0x04, 0x01}, output: []byte{0x01}, bytesDecoded: 2},
+	{val: []byte{0x04, 0xff}, output: []byte{0xff}, bytesDecoded: 2},
 	{val: []byte{0x08, 0x01, 0x01}, output: []byte{0x01, 0x01}},
 	{val: append([]byte{0x01, 0x01}, byteArray(64)...), output: byteArray(64)},
 	{val: append([]byte{0xfd, 0xff}, byteArray(16384)...), output: byteArray(16383)},
@@ -59,23 +62,25 @@ var decodeBoolTests = []decodeBoolTest{
 }
 
 var decodeTupleTests = []decodeTupleTest{
-	{val: []byte{0x04, 0x01, 0x08}, t: struct{Foo []byte; Bar int64}{}, output: struct{Foo []byte; Bar int64}{[]byte{0x01}, 2}},
+	{val: []byte{0x04, 0x01, 0x08}, t: &struct{Foo []byte; Bar int64}{}, output: &struct{Foo []byte; Bar int64}{[]byte{0x01}, 2}},
 }
 
 func TestDecodeInts(t *testing.T) {
 	for _, test := range decodeIntTests {
-		output, err := DecodeInteger(test.val)
+		output, bytesDecoded, err := DecodeInteger(test.val)
 		if err != nil {
 			t.Error(err)
 		} else if output != test.output {
 			t.Errorf("Fail: got %d expected %d", output, test.output)
+		} else if bytesDecoded != test.bytesDecoded {
+			t.Errorf("Fail: got %d bytesDecoded expected %d", bytesDecoded, test.bytesDecoded)
 		}
 	}
 }
 
 func TestDecodeByteArrays(t *testing.T) {
 	for _, test := range decodeByteArrayTests {
-		output, err := DecodeByteArray(test.val)
+		output, _, err := DecodeByteArray(test.val)
 		if err != nil {
 			t.Error(err)
 		} else if !bytes.Equal(output, test.output) {
@@ -108,7 +113,7 @@ func TestDecodeTuples(t *testing.T) {
 		output, err := DecodeTuple(test.val, test.t)
 		if err != nil {
 			t.Error(err)
-		} else if output != test.output {
+		} else if !reflect.DeepEqual(output, test.output) {
 			t.Errorf("Fail: got %t expected %t", output, test.output)
 		}
 	}
