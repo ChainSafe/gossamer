a8dfff99994e7a8ace6810b6a3fee22ca421cf27
dut <dustinbrickwood204@gmail.com>
2019-02-22 14:43:26 -0500

    added comments

--- trie/trie.go
+++ trie/trie.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 )
 
+// Trie is a Merkle Patricia Trie.
+// The zero value is an empty trie with no database.
+// Use NewTrie to create a trie that sits on top of a database.
 type Trie struct {
 	db *Database
 	root [32]byte
 }
 
+// NewTrie creates a trie with an existing root node from db.
 func NewTrie(db *Database, root [32]byte) *Trie {
 	return &Trie{
 		db:	  db,
@@ -17,6 +21,7 @@ func NewTrie(db *Database, root [32]byte) *Trie {
 	}
 }
 
+// Put associates key with value in the trie.
 func(t *Trie) Put(k, v  []byte){
 	if err := t.TryPut(k,v); err != nil {
 		log.Println("Error in trie", err.Error())
@@ -42,7 +47,8 @@ func(t *Trie) set(n node, k, v []byte) (bool, error) {
 	return false, nil
 }
 
-
+// Get returns the value for key stored in the trie.
 func(t *Trie) Get(key []byte){}
 
+// Delete removes any existing value for key from the trie.
 func(t *Trie) Del(key []byte){}
