af1ae2e28d2b70a9de52da4035d1cf7fe3a679fc
Amer Ameen <amerameen@gmail.com>
2019-03-11 00:16:02 -0400

    added string tests for mode = 1 and mode = 2. mode = 3 test commented out because num_bytes_n = 3 results in a negative number for num_bytes_n - 4

--- codec/encode_test.go
+++ codec/encode_test.go
@@ -2,6 +2,7 @@ package codec
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 )
 
@@ -31,16 +32,38 @@ var encodeTests = []encodeTest{
 	{val: byteArray(64), output: append([]byte{0x01, 0x01}, byteArray(64)...)},
 	{val: byteArray(16384), output: append([]byte{0x02, 0x00, 0x01, 0x00}, byteArray(16384)...)},
 
-	// strings
-	{val: string("a"), output: []byte{0x04, 0x61}},
-	{val: string("go-pre"), output: []byte{0x04, 0x67, 0x6F, 0x2D, 0x70, 0x72, 0x65}},
-
 	// booleans
 	{val: true, output: []byte{0x01}},
 	{val: false, output: []byte{0x00}},
 }
 
 func TestEncode(t *testing.T) {
+
+	// Test strings for various values of n, mode. Also test strings with special characters
+	// TODO: Unicode?
+	testString1 := "We love you! We believe in open source as wonderful form of giving."
+	testString2 := strings.Repeat("We need a longer string to test with. Let's multiple this several times.", 230) //n = 16560
+	//testString3 := strings.Repeat("We need a longer string to test with. Let's multiple this several times.", 14913081) //n = 1 073 741 832 (> 2^30 = 1 073 741 824)
+
+	var testStrings = []encodeTest{
+		{val: string("a"),
+		 output: []byte{0x04, 0x61}},
+		{val: string("go-pre"),																// n =  6 = 0b110, mode = 0 = 0b00
+		 output: append([]byte{0x18}, string("go-pre")...)}, 							 	// n|mode = 0b11000 = 0x18, Enc = n | mode | 0x("go-pre")
+		{val: testString1,																	// n = 67 = 0b1000011, mode = 1 = 0b01
+		 output: append([]byte{0x0D,0x01}, testString1...)}, 								// n|mode = 0b00000001 00001101 = 0x010D (big endian) = 0x0D01 (little endian), Enc = n | mode | 0x("We love you!...")
+		{val: testString2,																	// n = 16560 = 0b1000000 10110000, mode = 2 = 0b10
+		 output: append([]byte{0xC2,0x02,0x01,0x00}, testString2...)},					 	// n|mode = 0b 00000001 00000010 11000010 = 0x102C2 (big endian) = 0xC20201 (little endian), Enc = n | mode | 0x("We need a...")
+		//{val: testString3,																	// n = 14913081 = 11100011 10001110 00111001, mode = 3 = 0b11, num_bytes_n = 3
+		//output: append([]byte{0x08,0x00,0x00,0x40,0x03}, testString3...)},					// (num_bytes_n - 4)|mode|n = 0b 00000011 01000000 00000000 00000000 00001000 = 0x03 40 00 00 08 (big endian) = 0x08 00 00 40 03 (little endian), Enc = (num_bytes_n - 4)|mode|n| 0x("We need a...")
+	}
+
+	//Append stringTests to other encodeTests
+	for _, testString := range testStrings {
+		encodeTests = append(encodeTests, testString)
+	}
+
+	//Run all tests
 	for _, test := range encodeTests {
 		output, err := Encode(test.val)
 		if err != nil {
