8551b0a212dfac9ba123bf321bd543aa9f65df7d
noot <elizabethjbinks@gmail.com>
2019-05-14 10:59:48 -0400

    change Print to use log15

--- trie/iterator.go
+++ trie/iterator.go
@@ -17,24 +17,24 @@
 package trie
 
 import (
-	"fmt"
+	log "github.com/inconshreveable/log15"
 )
 
 // Print prints the trie through pre-order traversal
 func (t *Trie) Print() {
-	fmt.Println("printing trie...")
+	log.Info("printing trie...")
 	t.print(t.root, nil)
 }
 
 func (t *Trie) print(current node, prefix []byte) {
 	switch c := current.(type) {
 	case *branch:
-		fmt.Printf("branch key %x children %b value %s\n", nibblesToKeyLE(append(prefix, c.key...)), c.childrenBitmap(), c.value)
+		log.Info("branch key %x children %b value %s\n", nibblesToKeyLE(append(prefix, c.key...)), c.childrenBitmap(), c.value)
 		for i, child := range c.children {
 			t.print(child, append(append(prefix, byte(i)), c.key...))
 		}
 	case *leaf:
-		fmt.Printf("leaf key %x val %x\n", nibblesToKeyLE(append(prefix, c.key...)), c.value)
+		log.Info("leaf key %x val %x\n", nibblesToKeyLE(append(prefix, c.key...)), c.value)
 	default:
 		// do nothing
 	}
