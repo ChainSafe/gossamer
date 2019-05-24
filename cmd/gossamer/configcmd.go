2fd9dcd05503c2df5ce6e088cf2e1ef631df153d
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-05-24 14:26:30 -0500

    added dump config command to CLI

--- cmd/gossamer/configcmd.go
+++ cmd/gossamer/configcmd.go
@@ -16,6 +16,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ChainSafe/gossamer/cmd/utils"
 	cfg "github.com/ChainSafe/gossamer/config"
 	"github.com/ChainSafe/gossamer/dot"
@@ -26,10 +27,22 @@ import (
 	"github.com/urfave/cli"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
+	"unicode"
 )
 
 var (
+	dumpConfigCommand = cli.Command{
+		Action:      dumpConfig,
+		Name:        "dumpconfig",
+		Usage:       "Show configuration values",
+		ArgsUsage:   "",
+		Flags:       append(append(nodeFlags, rpcFlags...)),
+		Category:    "CONFIGURATION DEBUGGING",
+		Description: `The dumpconfig command shows configuration values.`,
+	}
+
 	configFileFlag = cli.StringFlag{
 		Name:  "config",
 		Usage: "TOML configuration file",
@@ -101,7 +114,7 @@ func loadConfig(file string) (*cfg.Config, error) {
 		}
 	}()
 	var config *cfg.Config
-	if err = toml.NewDecoder(f).Decode(&config); err != nil {
+	if err = tomlSettings.NewDecoder(f).Decode(&config); err != nil {
 		log.Error("decoding toml error", "err", err.Error())
 	}
 	return config, err
@@ -133,4 +146,48 @@ func startP2PService(cfg *p2p.ServiceConfig) *p2p.Service {
 		log.Error("error starting p2p", "err", err.Error())
 	}
 	return srv
+}
+
+// dumpConfig is the dumpconfig command.
+func dumpConfig(ctx *cli.Context) error {
+	fig, err := setConfig(ctx)
+	if err != nil {
+		return err
+	}
+	comment := ""
+
+	out, err := tomlSettings.Marshal(&fig)
+	if err != nil {
+		return err
+	}
+
+	dump := os.Stdout
+	if ctx.NArg() > 0 {
+		dump, err = os.OpenFile(ctx.Args().Get(0), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return err
+		}
+		defer dump.Close()
+	}
+	dump.WriteString(comment)
+	dump.Write(out)
+
+	return nil
+}
+
+// These settings ensure that TOML keys use the same names as Go struct fields.
+var tomlSettings = toml.Config{
+	NormFieldName: func(rt reflect.Type, key string) string {
+		return key
+	},
+	FieldToKey: func(rt reflect.Type, field string) string {
+		return field
+	},
+	MissingField: func(rt reflect.Type, field string) error {
+		link := ""
+		if unicode.IsUpper(rune(rt.Name()[0])) && rt.PkgPath() != "main" {
+			link = fmt.Sprintf(", see https://godoc.org/%s#%s for available fields", rt.PkgPath(), rt.Name())
+		}
+		return fmt.Errorf("field '%s' is not defined in %s%s", field, rt.String(), link)
+	},
 }
