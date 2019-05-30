1e4ced4b3057bb695721d97dbc0f1c7eda499313
noot <elizabethjbinks@gmail.com>
2019-05-30 12:28:10 -0400

    pass in block number 1 to Core_authorities in test

--- runtime/runtime_test.go
+++ runtime/runtime_test.go
@@ -118,14 +118,6 @@ func TestExecVersion(t *testing.T) {
 }
 
 func TestExecAuthorities(t *testing.T) {
-	// expected := &Version{
-	// 	Spec_name:         []byte("polkadot"),
-	// 	Impl_name:         []byte("parity-polkadot"),
-	// 	Authoring_version: 1,
-	// 	Spec_version:      109,
-	// 	Impl_version:      0,
-	// }
-
 	r, err := newRuntime(t)
 	if err != nil {
 		t.Fatal(err)
@@ -133,22 +125,14 @@ func TestExecAuthorities(t *testing.T) {
 		t.Fatal("did not create new VM")
 	}
 
-	res, err := r.Exec("Core_authorities", 0, 0)
+	var offset int64 = 1
+	var length int64 = 1
+	copy(r.vm.Memory[offset:offset+length], []byte{4})
+
+	res, err := r.Exec("Core_authorities", offset, length)
 	if err != nil {
 		t.Fatalf("could not exec wasm runtime: %s", err)
 	}
 
 	t.Logf("%v\n", res)
-
-	// version := res.(*Version)
-	// t.Logf("Spec_name: %s\n", version.Spec_name)
-	// t.Logf("Impl_name: %s\n", version.Impl_name)
-	// t.Logf("Authoring_version: %d\n", version.Authoring_version)
-	// t.Logf("Spec_version: %d\n", version.Spec_version)
-	// t.Logf("Impl_version: %d\n", version.Impl_version)
-
-	// if !reflect.DeepEqual(version, expected) {
-	// 	t.Errorf("Fail: got %v expected %v\n", version, expected)
-	// }
 }
-
