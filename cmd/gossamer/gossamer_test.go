9a6b65d52e957c625658f8f88d3a82ee2fc08170
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-06-10 20:11:34 -0500

    linted

--- cmd/gossamer/gossamer_test.go
+++ cmd/gossamer/gossamer_test.go
@@ -1,9 +1,8 @@
 package main_test
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/inconshreveable/log15"
-	"github.com/rendon/testcli"
 	"go/build"
 	"os"
 	"os/exec"
@@ -12,44 +11,50 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	cfg "github.com/ChainSafe/gossamer/config"
+	"github.com/ChainSafe/gossamer/p2p"
+	"github.com/ChainSafe/gossamer/polkadb"
+	"github.com/inconshreveable/log15"
+	"github.com/rendon/testcli"
 )
 
 var binaryname = "gossamer-test"
 
 const configTest = "config-test.toml"
-const timeFormat     = "2006-01-02T15:04:05-0700"
+const timeFormat = "2006-01-02T15:04:05-0700"
 
 func setup() {
 	gopath := os.Getenv("GOPATH")
 	if gopath == "" {
 		gopath = build.Default.GOPATH
 	}
-	r := exec.Command("go", "build", "-o", gopath + "/bin/gossamer-test")
+	r := exec.Command("go", "build", "-o", gopath+"/bin/gossamer-test")
 	err := r.Run()
 	if err != nil {
-		log15.Crit("could not execute binary", "executable",binaryname, "err",err)
+		log15.Crit("could not execute binary", "executable", binaryname, "err", err)
 		os.Exit(1)
 	}
 	run := exec.Command(`gossamer-test`)
 	err = run.Run()
 	if err != nil {
-		log15.Crit("could not execute binary", "executable",binaryname, "err",err)
+		log15.Crit("could not execute binary", "executable", binaryname, "err", err)
 		os.Exit(1)
 	}
 	fp, err := os.Create(configTest)
 	if err != nil {
-		log15.Crit("could not create test config", "config",configTest, "err",err)
+		log15.Crit("could not create test config", "config", configTest, "err", err)
 		os.Exit(1)
 	}
 
 	testConfig := fmt.Sprintf("%s%s%s%v%s%s%v%s%s%s",
 		"[ServiceConfig]\n", "BootstrapNodes = []\n", "Port = ",
-		7005,"\n","RandSeed = ", 0, "\n\n", "[DbConfig]\n",
+		7005, "\n", "RandSeed = ", 0, "\n\n", "[DbConfig]\n",
 		"Datadir = \"\"\x0A")
 
 	_, err = fp.WriteString(testConfig)
 	if err != nil {
-		log15.Crit("could not write to test config", "config","config-test.toml", "err",err)
+		log15.Crit("could not write to test config", "config", "config-test.toml", "err", err)
 		os.Exit(1)
 	}
 }
@@ -61,10 +66,10 @@ func teardown() {
 		os.Exit(1)
 	}
 	if err := os.RemoveAll("./chaindata"); err != nil {
-		log15.Warn("removal of temp directory bin failed", "err",err)
+		log15.Warn("removal of temp directory bin failed", "err", err)
 	}
 	if err := os.RemoveAll("./config-test.toml"); err != nil {
-		log15.Warn("removal of temp config.toml failed", "err",err)
+		log15.Warn("removal of temp config.toml failed", "err", err)
 	}
 }
 
@@ -92,16 +97,17 @@ func TestCliArgs(t *testing.T) {
 	res := expectedResponses()
 	defer teardown()
 	tests := []struct {
-		name    string
-		args    []string
+		name     string
+		args     []string
 		expected string
 	}{
-		{"dumpconfig with config set", []string{"--config", "config-test.toml","dumpconfig"}, res[0]},
+		{"dumpconfig with config set", []string{"--config", "config-test.toml", res[0]}, res[0]},
 		{"config specified", []string{"--config", "config-test.toml"}, res[1]},
 		{"default config", []string{}, res[1]},
 	}
 
-	for i, tt := range tests {
+	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			gopath := os.Getenv("GOPATH")
 			if gopath == "" {
@@ -114,30 +120,35 @@ func TestCliArgs(t *testing.T) {
 				teardown()
 				t.Fatal(err)
 			}
-			if i > 0 {
-				if !strings.ContainsAny(string(actual), res[1]) {
-					teardown()
-					t.Fatalf("actual = %s, expected = %s", string(actual), res[1])
-				}
-			} else {
-			if !reflect.DeepEqual(string(actual), res[0]) {
+			if !strings.ContainsAny(string(actual), tt.expected) {
 				teardown()
-				t.Fatalf("actual = %s, expected = %s", actual, res[0])
-			}}
+				t.Fatalf("actual = %s, expected = %s", string(actual), tt.expected)
+			}
 		})
 	}
 }
 
 func expectedResponses() []string {
-	dumpConfigExp := fmt.Sprintf("%s%s%s%v%s%s%v%s%s%s",
-		"[ServiceConfig]\n", "BootstrapNodes = []\n", "Port = ",
-		7005,"\n","RandSeed = ", 0, "\n\n", "[DbConfig]\n",
-		"Datadir = \"\"\x0A")
+	var testConfig = &cfg.Config{
+		ServiceConfig: &p2p.ServiceConfig{
+			BootstrapNodes: []string{},
+			Port:           7001,
+			RandSeed:       32,
+		},
+		DbConfig: polkadb.DbConfig{
+			Datadir: "",
+		},
+	}
 
+	b, err := json.Marshal(testConfig)
+	if err != nil {
+		log15.Error("could not marshal testConfig for expected response", "err", err)
+	}
+	dumpCfgExp := fmt.Sprintf("%v", string(b))
 	startingChainMsg := fmt.Sprintf("%s%v%s", "t=", time.Now().Format(timeFormat),
 		" lvl=info msg=\"🕸️ starting p2p service\" blockchain=gossamer\x0A")
 
-	response := []string{dumpConfigExp, startingChainMsg}
+	response := []string{dumpCfgExp, startingChainMsg}
 
 	return response
 }
