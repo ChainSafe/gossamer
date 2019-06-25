98004a50e89c38a1d84854cf83dc764021b34b7f
David Ansermino <david@chainsafe.io>
2019-06-25 11:49:39 -0400

    Migrates to go-libp2p-core

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -29,12 +29,11 @@ import (
 	ds "github.com/ipfs/go-datastore"
 	dsync "github.com/ipfs/go-datastore/sync"
 	libp2p "github.com/libp2p/go-libp2p"
+	core "github.com/libp2p/go-libp2p-core"
+	host "github.com/libp2p/go-libp2p-core/host"
+	net "github.com/libp2p/go-libp2p-core/network"
 	crypto "github.com/libp2p/go-libp2p-crypto"
-	host "github.com/libp2p/go-libp2p-host"
 	kaddht "github.com/libp2p/go-libp2p-kad-dht"
-	net "github.com/libp2p/go-libp2p-net"
-	peer "github.com/libp2p/go-libp2p-peer"
-	ps "github.com/libp2p/go-libp2p-peerstore"
 	rhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	ma "github.com/multiformats/go-multiaddr"
 )
@@ -47,7 +46,7 @@ type Service struct {
 	host           host.Host
 	hostAddr       ma.Multiaddr
 	dht            *kaddht.IpfsDHT
-	bootstrapNodes []*ps.PeerInfo
+	bootstrapNodes []*core.PeerAddrInfo
 }
 
 // Config is used to configure a p2p service
@@ -136,7 +135,7 @@ func (s *Service) Broadcast(msg []byte) (err error) {
 }
 
 // Send sends a message to a specific peer
-func (s *Service) Send(peer ps.PeerInfo, msg []byte) error {
+func (s *Service) Send(peer core.PeerAddrInfo, msg []byte) error {
 	err := s.host.Connect(s.ctx, peer)
 	if err != nil {
 		return err
@@ -156,7 +155,7 @@ func (s *Service) Send(peer ps.PeerInfo, msg []byte) error {
 }
 
 // Ping pings a peer
-func (s *Service) Ping(peer peer.ID) error {
+func (s *Service) Ping(peer core.PeerID) error {
 	ps, err := s.dht.FindPeer(s.ctx, peer)
 	if err != nil {
 		return fmt.Errorf("could not find peer: %s", err)
