cd04d333758e89bf4e42e99a79ee8358fa138f9a
noot <elizabethjbinks@gmail.com>
2019-07-02 18:36:36 -0400

    add sr25519_keypair_from_seed

--- crypto/sr25519.go
+++ crypto/sr25519.go
@@ -1,15 +1,50 @@
 package crypto
 
 /*
-#cgo LDFLAGS:  -Wl,-rpath,${SRCDIR}/sr25519 -L${SRCDIR}/sr25519 -lsr25519crust
+#cgo LDFLAGS:  -Wl,-rpath,${SRCDIR}/libsr25519 -L${SRCDIR}/libsr25519 -lsr25519crust
 #include "./sr25519.h"
 */
 import "C"
 
 import (
+	"errors"
 	"unsafe"
 )
 
-func sr25519_derive_keypair_hard(keypair_out, pair_ptr, cc_ptr []byte) {
-	C.sr25519_derive_keypair_hard((*C.uchar)(unsafe.Pointer(&keypair_out)), (*C.uchar)(unsafe.Pointer(&pair_ptr)), (*C.uchar)(unsafe.Pointer(&cc_ptr))) 
+const (
+	SR25519_CHAINCODE_SIZE = 32
+	SR25519_KEYPAIR_SIZE = 96
+	SR25519_PUBLIC_SIZE = 32
+	SR25519_SECRET_SIZE = 64
+	SR25519_SEED_SIZE = 32
+	SR25519_SIGNATURE_SIZE = 64
+)
+
+/**
+ * Generate a key pair.
+ *  keypair_out: keypair [32b key | 32b nonce | 32b public], pre-allocated output buffer of SR25519_KEYPAIR_SIZE bytes
+ *  seed: generation seed - input buffer of SR25519_SEED_SIZE bytes
+ */
+func sr25519_keypair_from_seed(keypair_out, seed_ptr *[]byte) error {
+	C.sr25519_keypair_from_seed((*C.uchar)(unsafe.Pointer(keypair_out)), (*C.uchar)(unsafe.Pointer(seed_ptr))) 
+	return nil
+}
+
+/**
+ * Perform a derivation on a secret
+ *  keypair_out: pre-allocated output buffer of SR25519_KEYPAIR_SIZE bytes
+ *  pair_ptr: existing keypair - input buffer of SR25519_KEYPAIR_SIZE bytes
+ *  cc_ptr: chaincode - input buffer of SR25519_CHAINCODE_SIZE bytes
+ */
+func sr25519_derive_keypair_hard(keypair_out, pair_ptr, cc_ptr *[]byte) error {
+	if len(*pair_ptr) != SR25519_KEYPAIR_SIZE {
+		return errors.New("pair_ptr length not equal to SR25519_KEYPAIR_SIZE")
+	}
+
+	if len(*cc_ptr) != SR25519_CHAINCODE_SIZE {
+		return errors.New("cc_ptr length not equal to SR25519_CHAINCODE_SIZE")
+	}
+
+	C.sr25519_derive_keypair_hard((*C.uchar)(unsafe.Pointer(keypair_out)), (*C.uchar)(unsafe.Pointer(pair_ptr)), (*C.uchar)(unsafe.Pointer(cc_ptr))) 
+	return nil
 }
--- crypto/sr25519_test.go
+++ crypto/sr25519_test.go
@@ -1,13 +1,43 @@
 package crypto
 
 import (
+	"math/rand"
 	"testing"
 )
 
+func TestKeypairFromSeed(t *testing.T) {
+	keypair_out := make([]byte, 96)
+	seed_ptr := []byte{}
+
+	buf := make([]byte, 32)
+	rand.Read(buf)
+	seed_ptr = buf
+
+	err := sr25519_keypair_from_seed(&keypair_out, &seed_ptr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(keypair_out)
+} 
+
 func TestDeriveKeypair(t *testing.T) {
-	keypair_out := []byte{}
+	keypair_out := make([]byte, 96)
 	pair_ptr := []byte{}
 	cc_ptr := []byte{}
 
-	sr25519_derive_keypair_hard(keypair_out, pair_ptr, cc_ptr)
+	buf := make([]byte, 96)
+	rand.Read(buf)
+	pair_ptr = buf
+
+	buf = make([]byte, 32)
+	rand.Read(buf)
+	cc_ptr = buf
+
+	err := sr25519_derive_keypair_hard(&keypair_out, &pair_ptr, &cc_ptr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(keypair_out)
 } 
