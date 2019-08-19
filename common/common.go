7c04ad7a7044fe260e48a57cf0fd051b325601b5
David Ansermino <david@chainsafe.io>
2019-08-19 07:23:41 -0400

    Adds error to indicated odd length string

--- common/common.go
+++ common/common.go
@@ -27,6 +27,10 @@ func HexToBytes(in string) ([]byte, error) {
 	if strings.Compare(in[:2], "0x") != 0 {
 		return nil, errors.New("could not byteify non 0x prefixed string")
 	}
+	// Ensure we have an even length, otherwise hex.DecodeString will fail and return zero hash
+	if len(in) % 2 != 0 {
+		return nil, errors.New("cannot decode a odd length string")
+	}
 	in = in[2:]
 	out, err := hex.DecodeString(in)
 	return out, err
