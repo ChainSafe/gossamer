ffd1755dcde64d109b9e56d4286a404ccfef4e6c
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-09-11 20:29:49 -0500

    removed extra line

--- rpc/modules/system_test.go
+++ rpc/modules/system_test.go
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the gossamer library. If not, see <http://www.gnu.org/licenses/>.
-//
 
 package modules
 
