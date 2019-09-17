a52c7499d5775e720cb1ee1afddd0fba233353bd
noot <elizabethjbinks@gmail.com>
2019-09-17 14:05:11 -0400

    fix err

--- runtime/wasmer_test.go
+++ runtime/wasmer_test.go
@@ -779,7 +779,7 @@ func TestExt_keccak_256(t *testing.T) {
 
 	// test case from https://github.com/debris/tiny-keccak/blob/master/tests/keccak.rs#L4
 	expected, err := common.HexToHash("0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470")
-	if errorr != nil {
+	if err != nil {
 		t.Fatal(err)
 	}
 
