ca54eded8cc0ea63bd87fb08887f8141cfe634ef
noot <elizabethjbinks@gmail.com>
2019-09-18 15:21:42 -0400

    implement basic tx queue

--- common/transaction/types.go
+++ common/transaction/types.go
@@ -4,18 +4,20 @@ import (
 	"github.com/ChainSafe/gossamer/common"
 )
 
-type Pool map[common.Hash]common.Extrinsic
+type Pool map[common.Hash]*ValidTransaction
 
 type Queue interface {
 	Pop() *ValidTransaction
 	Insert(vt *ValidTransaction)
 }
 
+type TransactionTag []byte
+
 // see: https://github.com/paritytech/substrate/blob/5420de3face1349a97eb954ae71c5b0b940c31de/core/sr-primitives/src/transaction_validity.rs#L178
 type Validity struct {
 	priority  uint64
-	requires  []byte
-	provides  []byte
+	requires  []TransactionTag
+	provides  []TransactionTag
 	longevity uint64
 	propagate bool
 }
