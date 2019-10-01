67a57ef1480ad02aa03c1c96345b30c9063a7994
noot <elizabethjbinks@gmail.com>
2019-10-01 16:49:22 -0400

    add Genesis file, struct, parser

---
+++ config/genesis/genesis.go
@@ -0,0 +1,35 @@
+package genesis
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type Genesis struct {
+	Name string
+	Id string
+	Bootnodes []string
+	ProtocolId string
+	Genesis genesisFields
+}
+
+type genesisFields struct {
+	Raw []map[string]string
+}
+
+func parseJson(file string) (*Genesis, error) {
+	fp, err := filepath.Abs(file)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	g := new(Genesis)
+	err = json.Unmarshal(data, g)
+	return g, err
+}
---
+++ config/genesis/genesis_test.go
@@ -0,0 +1,28 @@
+package genesis
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseJson(t *testing.T) {
+	file := "../../genesis.json"
+	genesis, err := parseJson(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &Genesis{
+		Name: "gossamer",
+		Id: 	"gossamer",
+		Bootnodes: 	[]string{"/ip4/104.211.54.233/tcp/30363/p2p/16Uiu2HAmFWPUx45xYYeCpAryQbvU3dY8PWGdMwS2tLm1dB1CsmCj"},
+		ProtocolId: "gossamer",
+		Genesis: genesisFields{
+			Raw: []map[string]string{map[string]string{"0x3a636f6465": "0x00"}},
+		},
+	}
+
+	if !reflect.DeepEqual(expected, genesis) {
+		t.Fatalf("Fail: expected %v got %v", expected, genesis)
+	}
+}
