b992cd91af054d2bf80f15ce9a3115f2aa3c5698
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-10-08 09:19:40 -0500

    Added initial block operations * BlockHeader and BlockData read/writes * Unit Tests * Prefixes for KV-store entry

--- core/rawdb/rawdb.go
+++ core/rawdb/rawdb.go
@@ -1,37 +1,34 @@
 package rawdb
 
 import (
-	"bytes"
 	"encoding/json"
+
 	"github.com/ChainSafe/gossamer/common"
 	"github.com/ChainSafe/gossamer/core/types"
 	"github.com/ChainSafe/gossamer/polkadb"
-	"math/big"
-
 	log "github.com/ChainSafe/log15"
 )
 
+// check checks to see if there an error if so writes err + message to terminal
 func check(e error, msg string) {
 	if e != nil {
-		log.Warn(msg, "err", e)
+		log.Crit(msg, "err", e)
 	}
 }
 
-// SetHeader stores a block header into the database
+// SetHeader stores a block header into the KV-store; key is headerPrefix + hash
 func SetHeader(db polkadb.Writer, header *types.BlockHeader) {
 	hash := header.Hash
 
 	// Write the encoded header
-	buf := new(bytes.Buffer)
-	if err := json.NewEncoder(buf).Encode(header); err != nil {
-		log.Crit("error encoding header to bytes", "err", err)
-	}
+	bh, err := json.Marshal(header)
+	check(err, "Failed to encode header to bytes")
 
-	if err := db.Put(headerKey(hash), buf.Bytes()); err != nil {
-		log.Crit("Failed to store header", "err", err)
-	}
+	err = db.Put(headerKey(hash), bh)
+	check(err, "Failed to store header")
 }
 
+// GetHeader retrieves block header from KV-store using headerKey
 func GetHeader(db polkadb.Reader, hash common.Hash) *types.BlockHeader {
 	var result *types.BlockHeader
 	data, err := db.Get(headerKey(hash))
@@ -42,17 +39,25 @@ func GetHeader(db polkadb.Reader, hash common.Hash) *types.BlockHeader {
 	return result
 }
 
-// BLOCK WRITES
+// SetBlockData writes blockData to KV-store; key is blockDataPrefix + hash
+func SetBlockData(db polkadb.Writer, blockData *types.BlockData) {
+	hash := blockData.Hash
 
-func SetBlockData(db polkadb.Writer, blockData *types.BlockData) {}
-func SetBlockHash(db polkadb.Writer, num *big.Int) {}
-func SetBestHash(db polkadb.Writer, hash *common.Hash) {}
-func SetBestNumber(db polkadb.Writer, num *big.Int) {}
+	// Write the encoded header
+	bh, err := json.Marshal(blockData)
+	check(err, "Failed to encode blockData to bytes")
 
-// BLOCK READS
+	err = db.Put(blockDataKey(hash), bh)
+	check(err, "Failed to store blockData")
+}
 
-func GetBlockHeader(db polkadb.Reader, hash *common.Hash) *types.BlockHeader { return &types.BlockHeader{}}
-func GetBlockData(db polkadb.Reader, hash *common.Hash) *types.BlockData { return &types.BlockData{}}
-func GetBlockHash(db polkadb.Reader, num *big.Int) *common.Hash { return &common.Hash{}}
-func GetBestHash(db polkadb.Reader) *common.Hash { return &common.Hash{}}
-func GetBestNumber(db polkadb.Reader) *big.Int { return &big.Int{}}
+// GetBlockData retrieves blockData from KV-store using blockDataKey
+func GetBlockData(db polkadb.Reader, hash common.Hash) *types.BlockData {
+	var result *types.BlockData
+	data, err := db.Get(blockDataKey(hash))
+	check(err, "Failed to retrieve block header")
+
+	err = json.Unmarshal(data, &result)
+	check(err, "Failed to unmarshal block header")
+	return result
+}
--- core/rawdb/rawdb_test.go
+++ core/rawdb/rawdb_test.go
@@ -1,32 +1,51 @@
 package rawdb
 
 import (
+	"math/big"
+	"testing"
+
 	"github.com/ChainSafe/gossamer/common"
 	"github.com/ChainSafe/gossamer/core/types"
 	"github.com/ChainSafe/gossamer/polkadb"
-	"math/big"
-	"testing"
 )
 
-func setup() polkadb.Database {
- 	return polkadb.NewMemDatabase()
+func setup() (polkadb.Database, *types.BlockHeader) {
+	h := &types.BlockHeader{
+		ParentHash:     common.BytesToHash([]byte("parent_test")),
+		Number:         big.NewInt(2),
+		StateRoot:      common.BytesToHash([]byte("state_root_test")),
+		ExtrinsicsRoot: common.BytesToHash([]byte("extrinsics_test")),
+		Digest:         []byte("digest_test"),
+		Hash:           common.BytesToHash([]byte("hash_test")),
+	}
+	return polkadb.NewMemDatabase(), h
 }
 
 func TestSetHeader(t *testing.T) {
-	memDB := setup()
-	h := &types.BlockHeader{
-		ParentHash: common.BytesToHash([]byte("parent_test")),
-		Number: big.NewInt(2),
-		StateRoot: common.BytesToHash([]byte("state_root_test")),
-		ExtrinsicsRoot: common.BytesToHash([]byte("extrinsics_test")),
-		Digest: []byte("digest_test"),
-		Hash: common.BytesToHash([]byte("hash_test")),
-	}
+	memDB, h := setup()
 
 	SetHeader(memDB, h)
 	if entry := GetHeader(memDB, h.Hash); entry == nil {
 		t.Fatalf("stored header not found")
 	} else if entry.Hash != h.Hash {
 		t.Fatalf("Retrieved header mismatch: have %v, want %v", entry, h)
+	}
+}
+
+func TestSetBlockData(t *testing.T) {
+	var body *types.BlockBody
+	memDB, h := setup()
+
+	bd := &types.BlockData{
+		Hash:   common.BytesToHash([]byte("bd_hash")),
+		Header: h,
+		Body:   body,
+	}
+
+	SetBlockData(memDB, bd)
+	if entry := GetBlockData(memDB, bd.Hash); entry == nil {
+		t.Fatalf("stored blockData not found")
+	} else if entry.Hash != bd.Hash {
+		t.Fatalf("Retrieved blockData mismatch: have %v, want %v", entry, bd)
 	}
 }
