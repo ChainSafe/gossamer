d17305753e1cc2a329645c4566e8f31047534e42
noot <elizabethjbinks@gmail.com>
2019-10-10 07:00:34 -0400

    update parse genesis test and genesis path

--- config/genesis/genesis_test.go
+++ config/genesis/genesis_test.go
@@ -1,17 +1,14 @@
 package genesis
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
 	"reflect"
 	"testing"
 )
 
 func TestParseGenesisJson(t *testing.T) {
-	file := "../../test_genesis.json"
-	genesis, err := LoadGenesisJsonFile(file)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	expected := &Genesis{
 		Name:       "gossamer",
 		Id:         "gossamer",
@@ -20,6 +17,29 @@ func TestParseGenesisJson(t *testing.T) {
 		Genesis: genesisFields{
 			Raw: []map[string]string{{"0x3a636f6465": "0x00"}},
 		},
+	}
+
+	// Create temp file
+	file, err := ioutil.TempFile("", "genesis-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	// Grab json encoded bytes
+	bz, err := json.Marshal(expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Write to temp file
+	_, err = file.Write(bz)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	genesis, err := LoadGenesisJsonFile(file.Name())
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	if !reflect.DeepEqual(expected, genesis) {
