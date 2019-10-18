01749c41f4d7c3fd08c8fc865c21b44063bebab3
David Ansermino <david@chainsafe.io>
2019-10-18 14:59:46 -0400

    Cleanup

--- cmd/gossamer/configcmd.go
+++ cmd/gossamer/configcmd.go
@@ -237,20 +237,3 @@ func dumpConfig(ctx *cli.Context) error {
 	}
 	return nil
 }
-
-// These settings ensure that TOML keys use the same names as Go struct fields.
-//var tomlSettings = toml.Config{
-//	NormFieldName: func(rt reflect.Type, key string) string {
-//		return key
-//	},
-//	FieldToKey: func(rt reflect.Type, field string) string {
-//		return field
-//	},
-//	MissingField: func(rt reflect.Type, field string) error {
-//		link := ""
-//		if unicode.IsUpper(rune(rt.Name()[0])) && rt.PkgPath() != "main" {
-//			link = fmt.Sprintf(", see https://godoc.org/%s#%s for available fields", rt.PkgPath(), rt.Name())
-//		}
-//		return fmt.Errorf("field '%s' is not defined in %s%s", field, rt.String(), link)
-//	},
-//}
