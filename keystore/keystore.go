86b609f892fec38810c21d46e954647c1f1773eb
noot <elizabethjbinks@gmail.com>
2019-11-22 12:52:42 -0500

    add mutex to keystore

--- keystore/keystore.go
+++ keystore/keystore.go
@@ -1,6 +1,8 @@
 package keystore
 
 import (
+	"sync"
+
 	"github.com/ChainSafe/gossamer/common"
 	"github.com/ChainSafe/gossamer/crypto"
 )
@@ -8,6 +10,7 @@ import (
 type Keystore struct {
 	// map of public key encodings to keypairs
 	keys map[common.Address]crypto.Keypair
+	lock sync.RWMutex
 }
 
 func NewKeystore() *Keystore {
@@ -17,11 +20,15 @@ func NewKeystore() *Keystore {
 }
 
 func (ks *Keystore) Insert(kp crypto.Keypair) {
+	ks.lock.Lock()
+	defer ks.lock.Unlock()
 	pub := kp.Public()
 	addr := crypto.PublicKeyToAddress(pub)
 	ks.keys[addr] = kp
 }
 
 func (ks *Keystore) Get(pub common.Address) crypto.Keypair {
+	ks.lock.RLock()
+	defer ks.lock.RUnlock()
 	return ks.keys[pub]
 }
