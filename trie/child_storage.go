d30b891e3534c5a15ff17e237dda468753e4d9ee
noot <elizabethjbinks@gmail.com>
2019-11-22 12:29:25 -0500

    add PutIntoChild

--- trie/child_storage.go
+++ trie/child_storage.go
@@ -2,18 +2,19 @@ package trie
 
 import (
 	"fmt"
+
+	"github.com/ChainSafe/gossamer/common"
 )
 
 var ChildStorageKeyPrefix = []byte(":child_storage:")
 
-func (t *Trie) PutChild(child *Trie) error {
+func (t *Trie) PutChild(child *Trie, childKey []byte) error {
 	childHash, err := child.Hash()
 	if err != nil {
 		return err
 	}
 
-	childBytes := [32]byte(childHash)
-	key := append(ChildStorageKeyPrefix, childBytes[:]...)
+	key := append(ChildStorageKeyPrefix, childKey[:]...)
 	exists, err := t.Get(key)
 	if err != nil {
 		return err
@@ -23,5 +24,21 @@ func (t *Trie) PutChild(child *Trie) error {
 		return fmt.Errorf("child already exists at %s%x", ChildStorageKeyPrefix, childHash)
 	}
 
-	return t.Put(key, childBytes[:])
+	value := [32]byte(childHash)
+	return t.Put(key, value[:])
 }
+
+func (t *Trie) PutIntoChild(storageKey, key, value []byte) error {
+	childHash, err := t.Get(storageKey)
+	if err != nil {
+		return err
+	}
+
+	hash := [32]byte{}
+	copy(hash[:], childHash)
+	childTrie := t.children[common.Hash(hash)]
+
+	return childTrie.Put(key, value)
+}
+
+//func (t *Trie)
