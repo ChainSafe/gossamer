8561acaa9053e64d17b213a619c1474f59ecbd87
noot <elizabethjbinks@gmail.com>
2019-12-03 12:41:11 -0500

    add types

---
+++ consensus/grandpa/types.go
@@ -0,0 +1,41 @@
+package grandpa
+
+import (
+	"github.com/ChainSafe/gossamer/common"
+	"github.com/ChainSafe/gossamer/crypto"
+)
+
+type Voter struct {
+	key     crypto.Keypair
+	voterId uint64
+}
+
+type State struct {
+	voters  []Voter
+	counter uint64
+	round   uint64
+}
+
+type Vote struct {
+	hash   common.Hash
+	number uint64
+}
+
+type VoteMessage struct {
+	round   uint64
+	counter uint64
+	pubkey  [32]byte // ed25519 public key
+	stage   byte     // 0 for pre-vote, 1 for pre-commit
+}
+
+type Justification struct {
+	vote      Vote
+	signature []byte
+	pubkey    [32]byte
+}
+
+type FinalizationMessage struct {
+	round         uint64
+	vote          Vote
+	justification Justification
+}
