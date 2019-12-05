40606c2cd2a9711c2ab6431cfb71f846d7e9fe8e
David Ansermino <david@chainsafe.io>
2019-12-05 09:55:18 -0500

    Adds nolint for unused

--- consensus/grandpa/types.go
+++ consensus/grandpa/types.go
@@ -7,41 +7,41 @@ import (
 
 //nolint:structcheck
 type Voter struct {
-	key     crypto.Keypair
-	voterId uint64
+	key     crypto.Keypair //nolint:unused
+	voterId uint64         //nolint:unused
 }
 
-//nolint:structcheck
+// nolint:structcheck
 type State struct {
-	voters  []Voter
-	counter uint64
-	round   uint64
+	voters  []Voter //nolint:unused
+	counter uint64  //nolint:unused
+	round   uint64  //nolint:unused
 }
 
-//nolint:structcheck
+// nolint:structcheck
 type Vote struct {
-	hash   common.Hash
-	number uint64
+	hash   common.Hash //nolint:unused
+	number uint64      //nolint:unused
 }
 
-//nolint:structcheck
+// nolint:structcheck
 type VoteMessage struct {
-	round   uint64
-	counter uint64
-	pubkey  [32]byte // ed25519 public key
-	stage   byte     // 0 for pre-vote, 1 for pre-commit
+	round   uint64   //nolint:unused
+	counter uint64   //nolint:unused
+	pubkey  [32]byte //nolint:unused // ed25519 public key
+	stage   byte     //nolint:unused  // 0 for pre-vote, 1 for pre-commit
 }
 
-//nolint:structcheck
+// nolint:structcheck
 type Justification struct {
-	vote      Vote
-	signature []byte
-	pubkey    [32]byte
+	vote      Vote     //nolint:unused
+	signature []byte   //nolint:unused
+	pubkey    [32]byte //nolint:unused
 }
 
-//nolint:structcheck
+// nolint:structcheck
 type FinalizationMessage struct {
-	round         uint64
-	vote          Vote
-	justification Justification
+	round         uint64        //nolint:unused
+	vote          Vote          //nolint:unused
+	justification Justification //nolint:unused
 }
