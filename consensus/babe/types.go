fd8f906a9d1cdfdc9bbe660b56d55bad6636fdb6
noot <elizabethjbinks@gmail.com>
2019-12-06 16:18:52 -0500

    remove unneeded types

--- consensus/babe/types.go
+++ consensus/babe/types.go
@@ -20,10 +20,6 @@ import (
 	"github.com/ChainSafe/gossamer/crypto/sr25519"
 )
 
-// TODO: change to Schnorrkel keys
-type VrfPublicKey [32]byte
-type VrfPrivateKey [64]byte
-
 // BabeConfiguration contains the starting data needed for Babe
 // see: https://github.com/paritytech/substrate/blob/426c26b8bddfcdbaf8d29f45b128e0864b57de1c/core/consensus/babe/primitives/src/lib.rs#L132
 type BabeConfiguration struct {
