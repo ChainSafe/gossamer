28955551828fb5a55a70e69fd9c854b54ca80312
edwardmack <ed@edwardmack.com>
2019-12-13 16:07:36 -0500

    make StorageState public

--- state/storage.go
+++ state/storage.go
@@ -6,48 +6,48 @@ import (
 	"github.com/ChainSafe/gossamer/trie"
 )
 
-type storageState struct {
+type StorageState struct {
 	trie *trie.Trie
 	Db   *polkadb.StateDB
 }
 
-func NewStorageState(dataDir string) (*storageState, error) {
+func NewStorageState(dataDir string) (*StorageState, error) {
 	stateDb, err := polkadb.NewStateDB(dataDir)
 	if err != nil {
 		return nil, err
 	}
-	return &storageState{
+	return &StorageState{
 		trie: &trie.Trie{},
 		Db:   stateDb,
 	}, nil
 }
 
-func (s *storageState) ExistsStorage(key []byte) (bool, error) {
+func (s *StorageState) ExistsStorage(key []byte) (bool, error) {
 	val, err := s.trie.Get(key)
 	return (val != nil), err
 }
 
-func (s *storageState) GetStorage(key []byte) ([]byte, error) {
+func (s *StorageState) GetStorage(key []byte) ([]byte, error) {
 	return s.trie.Get(key)
 }
 
-func (s *storageState) StorageRoot() (common.Hash, error) {
+func (s *StorageState) StorageRoot() (common.Hash, error) {
 	return s.trie.Hash()
 }
 
-func (s *storageState) EnumeratedTrieRoot(values [][]byte) {
+func (s *StorageState) EnumeratedTrieRoot(values [][]byte) {
 	//TODO
 }
 
-func (s *storageState) SetStorage(key []byte, value []byte) error {
+func (s *StorageState) SetStorage(key []byte, value []byte) error {
 	return s.trie.Put(key, value)
 }
 
-func (s *storageState) ClearPrefix(prefix []byte) {
+func (s *StorageState) ClearPrefix(prefix []byte) {
 	// Implemented in ext_clear_prefix
 }
 
-func (s *storageState) ClearStorage(key []byte) error {
+func (s *StorageState) ClearStorage(key []byte) error {
 	return s.trie.Delete(key)
 }
 
