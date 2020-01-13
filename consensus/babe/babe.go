fe0378c0917c0cf7c44d36909938732d17c95ab5
noot <elizabethjbinks@gmail.com>
2020-01-13 13:43:32 -0500

    add test; change type lengths to constants

--- consensus/babe/babe.go
+++ consensus/babe/babe.go
@@ -142,20 +142,6 @@ func (b *Session) runLottery(slot uint64) ([]byte, error) {
 	return nil, nil
 }
 
-func (b *Session) verifySlotWinner(slot uint64, header *BabeHeader) (bool, error) {
-	if len(b.authorityData) <= int(header.BlockProducerIndex) {
-		return false, fmt.Errorf("no authority data for index %d", header.BlockProducerIndex)
-	}
-
-	pub := b.authorityData[header.BlockProducerIndex].id
-
-	slotBytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(slotBytes, slot)
-	vrfInput := append(slotBytes, b.config.Randomness)
-
-	return pub.VrfVerify(vrfInput, header.VrfOutput[:], header.VrfProof[:])
-}
-
 func (b *Session) vrfSign(input []byte) (out []byte, proof []byte, err error) {
 	return b.keypair.VrfSign(input)
 }
