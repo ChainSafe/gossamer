d4984bd538230baf5d62beab6288cf07715cfeb3
noot <elizabethjbinks@gmail.com>
2020-02-19 11:54:49 -0500

    add nolint

--- network/utils.go
+++ network/utils.go
@@ -135,11 +135,13 @@ func saveKey(priv crypto.PrivKey, fp string) (err error) {
 }
 
 // uint64ToLEB128 applies leb128 variable-length encoding to a uint64
+//nolint:deadcode
 func uint64ToLEB128(in uint64) []byte {
 	return leb128.FromUInt64(in)
 }
 
 // LEB128ToUint64 decodes a leb128 variable-length encoded byte array into a uint64
+//nolint:deadcode
 func LEB128ToUint64(in []byte) uint64 {
 	return leb128.ToUInt64(in)
 }
