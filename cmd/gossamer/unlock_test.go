525327d6c8c6e12470f6d2e49bbbdfd45b3a93a5
Ryan <ryan@ryanchristo.com>
2020-03-02 14:31:38 -0800

    note occassional cmd error

--- cmd/gossamer/unlock_test.go
+++ cmd/gossamer/unlock_test.go
@@ -95,5 +95,7 @@ func TestUnlockFlag(t *testing.T) {
 	}
 
 	_, _, err = makeNode(ctx)
+
+	// TODO: fix occasional error "could not unlock keys: number of accounts to unlock is greater than number of accounts in keystore"
 	require.Nil(t, err)
 }
