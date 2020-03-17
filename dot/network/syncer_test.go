30634e4b50341391d42dc0fdc36d58027629818f
noot <elizabethjbinks@gmail.com>
2020-03-17 11:26:35 -0400

    update

--- dot/network/syncer_test.go
+++ dot/network/syncer_test.go
@@ -24,7 +24,6 @@ import (
 	"time"
 
 	"github.com/ChainSafe/gossamer/lib/common"
-	"github.com/ChainSafe/gossamer/lib/common/optional"
 	"github.com/stretchr/testify/require"
 )
 
@@ -63,16 +62,20 @@ func TestHandleStatusMessage(t *testing.T) {
 	dataDirA := path.Join(os.TempDir(), "gossamer-test", "nodeA")
 	defer os.RemoveAll(dataDirA)
 
+	syncChan := make(chan *big.Int)
+
 	configA := &Config{
 		DataDir:     dataDirA,
 		Port:        7001,
 		RandSeed:    1,
 		NoBootstrap: true,
 		NoMDNS:      true,
+		SyncChan:    syncChan,
 	}
 
-	blockStateA := newMockBlockState(big.NewInt(3))
-	nodeA, msgSendA, msgRecA := createTestServiceWithBlockState(t, configA, blockStateA)
+	heightA := big.NewInt(3)
+	blockStateA := newMockBlockState(heightA)
+	nodeA, msgRecA := createTestServiceWithBlockState(t, configA, blockStateA)
 	defer nodeA.Stop()
 
 	nodeA.noGossip = true
@@ -109,10 +112,11 @@ func TestHandleStatusMessage(t *testing.T) {
 		RandSeed:    2,
 		NoBootstrap: true,
 		NoMDNS:      true,
+		SyncChan:    syncChan,
 	}
 
 	blockStateB := newMockBlockState(big.NewInt(1))
-	nodeB, _, msgRecB := createTestServiceWithBlockState(t, configB, blockStateB)
+	nodeB, msgRecB := createTestServiceWithBlockState(t, configB, blockStateB)
 	defer nodeB.Stop()
 
 	nodeB.noGossip = true
@@ -140,34 +144,13 @@ func TestHandleStatusMessage(t *testing.T) {
 		t.Error("node B did not confirm status of node A")
 	}
 
-	// get latest block header from block state
-	latestHeader, err := blockStateB.BestBlockHeader()
-	require.Nil(t, err)
-	currentHash := latestHeader.Hash()
-
-	// expected block request message
-	var expectedMessage = &BlockRequestMessage{
-		RequestedData: 3,
-		StartingBlock: append([]byte{0}, currentHash[:]...),
-		EndBlockHash:  optional.NewHash(true, latestHeader.Hash()),
-		Direction:     1,
-		Max:           optional.NewUint32(false, 0),
-	}
-
 	select {
-	case msg := <-msgSendA:
-		require.NotNil(t, msg)
+	case num := <-syncChan:
+		require.NotNil(t, num)
 
-		// assert correct cast
-		actualBlockRequest, ok := msg.(*BlockRequestMessage)
-		require.True(t, ok)
-		require.NotNil(t, actualBlockRequest)
-
-		// assign ID since its random
-		actualBlockRequest.ID = expectedMessage.ID
-
-		// assert everything else
-		require.Equal(t, expectedMessage, actualBlockRequest)
+		if num.Cmp(heightA) != 0 {
+			t.Fatalf("Fail: got %d expected %d", num, heightA)
+		}
 
 	case <-time.After(TestMessageTimeout):
 		t.Error("node B timeout waiting for message from node A")
