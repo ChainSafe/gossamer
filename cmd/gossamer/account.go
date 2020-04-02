0df8357d1cd6f01e86debce7aa2e5d50bc4b75d9
noot <elizabethjbinks@gmail.com>
2020-04-02 19:28:38 -0400

    fix flags, broken account subcommand

--- cmd/gossamer/account.go
+++ cmd/gossamer/account.go
@@ -74,11 +74,13 @@ func accountAction(ctx *cli.Context) error {
 			password = getPassword("Enter password to encrypt keystore file:")
 		}
 
-		_, err = keystore.GenerateKeypair(keytype, datadir, password)
+		file, err := keystore.GenerateKeypair(keytype, datadir, password)
 		if err != nil {
 			log.Error("[cmd] Failed to generate keypair", "error", err)
 			return err
 		}
+
+		log.Info("[cmd] Generated key", "file", file)
 	}
 
 	// check if --import is set
