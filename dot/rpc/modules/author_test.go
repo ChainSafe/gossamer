994d974392fe3470d446d4d8aae93e686ae1bff2
Edward Mack <ed@edwardmack.com>
2020-04-10 12:48:07 -0400

    move newTestSerice to test_helper.go and make public

--- dot/rpc/modules/author_test.go
+++ dot/rpc/modules/author_test.go
@@ -2,85 +2,17 @@ package modules
 
 import (
 	"fmt"
-	"math/big"
 	"reflect"
 	"testing"
 
 	"github.com/ChainSafe/gossamer/dot/core"
-	"github.com/ChainSafe/gossamer/dot/network"
-	"github.com/ChainSafe/gossamer/lib/genesis"
-	"github.com/ChainSafe/gossamer/lib/keystore"
-	"github.com/ChainSafe/gossamer/lib/runtime"
-	"github.com/ChainSafe/gossamer/lib/trie"
-	"github.com/stretchr/testify/require"
-
 	"github.com/ChainSafe/gossamer/dot/state"
 	"github.com/ChainSafe/gossamer/dot/types"
 	"github.com/ChainSafe/gossamer/lib/transaction"
+	"github.com/stretchr/testify/require"
 )
 
 var testExt = []byte{3, 16, 110, 111, 111, 116, 1, 64, 103, 111, 115, 115, 97, 109, 101, 114, 95, 105, 115, 95, 99, 111, 111, 108}
-
-// testGenesisHeader is a test block header
-var testGenesisHeader = &types.Header{
-	Number:    big.NewInt(0),
-	StateRoot: trie.EmptyHash,
-}
-
-// newTestService creates a new test core service
-func newTestService(t *testing.T, cfg *core.Config) *core.Service {
-	if cfg == nil {
-		rt := runtime.NewTestRuntime(t, runtime.POLKADOT_RUNTIME_c768a7e4c70e)
-		cfg = &core.Config{
-			Runtime:         rt,
-			IsBabeAuthority: false,
-		}
-	}
-
-	if cfg.Keystore == nil {
-		cfg.Keystore = keystore.NewKeystore()
-	}
-
-	if cfg.NewBlocks == nil {
-		cfg.NewBlocks = make(chan types.Block)
-	}
-
-	if cfg.MsgRec == nil {
-		cfg.MsgRec = make(chan network.Message, 10)
-	}
-
-	if cfg.MsgSend == nil {
-		cfg.MsgSend = make(chan network.Message, 10)
-	}
-
-	if cfg.SyncChan == nil {
-		cfg.SyncChan = make(chan *big.Int, 10)
-	}
-
-	stateSrvc := state.NewService("")
-	stateSrvc.UseMemDB()
-
-	genesisData := new(genesis.Data)
-
-	err := stateSrvc.Initialize(genesisData, testGenesisHeader, trie.NewEmptyTrie())
-	require.Nil(t, err)
-
-	err = stateSrvc.Start()
-	require.Nil(t, err)
-
-	if cfg.BlockState == nil {
-		cfg.BlockState = stateSrvc.Block
-	}
-
-	if cfg.StorageState == nil {
-		cfg.StorageState = stateSrvc.Storage
-	}
-
-	s, err := core.NewService(cfg)
-	require.Nil(t, err)
-
-	return s
-}
 
 func TestAuthorModule_Pending(t *testing.T) {
 	txQueue := state.NewTransactionQueue()
@@ -142,7 +74,7 @@ func TestAuthorModule_SubmitExtrinsic(t *testing.T) {
 }
 
 func TestAuthorModule_InsertKey_Valid(t *testing.T) {
-	cs := newTestService(t, nil)
+	cs := core.NewTestService(t, nil)
 
 	auth := NewAuthorModule(cs, nil)
 	req := &KeyInsertRequest{"babe", "0xb7e9185065667390d2ad952a5324e8c365c9bf503dcf97c67a5ce861afe97309", "0x6246ddf254e0b4b4e7dffefc8adf69d212b98ac2b579c362b473fec8c40b4c0a"}
@@ -154,7 +86,7 @@ func TestAuthorModule_InsertKey_Valid(t *testing.T) {
 }
 
 func TestAuthorModule_InsertKey_InValid(t *testing.T) {
-	cs := newTestService(t, nil)
+	cs := core.NewTestService(t, nil)
 
 	auth := NewAuthorModule(cs, nil)
 	req := &KeyInsertRequest{"babe", "0xb7e9185065667390d2ad952a5324e8c365c9bf503dcf97c67a5ce861afe97309", "0x0000000000000000000000000000000000000000000000000000000000000000"}
@@ -164,7 +96,7 @@ func TestAuthorModule_InsertKey_InValid(t *testing.T) {
 }
 
 func TestAuthorModule_InsertKey_UnknownKeyType(t *testing.T) {
-	cs := newTestService(t, nil)
+	cs := core.NewTestService(t, nil)
 
 	auth := NewAuthorModule(cs, nil)
 	req := &KeyInsertRequest{"mack", "0xb7e9185065667390d2ad952a5324e8c365c9bf503dcf97c67a5ce861afe97309", "0x6246ddf254e0b4b4e7dffefc8adf69d212b98ac2b579c362b473fec8c40b4c0a"}
