eef39cab01ce50cf3bc1922d6960bb7d1a29dbe5
Edward Mack <ed@edwardmack.com>
2020-04-22 16:28:25 -0400

    lint issues

--- dot/rpc/websocket_test.go
+++ dot/rpc/websocket_test.go
@@ -2,29 +2,31 @@ package rpc
 
 import (
 	"flag"
-	"github.com/gorilla/websocket"
-	"github.com/stretchr/testify/require"
 	"log"
 	"net/url"
 	"testing"
 	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/require"
 )
 
 var addr = flag.String("addr", "localhost:8546", "http service address")
 var testCalls = []struct {
-	call []byte
-	expected  []byte
-} {
-	{[]byte(`{"jsonrpc":"2.0","method":"system_name","params":[],"id":1}`), []byte(`{"id":1,"jsonrpc":"2.0","result":"gossamer v0.0"}` + "\n")},  // working request
-	{[]byte(`{"jsonrpc":"2.0","method":"unknown","params":[],"id":1}`), []byte(`{"error":{"code":-32000,"data":null,"message":"rpc error method unknown not found"},"id":1,"jsonrpc":"2.0"}` + "\n")},  // unknown method
-	{[]byte{}, []byte(`{"error":{"code":-32700,"data":{"id":null,"jsonrpc":"","method":"","params":null},"message":"EOF"},"id":null,"jsonrpc":"2.0"}` + "\n")},  // empty request
+	call     []byte
+	expected []byte
+}{
+	{[]byte(`{"jsonrpc":"2.0","method":"system_name","params":[],"id":1}`), []byte(`{"id":1,"jsonrpc":"2.0","result":"gossamer v0.0"}` + "\n")},                                                       // working request
+	{[]byte(`{"jsonrpc":"2.0","method":"unknown","params":[],"id":1}`), []byte(`{"error":{"code":-32000,"data":null,"message":"rpc error method unknown not found"},"id":1,"jsonrpc":"2.0"}` + "\n")}, // unknown method
+	{[]byte{}, []byte(`{"error":{"code":-32700,"data":{"id":null,"jsonrpc":"","method":"","params":null},"message":"EOF"},"id":null,"jsonrpc":"2.0"}` + "\n")},                                        // empty request
 }
+
 func TestNewWebSocketServer(t *testing.T) {
 
 	cfg := &HTTPServerConfig{
 		Modules: []string{"system"},
 		RPCPort: 8545,
-		WSPort: 8546,
+		WSPort:  8546,
 	}
 	s := NewHTTPServer(cfg)
 	err := s.Start()
@@ -42,7 +44,7 @@ func TestNewWebSocketServer(t *testing.T) {
 	defer c.Close()
 
 	for _, item := range testCalls {
-		err = c.WriteMessage(websocket.TextMessage,  item.call)
+		err = c.WriteMessage(websocket.TextMessage, item.call)
 		require.Nil(t, err)
 
 		_, message, err := c.ReadMessage()
