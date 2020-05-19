5d66d7bb2a5a6411112850c8c6e3ecebcbbb4b07
noot <elizabethjbinks@gmail.com>
2020-05-19 12:00:13 -0400

    begin grandpa

--- lib/grandpa/grandpa.go
+++ lib/grandpa/grandpa.go
@@ -2,11 +2,11 @@ package grandpa
 
 // Grandpa represents the state of the grandpa protocol
 type Grandpa struct {
-	state         *state          // previous state
-	subround      subround        // current sub-round
-	votes         map[voter]*vote // votes for next state
-	equivocations map[voter]*vote // equivocatory votes for this stage
-	head          common.Hash     // most recently finalized block hash
+	state *state 	// previous state
+	subround 	subround 	// current sub-round
+	votes map[voter]*vote 	// votes for next state
+	equivocations map[voter]*vote 		// equivocatory votes for this stage
+	head common.Hash 	// most recently finalized block hash
 }
 
 // NewGrandpa returns a new GRANDPA instance.
@@ -18,12 +18,16 @@ func NewGrandpa() *Grandpa {
 // newState returns a new GRANDPA state
 func newState(voters []*Voter, counter, round uint64) *state {
 	return &state{
-		voters:  voters,
+		voters: voters,
 		counter: counter,
-		round:   round,
+		round: round,
 	}
 }
 
 func (s *State) validateVote(v *vote) bool {
+	// check if v.hash corresponds to a valid block
 
+	// check if the block is an eventual descendant of a previously finalized block
+
+	// check 
 }
