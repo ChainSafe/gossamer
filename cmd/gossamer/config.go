28a9c3ab12acc1f9ca172ed998fba4a10e220df9
Edward Mack <ed@edwardmack.com>
2020-07-20 11:28:01 -0400

    remove check for false condition (because it's a bool flag).

--- cmd/gossamer/config.go
+++ cmd/gossamer/config.go
@@ -411,8 +411,6 @@ func setDotRPCConfig(ctx *cli.Context, cfg *dot.RPCConfig) {
 	// check --rpc flag and update node configuration
 	if enabled := ctx.GlobalBool(RPCEnabledFlag.Name); enabled {
 		cfg.Enabled = true
-	} else if ctx.IsSet(RPCEnabledFlag.Name) && !enabled {
-		cfg.Enabled = false
 	}
 
 	// check --rpcport flag and update node configuration
@@ -436,8 +434,6 @@ func setDotRPCConfig(ctx *cli.Context, cfg *dot.RPCConfig) {
 
 	if wsenabled := ctx.GlobalBool(WSEnabledFlag.Name); wsenabled {
 		cfg.WSEnabled = true
-	} else if ctx.IsSet(WSEnabledFlag.Name) && !wsenabled {
-		cfg.WSEnabled = false
 	}
 
 	// format rpc modules
