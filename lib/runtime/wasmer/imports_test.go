192cd484b0d570f3050f341d29110c950af1c4c1
noot <elizabethjbinks@gmail.com>
2020-11-30 16:09:03 -0500

    add tests for imported funcs

--- lib/runtime/wasmer/imports_test.go
+++ lib/runtime/wasmer/imports_test.go
@@ -17,33 +17,243 @@
 package wasmer
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/common/optional"
 	"github.com/ChainSafe/gossamer/lib/runtime"
+	"github.com/ChainSafe/gossamer/lib/scale"
+	"github.com/ChainSafe/gossamer/lib/trie"
+
 	"github.com/stretchr/testify/require"
 )
 
 func Test_ext_hashing_blake2_128_version_1(t *testing.T) {
-	t.Skip()
 	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
-	mem := inst.inst.vm.Memory.Data()
 
 	data := []byte("helloworld")
-	ptr, err := inst.inst.ctx.Allocator.Allocate(uint32(len(data)))
+	enc, err := scale.Encode(data)
 	require.NoError(t, err)
 
-	copy(mem[ptr:ptr+uint32(len(data))], data)
-
-	testFunc, ok := inst.inst.vm.Exports["rtm_ext_hashing_blake2_128_version_1"]
-	require.True(t, ok)
-
-	out, err := testFunc(int32(ptr), int32(len(data)))
+	ret, err := inst.Exec("rtm_ext_hashing_blake2_128_version_1", enc)
 	require.NoError(t, err)
 
-	outInt := out.ToI32()
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
 
 	expected, err := common.Blake2b128(data)
 	require.NoError(t, err)
-	require.Equal(t, expected[:], mem[outInt:outInt+16])
+	require.Equal(t, expected[:], hash)
+}
+
+func Test_ext_hashing_blake2_256_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	data := []byte("helloworld")
+	enc, err := scale.Encode(data)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_hashing_blake2_256_version_1", enc)
+	require.NoError(t, err)
+
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
+
+	expected, err := common.Blake2bHash(data)
+	require.NoError(t, err)
+	require.Equal(t, expected[:], hash)
+}
+
+func Test_ext_hashing_keccak_256_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	data := []byte("helloworld")
+	enc, err := scale.Encode(data)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_hashing_keccak_256_version_1", enc)
+	require.NoError(t, err)
+
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
+
+	expected, err := common.Keccak256(data)
+	require.NoError(t, err)
+	require.Equal(t, expected[:], hash)
+}
+
+func Test_ext_hashing_twox_128_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	data := []byte("helloworld")
+	enc, err := scale.Encode(data)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_hashing_twox_128_version_1", enc)
+	require.NoError(t, err)
+
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
+
+	expected, err := common.Twox128Hash(data)
+	require.NoError(t, err)
+	require.Equal(t, expected[:], hash)
+}
+
+func Test_ext_hashing_twox_64_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	data := []byte("helloworld")
+	enc, err := scale.Encode(data)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_hashing_twox_64_version_1", enc)
+	require.NoError(t, err)
+
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
+
+	expected, err := common.Twox64(data)
+	require.NoError(t, err)
+	require.Equal(t, expected[:], hash)
+}
+
+func Test_ext_storage_clear_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	testkey := []byte("noot")
+	err := inst.inst.ctx.Storage.Set(testkey, []byte{1})
+	require.NoError(t, err)
+
+	enc, err := scale.Encode(testkey)
+	require.NoError(t, err)
+
+	_, err = inst.Exec("rtm_ext_storage_clear_version_1", enc)
+	require.NoError(t, err)
+
+	val, err := inst.inst.ctx.Storage.Get(testkey)
+	require.NoError(t, err)
+	require.Nil(t, val)
+}
+
+func Test_ext_storage_get_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	testkey := []byte("noot")
+	testvalue := []byte{1, 2}
+	err := inst.inst.ctx.Storage.Set(testkey, testvalue)
+	require.NoError(t, err)
+
+	enc, err := scale.Encode(testkey)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_storage_get_version_1", enc)
+	require.NoError(t, err)
+
+	buf := &bytes.Buffer{}
+	buf.Write(ret)
+
+	value, err := new(optional.Bytes).Decode(buf)
+	require.NoError(t, err)
+	require.Equal(t, testvalue, value.Value())
+}
+
+func Test_ext_storage_next_key_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	testkey := []byte("noot")
+	err := inst.inst.ctx.Storage.Set(testkey, []byte{1})
+	require.NoError(t, err)
+
+	nextkey := []byte("oot")
+	err = inst.inst.ctx.Storage.Set(nextkey, []byte{1})
+	require.NoError(t, err)
+
+	enc, err := scale.Encode(testkey)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_storage_next_key_version_1", enc)
+	require.NoError(t, err)
+
+	buf := &bytes.Buffer{}
+	buf.Write(ret)
+
+	next, err := new(optional.Bytes).Decode(buf)
+	require.NoError(t, err)
+	require.Equal(t, nextkey, next.Value())
+}
+
+func Test_ext_storage_read_version_1(t *testing.T) {
+	t.Skip() // TODO: figure out how to format multiple params
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	testkey := []byte("noot")
+	testvalue := []byte("washere")
+	err := inst.inst.ctx.Storage.Set(testkey, testvalue)
+	require.NoError(t, err)
+
+	testoffset := uint32(2)
+
+	testParams := struct {
+		key        []byte
+		offset     uint32
+		bufferSize uint32
+	}{
+		key:        testkey,
+		offset:     testoffset,
+		bufferSize: uint32(len(testvalue)),
+	}
+
+	enc, err := scale.Encode(testParams)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("rtm_ext_storage_read_version_1", enc)
+	require.NoError(t, err)
+
+	buf := &bytes.Buffer{}
+	buf.Write(ret)
+
+	read, err := new(optional.Bytes).Decode(buf)
+	require.NoError(t, err)
+	require.Equal(t, testvalue[testoffset:], read.Value())
+}
+
+func Test_ext_storage_root_version_1(t *testing.T) {
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	ret, err := inst.Exec("rtm_ext_storage_root_version_1", []byte{})
+	require.NoError(t, err)
+
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
+
+	expected := trie.EmptyHash
+	require.Equal(t, expected[:], hash)
+}
+
+func Test_ext_storage_set_version_1(t *testing.T) {
+	t.Skip() // TODO: figure out how to format multiple params
+	inst := NewTestInstance(t, runtime.HOST_API_TEST_RUNTIME)
+
+	testkey := []byte("noot")
+	testvalue := []byte("washere")
+
+	testParams := struct {
+		key   []byte
+		value []byte
+	}{
+		key:   testkey,
+		value: testvalue,
+	}
+
+	enc, err := scale.Encode(testParams)
+	require.NoError(t, err)
+
+	_, err = inst.Exec("rtm_ext_storage_set_version_1", enc)
+	require.NoError(t, err)
+
+	val, err := inst.inst.ctx.Storage.Get(testkey)
+	require.NoError(t, err)
+	require.Equal(t, testvalue, val)
 }
