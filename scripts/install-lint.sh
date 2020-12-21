31c6aa8304dc29ac65acb16163d3aa511b1cf50b
noot <elizabethjbinks@gmail.com>
2020-12-20 20:35:11 -0500

    echo path

--- scripts/install-lint.sh
+++ scripts/install-lint.sh
@@ -4,3 +4,6 @@ if ! command -v golangci-lint &> /dev/null
 then
 	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.32.2
 fi
+
+export PATH=$PATH:$(go env GOPATH)
+echo $PATH
