7739f8712a5ad5ea8ec6a4dcbc3fb1184beea08f
noot <elizabethjbinks@gmail.com>
2021-01-13 14:20:19 -0500

    skip Example in dot/state for race

--- dot/state/storage_notify_test.go
+++ dot/state/storage_notify_test.go
@@ -177,7 +177,11 @@ func TestStorageState_RegisterStorageChangeChannel_Multi_Filter(t *testing.T) {
 	}
 }
 
-func Example() {
+func Test_Example(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping subscription example")
+	}
+
 	// this is a working example of how to use db.Subscribe taken from
 	// https://github.com/dgraph-io/badger/blob/f50343ff404d8198df6dc83755ec2eab863d5ff2/db_test.go#L1939-L1948
 	prefix := []byte{'a'}
