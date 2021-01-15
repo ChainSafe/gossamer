cc52f1627347f86b91cfc3fd287b00f34bc89503
noot <elizabethjbinks@gmail.com>
2021-01-15 14:35:39 -0500

    add tests

--- dot/types/babe_digest_test.go
+++ dot/types/babe_digest_test.go
@@ -21,12 +21,11 @@ import (
 	"testing"
 
 	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
-	"github.com/ChainSafe/gossamer/lib/scale"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestBabePrimaryPreDigest_EncodeAndDecode(t *testing.T) {
-	// arbitrary test data
 	bh := &BabePrimaryPreDigest{
 		vrfOutput:      [sr25519.VrfOutputLength]byte{0, 91, 50, 25, 214, 94, 119, 36, 71, 216, 33, 152, 85, 184, 34, 120, 61, 161, 164, 223, 76, 53, 40, 246, 76, 38, 235, 204, 43, 31, 179, 28},
 		vrfProof:       [sr25519.VrfProofLength]byte{120, 23, 235, 159, 115, 122, 207, 206, 123, 232, 75, 243, 115, 255, 131, 181, 219, 241, 200, 206, 21, 22, 238, 16, 68, 49, 86, 99, 76, 139, 39, 0, 102, 106, 181, 136, 97, 141, 187, 1, 234, 183, 241, 28, 27, 229, 133, 8, 32, 246, 245, 206, 199, 142, 134, 124, 226, 217, 95, 30, 176, 246, 5, 3},
@@ -42,20 +41,32 @@ func TestBabePrimaryPreDigest_EncodeAndDecode(t *testing.T) {
 	require.Equal(t, bh, bh2)
 }
 
-// test decoding with DecodeCustom on BabePrimaryPreDigest type
-func TestDecodeCustom_DecodeBabePrimaryPreDigest(t *testing.T) {
-	t.Skip()
-	// arbitrary test data
-	expected := &BabePrimaryPreDigest{
+func TestBabeSecondaryPlainPreDigest_EncodeAndDecode(t *testing.T) {
+	bh := &BabeSecondaryPlainPreDigest{
+		authorityIndex: 17,
+		slotNumber:     420,
+	}
+
+	enc := bh.Encode()
+	r := &bytes.Buffer{}
+	_, _ = r.Write(enc)
+	bh2, err := DecodeBabePreDigest(r)
+	require.NoError(t, err)
+	require.Equal(t, bh, bh2)
+}
+
+func TestBabeSecondaryVRFPreDigest_EncodeAndDecode(t *testing.T) {
+	bh := &BabeSecondaryVRFPreDigest{
 		vrfOutput:      [sr25519.VrfOutputLength]byte{0, 91, 50, 25, 214, 94, 119, 36, 71, 216, 33, 152, 85, 184, 34, 120, 61, 161, 164, 223, 76, 53, 40, 246, 76, 38, 235, 204, 43, 31, 179, 28},
 		vrfProof:       [sr25519.VrfProofLength]byte{120, 23, 235, 159, 115, 122, 207, 206, 123, 232, 75, 243, 115, 255, 131, 181, 219, 241, 200, 206, 21, 22, 238, 16, 68, 49, 86, 99, 76, 139, 39, 0, 102, 106, 181, 136, 97, 141, 187, 1, 234, 183, 241, 28, 27, 229, 133, 8, 32, 246, 245, 206, 199, 142, 134, 124, 226, 217, 95, 30, 176, 246, 5, 3},
 		authorityIndex: 17,
 		slotNumber:     420,
 	}
-	encoded := []byte{0, 91, 50, 25, 214, 94, 119, 36, 71, 216, 33, 152, 85, 184, 34, 120, 61, 161, 164, 223, 76, 53, 40, 246, 76, 38, 235, 204, 43, 31, 179, 28, 120, 23, 235, 159, 115, 122, 207, 206, 123, 232, 75, 243, 115, 255, 131, 181, 219, 241, 200, 206, 21, 22, 238, 16, 68, 49, 86, 99, 76, 139, 39, 0, 102, 106, 181, 136, 97, 141, 187, 1, 234, 183, 241, 28, 27, 229, 133, 8, 32, 246, 245, 206, 199, 142, 134, 124, 226, 217, 95, 30, 176, 246, 5, 3, 17, 0, 0, 0, 0, 0, 0, 0, 164, 1, 0, 0, 0, 0, 0, 0}
-	decodedPreDigest := new(BabePrimaryPreDigest)
 
-	err := scale.DecodeCustom(encoded, decodedPreDigest)
+	enc := bh.Encode()
+	r := &bytes.Buffer{}
+	_, _ = r.Write(enc)
+	bh2, err := DecodeBabePreDigest(r)
 	require.NoError(t, err)
-	require.Equal(t, expected, decodedPreDigest)
+	require.Equal(t, bh, bh2)
 }
