56fe2c36362a873c1aee35fffcf2b61058bae926
Arijit Das <arijit@chainsafe.io>
2021-01-28 20:53:51 +0530

    Badger benchmark for MVCC.

--- dot/utils_test.go
+++ dot/utils_test.go
@@ -17,12 +17,17 @@
 package dot
 
 import (
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/ChainSafe/gossamer/lib/genesis"
 	"github.com/ChainSafe/gossamer/lib/runtime/wasmer"
 	"github.com/ChainSafe/gossamer/lib/utils"
+	"github.com/dgraph-io/badger/v2"
 	"github.com/stretchr/testify/require"
 )
 
@@ -85,4 +90,123 @@ func TestNewRuntimeFromGenesis(t *testing.T) {
 	gen := NewTestGenesis(t)
 	_, err := wasmer.NewRuntimeFromGenesis(gen, &wasmer.Config{})
 	require.NoError(t, err)
+}
+
+func TestBenchmarkWithTimeStamp(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test_timestampBenchmark")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		if err = os.RemoveAll(dir); err != nil {
+			t.Error(err)
+		}
+	})
+
+	cfg := NewTestConfig(t)
+	require.NotNil(t, cfg)
+
+	opts := badger.DefaultOptions(filepath.Join(dir, "badger"))
+	opts.SyncWrites = false
+	db, err := badger.OpenManaged(opts)
+
+	genRawFile := NewTestGenesisRawFile(t, cfg)
+	require.NotNil(t, genRawFile)
+	defer os.Remove(genRawFile.Name())
+
+	genRaw, err := genesis.NewGenesisFromJSONRaw(genRawFile.Name())
+	require.NoError(t, err)
+
+	start := time.Now()
+	for i := 1; i <= 1001; i++ {
+		// Write after every 100th entry (since runtime key is hardly changed).
+		if i%100 != 1 {
+			continue
+		}
+		// Start writing at a particular timestamp(MVCC).
+		wrtBatch := db.NewWriteBatchAt(uint64(i))
+		// Write all keys to database.
+		for k, v := range genRaw.Genesis.Raw["top"] {
+			err = wrtBatch.Set([]byte(k), append([]byte(v), uint8(i)))
+			require.NoError(t, err)
+		}
+		require.NoError(t, wrtBatch.Flush())
+		// Write to disk or data might be lost.
+		require.NoError(t, db.Sync())
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Writing to DB took %s", elapsed)
+
+	k := "0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da923a05cabf6d3bde7ca3ef0d11596b5611cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c"
+	val := "0x00000000000000000000000000000010"
+	expectedVal := append([]byte(val), 1)
+
+	for i := 1; i <= 1001; i++ {
+		txn := db.NewTransactionAt(uint64(i), false)
+
+		item, err := txn.Get([]byte(k))
+		require.NoError(t, err)
+
+		var data []byte
+		data, err = item.ValueCopy(data)
+		require.NoError(t, err)
+
+		// After every 100 iteration value is changing
+		if i%100 == 1 {
+			expectedVal = append([]byte(val), uint8(i))
+		}
+		require.Equal(t, expectedVal, data)
+	}
+	require.NoError(t, db.Close())
+}
+
+func TestBenchmark1(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test_benchmark")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		if err = os.RemoveAll(dir); err != nil {
+			t.Error(err)
+		}
+	})
+
+	cfg := NewTestConfig(t)
+	require.NotNil(t, cfg)
+
+	opts := badger.DefaultOptions(filepath.Join(dir, "badger"))
+	opts.SyncWrites = false
+	db, err := badger.Open(opts)
+	genRawFile := NewTestGenesisRawFile(t, cfg)
+	require.NotNil(t, genRawFile)
+	defer os.Remove(genRawFile.Name())
+
+	genRaw, err := genesis.NewGenesisFromJSONRaw(genRawFile.Name())
+	require.NoError(t, err)
+
+	start := time.Now()
+	for i := 1; i <= 1001; i++ {
+		txn := db.NewTransaction(true)
+		for k, v := range genRaw.Genesis.Raw["top"] {
+			err = txn.Set(append([]byte(k), uint8(i)), []byte(v))
+			require.NoError(t, err)
+		}
+		require.NoError(t, txn.Commit())
+		require.NoError(t, db.Sync())
+	}
+	elapsed := time.Since(start)
+	log.Printf("Writing to DB took %s", elapsed)
+
+	key := "0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da923a05cabf6d3bde7ca3ef0d11596b5611cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c"
+	expectedVal := "0x00000000000000000000000000000010"
+	for i := 1; i <= 1001; i++ {
+		txn := db.NewTransaction(true)
+		item, err := txn.Get(append([]byte(key), uint8(i)))
+		require.NoError(t, err)
+
+		var data []byte
+		data, err = item.ValueCopy(data)
+		require.NoError(t, err)
+		require.Equal(t, []byte(expectedVal), data)
+	}
+	require.NoError(t, db.Close())
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/OneOfOne/xxhash v1.2.5
 	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
 	github.com/bytecodealliance/wasmtime-go v0.20.0
-	github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
+	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/disiqueira/gotree v1.0.0
 	github.com/docker/docker v1.13.1
 	github.com/ethereum/go-ethereum v1.9.6
@@ -33,13 +33,15 @@ require (
 	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/naoina/toml v0.1.1
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/urfave/cli v1.20.0
 	github.com/wasmerio/go-ext-wasm v0.3.2-0.20200326095750-0a32be6068ec
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
 	google.golang.org/grpc v1.21.0 // indirect
 	google.golang.org/protobuf v1.23.0
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
 
