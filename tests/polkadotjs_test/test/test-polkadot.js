8303f5cf0b9dbbc937418ee5b3e4fed2fd1601e5
Edward Mack <ed@edwardmack.com>
2021-02-02 16:57:46 -0500

    add gossamer:test to package.json

--- tests/polkadotjs_test/test/test-polkadot.js
+++ tests/polkadotjs_test/test/test-polkadot.js
@@ -5,21 +5,18 @@ const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 describe('Testing polkadot calls:', function () {
     let api;
-    before (function () {
-         exec('pwd', function callback(error, stdout, stderr) {
-             console.log('out ' + stdout)
-         });
+    before (async function () {
         const wsProvider = new WsProvider('ws://127.0.0.1:8546');
-        api = ApiPromise.create({ provider: wsProvider });
+        api = await ApiPromise.create({ provider: wsProvider });
     });
 
     after(function () {
        console.log('Stop gossamer');
     });
 
-    it('call api.genesisHash', function () {
-       let genesisHash = api.genesisHash;
-       // console.log(`genesis hash: ${genesisHash}`);
-        expect(genesisHash).to.not.be.null;
+    it('call api.genesisHash', async function () {
+       let genesisHash = await api.genesisHash;
+       console.log(`genesis hash: ${genesisHash}`);
+       expect(genesisHash).to.be.not.null;
    });
 });
