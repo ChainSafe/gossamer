c75ddd1d252f062b9e2ac5ed5595d300ebba910d
noot <36753753+noot@users.noreply.github.com>
2021-02-09 13:15:10 -0500

    update runtime.Version to include TransactionVersion, fixes polkadot.js extrinsic submission (#1371)

--- lib/runtime/types.go
+++ lib/runtime/types.go
@@ -17,10 +17,7 @@
 package runtime
 
 import (
-	"bytes"
-
 	"github.com/ChainSafe/gossamer/lib/keystore"
-	"github.com/ChainSafe/gossamer/lib/scale"
 	log "github.com/ChainSafe/log15"
 )
 
@@ -86,61 +83,6 @@ type Context struct {
 	// commit all the changes, or if ext_storage_rollback_transaction is called, the changes
 	// will be discarded.
 	TransactionStorageChanges []*TransactionStorageChange
-}
-
-// Version struct
-type Version struct {
-	Spec_name         []byte
-	Impl_name         []byte
-	Authoring_version int32
-	Spec_version      int32
-	Impl_version      int32
-}
-
-// VersionAPI struct that holds Runtime Version info and API array
-type VersionAPI struct {
-	RuntimeVersion *Version
-	API            []*API_Item
-}
-
-// API_Item struct to hold runtime API Name and Version
-type API_Item struct {
-	Name []byte
-	Ver  int32
-}
-
-// Decode to scale decode []byte to VersionAPI struct
-func (v *VersionAPI) Decode(in []byte) error {
-	// decode runtime version
-	_, err := scale.Decode(in, v.RuntimeVersion)
-	if err != nil {
-		return err
-	}
-
-	// 1 + len(Spec_name) + 1 + len(Impl_name) + 12 for  3 int32's - 1 (zero index)
-	index := len(v.RuntimeVersion.Spec_name) + len(v.RuntimeVersion.Impl_name) + 14
-
-	// read byte at index for qty of apis
-	sd := scale.Decoder{Reader: bytes.NewReader(in[index : index+1])}
-	numApis, err := sd.DecodeInteger()
-	if err != nil {
-		return err
-	}
-	// put index on first value
-	index++
-	// load api_item objects
-	for i := 0; i < int(numApis); i++ {
-		ver, err := scale.Decode(in[index+8+(i*12):index+12+(i*12)], int32(0))
-		if err != nil {
-			return err
-		}
-		v.API = append(v.API, &API_Item{
-			Name: in[index+(i*12) : index+8+(i*12)],
-			Ver:  ver.(int32),
-		})
-	}
-
-	return nil
 }
 
 // NewValidateTransactionError returns an error based on a return value from TaggedTransactionQueueValidateTransaction
