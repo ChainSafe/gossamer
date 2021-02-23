0c4395a25cb436d4d675c9c16d8bfa82ed648edb
noot <elizabethjbinks@gmail.com>
2021-02-23 15:07:32 -0500

    go fmt

--- tests/stress/errors.go
+++ tests/stress/errors.go
@@ -7,8 +7,8 @@ import (
 //nolint
 var (
 	errFinalizedBlockMismatch = errors.New("node finalized head hashes don't match")
-	errNoFinalizedBlock = errors.New("did not finalize block for round")
-	errNoBlockAtNumber = errors.New("no blocks found for given number")
+	errNoFinalizedBlock       = errors.New("did not finalize block for round")
+	errNoBlockAtNumber        = errors.New("no blocks found for given number")
 	errBlocksAtNumberMismatch = errors.New("different blocks found for given number")
-	errChainHeadMismatch = errors.New("node chain head hashes don't match")
+	errChainHeadMismatch      = errors.New("node chain head hashes don't match")
 )
