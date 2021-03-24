83f625076be14341cbb7047abce4bb1646a8bd92
noot <elizabethjbinks@gmail.com>
2021-03-24 14:38:52 -0400

    maybe fix sync

--- dot/network/sync.go
+++ dot/network/sync.go
@@ -192,10 +192,11 @@ func (q *syncQueue) syncAtHead() {
 		prev = curr
 		start := uint64(curr.Number.Int64()) + 1
 		logger.Debug("haven't received new blocks since last check, pushing request", "start", start)
-		q.requestData.Store(start, requestData{
-			sent:     true,
-			received: false,
-		})
+		// q.requestData.Store(start, requestData{
+		// 	sent:     true,
+		// 	received: false,
+		// })
+		q.requestData.Delete(start)
 		q.pushRequest(start, 1, "")
 	}
 }
