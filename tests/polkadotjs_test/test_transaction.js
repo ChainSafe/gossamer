038d98d2ff65015750a8969bef10f812e1fb2aef
noot <elizabethjbinks@gmail.com>
2021-04-21 15:58:33 -0400

    cleanup

--- tests/polkadotjs_test/test_transaction.js
+++ tests/polkadotjs_test/test_transaction.js
@@ -19,11 +19,8 @@ async function main() {
     const ADDR_Bob = '0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22';
     // bob 5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty
 
-    const transfer = await api.tx.balances.transfer(bobKey.address, 12345)
-        .signAndSend(aliceKey,);
-
+    const transfer = await api.tx.balances.transfer(bobKey.address, 12345).signAndSend(aliceKey);
     console.log(`hxHash ${transfer}`);
-
 }
 
 main().catch(console.error);
