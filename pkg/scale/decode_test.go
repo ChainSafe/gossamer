91731d3f647100980ba3387f737b262369841c73
Tim Wu <tim.wu@chainsafe.io>
2021-05-14 13:32:09 -0400

    encodeBool and tests

--- pkg/scale/decode_test.go
+++ pkg/scale/decode_test.go
@@ -659,3 +659,56 @@ func Test_decodeState_decodeBytes(t *testing.T) {
 		})
 	}
 }
+
+func Test_decodeState_decodeBool(t *testing.T) {
+	var b bool
+	type args struct {
+		data []byte
+		dst  interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		want    interface{}
+	}{
+		{
+			args: args{
+				data: []byte{0x01},
+				dst:  &b,
+			},
+			want: true,
+		},
+		{
+			args: args{
+				data: []byte{0x00},
+				dst:  &b,
+			},
+			want: false,
+		},
+		{
+			name: "error case",
+			args: args{
+				data: []byte{0x03},
+				dst:  &b,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Unmarshal(tt.args.data, tt.args.dst)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err != nil {
+				return
+			}
+			got := reflect.ValueOf(tt.args.dst).Elem().Interface()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("decodeState.unmarshal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
