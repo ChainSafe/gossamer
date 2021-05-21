73172f6e0025fb5e7ec010c40247881aeac4b286
Tim Wu <tim.wu@chainsafe.io>
2021-05-21 17:17:04 -0400

    use shared tests in decode

--- pkg/scale/decode_test.go
+++ pkg/scale/decode_test.go
@@ -17,7 +17,6 @@
 package scale
 
 import (
-	"math/big"
 	"reflect"
 	"testing"
 
@@ -426,289 +425,96 @@ func Test_unmarshal_optionality_nil_case(t *testing.T) {
 }
 
 func Test_decodeState_decodeBigInt(t *testing.T) {
-	var (
-		bi *big.Int
-	)
-	type args struct {
-		data []byte
-		dst  interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-		want    interface{}
-	}{
-		{
-			name: "error case, ensure **big.Int",
-			args: args{
-				data: []byte{0x00},
-				dst:  bi,
-			},
-			wantErr: true,
-		},
-		{
-			args: args{
-				data: []byte{0x00},
-				dst:  &bi,
-			},
-			want: big.NewInt(0),
-		},
-		{
-			args: args{
-				data: []byte{0x04},
-				dst:  &bi,
-			},
-			want: big.NewInt(1),
-		},
-		{
-			args: args{
-				data: []byte{0xa8},
-				dst:  &bi,
-			},
-			want: big.NewInt(42),
-		},
-		{
-			args: args{
-				data: []byte{0x01, 0x01},
-				dst:  &bi,
-			},
-			want: big.NewInt(64),
-		},
-		{
-			args: args{
-				data: []byte{0x15, 0x01},
-				dst:  &bi,
-			},
-			want: big.NewInt(69),
-		},
-		{
-			args: args{
-				data: []byte{0xfd, 0xff},
-				dst:  &bi,
-			},
-			want: big.NewInt(16383),
-		},
-		{
-			args: args{
-				data: []byte{0x02, 0x00, 0x01, 0x00},
-				dst:  &bi,
-			},
-			want: big.NewInt(16384),
-		},
-		{
-			args: args{
-				data: []byte{0xfe, 0xff, 0xff, 0xff},
-				dst:  &bi,
-			},
-			want: big.NewInt(1073741823),
-		},
-		{
-			args: args{
-				data: []byte{0x03, 0x00, 0x00, 0x00, 0x40},
-				dst:  &bi,
-			},
-			want: big.NewInt(1073741824),
-		},
-		{
-			args: args{
-				data: []byte{0x03, 0xff, 0xff, 0xff, 0xff},
-				dst:  &bi,
-			},
-			want: big.NewInt(1<<32 - 1),
-		},
-		{
-			args: args{
-				data: []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x01},
-				dst:  &bi,
-			},
-			want: big.NewInt(1 << 32),
-		},
-	}
-	for _, tt := range tests {
+	for _, tt := range bigIntTests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := Unmarshal(tt.args.data, tt.args.dst)
-			if (err != nil) != tt.wantErr {
+			dst := reflect.ValueOf(tt.in).Interface()
+			if err := Unmarshal(tt.want, &dst); (err != nil) != tt.wantErr {
 				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
-				return
 			}
-			if err != nil {
-				return
-			}
-			got := reflect.ValueOf(tt.args.dst).Elem().Interface()
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("decodeState.unmarshal() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(dst, tt.in) {
+				t.Errorf("decodeState.unmarshal() = %v, want %v", dst, tt.in)
 			}
 		})
 	}
 }
 
 func Test_decodeState_decodeBytes(t *testing.T) {
-	var b []byte
-	var s string
-	type args struct {
-		data []byte
-		dst  interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-		want    interface{}
-	}{
-		{
-			args: args{
-				data: []byte{0x04, 0x01},
-				dst:  &b,
-			},
-			want: []byte{0x01},
-		},
-		{
-			args: args{
-				data: []byte{0x04, 0xff},
-				dst:  &b,
-			},
-			want: []byte{0xff},
-		},
-		{
-			args: args{
-				data: []byte{0x08, 0x01, 0x01},
-				dst:  &b,
-			},
-			want: []byte{0x01, 0x01},
-		},
-		{
-			args: args{
-				data: append([]byte{0x01, 0x01}, byteArray(64)...),
-				dst:  &b,
-			},
-			want: byteArray(64),
-		},
-		{
-			args: args{
-				data: append([]byte{0xfd, 0xff}, byteArray(16383)...),
-				dst:  &b,
-			},
-			want: byteArray(16383),
-		},
-		{
-			args: args{
-				data: append([]byte{0x02, 0x00, 0x01, 0x00}, byteArray(16384)...),
-				dst:  &b,
-			},
-			want: byteArray(16384),
-		},
-		// string
-		{
-			args: args{
-				data: []byte{0x04, 0x01},
-				dst:  &s,
-			},
-			want: string([]byte{0x01}),
-		},
-		{
-			args: args{
-				data: []byte{0x04, 0xff},
-				dst:  &s,
-			},
-			want: string([]byte{0xff}),
-		},
-		{
-			args: args{
-				data: []byte{0x08, 0x01, 0x01},
-				dst:  &s,
-			},
-			want: string([]byte{0x01, 0x01}),
-		},
-		{
-			args: args{
-				data: append([]byte{0x01, 0x01}, byteArray(64)...),
-				dst:  &s,
-			},
-			want: string(byteArray(64)),
-		},
-		{
-			args: args{
-				data: append([]byte{0xfd, 0xff}, byteArray(16383)...),
-				dst:  &s,
-			},
-			want: string(byteArray(16383)),
-		},
-		{
-			args: args{
-				data: append([]byte{0x02, 0x00, 0x01, 0x00}, byteArray(16384)...),
-				dst:  &s,
-			},
-			want: string(byteArray(16384)),
-		},
-	}
-	for _, tt := range tests {
+	for _, tt := range boolTests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := Unmarshal(tt.args.data, tt.args.dst)
-			if (err != nil) != tt.wantErr {
+			dst := reflect.ValueOf(tt.in).Interface()
+			if err := Unmarshal(tt.want, &dst); (err != nil) != tt.wantErr {
 				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
-				return
 			}
-			if err != nil {
-				return
-			}
-			got := reflect.ValueOf(tt.args.dst).Elem().Interface()
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("decodeState.unmarshal() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(dst, tt.in) {
+				t.Errorf("decodeState.unmarshal() = %v, want %v", dst, tt.in)
 			}
 		})
 	}
 }
 
 func Test_decodeState_decodeBool(t *testing.T) {
-	var b bool
-	type args struct {
-		data []byte
-		dst  interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-		want    interface{}
-	}{
-		{
-			args: args{
-				data: []byte{0x01},
-				dst:  &b,
-			},
-			want: true,
-		},
-		{
-			args: args{
-				data: []byte{0x00},
-				dst:  &b,
-			},
-			want: false,
-		},
-		{
-			name: "error case",
-			args: args{
-				data: []byte{0x03},
-				dst:  &b,
-			},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
+	for _, tt := range boolTests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := Unmarshal(tt.args.data, tt.args.dst)
-			if (err != nil) != tt.wantErr {
+			dst := reflect.ValueOf(tt.in).Interface()
+			if err := Unmarshal(tt.want, &dst); (err != nil) != tt.wantErr {
 				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
-				return
 			}
-			if err != nil {
-				return
-			}
-			got := reflect.ValueOf(tt.args.dst).Elem().Interface()
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("decodeState.unmarshal() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(dst, tt.in) {
+				t.Errorf("decodeState.unmarshal() = %v, want %v", dst, tt.in)
 			}
 		})
 	}
 }
+
+// func Test_decodeState_decodeBool(t *testing.T) {
+// 	var b bool
+// 	type args struct {
+// 		data []byte
+// 		dst  interface{}
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		wantErr bool
+// 		want    interface{}
+// 	}{
+// 		{
+// 			args: args{
+// 				data: []byte{0x01},
+// 				dst:  &b,
+// 			},
+// 			want: true,
+// 		},
+// 		{
+// 			args: args{
+// 				data: []byte{0x00},
+// 				dst:  &b,
+// 			},
+// 			want: false,
+// 		},
+// 		{
+// 			name: "error case",
+// 			args: args{
+// 				data: []byte{0x03},
+// 				dst:  &b,
+// 			},
+// 			wantErr: true,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			err := Unmarshal(tt.args.data, tt.args.dst)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if err != nil {
+// 				return
+// 			}
+// 			got := reflect.ValueOf(tt.args.dst).Elem().Interface()
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("decodeState.unmarshal() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
