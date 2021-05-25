43a5cf4221d5820c1e65efe9b6f75a68212a64d5
Arijit Das <arijit@chainsafe.io>
2021-05-25 20:56:20 +0530

    fix(dot/core): Fix handle transaction message test.

--- dot/core/messages_test.go
+++ dot/core/messages_test.go
@@ -27,11 +27,6 @@ import (
 	"github.com/ChainSafe/gossamer/lib/common"
 	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
 	"github.com/ChainSafe/gossamer/lib/keystore"
-	"github.com/ChainSafe/gossamer/lib/runtime"
-	"github.com/ChainSafe/gossamer/lib/runtime/wasmer"
-	"github.com/ChainSafe/gossamer/lib/trie"
-
-	log "github.com/ChainSafe/log15"
 	"github.com/stretchr/testify/require"
 )
 
@@ -76,12 +71,6 @@ func TestService_ProcessBlockAnnounceMessage(t *testing.T) {
 }
 
 func TestService_HandleTransactionMessage(t *testing.T) {
-	// this currently fails due to not being able to call validate_transaction
-
-	t.Skip()
-	tt := trie.NewEmptyTrie()
-	rt := wasmer.NewTestInstanceWithTrie(t, runtime.NODE_RUNTIME, tt, log.LvlTrace)
-
 	kp, err := sr25519.GenerateKeypair()
 	require.Nil(t, err)
 
@@ -91,16 +80,23 @@ func TestService_HandleTransactionMessage(t *testing.T) {
 	ks.Acco.Insert(kp)
 
 	cfg := &Config{
-		Runtime:          rt,
 		Keystore:         ks,
 		TransactionState: state.NewTransactionState(),
 		IsBlockProducer:  true,
+		BlockProducer:    &mockBlockProducer{},
 	}
 
 	s := NewTestService(t, cfg)
 
-	// https://github.com/paritytech/substrate/blob/5420de3face1349a97eb954ae71c5b0b940c31de/core/transaction-pool/src/tests.rs#L95
-	ext := []byte{1, 212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125, 142, 175, 4, 21, 22, 135, 115, 99, 38, 201, 254, 161, 126, 37, 252, 82, 135, 97, 54, 147, 201, 18, 144, 156, 178, 38, 170, 71, 148, 242, 106, 72, 69, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 216, 5, 113, 87, 87, 40, 221, 120, 247, 252, 137, 201, 74, 231, 222, 101, 85, 108, 102, 39, 31, 190, 210, 14, 215, 124, 19, 160, 180, 203, 54, 110, 167, 163, 149, 45, 12, 108, 80, 221, 65, 238, 57, 237, 199, 16, 10, 33, 185, 8, 244, 184, 243, 139, 5, 87, 252, 245, 24, 225, 37, 154, 163, 142}
+	parentHash := common.MustHexToHash("0x35a28a7dbaf0ba07d1485b0f3da7757e3880509edc8c31d0850cb6dd6219361d")
+	header, err := types.NewHeader(parentHash, common.Hash{}, common.Hash{}, big.NewInt(1), types.NewEmptyDigest())
+	require.NoError(t, err)
+
+	// initialise block header
+	err = s.rt.InitializeBlock(header)
+	require.NoError(t, err)
+
+	ext := types.Extrinsic(common.MustHexToBytes("0x410284ffd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d015a3e258da3ea20581b68fe1264a35d1f62d6a0debb1a44e836375eb9921ba33e3d0f265f2da33c9ca4e10490b03918300be902fcb229f806c9cf99af4cc10f8c0000000600ff8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a480b00c465f14670"))
 
 	msg := &network.TransactionMessage{Extrinsics: []types.Extrinsic{ext}}
 
@@ -109,6 +105,5 @@ func TestService_HandleTransactionMessage(t *testing.T) {
 
 	pending := s.transactionState.(*state.TransactionState).Pending()
 	require.NotEqual(t, 0, len(pending))
-	tx := []byte(pending[0].Extrinsic)
-	require.Equal(t, ext, tx)
+	require.Equal(t, ext, pending[0].Extrinsic)
 }
