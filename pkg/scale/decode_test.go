681384acd53ed681e0c66ada6303e56ce8a53897
Tim Wu <tim.wu@chainsafe.io>
2021-05-26 11:25:29 -0400

    struct decode tests, and unmarshal refactor

--- pkg/scale/decode_test.go
+++ pkg/scale/decode_test.go
@@ -439,7 +439,7 @@ func Test_decodeState_decodeBigInt(t *testing.T) {
 }
 
 func Test_decodeState_decodeBytes(t *testing.T) {
-	for _, tt := range boolTests {
+	for _, tt := range stringTests {
 		t.Run(tt.name, func(t *testing.T) {
 			dst := reflect.ValueOf(tt.in).Interface()
 			if err := Unmarshal(tt.want, &dst); (err != nil) != tt.wantErr {
@@ -466,55 +466,30 @@ func Test_decodeState_decodeBool(t *testing.T) {
 	}
 }
 
-// func Test_decodeState_decodeBool(t *testing.T) {
-// 	var b bool
-// 	type args struct {
-// 		data []byte
-// 		dst  interface{}
-// 	}
-// 	tests := []struct {
-// 		name    string
-// 		args    args
-// 		wantErr bool
-// 		want    interface{}
-// 	}{
-// 		{
-// 			args: args{
-// 				data: []byte{0x01},
-// 				dst:  &b,
-// 			},
-// 			want: true,
-// 		},
-// 		{
-// 			args: args{
-// 				data: []byte{0x00},
-// 				dst:  &b,
-// 			},
-// 			want: false,
-// 		},
-// 		{
-// 			name: "error case",
-// 			args: args{
-// 				data: []byte{0x03},
-// 				dst:  &b,
-// 			},
-// 			wantErr: true,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			err := Unmarshal(tt.args.data, tt.args.dst)
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if err != nil {
-// 				return
-// 			}
-// 			got := reflect.ValueOf(tt.args.dst).Elem().Interface()
-// 			if !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("decodeState.unmarshal() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func Test_decodeState_decodeStruct(t *testing.T) {
+	for _, tt := range structTests {
+		t.Run(tt.name, func(t *testing.T) {
+			dst := reflect.ValueOf(tt.in).Interface()
+			if err := Unmarshal(tt.want, &dst); (err != nil) != tt.wantErr {
+				t.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			switch reflect.ValueOf(tt.in).Kind() {
+			case reflect.Ptr:
+				if reflect.ValueOf(dst).IsZero() {
+					if !reflect.DeepEqual(dst, tt.in) {
+						t.Errorf("decodeState.unmarshal() = %v, want %v", dst, tt.in)
+					}
+				} else {
+					// have to do this since reflect.DeepEqual won't come back true for different addresses
+					if !reflect.DeepEqual(reflect.ValueOf(dst).Elem().Interface(), reflect.ValueOf(tt.in).Elem().Interface()) {
+						t.Errorf("decodeState.unmarshal() = %v, want %v", dst, tt.in)
+					}
+				}
+			default:
+				if !reflect.DeepEqual(dst, tt.in) {
+					t.Errorf("decodeState.unmarshal() = %v, want %v", dst, tt.in)
+				}
+			}
+		})
+	}
+}
