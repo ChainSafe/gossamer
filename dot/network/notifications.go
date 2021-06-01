b79fc86f699234b01ef070db6a38f486c0653663
Eclésio Melo Júnior <eclesiomelo.1@gmail.com>
2021-06-01 19:33:29 -0400

    chore: fix lint

--- dot/network/notifications.go
+++ dot/network/notifications.go
@@ -198,8 +198,7 @@ func (s *Service) createNotificationsMessageHandler(info *notificationsProtocol,
 }
 
 func (s *Service) sendData(peer peer.ID, hs Handshake, info *notificationsProtocol, msg NotificationsMessage) {
-	support, err := s.host.supportsProtocol(peer, info.protocolID)
-	if err != nil || !support {
+	if support, err := s.host.supportsProtocol(peer, info.protocolID); err != nil || !support {
 		logger.Warn("the peer does not supports the protocol", "protocol", info.protocolID, "peer", peer, "err", err)
 		return
 	}
@@ -269,7 +268,7 @@ func (s *Service) sendData(peer peer.ID, hs Handshake, info *notificationsProtoc
 	// we've completed the handshake with the peer, send message directly
 	logger.Trace("sending message", "protocol", info.protocolID, "peer", peer, "message", msg)
 
-	err = s.host.writeToStream(hsData.stream, msg)
+	err := s.host.writeToStream(hsData.stream, msg)
 	if err != nil {
 		logger.Trace("failed to send message to peer", "peer", peer, "error", err)
 	}
