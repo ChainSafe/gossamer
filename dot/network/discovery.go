3706836e29206c80ebe4117ef9003eb4eefb4e24
noot <elizabethjbinks@gmail.com>
2021-06-02 18:55:08 -0400

    decrease connectToPeersTimeout

--- dot/network/discovery.go
+++ dot/network/discovery.go
@@ -35,7 +35,7 @@ var (
 	startDHTTimeout             = time.Second * 10
 	initialAdvertisementTimeout = time.Millisecond
 	tryAdvertiseTimeout         = time.Second * 30
-	connectToPeersTimeout       = time.Minute * 5
+	connectToPeersTimeout       = time.Minute
 	findPeersTimeout            = time.Minute
 )
 
@@ -177,7 +177,7 @@ func (d *discovery) checkPeerCount() {
 }
 
 func (d *discovery) findPeers(ctx context.Context) {
-	logger.Debug("attempting to find DHT peers...")
+	logger.Info("attempting to find DHT peers...")
 	peerCh, err := d.rd.FindPeers(d.ctx, string(d.pid))
 	if err != nil {
 		logger.Warn("failed to begin finding peers via DHT", "err", err)
@@ -193,7 +193,7 @@ func (d *discovery) findPeers(ctx context.Context) {
 				continue
 			}
 
-			logger.Trace("found new peer via DHT", "peer", peer.ID)
+			logger.Info("found new peer via DHT", "peer", peer.ID)
 
 			// found a peer, try to connect if we need more peers
 			if len(d.h.Network().Peers()) < d.maxPeers {
