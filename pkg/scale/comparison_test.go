a84992a510bf30b68138d686836419b0c99551ce
Tim Wu <tim.wu@chainsafe.io>
2021-06-07 14:57:50 -0400

    add licenses

--- pkg/scale/comparison_test.go
+++ pkg/scale/comparison_test.go
@@ -1,3 +1,19 @@
+// Copyright 2019 ChainSafe Systems (ON) Corp.
+// This file is part of gossamer.
+//
+// The gossamer library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The gossamer library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the gossamer library. If not, see <http://www.gnu.org/licenses/>.
+
 package scale
 
 import (
@@ -123,19 +139,3 @@ func BenchmarkUnmarshal(b *testing.B) {
 		}
 	}
 }
-
-// func BenchmarkDecode(b *testing.B) {
-// 	for _, tt := range variableWidthIntegerTests {
-// 		dst := reflect.New(reflect.TypeOf(tt.in)).Interface()
-// 		fmt.Printf("%v %T\n", dst, dst)
-// 		// if err := Unmarshal(tt.want, &dst); (err != nil) != tt.wantErr {
-// 		// 	b.Errorf("decodeState.unmarshal() error = %v, wantErr %v", err, tt.wantErr)
-// 		// 	return
-// 		// }
-// 		_, err := oldScale.Decode(tt.want, dst)
-// 		if err != nil {
-// 			b.Errorf("%v", err)
-// 			return
-// 		}
-// 	}
-// }
