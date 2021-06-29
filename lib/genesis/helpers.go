d7f7fc88783f2da32a0bbcc3c3dfd269315b78a4
jimjbrettj <jdj221@lehigh.edu>
2021-06-29 10:48:20 -0600

    integrate scale into genesis

--- lib/genesis/helpers.go
+++ lib/genesis/helpers.go
@@ -21,13 +21,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/ChainSafe/gossamer/pkg/scale"
 	"io/ioutil"
 	"log"
 	"math/big"
 	"path/filepath"
 	"reflect"
-
-	"github.com/ChainSafe/gossamer/pkg/scale"
 
 	"github.com/ChainSafe/gossamer/dot/types"
 	"github.com/ChainSafe/gossamer/lib/common"
