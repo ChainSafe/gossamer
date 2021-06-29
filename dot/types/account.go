7e9ae9992bf89079f12882a04e7f748f19c82d37
Tim Wu <timwu20@gmail.com>
2021-06-28 22:22:52 -0400

    add new type, that uses scale.Uint128, comparison test

--- dot/types/account.go
+++ dot/types/account.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/pkg/scale"
 )
 
 // AccountInfo Information of an account.
@@ -17,5 +18,20 @@ type AccountInfo struct {
 		Reserved   common.Uint128
 		MiscFrozen common.Uint128
 		FreeFrozen common.Uint128
+	}
+}
+
+type AccountInfo1 struct {
+	// The number of transactions this account has sent.
+	Nonce uint32
+	// The number of other modules that currently depend on this account's existence. The account
+	// cannot be reaped until this is zero.
+	RefCount uint32
+	// The additional data that belongs to this account. Used to store the balance(s) in a lot of chains.
+	Data struct {
+		Free       *scale.Uint128
+		Reserved   *scale.Uint128
+		MiscFrozen *scale.Uint128
+		FreeFrozen *scale.Uint128
 	}
 }
