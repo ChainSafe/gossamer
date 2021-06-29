e99c18356ad4ddee2f2db5dd95d4d4c799cff591
jimjbrettj <jdj221@lehigh.edu>
2021-06-29 11:43:09 -0600

    integrate scale into rpc modules system-test and added nil check for encoding uint128

--- pkg/scale/encode.go
+++ pkg/scale/encode.go
@@ -351,6 +351,10 @@ func (es *encodeState) encodeUint(i uint) (err error) {
 
 // encodeUint128 encodes a Uint128
 func (es *encodeState) encodeUint128(i *Uint128) (err error) {
+	if i == nil {
+		err = fmt.Errorf("uint128 is nil: %v", i)
+		return
+	}
 	err = binary.Write(es, binary.LittleEndian, padBytes(i.Bytes(), binary.LittleEndian))
 	return
 }
