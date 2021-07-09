81c0ccd4d04f13935a0e6724eca53263db4bc99e
noot <elizabethjbinks@gmail.com>
2021-07-09 15:52:52 -0400

    potential fix to getBestFinalCandidate

--- lib/grandpa/grandpa.go
+++ lib/grandpa/grandpa.go
@@ -877,9 +877,9 @@ func (s *Service) getBestFinalCandidate() (*Vote, error) {
 
 	// if there are multiple blocks, get the one with the highest number
 	// that is also an ancestor of the prevoted block (or is the prevoted block)
-	if blocks[prevoted.Hash] != 0 {
-		return &prevoted, nil
-	}
+	// if blocks[prevoted.Hash] != 0 {
+	// 	return &prevoted, nil
+	// }
 
 	bfc := &Vote{
 		Number: 0,
