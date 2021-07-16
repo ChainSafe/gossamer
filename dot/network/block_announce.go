53a6505c9dc3b91d7ca49dadcfe1a9976e145868
JimboJ <jdj221@lehigh.edu>
2021-07-16 14:08:02 -0600

    wip debug

--- dot/network/block_announce.go
+++ dot/network/block_announce.go
@@ -124,7 +124,7 @@ func (bm *BlockAnnounceMessage) IsHandshake() bool {
 
 func decodeBlockAnnounceHandshake(in []byte) (Handshake, error) {
 	var hs = new(BlockAnnounceHandshake)
-	err := scale.Unmarshal(in, &hs)
+	err := scale.Unmarshal(in, hs)
 	if err != nil {
 		return nil, err
 	}
@@ -166,12 +166,12 @@ func (hs *BlockAnnounceHandshake) String() string {
 
 // Encode encodes a BlockAnnounceHandshake message using SCALE
 func (hs *BlockAnnounceHandshake) Encode() ([]byte, error) {
-	return scale.Marshal(hs)
+	return scale.Marshal(*hs)
 }
 
 // Decode the message into a BlockAnnounceHandshake
 func (hs *BlockAnnounceHandshake) Decode(in []byte) error {
-	err := scale.Unmarshal(in, &hs)
+	err := scale.Unmarshal(in, hs)
 	if err != nil {
 		return err
 	}
