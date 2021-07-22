7c4e3ba6018f77248e777163a99e924adafbffcf
Edward Mack <ed@edwardmack.com>
2021-07-22 15:12:03 -0400

    stub test files

---
+++ lib/runtime/life/resolver_test.go
@@ -0,0 +1,44 @@
+// Copyright 2019 ChainSafe Systems (ON) Corp.
+// This file is part of gossamer.
+//
+// The gossamer library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The gossamer library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the gossamer library. If not, see <http://www.gnu.org/licenses/>.
+
+package life
+
+import (
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/scale"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_ext_hashing_blake2_256_version_1(t *testing.T) {
+
+	inst := newInstanceFromGenesis(t)
+
+	data := []byte("helloworld")
+	enc, err := scale.Encode(data)
+	require.NoError(t, err)
+
+	ret, err := inst.Exec("ext_offchain_index_set_version_1", enc)
+	//ret, err := inst.Exec("ext_hashing_blake2_256_version_1", enc)
+	require.NoError(t, err)
+
+	hash, err := scale.Decode(ret, []byte{})
+	require.NoError(t, err)
+
+	expected, err := common.Blake2b128(data)
+	require.NoError(t, err)
+	require.Equal(t, expected[:], hash)
+}
