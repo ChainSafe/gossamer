dcbcc1a5a70b0a285ec5aff05fa288b41b952443
Eclésio Júnior <eclesiomelo.1@gmail.com>
2021-08-16 21:20:51 -0400

    chore: add merkle proofs

--- lib/trie/proof.go
+++ lib/trie/proof.go
@@ -1,35 +1,128 @@
 package trie
 
 import (
+	"bytes"
 	"errors"
-)
+	"fmt"
 
-type databaseWriter interface {
-	Put(hash, value []byte)
-}
+	"github.com/ChainSafe/chaindb"
+	"github.com/ChainSafe/gossamer/lib/common"
+)
 
 var (
-	ErrInvalidProof = errors.New("provided key is not present at trie")
+	ErrEmptyTrieRoot = errors.New("provided trie must have a root")
+	ErrEmptyNibbles  = errors.New("empty nibbles provided from key")
+	ErrInvalidProof  = errors.New("provided key is not present at trie")
 )
 
-func (t *Trie) Prove(key []byte, lvl uint, db databaseWriter) error {
-
-}
-
-func VerifyProof(t *Trie, key []byte) (value []byte, err error) {
-	if t.root == nil {
-		return nil, errors.New("cannot verify proof of an empty")
-	}
-
+func (t *Trie) Prove(key []byte, fromLvl uint, db chaindb.Writer) error {
 	key = keyToNibbles(key)
 	if len(key) == 0 {
-		return nil, errors.New("cannot verify proof of an empty key")
+		return ErrEmptyNibbles
 	}
 
-	retrievedNode := t.retrieve(t.root, key)
-	if retrievedNode == nil {
-		return nil, ErrInvalidProof
+	var nodes []node
+	currNode := t.root
+
+proveLoop:
+	for {
+		switch n := currNode.(type) {
+		case nil:
+			break proveLoop
+
+		case *leaf:
+			nodes = append(nodes, n)
+
+			if bytes.Equal(n.key, key) {
+				break proveLoop
+			}
+
+			return errors.New("could not found key")
+
+		case *branch:
+			nodes = append(nodes, n)
+			if bytes.Equal(n.key, key) || len(key) == 0 {
+				break proveLoop
+			}
+
+			length := lenCommonPrefix(n.key, key)
+			if length > 0 && len(key) < len(n.key) {
+				return errors.New("could not found key")
+			}
+
+			currNode = n.children[key[length]]
+			key = key[length+1:]
+		}
 	}
 
-	return retrievedNode.value, nil
+	for _, n := range nodes {
+		if fromLvl > 0 {
+			fromLvl--
+			continue
+		}
+
+		var (
+			hashNode    []byte
+			encHashNode []byte
+			err         error
+		)
+
+		if encHashNode, hashNode, err = n.encodeAndHash(); err != nil {
+			return fmt.Errorf("problems while encoding and hashing the node: %w", err)
+		}
+
+		fmt.Printf("0x%x\n", hashNode[:])
+
+		if err = db.Put(hashNode, encHashNode); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func VerifyProof(rootHash common.Hash, key []byte, db chaindb.Reader) ([]byte, error) {
+	key = keyToNibbles(key)
+	wantedHash := rootHash
+
+	for {
+		fmt.Printf("verifying 0x%x\n", wantedHash)
+		enc, err := db.Get(wantedHash[:])
+		if err != nil {
+			return nil, fmt.Errorf("could not get hash %s while verifying proof: %w", wantedHash, err)
+		}
+
+		node, err := decodeBytes(enc)
+		if err != nil {
+			return nil, fmt.Errorf("could not decode node bytes: %w", err)
+		}
+
+		switch n := node.(type) {
+		case nil:
+			return nil, ErrInvalidProof
+		case *leaf:
+			if bytes.Equal(n.key, key) {
+				return n.value, nil
+			}
+
+			return nil, ErrInvalidProof
+		case *branch:
+			if bytes.Equal(n.key, key) {
+				return n.value, nil
+			}
+
+			if len(key) == 0 {
+				return nil, ErrInvalidProof
+			}
+
+			length := lenCommonPrefix(n.key, key)
+			node = n.children[key[length]]
+			if node == nil {
+				return nil, fmt.Errorf("key: 0x%x not found on branch to key 0x%x,", key[0], key[:])
+			}
+
+			key = key[length+1:]
+			copy(wantedHash[:], node.getHash())
+		}
+	}
 }
--- lib/trie/proof_test.go
+++ lib/trie/proof_test.go
@@ -1,49 +1,89 @@
 package trie
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
+	crand "crypto/rand"
+
+	"github.com/ChainSafe/chaindb"
 	"github.com/stretchr/testify/require"
 )
 
-func setupTrie(t *testing.T) *Trie {
+func inMemoryChainDB(t *testing.T) (*chaindb.BadgerDB, func()) {
 	t.Helper()
 
-	tr := NewEmptyTrie()
+	tmpdir, err := os.MkdirTemp(os.TempDir(), "trie-chaindb-*")
+	require.NoError(t, err)
 
-	tr.Put([]byte("transaction.data"), []byte("some-value"))
-	tr.Put([]byte("transaction.sender"), []byte("some-sender"))
-	tr.Put([]byte("xcm-message"), []byte("some-message"))
+	db, err := chaindb.NewBadgerDB(&chaindb.Config{
+		InMemory: true,
+		DataDir:  tmpdir,
+	})
+	require.NoError(t, err)
 
-	return tr
+	clear := func() {
+		err := db.ClearAll()
+		require.NoError(t, err)
+
+		err = db.Close()
+		require.NoError(t, err)
+
+		err = os.RemoveAll(tmpdir)
+		require.NoError(t, err)
+	}
+
+	return db, clear
 }
 
 func TestVerifyProof(t *testing.T) {
-	tr := setupTrie(t)
-
-	value, err := VerifyProof(tr, []byte("transaction.data"))
+	trie, entries := randomTrie(t, 500)
+	root, err := trie.Hash()
 	require.NoError(t, err)
-	require.Equal(t, value, []byte("some-value"))
+
+	for _, kv := range entries {
+		t.Run("", func(t *testing.T) {
+			proof, clear := inMemoryChainDB(t)
+
+			fmt.Printf("Prove 0x%x\n", kv.k)
+
+			err := trie.Prove(kv.k, 0, proof)
+			require.NoError(t, err)
+
+			fmt.Printf("Verifying 0x%x\n", kv.k)
+			v, err := VerifyProof(root, kv.k, proof)
+
+			require.NoError(t, err)
+			require.Equal(t, kv.v, v)
+
+			clear()
+		})
+	}
 }
 
-func TestVerifyProof_KeyDoesntValid(t *testing.T) {
-	tr := setupTrie(t)
-
-	value, err := VerifyProof(tr, []byte("another-key"))
-	require.Error(t, err, ErrInvalidProof)
-	require.Nil(t, value)
+type kv struct {
+	k []byte
+	v []byte
 }
 
-func TestVerifyProof_EmptyInputs(t *testing.T) {
-	tr := NewEmptyTrie()
-	value, err := VerifyProof(tr, []byte("transaction.data"))
-	require.Error(t, err, "cannot verify proof of an empty")
-	require.Nil(t, value)
+func randomTrie(t *testing.T, n int) (*Trie, map[string]*kv) {
+	t.Helper()
 
-	tr = setupTrie(t)
+	trie := NewEmptyTrie()
+	vals := make(map[string]*kv)
 
-	value, err = VerifyProof(tr, []byte{})
-	require.Error(t, err, "cannot verify proof of an empty key")
-	require.Nil(t, value)
+	for i := 0; i < n; i++ {
+		v := &kv{randBytes(32), randBytes(20)}
+		trie.Put(v.k, v.v)
+		vals[string(v.k)] = v
+	}
 
+	return trie, vals
+}
+
+func randBytes(n int) []byte {
+	r := make([]byte, n)
+	crand.Read(r)
+	return r
 }
