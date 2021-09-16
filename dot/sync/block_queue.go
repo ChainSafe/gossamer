590efcaa9311405224c4ad06db8e409c6e512226
noot <elizabethjbinks@gmail.com>
2021-09-16 16:28:13 -0400

    make blockQueue blocking

--- dot/sync/block_queue.go
+++ dot/sync/block_queue.go
@@ -12,44 +12,35 @@ var errQueueFull = errors.New("cannot push item; queue is at capacity")
 
 type blockQueue struct {
 	sync.RWMutex
-	cap    int
-	ch     chan *types.BlockData
+	cap int
+	ch chan *types.BlockData
 	blocks map[common.Hash]*types.BlockData
 }
 
 // newBlockQueue initializes a queue of *types.BlockData with the given capacity.
 func newBlockQueue(cap int) *blockQueue {
 	return &blockQueue{
-		cap:    cap,
-		ch:     make(chan *types.BlockData, cap),
-		blocks: make(map[common.Hash]*types.BlockData, cap),
+		cap: cap,
+		ch: make(chan *types.BlockData, cap),
+		blocks: make(map[common.Hash]*types.BlockData),
 	}
 }
 
-func (q *blockQueue) push(bd *types.BlockData) error {
+func (q *blockQueue) push(bd *types.BlockData) {
 	q.Lock()
 	defer q.Unlock()
 
-	if len(q.blocks) == q.cap {
-		return errQueueFull
-	}
-
 	q.blocks[bd.Hash] = bd
 	q.ch <- bd
-	return nil
 }
 
 func (q *blockQueue) pop() *types.BlockData {
 	q.Lock()
 	defer q.Unlock()
 
-	select {
-	case bd := <-q.ch:
-		delete(q.blocks, bd.Hash)
-		return bd
-	default:
-		return nil
-	}
+ 	bd := <-q.ch
+	delete(q.blocks, bd.Hash)
+	return bd	
 }
 
 func (q *blockQueue) has(hash common.Hash) bool {
@@ -61,6 +52,6 @@ func (q *blockQueue) has(hash common.Hash) bool {
 
 func (q *blockQueue) get(hash common.Hash) *types.BlockData {
 	q.RLock()
-	defer q.RUnlock()
+	defer q.RUnlock()	
 	return q.blocks[hash]
 }
