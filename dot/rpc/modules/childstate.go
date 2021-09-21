7b634a17dd1b11dcb34d31440a35d1af1c036ab7
Eclésio Júnior <eclesiomelo.1@gmail.com>
2021-09-21 19:04:22 -0400

    chore: finish unit tests

--- dot/rpc/modules/childstate.go
+++ dot/rpc/modules/childstate.go
@@ -7,14 +7,21 @@ import (
 )
 
 type GetKeysRequest struct {
-	Key    string
-	Prefix string
+	Key    []byte
+	Prefix []byte
 	Hash   common.Hash
 }
 
 type ChildStateModule struct {
 	storageAPI StorageAPI
 	blockAPI   BlockAPI
+}
+
+func NewChildStateModule(s StorageAPI, b BlockAPI) *ChildStateModule {
+	return &ChildStateModule{
+		storageAPI: s,
+		blockAPI:   b,
+	}
 }
 
 func (cs *ChildStateModule) GetKeys(r *http.Request, req *GetKeysRequest, res *[]string) error {
@@ -27,12 +34,12 @@ func (cs *ChildStateModule) GetKeys(r *http.Request, req *GetKeysRequest, res *[
 		return err
 	}
 
-	trie, err := cs.storageAPI.GetStorageChild(stateRoot, []byte(req.Key))
+	trie, err := cs.storageAPI.GetStorageChild(stateRoot, req.Key)
 	if err != nil {
 		return err
 	}
 
-	keys := trie.GetKeysWithPrefix([]byte(req.Hash.String()))
+	keys := trie.GetKeysWithPrefix(req.Prefix)
 	hexKeys := make([]string, len(keys))
 	for idx, k := range keys {
 		hex := common.BytesToHex(k)
---
+++ dot/rpc/modules/childstate_test.go
@@ -0,0 +1,96 @@
+package modules
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ChainSafe/gossamer/dot/types"
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/trie"
+	"github.com/stretchr/testify/require"
+)
+
+func TestChildStateGetKeys(t *testing.T) {
+	childStateModule, currBlockHash := setupChildStateStorage(t)
+
+	req := &GetKeysRequest{
+		Key:    []byte(":child_storage_key"),
+		Prefix: []byte{},
+		Hash:   common.EmptyHash,
+	}
+
+	res := make([]string, 0)
+	err := childStateModule.GetKeys(nil, req, &res)
+	require.NoError(t, err)
+	require.Len(t, res, 3)
+
+	for _, r := range res {
+		b, err := common.HexToBytes(r)
+		require.NoError(t, err)
+		require.Contains(t, []string{
+			":child_first", ":child_second", ":another_child",
+		}, string(b))
+	}
+
+	req = &GetKeysRequest{
+		Key:    []byte(":child_storage_key"),
+		Prefix: []byte(":child_"),
+		Hash:   currBlockHash,
+	}
+
+	err = childStateModule.GetKeys(nil, req, &res)
+	require.NoError(t, err)
+	require.Len(t, res, 2)
+
+	for _, r := range res {
+		b, err := common.HexToBytes(r)
+		require.NoError(t, err)
+		require.Contains(t, []string{
+			":child_first", ":child_second",
+		}, string(b))
+	}
+}
+
+func setupChildStateStorage(t *testing.T) (*ChildStateModule, common.Hash) {
+	t.Helper()
+
+	st := newTestStateService(t)
+
+	tr, err := st.Storage.TrieState(nil)
+	require.NoError(t, err)
+
+	tr.Set([]byte(":first_key"), []byte(":value1"))
+	tr.Set([]byte(":second_key"), []byte(":second_value"))
+
+	childTr := trie.NewEmptyTrie()
+	childTr.Put([]byte(":child_first"), []byte(":child_first_value"))
+	childTr.Put([]byte(":child_second"), []byte(":child_second_value"))
+	childTr.Put([]byte(":another_child"), []byte("value"))
+
+	err = tr.SetChild([]byte(":child_storage_key"), childTr)
+	require.NoError(t, err)
+
+	stateRoot, err := tr.Root()
+	require.NoError(t, err)
+
+	bb, err := st.Block.BestBlock()
+	require.NoError(t, err)
+
+	err = st.Storage.StoreTrie(tr, nil)
+	require.NoError(t, err)
+
+	b := &types.Block{
+		Header: &types.Header{
+			ParentHash: bb.Header.Hash(),
+			Number:     big.NewInt(0).Add(big.NewInt(1), bb.Header.Number),
+			StateRoot:  stateRoot,
+		},
+		Body: types.NewBody([]byte{}),
+	}
+
+	err = st.Block.AddBlock(b)
+	require.NoError(t, err)
+
+	hash, _ := st.Block.GetBlockHash(b.Header.Number)
+	return NewChildStateModule(st.Storage, st.Block), hash
+}
