539bc7a516ffb35b8c3e19adb522b71314c69dd5
Eclésio Júnior <eclesiomelo.1@gmail.com>
2021-09-24 14:18:36 -0400

    chore: apply changes and add copyright

--- dot/rpc/modules/childstate.go
+++ dot/rpc/modules/childstate.go
@@ -1,3 +1,19 @@
+// Copyright 2019 ChainSafe Systems (ON) Corp.
+// This file is part of gossamer.
+//
+// The gossamer library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The gossamer library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the gossamer library. If not, see <http://www.gnu.org/licenses/>.
+
 package modules
 
 import (
@@ -46,8 +62,7 @@ func (cs *ChildStateModule) GetKeys(_ *http.Request, req *GetKeysRequest, res *[
 	keys := trie.GetKeysWithPrefix(req.Prefix)
 	hexKeys := make([]string, len(keys))
 	for idx, k := range keys {
-		hex := common.BytesToHex(k)
-		hexKeys[idx] = hex
+		hexKeys[idx] = common.BytesToHex(k)
 	}
 
 	*res = hexKeys
