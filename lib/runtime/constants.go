630dd4ba428715446df8094eb8de14a22b923cd4
Edward Mack <ed@edwardmack.com>
2021-10-18 15:38:19 -0400

    go fmt

--- lib/runtime/constants.go
+++ lib/runtime/constants.go
@@ -45,8 +45,8 @@ const (
 	POLKADOT_RUNTIME_URL = "https://github.com/noot/polkadot/blob/noot/v0.8.25/polkadot_runtime.wasm?raw=true"
 
 	// v0.8 test API wasm
-	HOST_API_TEST_RUNTIME    = "hostapi_runtime"
-	HOST_API_TEST_RUNTIME_FP = "hostapi_runtime.compact.wasm"
+	HOST_API_TEST_RUNTIME     = "hostapi_runtime"
+	HOST_API_TEST_RUNTIME_FP  = "hostapi_runtime.compact.wasm"
 	HOST_API_TEST_RUNTIME_URL = "https://github.com/ChainSafe/polkadot-spec/blob/5edcd0122ed55deee20ce07440b90ea51f6b687b/test/hostapi_runtime.compact.wasm?raw=true"
 
 	// v0.8 substrate runtime with modified name and babe C=(1, 1)
