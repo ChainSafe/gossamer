ee2a2c867501e3b19bb666502b60ee87ed4fe2f5
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-10-21 18:25:32 +0000

    Unexport `MockMemory` as `mockMemory`

--- lib/runtime/memory.go
+++ lib/runtime/memory.go
@@ -19,7 +19,7 @@ package runtime
 // PageSize is 65kb
 const PageSize = 65536
 
-//go:generate mockery --name Memory --structname MockMemory --case underscore --inpackage
+//go:generate mockery --name Memory --structname mockMemory --case underscore --inpackage --filename mock_memory_test.go
 
 // Memory is a raw memory interface
 type Memory interface {
