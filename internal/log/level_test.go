86452fba188dabc3d3e853184d26e07f2c4c6faa
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-10-27 18:54:30 +0000

    More unit tests

---
+++ internal/log/level_test.go
@@ -0,0 +1,114 @@
+package log
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Level_String(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		level Level
+		s     string
+	}{
+		"trace": {
+			level: LevelTrace,
+			s:     "TRCE",
+		},
+		"debug": {
+			level: LevelDebug,
+			s:     "DBUG",
+		},
+		"info": {
+			level: LevelInfo,
+			s:     "INFO",
+		},
+		"warn": {
+			level: LevelWarn,
+			s:     "WARN",
+		},
+		"error": {
+			level: LevelError,
+			s:     "EROR",
+		},
+		"critical": {
+			level: LevelCritical,
+			s:     "CRIT",
+		},
+		"unknown": {
+			level: 178,
+			s:     "???",
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			s := testCase.level.String()
+
+			assert.Equal(t, testCase.s, s)
+		})
+	}
+}
+
+func Test_ParseLevel(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		s     string
+		level Level
+		err   error
+	}{
+		"trace": {
+			s:     "TRCE",
+			level: LevelTrace,
+		},
+		"debug": {
+			s:     "DBUG",
+			level: LevelDebug,
+		},
+		"info": {
+			s:     "INFO",
+			level: LevelInfo,
+		},
+		"warn": {
+			s:     "WARN",
+			level: LevelWarn,
+		},
+		"error": {
+			s:     "EROR",
+			level: LevelError,
+		},
+		"critical": {
+			s:     "CRIT",
+			level: LevelCritical,
+		},
+		"invalid": {
+			s:   "someinvalid",
+			err: errors.New("level is not recognised: someinvalid"),
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			level, err := ParseLevel(testCase.s)
+
+			if testCase.err != nil {
+				require.EqualError(t, err, testCase.err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.level, level)
+		})
+	}
+}
--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -4,42 +4,165 @@ import (
 	"bytes"
 	"regexp"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Logger_Debug(t *testing.T) {
+func Test_Logger_log(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		logger      *Logger
+		level       Level
+		s           string
+		args        []interface{}
+		outputRegex string
+	}{
+		"log at trace": {
+			logger: &Logger{
+				settings: settings{
+					level:  levelPtr(LevelTrace),
+					caller: newCallerSettings(false, false, false),
+				},
+				mutex: new(sync.Mutex),
+			},
+			level:       LevelTrace,
+			s:           "some words",
+			outputRegex: timePrefixRegex + "TRCE some words\n$",
+		},
+		"do not log at trace": {
+			logger: &Logger{
+				settings: settings{
+					level:  levelPtr(LevelDebug),
+					caller: newCallerSettings(false, false, false),
+				},
+				mutex: new(sync.Mutex),
+			},
+			level:       LevelTrace,
+			s:           "some words",
+			outputRegex: "^$",
+		},
+		"format string": {
+			logger: &Logger{
+				settings: settings{
+					level:  levelPtr(LevelTrace),
+					caller: newCallerSettings(false, false, false),
+				},
+				mutex: new(sync.Mutex),
+			},
+			level:       LevelTrace,
+			s:           "some %s",
+			args:        []interface{}{"words"},
+			outputRegex: timePrefixRegex + "TRCE some words\n$",
+		},
+		"show caller": {
+			logger: &Logger{
+				settings: settings{
+					level:  levelPtr(LevelTrace),
+					caller: newCallerSettings(true, true, true),
+				},
+				mutex: new(sync.Mutex),
+			},
+			level:       LevelTrace,
+			s:           "some words",
+			outputRegex: timePrefixRegex + "TRCE some words\tlog_test.go:L[0-9]+:func[0-9]+\n$",
+		},
+		"context": {
+			logger: &Logger{
+				settings: settings{
+					level:  levelPtr(LevelTrace),
+					caller: newCallerSettings(false, false, false),
+					context: []contextKeyValues{
+						{key: "key1", values: []string{"a", "b"}},
+						{key: "key2", values: []string{"c", "d"}},
+					},
+				},
+				mutex: new(sync.Mutex),
+			},
+			level:       LevelTrace,
+			s:           "some words",
+			outputRegex: timePrefixRegex + "TRCE some words\tkey1=a,b key2=c,d\n$",
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			buffer := bytes.NewBuffer(nil)
+			testCase.logger.settings.writer = buffer
+
+			logWrapper := func() { // wrap for caller depth of 3
+				testCase.logger.log(testCase.level, testCase.s, testCase.args...)
+			}
+
+			logWrapper()
+
+			line := buffer.String()
+			buffer.Reset()
+
+			regex, err := regexp.Compile(testCase.outputRegex)
+			require.NoError(t, err)
+
+			assert.True(t, regex.MatchString(line),
+				"line %q does not match regex %q", line, regex.String())
+		})
+	}
+}
+
+func Test_Logger_LevelsLog(t *testing.T) {
 	t.Parallel()
 
 	buffer := bytes.NewBuffer(nil)
 
-	logger := New(SetLevel(LevelDebug), SetWriter(buffer),
-		SetCallerFile(true), SetCallerLine(true))
+	logger := New(SetLevel(LevelTrace), SetWriter(buffer))
+	logger.Trace("some trace")
+	logger.Debug("some debug")
+	logger.Info("some info")
+	logger.Warn("some warn")
+	logger.Error("some error")
+	logger.Critical("some critical")
+	logger.Tracef("some %dnd trace", 2)
+	logger.Debugf("some %dnd debug", 2)
+	logger.Infof("some %dnd info", 2)
+	logger.Warnf("some %dnd warn", 2)
+	logger.Errorf("some %dnd error", 2)
+	logger.Criticalf("some %dnd critical", 2)
 
-	logger.Debug("isn't this \"function\"...")
-	logger.Debug("...fun?")
-
-	result := buffer.String()
+	lines := strings.Split(buffer.String(), "\n")
 	buffer.Reset()
 
-	result = strings.TrimSuffix(result, "\n")
+	// Check for trailing newline
+	require.NotEmpty(t, lines)
+	assert.Equal(t, "", lines[len(lines)-1])
+	lines = lines[:len(lines)-1]
 
-	lines := strings.Split(result, "\n")
-	require.Len(t, lines, 2)
-
-	expectedVariablePrefix := regexp.MustCompile(`2[0-9]{3}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9]Z `)
-
-	expectedLinesWithoutPrefix := []string{
-		`DBUG isn't this "function"...	log_test.go:L21`,
-		`DBUG ...fun?	log_test.go:L22`,
+	expectedRegexes := []string{
+		timePrefixRegex + "TRCE some trace$",
+		timePrefixRegex + "DBUG some debug$",
+		timePrefixRegex + "INFO some info$",
+		timePrefixRegex + "WARN some warn$",
+		timePrefixRegex + "EROR some error$",
+		timePrefixRegex + "CRIT some critical$",
+		timePrefixRegex + "TRCE some 2nd trace$",
+		timePrefixRegex + "DBUG some 2nd debug$",
+		timePrefixRegex + "INFO some 2nd info$",
+		timePrefixRegex + "WARN some 2nd warn$",
+		timePrefixRegex + "EROR some 2nd error$",
+		timePrefixRegex + "CRIT some 2nd critical$",
 	}
 
-	for i, line := range lines {
-		prefix := expectedVariablePrefix.FindString(line)
-		assert.NotEmpty(t, prefix)
-		line = line[len(prefix):]
-		assert.Equal(t, expectedLinesWithoutPrefix[i], line)
+	require.Equal(t, len(expectedRegexes), len(lines))
+
+	for i := range lines {
+		regex, err := regexp.Compile(expectedRegexes[i])
+		require.NoError(t, err)
+
+		assert.True(t, regex.MatchString(lines[i]),
+			"line %q does not match regex %q", lines[i], expectedRegexes[i])
 	}
 }
