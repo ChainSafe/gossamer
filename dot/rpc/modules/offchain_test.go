8de6a585a7a2fe9922db56dd95f0978a8d8b601b
jimjbrettj <jamesdjohnson218@gmail.com>
2021-10-28 16:01:20 -0600

    migrate rpc/module tests to integration tests TODO:write unit tests

--- dot/rpc/modules/offchain_test.go
+++ dot/rpc/modules/offchain_test.go
@@ -1,4 +1,1 @@
-// Copyright 2021 ChainSafe Systems (ON)
-// SPDX-License-Identifier: LGPL-3.0-only
-
 package modules
--- dot/rpc/modules/system_test.go
+++ dot/rpc/modules/system_test.go
@@ -1,4 +1,8 @@
 // Copyright 2021 ChainSafe Systems (ON)
 // SPDX-License-Identifier: LGPL-3.0-only
 
+<<<<<<< HEAD
 package modules
+=======
+package modules
+>>>>>>> migrate rpc/module tests to integration tests TODO:write unit tests
