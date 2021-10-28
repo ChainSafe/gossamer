06f354e20752c0be3821307e864c336df87a41fb
Omar <omar391@users.noreply.github.com>
2021-10-28 15:56:12 +0000

    Micro optimization

--- dot/network/service.go
+++ dot/network/service.go
@@ -715,11 +715,9 @@ func (s *Service) NodeRoles() byte {
 
 // CollectGauge will be used to collect countable metrics from network service
 func (s *Service) CollectGauge() map[string]int64 {
-	var isSynced int64
+	var isSynced int64 = 0
 	if !s.syncer.IsSynced() {
 		isSynced = 1
-	} else {
-		isSynced = 0
 	}
 
 	return map[string]int64{
