2371af05019b72a300786fb4bf1a2371fa80668c
jimjbrettj <jamesdjohnson218@gmail.com>
2021-10-29 13:41:00 -0600

    use mockery to generate BlockProducerAPI

--- dot/rpc/modules/dev_test.go
+++ dot/rpc/modules/dev_test.go
@@ -1,1 +1,77 @@
 package modules
+
+import (
+	"net/http"
+	"testing"
+)
+
+func Test_uint64ToHex(t *testing.T) {
+	type args struct {
+		input uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "uint64ToHex one",
+			args: args{
+				input : uint64(1),
+			},
+			want: "0x0100000000000000",
+		},
+		{
+			name: "uint64ToHex zero",
+			args: args{
+				input : uint64(0),
+			},
+			want: "0x0000000000000000",
+		},
+		{
+			name: "uint64ToHex max",
+			args: args{
+				input : uint64(18446744073709551615),
+			},
+			want: "0xffffffffffffffff",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uint64ToHex(tt.args.input); got != tt.want {
+				t.Errorf("uint64ToHex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDevModule_EpochLength(t *testing.T) {
+	type fields struct {
+		networkAPI       NetworkAPI
+		blockProducerAPI BlockProducerAPI
+	}
+	type args struct {
+		r   *http.Request
+		req *EmptyRequest
+		res *string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &DevModule{
+				networkAPI:       tt.fields.networkAPI,
+				blockProducerAPI: tt.fields.blockProducerAPI,
+			}
+			if err := m.EpochLength(tt.args.r, tt.args.req, tt.args.res); (err != nil) != tt.wantErr {
+				t.Errorf("EpochLength() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
