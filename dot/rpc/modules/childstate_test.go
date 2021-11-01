374ef2f7ac9288de5511ecbd74fb82cdaa38b953
jimjbrettj <jamesdjohnson218@gmail.com>
2021-11-01 15:48:48 -0600

    test cov for getKeys in childState

--- dot/rpc/modules/childstate_test.go
+++ dot/rpc/modules/childstate_test.go
@@ -2,3 +2,160 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 package modules
+
+import (
+	"errors"
+	apimocks "github.com/ChainSafe/gossamer/dot/rpc/modules/mocks"
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/genesis"
+	rtstorage "github.com/ChainSafe/gossamer/lib/runtime/storage"
+	"github.com/ChainSafe/gossamer/lib/trie"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+
+func createTestTrieState(t *testing.T) (*trie.Trie, common.Hash) {
+	t.Helper()
+
+	_, genTrie, _ := genesis.NewTestGenesisWithTrieAndHeader(t)
+	tr, err := rtstorage.NewTrieState(genTrie)
+	require.NoError(t, err)
+
+	tr.Set([]byte(":first_key"), []byte(":value1"))
+	tr.Set([]byte(":second_key"), []byte(":second_value"))
+
+	childTr := trie.NewEmptyTrie()
+	childTr.Put([]byte(":child_first"), []byte(":child_first_value"))
+	childTr.Put([]byte(":child_second"), []byte(":child_second_value"))
+	childTr.Put([]byte(":another_child"), []byte("value"))
+
+	err = tr.SetChild([]byte(":child_storage_key"), childTr)
+	require.NoError(t, err)
+
+	stateRoot, err := tr.Root()
+	require.NoError(t, err)
+
+	return tr.Trie(), stateRoot
+}
+
+func TestChildStateModule_GetKeys(t *testing.T) {
+	tr, sr := createTestTrieState(t)
+
+	mockStorageAPI := new(apimocks.StorageAPI)
+	mockErrorStorageAPI1 := new(apimocks.StorageAPI)
+	mockErrorStorageAPI2 := new(apimocks.StorageAPI)
+	mockBlockAPI := new(apimocks.BlockAPI)
+	
+	hash := common.MustHexToHash("0x3aa96b0149b6ca3688878bdbd19464448624136398e3ce45b9e755d3ab61355a")
+	mockBlockAPI.On("GetBlockHash").Return(hash)
+	mockBlockAPI.On("BestBlockHash").Return(hash)
+
+	mockStorageAPI.On("GetStateRootFromBlock", &hash).Return(&sr, nil)
+	mockStorageAPI.On("GetStorageChild", &sr, []byte(":child_storage_key")).Return(tr, nil)
+
+	mockErrorStorageAPI1.On("GetStateRootFromBlock", mock.AnythingOfType("*common.Hash")).Return(nil, nil)
+	mockErrorStorageAPI1.On("GetStorageChild", mock.AnythingOfType("*common.Hash"), []byte{}).Return(nil, errors.New("GetStorageChild error"))
+
+	mockErrorStorageAPI2.On("GetStateRootFromBlock", &hash).Return(nil, errors.New("GetStateRootFromBlock error"))
+
+	childStateModule := NewChildStateModule(mockStorageAPI, mockBlockAPI)
+	res := make([]string, 0)
+	type fields struct {
+		storageAPI StorageAPI
+		blockAPI   BlockAPI
+	}
+	type args struct {
+		in0 *http.Request
+		req *GetKeysRequest
+		res *[]string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Get Keys Nil Hash",
+			fields: fields{
+				childStateModule.storageAPI,
+				childStateModule.blockAPI,
+			},
+			args: args{
+				in0 : nil,
+				req: &GetKeysRequest{
+					Key:    []byte(":child_storage_key"),
+					Prefix: []byte{},
+					Hash:   nil,
+				},
+				res: &res,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Get Keys with Hash",
+			fields: fields{
+				childStateModule.storageAPI,
+				childStateModule.blockAPI,
+			},
+			args: args{
+				in0 : nil,
+				req: &GetKeysRequest{
+					Key:    []byte(":child_storage_key"),
+					Prefix: []byte{},
+					Hash:   &hash,
+				},
+				res: &res,
+			},
+			wantErr: false,
+		},
+		{
+			name: "GetStorageChild error",
+			fields: fields{
+				mockErrorStorageAPI1,
+				mockBlockAPI,
+			},
+			args: args{
+				in0 : nil,
+				req: &GetKeysRequest{
+					Key:    []byte{},
+					Prefix: []byte{},
+					Hash:   &common.Hash{},
+				},
+				res: &res,
+			},
+			wantErr: true,
+		},
+		{
+			name: "GetStateRootFromBlock error",
+			fields: fields{
+				mockErrorStorageAPI2,
+				mockBlockAPI,
+			},
+			args: args{
+				in0 : nil,
+				req: &GetKeysRequest{
+					Key:    []byte(":child_storage_key"),
+					Prefix: []byte{},
+					Hash:   nil,
+				},
+				res: &res,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &ChildStateModule{
+				storageAPI: tt.fields.storageAPI,
+				blockAPI:   tt.fields.blockAPI,
+			}
+			if err := cs.GetKeys(tt.args.in0, tt.args.req, tt.args.res); (err != nil) != tt.wantErr {
+				t.Errorf("GetKeys() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
