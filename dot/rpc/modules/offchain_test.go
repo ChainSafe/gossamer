8761b0d33c08406d9821485d7e5cb0b3763e6b55
jimjbrettj <jamesdjohnson218@gmail.com>
2021-11-02 10:52:32 -0600

    full coverage for offchain unit tests

--- dot/rpc/modules/offchain_test.go
+++ dot/rpc/modules/offchain_test.go
@@ -103,3 +103,117 @@ func TestOffchainModule_LocalStorageGet(t *testing.T) {
 		})
 	}
 }
+
+func TestOffchainModule_LocalStorageSet(t *testing.T) {
+	mockRuntimeStorageAPI := new(apimocks.RuntimeStorageAPI)
+	mockRuntimeStorageAPI.On("SetLocal", mock.AnythingOfType("[]uint8"), mock.AnythingOfType("[]uint8")).Return( nil)
+	mockRuntimeStorageAPI.On("SetPersistent", mock.AnythingOfType("[]uint8"), mock.AnythingOfType("[]uint8")).Return( errors.New("SetPersistent error"))
+
+
+	var res StringResponse
+	type fields struct {
+		nodeStorage RuntimeStorageAPI
+	}
+	type args struct {
+		in0 *http.Request
+		req *OffchainLocalStorageSet
+		in2 *StringResponse
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "setLocal OK",
+			fields: fields{
+				mockRuntimeStorageAPI,
+			},
+			args: args{
+				in0: nil,
+				req: &OffchainLocalStorageSet{
+					Kind:  offchainLocal,
+					Key:   "0x11111111111111",
+					Value: "0x22222222222222",
+				},
+				in2: &res,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid Key",
+			fields: fields{
+				mockRuntimeStorageAPI,
+			},
+			args: args{
+				in0: nil,
+				req: &OffchainLocalStorageSet{
+					Kind:  offchainLocal,
+					Key:   "0x1",
+					Value: "0x22222222222222",
+				},
+				in2: &res,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Value",
+			fields: fields{
+				mockRuntimeStorageAPI,
+			},
+			args: args{
+				in0: nil,
+				req: &OffchainLocalStorageSet{
+					Kind:  offchainLocal,
+					Key:   "0x11111111111111",
+					Value: "0x2",
+				},
+				in2: &res,
+			},
+			wantErr: true,
+		},
+		{
+			name: "setPersistentError",
+			fields: fields{
+				mockRuntimeStorageAPI,
+			},
+			args: args{
+				in0: nil,
+				req: &OffchainLocalStorageSet{
+					Kind:  offchainPersistent,
+					Key:   "0x11111111111111",
+					Value: "0x22222222222222",
+				},
+				in2: &res,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Kind",
+			fields: fields{
+				mockRuntimeStorageAPI,
+			},
+			args: args{
+				in0: nil,
+				req: &OffchainLocalStorageSet{
+					Kind:  "bad kind",
+					Key:   "0x11111111111111",
+					Value: "0x22222222222222",
+				},
+				in2: &res,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &OffchainModule{
+				nodeStorage: tt.fields.nodeStorage,
+			}
+			if err := s.LocalStorageSet(tt.args.in0, tt.args.req, tt.args.in2); (err != nil) != tt.wantErr {
+				t.Errorf("LocalStorageSet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
