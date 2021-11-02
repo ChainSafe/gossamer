d57f93a95b90a54c4291787617db0db9a4321846
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-11-02 21:10:53 +0000

    Tabs to spaces 😎

--- dot/network/pool_test.go
+++ dot/network/pool_test.go
@@ -15,7 +15,7 @@ func Benchmark_sizedBufferPool(b *testing.B) {
 	})
 }
 
-// Before: 				104853	    	 11119 ns/op	   65598 B/op	       1 allocs/op
-// Array ptr: 		2742781	       438.3 ns/op	       2 B/op	       0 allocs/op
-// Slices: 				2560960	       463.8 ns/op	       2 B/op	       0 allocs/op
+// Before:        104853	    	 11119 ns/op	   65598 B/op	       1 allocs/op
+// Array ptr:     2742781	       438.3 ns/op	       2 B/op	       0 allocs/op
+// Slices:        2560960	       463.8 ns/op	       2 B/op	       0 allocs/op
 // Slice pointer: 2683528	       460.8 ns/op	       2 B/op	       0 allocs/op
