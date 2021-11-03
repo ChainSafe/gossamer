466651854f8157d227f4b88b81aebd1d774bb819
noot <elizabethjbinks@gmail.com>
2021-11-03 16:58:03 -0400

    add lots of logs, comment out maintainTransactionPool for now

--- dot/core/service.go
+++ dot/core/service.go
@@ -314,7 +314,11 @@ func (s *Service) handleBlocksAsync() {
 		prev := s.blockState.BestBlockHash()
 
 		select {
-		case block := <-s.blockAddCh:
+		case block, ok := <-s.blockAddCh:
+			if !ok {
+				return
+			}
+
 			if block == nil {
 				continue
 			}
@@ -323,8 +327,7 @@ func (s *Service) handleBlocksAsync() {
 				logger.Warn("failed to re-add transactions to chain upon re-org", "error", err)
 			}
 
-			s.maintainTransactionPool(block)
-
+			//s.maintainTransactionPool(block)
 		case <-s.ctx.Done():
 			return
 		}
@@ -335,10 +338,14 @@ func (s *Service) handleBlocksAsync() {
 // previous chain head). If there is a re-org, it moves the transactions that were included on the previous
 // chain back into the transaction pool.
 func (s *Service) handleChainReorg(prev, curr common.Hash) error {
+	logger.Trace("handleChainReorg", "prev", prev, "curr", curr)
+
 	ancestor, err := s.blockState.HighestCommonAncestor(prev, curr)
 	if err != nil {
 		return err
 	}
+
+	logger.Trace("got HighestCommonAncestor", "ancestor", ancestor)
 
 	// if the highest common ancestor of the previous chain head and current chain head is the previous chain head,
 	// then the current chain head is the descendant of the previous and thus are on the same chain
@@ -346,15 +353,23 @@ func (s *Service) handleChainReorg(prev, curr common.Hash) error {
 		return nil
 	}
 
+	logger.Trace("getting subchain...")
+
 	subchain, err := s.blockState.SubChain(ancestor, prev)
 	if err != nil {
 		return err
 	}
 
+	logger.Trace("got subchain")
+
 	// subchain contains the ancestor as well so we need to remove it.
 	if len(subchain) > 0 {
 		subchain = subchain[1:]
+	} else {
+		return nil
 	}
+
+	logger.Trace("getting runtime")
 
 	// Check transaction validation on the best block.
 	rt, err := s.blockState.GetRuntime(nil)
@@ -362,12 +377,26 @@ func (s *Service) handleChainReorg(prev, curr common.Hash) error {
 		return err
 	}
 
+	if rt == nil {
+		panic("runtime is nil!")
+	}
+
+	logger.Trace("got runtime")
+
 	// for each block in the previous chain, re-add its extrinsics back into the pool
 	for _, hash := range subchain {
+		logger.Trace("handling subchain block", "hash", hash)
+
 		body, err := s.blockState.GetBlockBody(hash)
 		if err != nil {
 			continue
 		}
+
+		if body == nil {
+			panic("body is nil!")
+		}
+
+		logger.Trace("handling block body...", "body", body)
 
 		for _, ext := range *body {
 			logger.Trace("validating transaction on re-org chain", "extrinsic", ext)
@@ -378,10 +407,11 @@ func (s *Service) handleChainReorg(prev, curr common.Hash) error {
 
 			// decode extrinsic and make sure it's not an inherent.
 			decExt := &types.ExtrinsicData{}
-			err = decExt.DecodeVersion(encExt)
-			if err != nil {
-				return err
+			if err = decExt.DecodeVersion(encExt); err != nil {
+				continue
 			}
+
+			logger.Trace("decoded ext version", decExt)
 
 			// Inherent are not signed.
 			if !decExt.IsSigned() {
@@ -391,12 +421,16 @@ func (s *Service) handleChainReorg(prev, curr common.Hash) error {
 			externalExt := types.Extrinsic(append([]byte{byte(types.TxnExternal)}, encExt...))
 			txv, err := rt.ValidateTransaction(externalExt)
 			if err != nil {
-				logger.Info("failed to validate transaction", "error", err, "extrinsic", ext)
+				logger.Debug("failed to validate transaction", "error", err, "extrinsic", ext)
 				continue
 			}
 
+			logger.Trace("validated tx", "ext", ext)
+
 			vtx := transaction.NewValidTransaction(encExt, txv)
 			s.transactionState.AddToPool(vtx)
+
+			logger.Trace("added tx to pool")
 		}
 	}
 
