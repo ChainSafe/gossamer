ffc960550c7bdc12895eb9b3cac8d5085a2a8621
Omar <omar391@users.noreply.github.com>
2021-11-08 16:19:30 +0000

    Made the test to t.Parallel()

--- dot/sync/utils_test.go
+++ dot/sync/utils_test.go
@@ -24,6 +24,7 @@ import (
 )
 
 func TestRemoveOutlier(t *testing.T) {
+	t.Parallel()
 	count := int64(0)
 	arr := []interface{}{big.NewInt(-100), big.NewInt(20), big.NewInt(40), big.NewInt(50), big.NewInt(60), big.NewInt(80), big.NewInt(100), big.NewInt(1000)}
 
