caf6418c8514cfa8bbf075b10c1a96613560699b
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-11-19 11:06:55 +0000

    `testdata` zip files for the fat strings

---
+++ lib/grandpa/testdata/netdata.go
@@ -0,0 +1,39 @@
+package testdata
+
+import (
+	"archive/zip"
+	"bytes"
+	_ "embed"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+//go:embed data.3b1b0.zip
+var data3b1b0Zip []byte
+
+// Data3b1b0 returns the bytes of the network data starting with
+// 0x3b1b0.
+func Data3b1b0(t *testing.T) (b []byte) {
+	bytesReader := bytes.NewReader(data3b1b0Zip)
+
+	zipReader, err := zip.NewReader(bytesReader, int64(bytesReader.Len()))
+	require.NoError(t, err)
+
+	require.Len(t, zipReader.File, 1)
+	zipFile := zipReader.File[0]
+
+	file, err := zipFile.Open()
+	require.NoError(t, err)
+
+	defer func() {
+		err := file.Close()
+		require.NoError(t, err)
+	}()
+
+	b, err = io.ReadAll(file)
+	require.NoError(t, err)
+
+	return b
+}
