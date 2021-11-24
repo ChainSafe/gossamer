b1dd873975c7092843d8558c7c7c0555c3d87c37
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-11-24 11:03:56 +0000

    chore(httpserver): optional shutdown timeout parameter

---
+++ internal/httpserver/option_test.go
@@ -0,0 +1,42 @@
+package httpserver
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_newOptionalSettings(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		options  []Option
+		settings optionalSettings
+	}{
+		"no option": {
+			settings: optionalSettings{
+				shutdownTimeout: 3 * time.Second,
+			},
+		},
+		"shutdown option": {
+			options: []Option{
+				ShutdownTimeout(time.Second),
+			},
+			settings: optionalSettings{
+				shutdownTimeout: time.Second,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			settings := newOptionalSettings(testCase.options)
+
+			assert.Equal(t, testCase.settings, settings)
+		})
+	}
+}
