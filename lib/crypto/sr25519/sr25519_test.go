f223d65cc4506ee3cbc9557b49001e3810bc3300
1garo <alevardai427@gmail.com>
2021-11-30 19:52:26 -0300

    make use of constants

--- lib/crypto/sr25519/sr25519_test.go
+++ lib/crypto/sr25519/sr25519_test.go
@@ -153,7 +153,7 @@ func TestVerifySignature(t *testing.T) {
 			publicKey: keypair.public.Encode(),
 			signature: []byte{},
 			message:   message,
-			err:       errors.New("sr25519: failed to verify signature: invalid signature length"),
+			err:       fmt.Errorf("sr25519: %w: invalid signature length", crypto.ErrSignatureVerificationFailed),
 		},
 		"verification failed": {
 			publicKey: keypair.public.Encode(),
