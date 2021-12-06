d0d4290bb026a3e117b49d54d631a7501bb3860c
jimjbrettj <jamesdjohnson218@gmail.com>
2021-12-06 15:23:52 -0700

    test getSecondarySlotAuthor

--- lib/babe/secondary_test.go
+++ lib/babe/secondary_test.go
@@ -2,3 +2,44 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 package babe
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_getSecondarySlotAuthor(t *testing.T) {
+	type args struct {
+		slot       uint64
+		numAuths   int
+		randomness Randomness
+	}
+	tests := []struct {
+		name    string
+		args    args
+		exp     uint32
+		wantErr bool
+		expErr  error
+	}{
+		{
+			name: "happy path",
+			args: args{
+				slot:       21,
+				numAuths:   21,
+				randomness: Randomness{},
+			},
+			exp: 14,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := getSecondarySlotAuthor(tt.args.slot, tt.args.numAuths, tt.args.randomness)
+			if tt.expErr != nil {
+				assert.EqualError(t, err, tt.expErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.exp, res)
+		})
+	}
+}
