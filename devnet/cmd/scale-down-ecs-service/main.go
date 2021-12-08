e2f0d6cb03f7b576020aac2f5a1aec91df44bfa2
Tim Wu <timwu20@gmail.com>
2021-12-07 22:36:03 -0500

    use serviceScaler type

--- devnet/cmd/scale-down-ecs-service/main.go
+++ devnet/cmd/scale-down-ecs-service/main.go
@@ -2,123 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"regexp"
 	"syscall"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/jessevdk/go-flags"
 )
 
-var (
-	svc *ecs.ECS
-)
-
-func init() {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc = ecs.New(sess)
-}
-
-func findServiceArns(ctx context.Context, cluster, serviceRegex string) (serviceArns []*string, err error) {
-	r, err := regexp.Compile(serviceRegex)
-	if err != nil {
-		return
-	}
-
-	var lsi = &ecs.ListServicesInput{
-		Cluster: &cluster,
-	}
-	for {
-		var lso *ecs.ListServicesOutput
-		lso, err = svc.ListServicesWithContext(ctx, lsi)
-		if err != nil {
-			return
-		}
-		for _, arn := range lso.ServiceArns {
-			if r.MatchString(*arn) {
-				serviceArns = append(serviceArns, arn)
-			}
-		}
-		if lso.NextToken != nil {
-			lsi.NextToken = lso.NextToken
-		} else {
-			break
-		}
-	}
-	return
-}
-
-func updateServices(ctx context.Context, cluster string, serviceArns []*string) (err error) {
-	for _, serviceArn := range serviceArns {
-		_, err = svc.UpdateServiceWithContext(ctx, &ecs.UpdateServiceInput{
-			Cluster:      &cluster,
-			Service:      serviceArn,
-			DesiredCount: aws.Int64(0),
-		})
-		if err != nil {
-			return
-		}
-	}
-	return
-}
-
-func waitForRunningCount(ctx context.Context, cluster string, serviceArns []*string) (err error) {
-	ticker := time.NewTicker(time.Second * 5)
-main:
-	for {
-		select {
-		case <-ticker.C:
-			var dso *ecs.DescribeServicesOutput
-			dso, err = svc.DescribeServicesWithContext(ctx, &ecs.DescribeServicesInput{
-				Cluster:  &cluster,
-				Services: serviceArns,
-			})
-			if err != nil {
-				break main
-			}
-			scaledDown := make(map[string]bool)
-			for _, service := range dso.Services {
-				if service.RunningCount != nil && *service.RunningCount == 0 {
-					scaledDown[*service.ServiceArn] = true
-				}
-			}
-			if len(scaledDown) == len(serviceArns) {
-				break main
-			}
-		case <-ctx.Done():
-			err = fmt.Errorf("aborting waiting, received done from context")
-			break main
-		}
-	}
-	return
-}
-
-func scaleServices(ctx context.Context, cluster string, servicesRegex string) (err error) {
-	serviceArns, err := findServiceArns(ctx, cluster, servicesRegex)
-	if err != nil {
-		return
-	}
-
-	err = updateServices(ctx, cluster, serviceArns)
-	if err != nil {
-		return
-	}
-
-	err = waitForRunningCount(ctx, cluster, serviceArns)
-	return
-}
-
 type options struct {
-	ServicesRegex string `short:"s" long:"services" description:"regex query used to match against AWS service names" required:"true"` //nolint:lll
-	Cluster       string `short:"c" long:"cluster" description:"ECS cluster name, must be exact match"`
+	ServicesRegex   string        `short:"s" long:"services" description:"regex query used to match against AWS service names" required:"true"`          //nolint:lll
+	Cluster         string        `short:"c" long:"cluster" description:"ECS cluster name, must be exact match" required:"true"`                         //nolint:lll
+	RequestInterval time.Duration `short:"i" long:"interval" description:"Interval between AWS requests when waiting for service to scale" default:"5s"` //nolint:lll
 }
 
 func main() {
@@ -131,10 +29,15 @@ func main() {
 		log.Panic(err)
 	}
 
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
 	ctx, cancel := context.WithCancel(context.Background())
 	done := make(chan error)
 	go func() {
-		err = scaleServices(ctx, opts.Cluster, opts.ServicesRegex)
+		ss := newServiceScaler(opts.RequestInterval, opts.Cluster, ecs.New(sess))
+		err := ss.scaleServices(ctx, opts.ServicesRegex)
 		done <- err
 	}()
 
---
+++ devnet/cmd/scale-down-ecs-service/service_scaler.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/aws/aws-sdk-go/service/ecs/ecsiface"
+)
+
+type serviceScaler struct {
+	tickerDuration time.Duration
+	cluster        string
+	ecs            ecsiface.ECSAPI
+}
+
+func newServiceScaler(tickerDuration time.Duration, cluster string, ecs ecsiface.ECSAPI) *serviceScaler {
+	return &serviceScaler{
+		tickerDuration: tickerDuration,
+		cluster:        cluster,
+		ecs:            ecs,
+	}
+}
+
+func (ss serviceScaler) findServiceArns(ctx context.Context, serviceRegex string) (serviceArns []*string, err error) {
+	r, err := regexp.Compile(serviceRegex)
+	if err != nil {
+		return
+	}
+
+	var lsi = &ecs.ListServicesInput{
+		Cluster: &ss.cluster,
+	}
+	for {
+		var lso *ecs.ListServicesOutput
+		lso, err = ss.ecs.ListServicesWithContext(ctx, lsi)
+		if err != nil {
+			return
+		}
+		for _, arn := range lso.ServiceArns {
+			if r.MatchString(*arn) {
+				serviceArns = append(serviceArns, arn)
+			}
+		}
+		if lso.NextToken != nil {
+			lsi.NextToken = lso.NextToken
+		} else {
+			break
+		}
+	}
+
+	if len(serviceArns) == 0 {
+		err = fmt.Errorf("unable to locate any services with query: %s", serviceRegex)
+	}
+	return
+}
+
+func (ss serviceScaler) updateServices(ctx context.Context, serviceArns []*string) (err error) {
+	for _, serviceArn := range serviceArns {
+		_, err = ss.ecs.UpdateServiceWithContext(ctx, &ecs.UpdateServiceInput{
+			Cluster:      &ss.cluster,
+			Service:      serviceArn,
+			DesiredCount: aws.Int64(0),
+		})
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (ss serviceScaler) waitForRunningCount(ctx context.Context, serviceArns []*string) (err error) {
+	ticker := time.NewTicker(ss.tickerDuration)
+main:
+	for {
+		select {
+		case <-ticker.C:
+			var dso *ecs.DescribeServicesOutput
+			dso, err = ss.ecs.DescribeServicesWithContext(ctx, &ecs.DescribeServicesInput{
+				Cluster:  &ss.cluster,
+				Services: serviceArns,
+			})
+			if err != nil {
+				break main
+			}
+			scaledDown := make(map[string]bool)
+			for _, service := range dso.Services {
+				if service.RunningCount != nil && *service.RunningCount == 0 {
+					scaledDown[*service.ServiceArn] = true
+				}
+			}
+			if len(scaledDown) == len(serviceArns) {
+				break main
+			}
+		case <-ctx.Done():
+			err = fmt.Errorf("aborting waiting, received done from context")
+			break main
+		}
+	}
+	return
+}
+
+func (ss serviceScaler) scaleServices(ctx context.Context, servicesRegex string) (err error) {
+	serviceArns, err := ss.findServiceArns(ctx, servicesRegex)
+	if err != nil {
+		return
+	}
+
+	err = ss.updateServices(ctx, serviceArns)
+	if err != nil {
+		return
+	}
+
+	err = ss.waitForRunningCount(ctx, serviceArns)
+	return
+}
