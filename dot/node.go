2880f52520e15c29ab104cbf7d9d1974ae14334d
Edward Mack <ed@edwardmack.com>
2021-12-09 16:09:26 -0500

    add to comment

--- dot/node.go
+++ dot/node.go
@@ -120,7 +120,7 @@ func (nodeInterface) nodeInitialised(basepath string) bool {
 	return true
 }
 
-// InitNode init Node
+// InitNode initialise the node with given Config
 func InitNode(cfg *Config) error {
 	nodeI := nodeInterface{}
 	return nodeI.initNode(cfg)
