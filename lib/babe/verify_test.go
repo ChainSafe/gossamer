93df58ba51ddb9aa1d01717f605203c4b103c89a
jimjbrettj <jamesdjohnson218@gmail.com>
2021-12-09 16:46:14 -0700

    getAuthorityIndex test

--- lib/babe/verify_test.go
+++ lib/babe/verify_test.go
@@ -2,3 +2,135 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 package babe
+
+import (
+	"errors"
+	"github.com/ChainSafe/gossamer/dot/types"
+	"github.com/ChainSafe/gossamer/pkg/scale"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_getAuthorityIndex(t *testing.T) {
+	digest := types.NewDigest()
+	err := digest.Add(types.SealDigest{
+		ConsensusEngineID: types.ConsensusEngineID{},
+		Data:              nil,
+	})
+	assert.NoError(t, err)
+	headerNoPre := types.NewEmptyHeader()
+	headerNoPre.Digest = digest
+
+	digest2 := types.NewDigest()
+	err = digest2.Add(types.PreRuntimeDigest{
+		ConsensusEngineID: types.BabeEngineID,
+		Data:              []byte{1},
+	})
+	assert.NoError(t, err)
+	headerInvalidPre := types.NewEmptyHeader()
+	headerInvalidPre.Digest = digest2
+
+	// BabePrimaryPreDigest Case
+	babeDigest := types.NewBabeDigest()
+	err = babeDigest.Set(types.BabePrimaryPreDigest{AuthorityIndex: 21})
+	assert.NoError(t, err)
+
+	bdEnc, err := scale.Marshal(babeDigest)
+	require.NoError(t, err)
+
+	digestPrimary := types.NewDigest()
+	err = digestPrimary.Add(types.PreRuntimeDigest{
+		ConsensusEngineID: types.BabeEngineID,
+		Data:              bdEnc,
+	})
+	assert.NoError(t, err)
+	headerPrimary := types.NewEmptyHeader()
+	headerPrimary.Digest = digestPrimary
+
+	//BabeSecondaryVRFPreDigest Case
+	babeDigest2 := types.NewBabeDigest()
+	err = babeDigest2.Set(types.BabeSecondaryVRFPreDigest{AuthorityIndex: 21})
+	assert.NoError(t, err)
+
+	bdEnc2, err := scale.Marshal(babeDigest2)
+	require.NoError(t, err)
+
+	digestSecondary := types.NewDigest()
+	err = digestSecondary.Add(types.PreRuntimeDigest{
+		ConsensusEngineID: types.BabeEngineID,
+		Data:              bdEnc2,
+	})
+	assert.NoError(t, err)
+	headerSecondary := types.NewEmptyHeader()
+	headerSecondary.Digest = digestSecondary
+
+	//BabeSecondaryPlainPreDigest case
+	babeDigest3 := types.NewBabeDigest()
+	err = babeDigest3.Set(types.BabeSecondaryPlainPreDigest{AuthorityIndex: 21})
+	assert.NoError(t, err)
+
+	bdEnc3, err := scale.Marshal(babeDigest3)
+	require.NoError(t, err)
+
+	digestSecondaryPlain := types.NewDigest()
+	err = digestSecondaryPlain.Add(types.PreRuntimeDigest{
+		ConsensusEngineID: types.BabeEngineID,
+		Data:              bdEnc3,
+	})
+	assert.NoError(t, err)
+	headerSecondaryPlain := types.NewEmptyHeader()
+	headerSecondaryPlain.Digest = digestSecondaryPlain
+
+	type args struct {
+		header *types.Header
+	}
+	tests := []struct {
+		name    string
+		args    args
+		exp    uint32
+		expErr error
+	}{
+		{
+			name: "No Digest",
+			args: args{types.NewEmptyHeader()},
+			expErr: errors.New("no digest provided"),
+		},
+		{
+			name: "First Digest Invalid Type",
+			args: args{headerNoPre},
+			expErr: errors.New("first digest item is not pre-runtime digest"),
+		},
+		{
+			name: "Invalid Preruntime Digest Type",
+			args: args{headerInvalidPre},
+			expErr: errors.New("cannot decode babe header from pre-digest: EOF, field: 0"),
+		},
+		{
+			name: "BabePrimaryPreDigest Type",
+			args: args{headerPrimary},
+			exp: 21,
+		},
+		{
+			name: "BabeSecondaryVRFPreDigest Type",
+			args: args{headerSecondary},
+			exp: 21,
+		},
+		{
+			name: "BabeSecondaryPlainPreDigest Type",
+			args: args{headerSecondaryPlain},
+			exp: 21,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := getAuthorityIndex(tt.args.header)
+			if tt.expErr != nil {
+				assert.EqualError(t, err, tt.expErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.exp, res)
+		})
+	}
+}
