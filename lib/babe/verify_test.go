9e20f8af284505cf0d9057ab275b050a8cc5b7bb
jimjbrettj <jamesdjohnson218@gmail.com>
2021-12-10 14:31:53 -0700

    test verifyPreruntime Digest

--- lib/babe/verify_test.go
+++ lib/babe/verify_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ChainSafe/gossamer/lib/babe/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"testing"
 )
 
@@ -166,7 +167,7 @@ func Test_verifier_verifyPrimarySlotWinner(t *testing.T) {
 
 	v1, err := newVerifier(mockBlockState, 1, vi1)
 	assert.NoError(t, err)
-	
+
 	output, proof, err := kp.VrfSign(makeTranscript(Randomness{}, uint64(1), 1))
 	assert.NoError(t, err)
 
@@ -220,6 +221,202 @@ func Test_verifier_verifyPrimarySlotWinner(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			b := &tt.verifier
 			res, err := b.verifyPrimarySlotWinner(tt.args.authorityIndex, tt.args.slot, tt.args.vrfOutput, tt.args.vrfProof)
+			if tt.expErr != nil {
+				assert.EqualError(t, err, tt.expErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.exp, res)
+		})
+	}
+}
+
+func Test_verifier_verifyPreRuntimeDigest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockBlockState := mocks.NewMockBlockState(ctrl)
+
+	//Generate keys
+	kp, err := sr25519.GenerateKeypair()
+	assert.NoError(t, err)
+
+	//BabePrimaryPreDigest case
+	output, proof, err := kp.VrfSign(makeTranscript(Randomness{}, uint64(1), 1))
+	assert.NoError(t, err)
+
+	secDigest1 := types.BabePrimaryPreDigest{
+		AuthorityIndex: 0,
+		SlotNumber: uint64(1),
+		VRFOutput: output,
+		VRFProof: proof,
+	}
+	prd1, err := secDigest1.ToPreRuntimeDigest()
+	assert.NoError(t, err)
+
+	auth := types.NewAuthority(kp.Public(), uint64(1))
+	vi := &verifierInfo{
+		authorities:    []types.Authority{*auth, *auth},
+		randomness:     Randomness{},
+		threshold:      scale.MaxUint128,
+		secondarySlots: false,
+	}
+
+	v, err := newVerifier(mockBlockState, 1, vi)
+	assert.NoError(t, err)
+
+	// Invalid
+	v2, err := newVerifier(mockBlockState, 13, vi)
+	assert.NoError(t, err)
+
+	// Above threshold case
+	vi1 := &verifierInfo{
+		authorities:    []types.Authority{*auth, *auth},
+		randomness:     Randomness{},
+		threshold:      &scale.Uint128{},
+		secondarySlots: false,
+	}
+
+	v1, err := newVerifier(mockBlockState, 1, vi1)
+	assert.NoError(t, err)
+
+	//BabeSecondaryVRFPreDigest case
+	secVRFDigest := types.BabeSecondaryVRFPreDigest{
+		AuthorityIndex: 0,
+		SlotNumber: uint64(1),
+		VrfOutput: output,
+		VrfProof: proof,
+	}
+
+	digestSecondaryVRF := types.NewBabeDigest()
+	err = digestSecondaryVRF.Set(secVRFDigest)
+	assert.NoError(t, err)
+
+	bdEnc, err := scale.Marshal(digestSecondaryVRF)
+	require.NoError(t, err)
+
+	babePRD := types.NewBABEPreRuntimeDigest(bdEnc)
+
+	authVRFSec := types.NewAuthority(kp.Public(), uint64(1))
+	viVRFSec := &verifierInfo{
+		authorities:    []types.Authority{*authVRFSec, *authVRFSec},
+		randomness:     Randomness{},
+		threshold:      scale.MaxUint128,
+		secondarySlots: false,
+	}
+
+	viVRFSec2 := &verifierInfo{
+		authorities:    []types.Authority{*authVRFSec, *authVRFSec},
+		randomness:     Randomness{},
+		threshold:      scale.MaxUint128,
+		secondarySlots: true,
+	}
+
+	vVRFSec, err := newVerifier(mockBlockState, 1, viVRFSec)
+	assert.NoError(t, err)
+
+	vVRFSec2, err := newVerifier(mockBlockState, 1, viVRFSec2)
+	assert.NoError(t, err)
+
+	//BabeSecondaryPlainPreDigest case
+	secDigest := types.BabeSecondaryPlainPreDigest{AuthorityIndex: 0, SlotNumber: uint64(1)}
+	prd, err := secDigest.ToPreRuntimeDigest()
+	assert.NoError(t, err)
+
+	authSec := types.NewAuthority(kp.Public(), uint64(1))
+	viSec := &verifierInfo{
+		authorities:    []types.Authority{*authSec, *authSec},
+		randomness:     Randomness{},
+		threshold:      scale.MaxUint128,
+		secondarySlots: false,
+	}
+
+	viSec2 := &verifierInfo{
+		authorities:    []types.Authority{*authSec, *authSec},
+		randomness:     Randomness{},
+		threshold:      scale.MaxUint128,
+		secondarySlots: true,
+	}
+
+	vSec, err := newVerifier(mockBlockState, 1, viSec)
+	assert.NoError(t, err)
+
+	vSec2, err := newVerifier(mockBlockState, 1, viSec2)
+	assert.NoError(t, err)
+
+
+	type args struct {
+		digest *types.PreRuntimeDigest
+	}
+	tests := []struct {
+		name    string
+		verifier  verifier
+		args    args
+		exp    scale.VaryingDataTypeValue
+		expErr error
+	}{
+		{
+			name: "Invalid PreRuntimeDigest",
+			verifier: verifier{},
+			args: args{&types.PreRuntimeDigest{Data: []byte{0}}},
+			expErr: errors.New("unable to find VaryingDataTypeValue with index: 0"),
+		},
+		{
+			name: "Invalid BlockProducer Index",
+			verifier: verifier{},
+			args: args{prd},
+			expErr: ErrInvalidBlockProducerIndex,
+		},
+		{
+			name: "BabePrimaryPreDigest Case OK",
+			verifier: *v,
+			args: args{prd1},
+			exp: types.BabePrimaryPreDigest{
+				AuthorityIndex: 0,
+				SlotNumber: uint64(1),
+				VRFOutput: output,
+				VRFProof: proof,
+			},
+		},
+		{
+			name: "BabePrimaryPreDigest Case output over threshold",
+			verifier: *v1,
+			args: args{prd1},
+			expErr: errors.New("vrf output over threshold"),
+		},
+		{
+			name: "BabePrimaryPreDigest Case Invalid",
+			verifier: *v2,
+			args: args{prd1},
+			expErr: ErrBadSlotClaim,
+		},
+		{
+			name: "BabeSecondaryPlainPreDigest SecondarySlot false",
+			verifier: *vSec,
+			args: args{prd},
+			expErr: ErrBadSlotClaim,
+		},
+		{
+			name: "BabeSecondaryPlainPreDigest invalid claim",
+			verifier: *vSec2,
+			args: args{prd},
+			expErr: errors.New("invalid secondary slot claim"),
+		},
+		{
+			name: "BabeSecondaryVRFPreDigest SecondarySlot false",
+			verifier: *vVRFSec,
+			args: args{babePRD},
+			expErr: ErrBadSlotClaim,
+		},
+		{
+			name: "BabeSecondaryVRFPreDigest invalid claim",
+			verifier: *vVRFSec2,
+			args: args{babePRD},
+			expErr: errors.New("invalid secondary slot claim"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &tt.verifier
+			res, err := b.verifyPreRuntimeDigest(tt.args.digest)
 			if tt.expErr != nil {
 				assert.EqualError(t, err, tt.expErr.Error())
 			} else {
