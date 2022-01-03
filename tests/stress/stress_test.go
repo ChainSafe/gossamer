a7e4fc313659876c32c308334422d16c42a18b75
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-01-03 09:10:13 +0000

    Replace fmt.Fprintln with t.Log and fmt.Println

--- tests/stress/stress_test.go
+++ tests/stress/stress_test.go
@@ -27,7 +27,7 @@ import (
 
 func TestMain(m *testing.M) {
 	if utils.MODE != "stress" {
-		_, _ = fmt.Fprintln(os.Stdout, "Skipping stress test")
+		fmt.Println("Skipping stress test")
 		return
 	}
 
