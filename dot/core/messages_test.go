8dcdc6a244fc1777373969578856683b54b70681
jimboj <jamesdjohnson218@gmail.com>
2022-01-12 11:04:59 -0700

    WIP/hanldeTxnMsgTest

--- dot/core/messages_test.go
+++ dot/core/messages_test.go
@@ -4,6 +4,9 @@
 package core
 
 import (
+	"github.com/ChainSafe/gossamer/dot/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/ChainSafe/gossamer/lib/transaction"
@@ -39,9 +42,45 @@ func TestService_TransactionsCount(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := tt.service
-			if got := s.TransactionsCount(); got != tt.exp {
-				t.Errorf("TransactionsCount() = %v, want %v", got, tt.exp)
+			res := s.TransactionsCount()
+			assert.Equal(t, tt.exp, res)
+		})
+	}
+}
+
+func TestServiceHandleTransactionMessage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockNotSyncedNet := NewMockNetwork(ctrl)
+	mockNotSyncedNet.EXPECT().IsSynced().Return(false)
+
+	type args struct {
+		peerID peer.ID
+		msg    *network.TransactionMessage
+	}
+	tests := []struct {
+		name    string
+		service *Service
+		args    args
+		exp     bool
+		expErr  error
+	}{
+		{
+			name:    "not synced",
+			service: &Service{net: mockNotSyncedNet},
+			args:    args{},
+			exp:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.service
+			res, err := s.HandleTransactionMessage(tt.args.peerID, tt.args.msg)
+			if tt.expErr != nil {
+				assert.EqualError(t, err, tt.expErr.Error())
+			} else {
+				assert.NoError(t, err)
 			}
+			assert.Equal(t, tt.exp, res)
 		})
 	}
 }
