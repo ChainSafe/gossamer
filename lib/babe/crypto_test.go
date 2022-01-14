e1f7f96cb8cb7e82156f7333b9a0704ab8151fd2
JimboJ <40345116+jimjbrettj@users.noreply.github.com>
2022-01-14 14:44:23 -0700

    Jimmy/babe unit tests (#2127)
    * chore(pkg/scale): return error if `Result` already has an assigned value  (#2143)
    
    * fix: return error if result already has an assigned value
    
    * chore: include unit test
    
    * chore: fix typo `ErrResultAlreadySet`
    
    * chore: remove unneeded `require.Error`
    
    * chore: fix undeclared name
    
    * chore: remove packaged scope var to avoid problems with result type
    
    * chore: fix result.Set error at offchain test
    
    * init
    
    * migrate current tests to integration tests
    
    * fix linting
    
    * test line endings
    
    * remove test comments
    
    * test getSecondarySlotAuthor
    
    * secondary unit tests
    
    * calculate threshold tests
    
    * remove logs
    
    * test checkPrimaryThreshold
    
    * test babe/crypto
    
    * getAuthorityIndex test
    
    * babe mocks with mockgen
    
    * test verifyPrimarySlotWinner
    
    * test verifyPreruntime Digest
    
    * WIP/Test cases for verify authorship right
    
    * finish hitting test cases for equivocation
    
    * clean up naming for verifyBlockAuthor test
    
    * create helper for making verifier
    
    * use helpers to clean up verifyAuthorship test
    
    * clean up mocks
    
    * getConfigData test
    
    * test getVerifierInfo
    
    * remove logs
    
    * test first block cases for block verification
    
    * test VerifyBlock
    
    * finish first half of babe unit tests
    
    * remove coverage files
    
    * remove accidental diff
    
    * remove accidental diff again
    
    * fix linting
    
    * remove testing aliases
    
    * make calculate threshold error global
    
    * add go generate comment for babe state TODO/make sure ok form
    
    * fix mock generation
    
    * add newline to end of build integration test
    
    * newlines
    
    * wip/newline
    
    * test line endings
    
    * wip
    
    * remove crypto integration test
    
    * change threshold err message
    
    * wip
    
    * test
    
    * remove test comments
    
    * clean up
    
    * fix build error
    
    * CR feedback
    
    * more CR feedback
    
    * change threshold error name
    
    * remove empty fields
    
    * change how mockgen used
    
    * remove test log
    
    * cr feedback
    
    * remove errors from helper funcs
    
    * final helper fix
    
    * rebase and lint
    
    * CR feedback
    
    * lint
    
    Co-authored-by: Eclésio Junior <eclesiomelo.1@gmail.com>

--- lib/babe/crypto_test.go
+++ lib/babe/crypto_test.go
@@ -4,46 +4,170 @@
 package babe
 
 import (
-	"testing"
+	"errors"
 
-	"github.com/stretchr/testify/require"
+	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
+	"github.com/ChainSafe/gossamer/lib/keystore"
+	"github.com/ChainSafe/gossamer/pkg/scale"
+	"github.com/stretchr/testify/assert"
+
+	"testing"
 )
 
-func TestRunLottery(t *testing.T) {
-	babeService := createTestService(t, nil)
-
-	babeService.epochData.threshold = maxThreshold
-
-	outAndProof, err := babeService.runLottery(0, testEpochIndex)
-	require.NoError(t, err)
-	require.NotNil(t, outAndProof)
-}
-
-func TestRunLottery_False(t *testing.T) {
-	babeService := createTestService(t, nil)
-	babeService.epochData.threshold = minThreshold
-
-	outAndProof, err := babeService.runLottery(0, testEpochIndex)
-	require.ErrorIs(t, err, errOverPrimarySlotThreshold)
-	require.Nil(t, outAndProof)
-}
-
 func TestCalculateThreshold(t *testing.T) {
-	// C = 1
-	var C1 uint64 = 1
-	var C2 uint64 = 1
-
-	expected := maxThreshold
-
-	threshold, err := CalculateThreshold(C1, C2, 3)
-	require.NoError(t, err)
-	require.Equal(t, expected, threshold)
+	type args struct {
+		C1       uint64
+		C2       uint64
+		numAuths int
+	}
+	tests := []struct {
+		name   string
+		args   args
+		exp    *scale.Uint128
+		expErr error
+	}{
+		{
+			name: "happy path",
+			args: args{
+				C1:       1,
+				C2:       2,
+				numAuths: 3,
+			},
+			exp: &scale.Uint128{Upper: 0x34d00ad6148e1800, Lower: 0x0},
+		},
+		{
+			name: "0 value input",
+			args: args{
+				C1:       0,
+				C2:       0,
+				numAuths: 0,
+			},
+			expErr: ErrThresholdOneIsZero,
+		},
+		{
+			name: "C1 > C2",
+			args: args{
+				C1:       5,
+				C2:       2,
+				numAuths: 0,
+			},
+			expErr: errors.New("invalid C1/C2: greater than 1"),
+		},
+		{
+			name: "max threshold",
+			args: args{
+				C1:       1,
+				C2:       1,
+				numAuths: 3,
+			},
+			exp: scale.MaxUint128,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := CalculateThreshold(tt.args.C1, tt.args.C2, tt.args.numAuths)
+			if tt.expErr != nil {
+				assert.EqualError(t, err, tt.expErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.exp, res)
+		})
+	}
 }
 
-func TestCalculateThreshold_Failing(t *testing.T) {
-	var C1 uint64 = 5
-	var C2 uint64 = 4
+func Test_checkPrimaryThreshold(t *testing.T) {
+	keyring, _ := keystore.NewSr25519Keyring()
+	aliceKeypair := keyring.Alice().(*sr25519.Keypair)
+	type args struct {
+		randomness Randomness
+		slot       uint64
+		epoch      uint64
+		output     [sr25519.VRFOutputLength]byte
+		threshold  *scale.Uint128
+		pub        *sr25519.PublicKey
+	}
+	tests := []struct {
+		name string
+		args args
+		exp  bool
+	}{
+		{
+			name: "happy path true",
+			args: args{
+				threshold: scale.MaxUint128,
+				pub:       aliceKeypair.Public().(*sr25519.PublicKey),
+			},
+			exp: true,
+		},
+		{
+			name: "happy path false",
+			args: args{
+				threshold: &scale.Uint128{},
+				pub:       aliceKeypair.Public().(*sr25519.PublicKey),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := checkPrimaryThreshold(tt.args.randomness, tt.args.slot, tt.args.epoch,
+				tt.args.output, tt.args.threshold, tt.args.pub)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.exp, res)
+		})
+	}
+}
 
-	_, err := CalculateThreshold(C1, C2, 3)
-	require.NotNil(t, err)
+func Test_claimPrimarySlot(t *testing.T) {
+	keyring, _ := keystore.NewSr25519Keyring()
+	type args struct {
+		randomness Randomness
+		slot       uint64
+		epoch      uint64
+		threshold  *scale.Uint128
+		keypair    *sr25519.Keypair
+	}
+	tests := []struct {
+		name   string
+		args   args
+		exp    *VrfOutputAndProof
+		expErr error
+	}{
+		{
+			name: "authority not authorized",
+			args: args{
+				slot:      1,
+				epoch:     2,
+				threshold: &scale.Uint128{},
+				keypair:   keyring.Alice().(*sr25519.Keypair),
+			},
+			expErr: errors.New("cannot claim slot, over primary threshold: for slot 1, epoch 2 and threshold 0"),
+		},
+		{
+			name: "authority authorized",
+			args: args{
+				slot:      1,
+				epoch:     2,
+				threshold: scale.MaxUint128,
+				keypair:   keyring.Alice().(*sr25519.Keypair),
+			},
+			exp: &VrfOutputAndProof{
+				output: [32]uint8{0x80, 0xf0, 0x8a, 0x7d, 0xa1, 0x71, 0x77, 0xdc, 0x7, 0x7f, 0x6, 0xd5, 0xc1, 0x5d, 0x90,
+					0x4f, 0x64, 0x21, 0xb6, 0x1d, 0x1c, 0xa8, 0x55, 0x3a, 0x97, 0x1a, 0xbb, 0xf3, 0x35, 0x12, 0x25, 0x18},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := claimPrimarySlot(tt.args.randomness, tt.args.slot, tt.args.epoch, tt.args.threshold, tt.args.keypair)
+			if tt.expErr != nil {
+				assert.EqualError(t, err, tt.expErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			if tt.exp != nil && res != nil {
+				assert.Equal(t, tt.exp.output, res.output)
+			}
+		})
+	}
 }
