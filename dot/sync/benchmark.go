99d444df0d1819632990de5062f7b6a65f566525
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2022-01-25 16:11:49 +0000

    Fix sync benchmark - only keep 30 latest samples - Inject time to benchmark functions - Remove force setting blocks to `1` when `0`

--- dot/sync/benchmark.go
+++ dot/sync/benchmark.go
@@ -11,31 +11,35 @@ type syncBenchmarker struct {
 	start           time.Time
 	startBlock      uint64
 	blocksPerSecond []float64
+	samplesToKeep   int
 }
 
-func newSyncBenchmarker() *syncBenchmarker {
+func newSyncBenchmarker(samplesToKeep int) *syncBenchmarker {
 	return &syncBenchmarker{
-		blocksPerSecond: []float64{},
+		blocksPerSecond: make([]float64, 0, samplesToKeep),
+		samplesToKeep:   samplesToKeep,
 	}
 }
 
-func (b *syncBenchmarker) begin(block uint64) {
-	b.start = time.Now()
+func (b *syncBenchmarker) begin(now time.Time, block uint64) {
+	b.start = now
 	b.startBlock = block
 }
 
-func (b *syncBenchmarker) end(block uint64) {
-	duration := time.Since(b.start)
+func (b *syncBenchmarker) end(now time.Time, block uint64) {
+	duration := now.Sub(b.start)
 	blocks := block - b.startBlock
-	if blocks == 0 {
-		blocks = 1
-	}
 	bps := float64(blocks) / duration.Seconds()
+
+	if len(b.blocksPerSecond) == b.samplesToKeep {
+		b.blocksPerSecond = b.blocksPerSecond[1:]
+	}
+
 	b.blocksPerSecond = append(b.blocksPerSecond, bps)
 }
 
 func (b *syncBenchmarker) average() float64 {
-	sum := float64(0)
+	var sum float64
 	for _, bps := range b.blocksPerSecond {
 		sum += bps
 	}
