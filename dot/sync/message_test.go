bf9d113f0d707bc292629e1bd9d6d74725983172
edwardmack <ed@edwardmack.com>
2022-01-31 16:26:18 -0500

    implement messages tests

--- dot/sync/message_test.go
+++ dot/sync/message_test.go
@@ -4,6 +4,7 @@
 package sync
 
 import (
+	"errors"
 	"math/big"
 	"reflect"
 	"testing"
@@ -11,9 +12,32 @@ import (
 	"github.com/ChainSafe/gossamer/dot/network"
 	"github.com/ChainSafe/gossamer/dot/types"
 	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/common/variadic"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestService_CreateBlockResponse(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockBlockState := NewMockBlockState(ctrl)
+	mockBlockState.EXPECT().BestBlockNumber().Return(big.NewInt(1), nil).Times(8)
+	mockBlockState.EXPECT().GetHashByNumber(gomock.AssignableToTypeOf(&big.Int{})).DoAndReturn(func(*big.Int) (
+		common.Hash, error) {
+		return common.Hash{1, 2}, nil
+	}).Times(5)
+	mockBlockState.EXPECT().IsDescendantOf(gomock.AssignableToTypeOf(common.Hash{}),
+		gomock.AssignableToTypeOf(common.Hash{})).Return(true, nil).Times(4)
+	mockBlockState.EXPECT().GetHeader(gomock.AssignableToTypeOf(common.Hash{})).Return(&types.Header{
+		Number: big.NewInt(1),
+	}, nil).Times(4)
+	mockBlockState.EXPECT().SubChain(gomock.AssignableToTypeOf(common.Hash{}),
+		gomock.AssignableToTypeOf(common.Hash{})).Return([]common.Hash{{1, 2}}, nil).Times(4)
+	mockBlockState.EXPECT().GetHeaderByNumber(big.NewInt(1)).Return(&types.Header{
+		Number: big.NewInt(1),
+	}, nil)
+
 	type fields struct {
 		blockState     BlockState
 		chainSync      ChainSync
@@ -24,13 +48,128 @@ func TestService_CreateBlockResponse(t *testing.T) {
 		req *network.BlockRequestMessage
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *network.BlockResponseMessage
-		wantErr bool
+		name   string
+		fields fields
+		args   args
+		want   *network.BlockResponseMessage
+		err    error
 	}{
-		// TODO: Add test cases.
+		{
+			name: "invalid block request",
+			args: args{req: &network.BlockRequestMessage{}},
+			err:  ErrInvalidBlockRequest,
+		},
+		{
+			name: "ascending request, nil startHash, nil endHash",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(0),
+				Direction:     network.Ascending,
+			}},
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{{
+				Hash: common.Hash{1, 2},
+			}}},
+		},
+		{
+			name: "ascending request, start number higher",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(2),
+				Direction:     network.Ascending,
+			}},
+			err:  errRequestStartTooHigh,
+			want: nil,
+		},
+		{
+			name: "ascending request, endHash not nil",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(0),
+				EndBlockHash:  &common.Hash{1, 2, 3},
+				Direction:     network.Ascending,
+			}},
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{{
+				Hash: common.Hash{1, 2},
+			}}},
+		},
+		{
+			name: "descending request, nil startHash, nil endHash",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(0),
+				Direction:     network.Descending,
+			}},
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{}},
+		},
+		{
+			name: "descending request, start number higher",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(2),
+				Direction:     network.Descending,
+			}},
+			err: nil,
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{{
+				Hash: common.Hash{1, 2},
+			}}},
+		},
+		{
+			name: "descending request, endHash not nil",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(0),
+				EndBlockHash:  &common.Hash{1, 2, 3},
+				Direction:     network.Descending,
+			}},
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{{
+				Hash: common.Hash{1, 2},
+			}}},
+		},
+		{
+			name: "ascending request, startHash, nil endHash",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(common.Hash{}),
+				Direction:     network.Ascending,
+			}},
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{{
+				Hash: common.Hash{1, 2},
+			}}},
+		},
+		{
+			name: "descending request, startHash, nil endHash",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{req: &network.BlockRequestMessage{
+				StartingBlock: *variadic.MustNewUint64OrHash(common.Hash{}),
+				Direction:     network.Descending,
+			}},
+			want: &network.BlockResponseMessage{BlockData: []*types.BlockData{{
+				Hash: common.Hash{1, 2},
+			}}},
+		},
+		{
+			name: "invalid direction",
+			args: args{req: &network.BlockRequestMessage{
+				Direction: network.SyncDirection(3),
+			}},
+			err: errInvalidRequestDirection,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -41,9 +180,10 @@ func TestService_CreateBlockResponse(t *testing.T) {
 				network:        tt.fields.network,
 			}
 			got, err := s.CreateBlockResponse(tt.args.req)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("CreateBlockResponse() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			if tt.err != nil {
+				assert.EqualError(t, err, tt.err.Error())
+			} else {
+				assert.NoError(t, err)
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("CreateBlockResponse() got = %v, want %v", got, tt.want)
@@ -94,37 +234,153 @@ func TestService_checkOrGetDescendantHash(t *testing.T) {
 }
 
 func TestService_getBlockData(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockBlockState := NewMockBlockState(ctrl)
+	mockBlockState.EXPECT().GetHeader(common.Hash{}).Return(nil, errors.New("empty hash"))
+	mockBlockState.EXPECT().GetHeader(common.Hash{1}).Return(&types.Header{
+		Number: big.NewInt(2),
+	}, nil)
+	mockBlockState.EXPECT().GetBlockBody(common.Hash{}).Return(nil, errors.New("empty hash"))
+	mockBlockState.EXPECT().GetBlockBody(common.Hash{1}).Return(&types.Body{[]byte{1}}, nil)
+	mockBlockState.EXPECT().GetReceipt(common.Hash{1}).Return([]byte{1}, nil)
+	mockBlockState.EXPECT().GetMessageQueue(common.Hash{2}).Return([]byte{2}, nil)
+	mockBlockState.EXPECT().GetJustification(common.Hash{3}).Return([]byte{3}, nil)
+
 	type fields struct {
-		blockState     BlockState
-		chainSync      ChainSync
-		chainProcessor ChainProcessor
-		network        Network
+		blockState BlockState
 	}
 	type args struct {
 		hash          common.Hash
 		requestedData byte
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *types.BlockData
-		wantErr bool
+		name   string
+		fields fields
+		args   args
+		want   *types.BlockData
+		err    error
 	}{
-		// TODO: Add test cases.
+		{
+			name: "requestedData 0",
+			args: args{
+				hash:          common.Hash{},
+				requestedData: 0,
+			},
+			want: &types.BlockData{},
+		},
+		{
+			name: "requestedData RequestedDataHeader, error",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{0},
+				requestedData: network.RequestedDataHeader,
+			},
+			want: &types.BlockData{
+				Hash: common.Hash{},
+			},
+		},
+		{
+			name: "requestedData RequestedDataHeader",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{1},
+				requestedData: network.RequestedDataHeader,
+			},
+			want: &types.BlockData{
+				Hash: common.Hash{1},
+				Header: &types.Header{
+					Number: big.NewInt(2),
+				},
+			},
+		},
+		{
+			name: "requestedData RequestedDataBody, error",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{},
+				requestedData: network.RequestedDataBody,
+			},
+			want: &types.BlockData{
+				Hash: common.Hash{},
+			},
+		},
+		{
+			name: "requestedData RequestedDataBody",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{1},
+				requestedData: network.RequestedDataBody,
+			},
+			want: &types.BlockData{
+				Hash: common.Hash{1},
+				Body: &types.Body{[]byte{1}},
+			},
+		},
+		{
+			name: "requestedData RequestedDataReceipt",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{1},
+				requestedData: network.RequestedDataReceipt,
+			},
+			want: &types.BlockData{
+				Hash:    common.Hash{1},
+				Receipt: &[]byte{1},
+			},
+		},
+		{
+			name: "requestedData RequestedDataMessageQueue",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{2},
+				requestedData: network.RequestedDataMessageQueue,
+			},
+			want: &types.BlockData{
+				Hash:         common.Hash{2},
+				MessageQueue: &[]byte{2},
+			},
+		},
+		{
+			name: "requestedData RequestedDataJustification",
+			fields: fields{
+				blockState: mockBlockState,
+			},
+			args: args{
+				hash:          common.Hash{3},
+				requestedData: network.RequestedDataJustification,
+			},
+			want: &types.BlockData{
+				Hash:          common.Hash{3},
+				Justification: &[]byte{3},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &Service{
-				blockState:     tt.fields.blockState,
-				chainSync:      tt.fields.chainSync,
-				chainProcessor: tt.fields.chainProcessor,
-				network:        tt.fields.network,
+				blockState: tt.fields.blockState,
 			}
 			got, err := s.getBlockData(tt.args.hash, tt.args.requestedData)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("getBlockData() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			if tt.err != nil {
+				assert.EqualError(t, err, tt.err.Error())
+			} else {
+				assert.NoError(t, err)
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getBlockData() got = %v, want %v", got, tt.want)
