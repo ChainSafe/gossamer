b2dd20ceb1a73765673908e2eb9b5f8bc34bb2f7
Ed Mack <ed@edwardmack.com>
2022-02-11 17:59:56 +0000

    tests(dot): modify integration test files - add `integration` build tag to test files - improve existing integration test code

--- dot/config_integration_test.go
+++ dot/config_integration_test.go
@@ -1,26 +1,29 @@
 // Copyright 2021 ChainSafe Systems (ON)
 // SPDX-License-Identifier: LGPL-3.0-only
 
+//go:build integration
+// +build integration
+
 package dot
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-// TestExportConfig tests exporting a toml configuration file
 func TestExportConfig(t *testing.T) {
 	cfg, cfgFile := newTestConfigWithFile(t)
-	require.NotNil(t, cfg)
 
 	genFile := NewTestGenesisRawFile(t, cfg)
 
 	cfg.Init.Genesis = genFile
 
 	err := InitNode(cfg)
-	require.Nil(t, err)
+	require.NoError(t, err)
 
 	file := exportConfig(cfg, cfgFile.Name())
 	require.NotNil(t, file)
+	os.Remove(file.Name())
 }
