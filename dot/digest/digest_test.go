e193a0c99004bab7fcf05762b91b7f244624a129
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2022-03-28 14:10:08 -0400

    wip: adding grandpa consensus messages digest

--- dot/digest/digest_test.go
+++ dot/digest/digest_test.go
@@ -4,6 +4,7 @@
 package digest
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -11,19 +12,21 @@ import (
 	"github.com/ChainSafe/gossamer/dot/types"
 	"github.com/ChainSafe/gossamer/internal/log"
 	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/crypto"
 	"github.com/ChainSafe/gossamer/lib/crypto/ed25519"
 	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
 	"github.com/ChainSafe/gossamer/lib/genesis"
 	"github.com/ChainSafe/gossamer/lib/keystore"
 	"github.com/ChainSafe/gossamer/pkg/scale"
 	"github.com/golang/mock/gomock"
+	"github.com/gtank/merlin"
 
 	"github.com/stretchr/testify/require"
 )
 
 //go:generate mockgen -destination=mock_telemetry_test.go -package $GOPACKAGE github.com/ChainSafe/gossamer/dot/telemetry Client
 
-func newTestHandler(t *testing.T) *Handler {
+func newTestHandler(t *testing.T) (*Handler, *state.Service) {
 	testDatadirPath := t.TempDir()
 
 	ctrl := gomock.NewController(t)
@@ -49,11 +52,11 @@ func newTestHandler(t *testing.T) *Handler {
 
 	dh, err := NewHandler(log.Critical, stateSrvc.Block, stateSrvc.Epoch, stateSrvc.Grandpa)
 	require.NoError(t, err)
-	return dh
+	return dh, stateSrvc
 }
 
 func TestHandler_GrandpaScheduledChange(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -112,7 +115,7 @@ func TestHandler_GrandpaScheduledChange(t *testing.T) {
 }
 
 func TestHandler_GrandpaForcedChange(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -161,7 +164,7 @@ func TestHandler_GrandpaForcedChange(t *testing.T) {
 }
 
 func TestHandler_GrandpaPauseAndResume(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -224,7 +227,7 @@ func TestHandler_GrandpaPauseAndResume(t *testing.T) {
 }
 
 func TestNextGrandpaAuthorityChange_OneChange(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -264,7 +267,7 @@ func TestNextGrandpaAuthorityChange_OneChange(t *testing.T) {
 }
 
 func TestNextGrandpaAuthorityChange_MultipleChanges(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -337,7 +340,7 @@ func TestNextGrandpaAuthorityChange_MultipleChanges(t *testing.T) {
 }
 
 func TestHandler_HandleBABEOnDisabled(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	header := &types.Header{
 		Number: 1,
 	}
@@ -384,7 +387,7 @@ func createHeaderWithPreDigest(t *testing.T, slotNumber uint64) *types.Header {
 func TestHandler_HandleNextEpochData(t *testing.T) {
 	expData := common.MustHexToBytes("0x0108d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d01000000000000008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4801000000000000004d58630000000000000000000000000000000000000000000000000000000000") //nolint:lll
 
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -437,7 +440,7 @@ func TestHandler_HandleNextEpochData(t *testing.T) {
 }
 
 func TestHandler_HandleNextConfigData(t *testing.T) {
-	handler := newTestHandler(t)
+	handler, _ := newTestHandler(t)
 	handler.Start()
 	defer handler.Stop()
 
@@ -470,4 +473,162 @@ func TestHandler_HandleNextConfigData(t *testing.T) {
 	stored, err := handler.epochState.(*state.EpochState).GetConfigData(1)
 	require.NoError(t, err)
 	require.Equal(t, act.ToConfigData(), stored)
+}
+
+func TestGrandpaScheduledChanges(t *testing.T) {
+	keyring, err := keystore.NewSr25519Keyring()
+	require.NoError(t, err)
+
+	keyPairs := []*sr25519.Keypair{
+		keyring.KeyAlice, keyring.KeyBob, keyring.KeyCharlie,
+		keyring.KeyDave, keyring.KeyEve, keyring.KeyFerdie,
+		keyring.KeyGeorge, keyring.KeyHeather, keyring.KeyIan,
+	}
+
+	authorities := make([]types.AuthorityRaw, len(keyPairs))
+	for i, keyPair := range keyPairs {
+		authorities[i] = types.AuthorityRaw{
+			Key: keyPair.Public().(*sr25519.PublicKey).AsBytes(),
+		}
+	}
+
+	digestHandler, stateService := newTestHandler(t)
+
+	forksGRANPDAScheduledChanges := []types.GrandpaScheduledChange{
+		{
+			Auths: []types.GrandpaAuthoritiesRaw{
+				{
+					Key: authorities[0].Key,
+					ID:  0,
+				},
+				{
+					Key: authorities[1].Key,
+					ID:  1,
+				},
+			},
+			Delay: 1,
+		},
+		{
+			Auths: []types.GrandpaAuthoritiesRaw{
+				{
+					Key: authorities[3].Key,
+					ID:  3,
+				},
+				{
+					Key: authorities[4].Key,
+					ID:  4,
+				},
+			},
+			Delay: 1,
+		},
+		{
+			Auths: []types.GrandpaAuthoritiesRaw{
+				{
+					Key: authorities[6].Key,
+					ID:  6,
+				},
+				{
+					Key: authorities[7].Key,
+					ID:  7,
+				},
+			},
+			Delay: 1,
+		},
+	}
+
+	genesisHeader, err := stateService.Block.BestBlockHeader()
+	require.NoError(t, err)
+
+	forkA := issueBlocksWithGRANDPAScheduledChanges(t, keyring.KeyAlice, digestHandler, stateService,
+		genesisHeader, forksGRANPDAScheduledChanges[0], 2, 3)
+	forkB := issueBlocksWithGRANDPAScheduledChanges(t, keyring.KeyBob, digestHandler, stateService,
+		forkA[1], forksGRANPDAScheduledChanges[1], 1, 3)
+	forkC := issueBlocksWithGRANDPAScheduledChanges(t, keyring.KeyCharlie, digestHandler, stateService,
+		forkA[1], forksGRANPDAScheduledChanges[2], 1, 3)
+
+	for _, fork := range forkA {
+		fmt.Printf("%d - %s\n", fork.Number, fork.Hash())
+	}
+
+	for _, fork := range forkB {
+		fmt.Printf("%d - %s\n", fork.Number, fork.Hash())
+	}
+
+	for _, fork := range forkC {
+		fmt.Printf("%d - %s\n", fork.Number, fork.Hash())
+	}
+
+	// current authorities at scheduled changes
+	digestHandler.grandpaScheduledChange.auths
+	//stateService.Block.AddBlock()
+
+	//handler.handleConsensusDigest()
+}
+
+func issueBlocksWithGRANDPAScheduledChanges(t *testing.T, kp *sr25519.Keypair, dh *Handler,
+	stateSvc *state.Service, parentHeader *types.Header,
+	sc types.GrandpaScheduledChange, atBlock int, size int) (headers []*types.Header) {
+	t.Helper()
+
+	transcript := merlin.NewTranscript("BABE") //string(types.BabeEngineID[:])
+	crypto.AppendUint64(transcript, []byte("slot number"), 1)
+	crypto.AppendUint64(transcript, []byte("current epoch"), 1)
+	transcript.AppendMessage([]byte("chain randomness"), []byte{})
+
+	output, proof, err := kp.VrfSign(transcript)
+	require.NoError(t, err)
+
+	babePrimaryPreDigest := types.BabePrimaryPreDigest{
+		SlotNumber: 1,
+		VRFOutput:  output,
+		VRFProof:   proof,
+	}
+
+	preRuntimeDigest, err := babePrimaryPreDigest.ToPreRuntimeDigest()
+	require.NoError(t, err)
+
+	digest := types.NewDigest()
+
+	// include the consensus in the block being produced
+	if parentHeader.Number+1 == uint(atBlock) {
+		grandpaConsensusDigest := types.NewGrandpaConsensusDigest()
+		err = grandpaConsensusDigest.Set(sc)
+		require.NoError(t, err)
+
+		grandpaDigest, err := scale.Marshal(grandpaConsensusDigest)
+		require.NoError(t, err)
+
+		consensusDigest := types.ConsensusDigest{
+			ConsensusEngineID: types.GrandpaEngineID,
+			Data:              grandpaDigest,
+		}
+		require.NoError(t, digest.Add(*preRuntimeDigest, consensusDigest))
+	} else {
+		require.NoError(t, digest.Add(*preRuntimeDigest))
+	}
+
+	header := &types.Header{
+		ParentHash: parentHeader.Hash(),
+		Number:     parentHeader.Number + 1,
+		Digest:     digest,
+	}
+
+	block := &types.Block{
+		Header: *header,
+		Body:   *types.NewBody([]types.Extrinsic{}),
+	}
+
+	err = stateSvc.Block.AddBlock(block)
+	require.NoError(t, err)
+
+	dh.HandleDigests(header)
+
+	if size <= 0 {
+		headers = append(headers, header)
+		return headers
+	}
+
+	headers = append(headers, header)
+	headers = append(headers, issueBlocksWithGRANDPAScheduledChanges(t, kp, dh, stateSvc, header, sc, atBlock, size-1)...)
+	return headers
 }
