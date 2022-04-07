d69c694e1bdfe76047272cd3b399cf267108a111
Eclésio Junior <eclesiomelo.1@gmail.com>
2022-04-07 14:34:17 -0400

    chore: improve test comment
    Co-authored-by: Quentin McGaw <quentin.mcgaw@gmail.com>

--- dot/state/epoch_test.go
+++ dot/state/epoch_test.go
@@ -597,7 +597,7 @@ func TestStoreAndFinalizeBabeNextConfigData(t *testing.T) {
 			err := epochState.blockState.db.Put(headerKey(tt.finalizeHash), []byte{})
 			require.NoError(t, err)
 
-			// before finalize next config data this epoch should not be defined
+			// before finalize next config data for this epoch should not be defined
 			definedEpochData, definedConfigData, err := epochState.AlreadyDefined(tt.finalizeEpoch)
 			require.NoError(t, err)
 			require.False(t, definedEpochData)
