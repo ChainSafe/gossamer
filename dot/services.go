9c8ae7e7aef00ac6e649695265eb60e5c1ed5463
Tim Wu <timwu20@gmail.com>
2022-05-17 11:23:13 -0400

    fix dot tests, remove life runtime

--- dot/services.go
+++ dot/services.go
@@ -30,7 +30,6 @@ import (
 	"github.com/ChainSafe/gossamer/lib/grandpa"
 	"github.com/ChainSafe/gossamer/lib/keystore"
 	"github.com/ChainSafe/gossamer/lib/runtime"
-	"github.com/ChainSafe/gossamer/lib/runtime/life"
 	"github.com/ChainSafe/gossamer/lib/runtime/wasmer"
 	"github.com/ChainSafe/gossamer/lib/utils"
 )
@@ -148,23 +147,6 @@ func createRuntime(cfg *Config, ns runtime.NodeStorage, st *state.Service,
 
 		// create runtime executor
 		rt, err = wasmer.NewInstance(code, rtCfg)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create runtime executor: %s", err)
-		}
-	case life.Name:
-		rtCfg := &life.Config{
-			Resolver: new(life.Resolver),
-		}
-		rtCfg.Storage = ts
-		rtCfg.Keystore = ks
-		rtCfg.LogLvl = cfg.Log.RuntimeLvl
-		rtCfg.NodeStorage = ns
-		rtCfg.Network = net
-		rtCfg.Role = cfg.Core.Roles
-		rtCfg.CodeHash = codeHash
-
-		// create runtime executor
-		rt, err = life.NewInstance(code, rtCfg)
 		if err != nil {
 			return nil, fmt.Errorf("failed to create runtime executor: %s", err)
 		}
