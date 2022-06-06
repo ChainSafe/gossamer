781d4624fe8050ce0d57895e315221cf545e63e0
Kishan Sagathiya <kishansagathiya@gmail.com>
2022-06-06 14:33:41 +0530

    Update lib/babe/parachain_inherents.go
    Co-authored-by: Timothy Wu <timwu20@gmail.com>

--- lib/babe/parachain_inherents.go
+++ lib/babe/parachain_inherents.go
@@ -104,7 +104,7 @@ func (ex ExplicitInvalidDisputeStatementKind) Index() uint {
 }
 
 // GetDisputeStatement returns a GetDisputeStatement varying data type.
-func GetDisputeStatement() DisputeStatement {
+func NewDisputeStatement() DisputeStatement {
 	invalidDisputeStatementKind, err := scale.NewVaryingDataType(ExplicitInvalidDisputeStatementKind{})
 	if err != nil {
 		panic(err)
