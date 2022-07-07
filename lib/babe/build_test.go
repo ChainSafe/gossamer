9b1039cce5ac481ed0efacef53e1e32dd284b103
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-07-07 21:30:44 +0000

    wip

--- lib/babe/build_test.go
+++ lib/babe/build_test.go
@@ -17,47 +17,151 @@ import (
 func TestBlockBuilder_buildBlockExtrinsics(t *testing.T) {
 	type fields struct {
 		keypair                 *sr25519.Keypair
-		transactionStateBuilder func(ctrl *gomock.Controller, tickerCancel chan<- struct{}) TransactionState
+		transactionStateBuilder func(ctrl *gomock.Controller) TransactionState
 		blockState              BlockState
 		currentAuthorityIndex   uint32
 		preRuntimeDigest        *types.PreRuntimeDigest
 	}
 	type args struct {
-		slot Slot
-		rt   runtime.Instance
+		slot                   Slot
+		runtimeInstanceBuilder func(ctrl *gomock.Controller) (instance runtime.Instance, forceReturn <-chan struct{})
 	}
 	tests := map[string]struct {
 		fields fields
 		args   args
 		want   []*transaction.ValidTransaction
 	}{
-		"initial empty transaction queue": {
+		"return no transaction whilst waiting for queue": {
 			args: args{
 				slot: Slot{
-					start:    time.Now(),
-					duration: 30 * time.Second,
+					// we don't set the start time or duration
+					// so the timeout is effectively 0 such that
+					// the timer triggers on the second iteration
+					// of the for loop
+				},
+				runtimeInstanceBuilder: func(ctrl *gomock.Controller) (instance runtime.Instance, forceReturn <-chan struct{}) {
+					return nil, nil
 				},
 			},
 			fields: fields{
-				transactionStateBuilder: func(ctrl *gomock.Controller, tickerCancel chan<- struct{}) TransactionState {
-					mockTransactionState := NewMockTransactionState(ctrl)
+				transactionStateBuilder: func(ctrl *gomock.Controller) TransactionState {
+					transactionState := NewMockTransactionState(ctrl)
 
-					call := mockTransactionState.EXPECT().Pop().DoAndReturn(func() (*transaction.ValidTransaction,
-						error) {
-						tickerCancel <- struct{}{}
-						t.Log("GOT TO HERE")
-						return nil, nil // nolint: nilnil
-					})
+					transactionState.EXPECT().Pop().Return(nil)
+					transactionState.EXPECT().NextPushWatcher().Return(nil)
 
-					watcherOne := make(chan struct{})
-					close(watcherOne)
-					call2 := mockTransactionState.EXPECT().NextPushWatcher().
-						Return(watcherOne).After(call)
-					call3 := mockTransactionState.EXPECT().Pop().Return(nil).After(call2)
-					mockTransactionState.EXPECT().NextPushWatcher().
-						Return(watcherOne).After(call3)
-					return mockTransactionState
+					return transactionState
 				},
+			},
+		},
+		"return one transaction": {
+			args: args{
+				slot: Slot{
+					// we don't set the start time or duration
+					// so the timeout is effectively 0 such that
+					// the timer triggers on the second iteration
+					// of the for loop
+				},
+				runtimeInstanceBuilder: func(ctrl *gomock.Controller) (instance runtime.Instance, forceReturn <-chan struct{}) {
+					mockInstance := NewMockInstance(ctrl)
+					mockInstance.EXPECT().ApplyExtrinsic(types.Extrinsic{1}).
+						Return([]byte{0, 0}, nil)
+					return mockInstance, nil
+				},
+			},
+			fields: fields{
+				transactionStateBuilder: func(ctrl *gomock.Controller) TransactionState {
+					transactionState := NewMockTransactionState(ctrl)
+					transaction := &transaction.ValidTransaction{
+						Extrinsic: types.Extrinsic{1},
+					}
+					transactionState.EXPECT().Pop().Return(transaction)
+					return transactionState
+				},
+			},
+			want: []*transaction.ValidTransaction{
+				{Extrinsic: types.Extrinsic{1}},
+			},
+		},
+		"return one transaction after waiting for queue": {
+			args: args{
+				slot: Slot{
+					// we don't set the start time or duration
+					// so the timeout is effectively 0 such that
+					// the timer triggers on the second iteration
+					// of the for loop
+				},
+				runtimeInstanceBuilder: func(ctrl *gomock.Controller) (instance runtime.Instance, forceReturn <-chan struct{}) {
+					mockInstance := NewMockInstance(ctrl)
+					mockInstance.EXPECT().ApplyExtrinsic(types.Extrinsic{1}).
+						Return([]byte{0, 0}, nil)
+					return mockInstance, nil
+				},
+			},
+			fields: fields{
+				transactionStateBuilder: func(ctrl *gomock.Controller) TransactionState {
+					transactionState := NewMockTransactionState(ctrl)
+
+					firstPop := transactionState.EXPECT().Pop().Return(nil)
+
+					pushWatcher := make(chan struct{})
+					close(pushWatcher)
+					transactionState.EXPECT().NextPushWatcher().Return(pushWatcher)
+
+					transaction := &transaction.ValidTransaction{
+						Extrinsic: types.Extrinsic{1},
+					}
+					transactionState.EXPECT().Pop().Return(transaction).After(firstPop)
+
+					return transactionState
+				},
+			},
+			want: []*transaction.ValidTransaction{
+				{Extrinsic: types.Extrinsic{1}},
+			},
+		},
+		"return two transactions": {
+			args: args{
+				slot: Slot{
+					start:    time.Now(),
+					duration: time.Hour,
+				},
+				runtimeInstanceBuilder: func(ctrl *gomock.Controller) (instance runtime.Instance, forceReturn <-chan struct{}) {
+					forceReturnCh := make(chan struct{}, 1)
+					mockInstance := NewMockInstance(ctrl)
+
+					firstCall := mockInstance.EXPECT().ApplyExtrinsic(types.Extrinsic{1}).
+						Return([]byte{0, 0}, nil)
+
+					mockInstance.EXPECT().ApplyExtrinsic(types.Extrinsic{2}).
+						DoAndReturn(func(data types.Extrinsic) ([]byte, error) {
+							forceReturnCh <- struct{}{} // triggers the timer on the select case
+							return []byte{0, 0}, nil
+						}).After(firstCall)
+
+					return mockInstance, forceReturnCh
+				},
+			},
+			fields: fields{
+				transactionStateBuilder: func(ctrl *gomock.Controller) TransactionState {
+					transactionState := NewMockTransactionState(ctrl)
+
+					transaction1 := &transaction.ValidTransaction{
+						Extrinsic: types.Extrinsic{1},
+					}
+					call := transactionState.EXPECT().Pop().Return(transaction1)
+
+					transaction2 := &transaction.ValidTransaction{
+						Extrinsic: types.Extrinsic{2},
+					}
+					transactionState.EXPECT().Pop().Return(transaction2).After(call)
+
+					return transactionState
+				},
+			},
+			want: []*transaction.ValidTransaction{
+				{Extrinsic: types.Extrinsic{1}},
+				{Extrinsic: types.Extrinsic{2}},
 			},
 		},
 	}
@@ -65,16 +169,18 @@ func TestBlockBuilder_buildBlockExtrinsics(t *testing.T) {
 		tt := tt
 		t.Run(name, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
-			tickerCancel := make(chan struct{}, 1)
+
+			runtimeInstance, forceReturnCh := tt.args.runtimeInstanceBuilder(ctrl)
+
 			b := &BlockBuilder{
 				keypair:               tt.fields.keypair,
-				transactionState:      tt.fields.transactionStateBuilder(ctrl, tickerCancel),
+				transactionState:      tt.fields.transactionStateBuilder(ctrl),
 				blockState:            tt.fields.blockState,
 				currentAuthorityIndex: tt.fields.currentAuthorityIndex,
 				preRuntimeDigest:      tt.fields.preRuntimeDigest,
-				testSkipTimer:         tickerCancel,
+				testForceReturn:       forceReturnCh,
 			}
-			got := b.buildBlockExtrinsics(tt.args.slot, tt.args.rt)
+			got := b.buildBlockExtrinsics(tt.args.slot, runtimeInstance)
 			assert.Equal(t, tt.want, got)
 		})
 	}
