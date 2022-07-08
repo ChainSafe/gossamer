95a75a0b5f45641ea6d333481a09f32f887a5c8d
edwardmack <ed@edwardmack.com>
2022-07-08 15:42:00 -0400

    add copyright notice

--- lib/babe/mocks_generate_test.go
+++ lib/babe/mocks_generate_test.go
@@ -1,3 +1,5 @@
+// Copyright 2022 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
 package babe
 
 //go:generate mockgen -destination=mocks_test.go -package=$GOPACKAGE github.com/ChainSafe/gossamer/lib/runtime Instance
