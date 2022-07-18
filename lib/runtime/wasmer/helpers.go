1049be798e6051e99cb3ddd80119f55f70085c86
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-07-18 13:53:19 +0000

    Review variable names - Use `ptr` instead of `out` for 32 bit pointers - Use `pointerSize` instead of `span` for 64 bit pointers size - Name return values - Other minor renamings such as `res` to `result`, `enc` to `encodedResult`

--- lib/runtime/wasmer/helpers.go
+++ lib/runtime/wasmer/helpers.go
@@ -18,60 +18,63 @@ import (
 )
 
 // Convert 64bit wasm span descriptor to Go memory slice
-func asMemorySlice(context wasmer.InstanceContext, span C.int64_t) []byte {
+func asMemorySlice(context wasmer.InstanceContext, pointerSize C.int64_t) (data []byte) {
 	memory := context.Memory().Data()
-	ptr, size := runtime.Int64ToPointerAndSize(int64(span))
+	ptr, size := runtime.Int64ToPointerAndSize(int64(pointerSize))
 	return memory[ptr : ptr+size]
 }
 
 // Copy a byte slice to wasm memory and return the resulting 64bit span descriptor
-func toWasmMemory(context wasmer.InstanceContext, data []byte) (int64, error) {
+func toWasmMemory(context wasmer.InstanceContext, data []byte) (
+	pointerSize int64, err error) {
 	allocator := context.Data().(*runtime.Context).Allocator
 	size := uint32(len(data))
 
-	out, err := allocator.Allocate(size)
+	ptr, err := allocator.Allocate(size)
 	if err != nil {
 		return 0, err
 	}
 
 	memory := context.Memory().Data()
 
-	if uint32(len(memory)) < out+size {
-		panic(fmt.Sprintf("length of memory is less than expected, want %d have %d", out+size, len(memory)))
+	if uint32(len(memory)) < ptr+size {
+		panic(fmt.Sprintf("length of memory is less than expected, want %d have %d", ptr+size, len(memory)))
 	}
 
-	copy(memory[out:out+size], data)
-	return runtime.PointerAndSizeToInt64(int32(out), int32(size)), nil
+	copy(memory[ptr:ptr+size], data)
+	pointerSize = runtime.PointerAndSizeToInt64(int32(ptr), int32(size))
+	return pointerSize, nil
 }
 
 // Copy a byte slice of a fixed size to wasm memory and return resulting pointer
-func toWasmMemorySized(context wasmer.InstanceContext, data []byte, size uint32) (uint32, error) {
+func toWasmMemorySized(context wasmer.InstanceContext, data []byte, size uint32) (
+	pointer uint32, err error) {
 	if int(size) != len(data) {
 		return 0, errors.New("internal byte array size missmatch")
 	}
 
 	allocator := context.Data().(*runtime.Context).Allocator
 
-	out, err := allocator.Allocate(size)
+	ptr, err := allocator.Allocate(size)
 	if err != nil {
 		return 0, err
 	}
 
 	memory := context.Memory().Data()
-	copy(memory[out:out+size], data)
+	copy(memory[ptr:ptr+size], data)
 
-	return out, nil
+	return ptr, nil
 }
 
 // Wraps slice in optional.Bytes and copies result to wasm memory. Returns resulting 64bit span descriptor
-func toWasmMemoryOptional(context wasmer.InstanceContext, data []byte) (int64, error) {
-	var opt *[]byte
+func toWasmMemoryOptional(context wasmer.InstanceContext, data []byte) (
+	pointerSize int64, err error) {
+	var optionalBytes *[]byte
 	if data != nil {
-		temp := data
-		opt = &temp
+		optionalBytes = &data
 	}
 
-	enc, err := scale.Marshal(opt)
+	enc, err := scale.Marshal(optionalBytes)
 	if err != nil {
 		return 0, err
 	}
@@ -80,82 +83,79 @@ func toWasmMemoryOptional(context wasmer.InstanceContext, data []byte) (int64, e
 }
 
 // Wraps slice in Result type and copies result to wasm memory. Returns resulting 64bit span descriptor
-func toWasmMemoryResult(context wasmer.InstanceContext, data []byte) (int64, error) {
-	var res *types.Result
+func toWasmMemoryResult(context wasmer.InstanceContext, data []byte) (
+	pointerSize int64, err error) {
+	var result *types.Result
 	if len(data) == 0 {
-		res = types.NewResult(byte(1), nil)
+		result = types.NewResult(byte(1), nil)
 	} else {
-		res = types.NewResult(byte(0), data)
+		result = types.NewResult(byte(0), data)
 	}
 
-	enc, err := res.Encode()
+	encodedResult, err := result.Encode()
 	if err != nil {
 		return 0, err
 	}
 
-	return toWasmMemory(context, enc)
+	return toWasmMemory(context, encodedResult)
 }
 
 // Wraps slice in optional and copies result to wasm memory. Returns resulting 64bit span descriptor
-func toWasmMemoryOptionalUint32(context wasmer.InstanceContext, data *uint32) (int64, error) {
-	var opt *uint32
-	if data != nil {
-		temp := *data
-		opt = &temp
-	}
-
-	enc, err := scale.Marshal(opt)
+func toWasmMemoryOptionalUint32(context wasmer.InstanceContext, data *uint32) (
+	pointerSize int64, err error) {
+	enc, err := scale.Marshal(data)
 	if err != nil {
-		return int64(0), err
+		return 0, err
 	}
 	return toWasmMemory(context, enc)
 }
 
 // toKillStorageResult returns enum encoded value
-func toKillStorageResultEnum(allRemoved bool, numRemoved uint32) ([]byte, error) {
-	var b, sbytes []byte
-	sbytes, err := scale.Marshal(numRemoved)
+func toKillStorageResultEnum(allRemoved bool, numRemoved uint32) (
+	encodedEnumValue []byte, err error) {
+	encodedNumRemoved, err := scale.Marshal(numRemoved)
 	if err != nil {
 		return nil, err
 	}
 
 	if allRemoved {
 		// No key remains in the child trie.
-		b = append(b, byte(0))
+		encodedEnumValue = append(encodedEnumValue, byte(0))
 	} else {
 		// At least one key still resides in the child trie due to the supplied limit.
-		b = append(b, byte(1))
+		encodedEnumValue = append(encodedEnumValue, byte(1))
 	}
 
-	b = append(b, sbytes...)
+	encodedEnumValue = append(encodedEnumValue, encodedNumRemoved...)
 
-	return b, err
+	return encodedEnumValue, nil
 }
 
 // Wraps slice in optional.FixedSizeBytes and copies result to wasm memory. Returns resulting 64bit span descriptor
-func toWasmMemoryFixedSizeOptional(context wasmer.InstanceContext, data []byte) (int64, error) {
-	var opt [64]byte
-	copy(opt[:], data)
-	enc, err := scale.Marshal(&opt)
+func toWasmMemoryFixedSizeOptional(context wasmer.InstanceContext, data []byte) (
+	pointerSize int64, err error) {
+	var optionalFixedSize [64]byte
+	copy(optionalFixedSize[:], data)
+	encodedOptionalFixedSize, err := scale.Marshal(&optionalFixedSize)
 	if err != nil {
 		return 0, err
 	}
-	return toWasmMemory(context, enc)
+	return toWasmMemory(context, encodedOptionalFixedSize)
 }
 
 func storageAppend(storage runtime.Storage, key, valueToAppend []byte) error {
 	nextLength := big.NewInt(1)
-	var valueRes []byte
+	var valueResult []byte
 
 	// this function assumes the item in storage is a SCALE encoded array of items
 	// the valueToAppend is a new item, so it appends the item and increases the length prefix by 1
-	valueCurr := storage.Get(key)
+	currentValue := storage.Get(key)
 
-	if len(valueCurr) == 0 {
-		valueRes = valueToAppend
+	if len(currentValue) == 0 {
+		valueResult = valueToAppend
 	} else {
-		var currLength *big.Int
-		err := scale.Unmarshal(valueCurr, &currLength)
+		var currentLength *big.Int
+		err := scale.Unmarshal(currentValue, &currentLength)
 		if err != nil {
 			logger.Tracef(
 				"item in storage is not SCALE encoded, overwriting at key 0x%x", key)
@@ -163,27 +163,27 @@ func storageAppend(storage runtime.Storage, key, valueToAppend []byte) error {
 			return nil //nolint:nilerr
 		}
 
-		lengthBytes, err := scale.Marshal(currLength)
+		lengthBytes, err := scale.Marshal(currentLength)
 		if err != nil {
 			return err
 		}
 		// append new item, pop off number of bytes required for length encoding,
 		// since we're not using old scale.Decoder
-		valueRes = append(valueCurr[len(lengthBytes):], valueToAppend...)
+		valueResult = append(currentValue[len(lengthBytes):], valueToAppend...)
 
 		// increase length by 1
-		nextLength = big.NewInt(0).Add(currLength, big.NewInt(1))
+		nextLength = big.NewInt(0).Add(currentLength, big.NewInt(1))
 	}
 
-	lengthEnc, err := scale.Marshal(nextLength)
+	encodedLength, err := scale.Marshal(nextLength)
 	if err != nil {
 		logger.Tracef("failed to encode new length: %s", err)
 		return err
 	}
 
 	// append new length prefix to start of items array
-	lengthEnc = append(lengthEnc, valueRes...)
-	logger.Debugf("resulting value: 0x%x", lengthEnc)
-	storage.Set(key, lengthEnc)
+	encodedValue := append(encodedLength, valueResult...)
+	logger.Debugf("resulting value: 0x%x", encodedValue)
+	storage.Set(key, encodedValue)
 	return nil
 }
