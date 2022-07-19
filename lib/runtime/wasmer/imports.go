756d00ef35ba433e62fc6c35f75bff1202c4bdfe
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-07-19 19:47:39 +0000

    Add more comments to version.Encode usage

--- lib/runtime/wasmer/imports.go
+++ lib/runtime/wasmer/imports.go
@@ -953,7 +953,9 @@ func ext_misc_runtime_version_version_1(context unsafe.Pointer, dataSpan C.int64
 
 	// Note: we must call the `Encode` method and NOT
 	// scale.Marshal or this one would encode the Version
-	// interface but not the actual implementation.
+	// interface pointer instead of the actual struct implementation.
+	// Encode also respects the legacy boolean field of the version
+	// and encodes the version differently if it is set to true.
 	encodedData, err := version.Encode()
 	if err != nil {
 		logger.Errorf("failed to encode result: %s", err)
