8506565df12f7c9f96752c12062f8af467d2b136
Timothy Wu <timwu20@gmail.com>
2022-07-25 15:59:06 -0400

    Update lib/genesis/test_utils.go
    Co-authored-by: Quentin McGaw <quentin.mcgaw@gmail.com>

--- lib/genesis/test_utils.go
+++ lib/genesis/test_utils.go
@@ -78,7 +78,6 @@ func CreateTestGenesisJSONFile(t *testing.T, fields Fields) (filename string) {
 // NewTestGenesisWithTrieAndHeader generates genesis, genesis trie and genesis header
 func NewTestGenesisWithTrieAndHeader(t *testing.T) (*Genesis, *trie.Trie, *types.Header) {
 	genesisPath := utils.GetGssmrV3SubstrateGenesisRawPathTest(t)
-	t.Logf("genesisPath: %s", genesisPath)
 	gen, err := NewGenesisFromJSONRaw(genesisPath)
 	require.NoError(t, err)
 
