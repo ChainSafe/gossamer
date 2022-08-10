ddf17e3b6e7ac0eafaf31eca69e353946ae544b0
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-08-10 22:40:28 +0000

    Remove `orStrings` and just make it simple!

--- lib/trie/version.go
+++ lib/trie/version.go
@@ -38,29 +38,7 @@ func ParseVersion(s string) (version Version, err error) {
 	case strings.EqualFold(s, V0.String()):
 		return V0, nil
 	default:
-		choices := orStrings([]string{V0.String()})
 		return version, fmt.Errorf("%w: %q must be %s",
-			ErrParseVersion, s, choices)
+			ErrParseVersion, s, V0)
 	}
-}
-
-func orStrings(strings []string) (result string) {
-	return joinStrings(strings, "or")
-}
-
-func joinStrings(strings []string, lastJoin string) (result string) {
-	if len(strings) == 0 {
-		return ""
-	}
-
-	result = strings[0]
-	for i := 1; i < len(strings); i++ {
-		if i < len(strings)-1 {
-			result += ", " + strings[i]
-		} else {
-			result += " " + lastJoin + " " + strings[i]
-		}
-	}
-
-	return result
 }
