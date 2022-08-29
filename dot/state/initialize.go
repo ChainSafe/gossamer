1f4fc20e07807b0b43b567e35fc43fd24dd23c5b
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-08-29 07:01:02 -0400

    chore(dot/state): refactor `Tries` initialization (#2782)
    - `NewTries` does not return an error (always nil)
    - `NewTries` does not take an initial as argument (which won't get versioned)
    - Add `(*Tries) SetEmptyTrie()` method (which won't get versioned)
    - Add `(*Tries) SetTrie(*trie.Trie)` method (which **will** get versioned)

--- dot/state/initialize.go
+++ dot/state/initialize.go
@@ -63,10 +63,8 @@ func (s *Service) Initialise(gen *genesis.Genesis, header *types.Header, t *trie
 		return fmt.Errorf("failed to write genesis values to database: %s", err)
 	}
 
-	tries, err := NewTries(t)
-	if err != nil {
-		return fmt.Errorf("cannot setup tries: %w", err)
-	}
+	tries := NewTries()
+	tries.SetTrie(t)
 
 	// create block state from genesis block
 	blockState, err := NewBlockStateFromGenesis(db, tries, header, s.Telemetry)
