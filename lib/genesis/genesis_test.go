0e1f5e07333f151fa5691d985745a541f39ff4b8
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-09-02 13:59:48 +0000

    Fix and improve Test_interfaceToTelemetryEndpoint

--- lib/genesis/genesis_test.go
+++ lib/genesis/genesis_test.go
@@ -6,47 +6,59 @@ package genesis
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestInterfaceToTelemetryEndpoint(t *testing.T) {
-	testcases := []struct {
-		description string
-		values      []interface{}
-		expected    []*TelemetryEndpoint
+func Test_interfaceToTelemetryEndpoint(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		endpoints []interface{}
+		expected  []*TelemetryEndpoint
 	}{
-		{
-			"Test with wrong interface type",
-			[]interface{}{"string"},
-			nil,
+		"sub element not a slice": {
+			endpoints: []interface{}{
+				struct{}{},
+			},
 		},
-		{
-			"Test with interface field len != 2",
-			[]interface{}{"wss://telemetry.polkadot.io/submit/"},
-			nil,
+		"wrong interface type": {
+			endpoints: []interface{}{
+				[]interface{}{"string"},
+			},
 		},
-		{
-			"Test with interface field 0 wrong type",
-			[]interface{}{float32(0), "wss://telemetry.polkadot.io/submit/"},
-			nil,
+		"wrong interface field length": {
+			endpoints: []interface{}{
+				[]interface{}{"wss://telemetry.polkadot.io/submit/"},
+			},
 		},
-		{
-			"Test with interface field 1 wrong type",
-			[]interface{}{"wss://telemetry.polkadot.io/submit/", "1"},
-			nil,
+		"wrong interface field position": {
+			endpoints: []interface{}{
+				[]interface{}{float64(0), "wss://telemetry.polkadot.io/submit/"},
+			},
 		},
-		{
-			"Test with correctly formed values",
-			[]interface{}{"wss://telemetry.polkadot.io/submit/", float64(1)},
-			[]*TelemetryEndpoint{{
+		"interface field 1 wrong type": {
+			endpoints: []interface{}{
+				[]interface{}{"wss://telemetry.polkadot.io/submit/", "1"},
+			},
+		},
+		"success": {
+			endpoints: []interface{}{
+				[]interface{}{"wss://telemetry.polkadot.io/submit/", float64(1)},
+			},
+			expected: []*TelemetryEndpoint{{
 				Endpoint:  "wss://telemetry.polkadot.io/submit/",
 				Verbosity: 1,
 			}},
 		},
 	}
 
-	for _, test := range testcases {
-		res := interfaceToTelemetryEndpoint(test.values)
-		require.Equal(t, test.expected, res)
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			telemetryEndpoints := interfaceToTelemetryEndpoint(testCase.endpoints)
+			assert.Equal(t, testCase.expected, telemetryEndpoints)
+		})
 	}
 }
