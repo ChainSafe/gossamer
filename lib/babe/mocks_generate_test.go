65f649e4d315766e287c9f8b79abd7297f3a0fba
Kishan Sagathiya <kishansagathiya@gmail.com>
2022-09-26 16:52:01 +0530

    added mock generate file

---
+++ lib/babe/mocks_generate_test.go
@@ -0,0 +1,6 @@
+// Copyright 2022 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package babe
+
+//go:generate mockgen -destination=mocks/network.go -source=../../dot/core/interface.go -package mocks dot/core Network
