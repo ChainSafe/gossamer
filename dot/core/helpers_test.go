9a83c71139ed6a2d98add7109616f0b2aaf23294
jimboj <jamesdjohnson218@gmail.com>
2022-09-28 11:45:02 -0600

    remove nil checks from createTestService

--- dot/core/helpers_test.go
+++ dot/core/helpers_test.go
@@ -38,7 +38,6 @@ func balanceKey(t *testing.T, pub []byte) (bKey []byte) {
 	return concatenateByteSlices(bytes)
 }
 
-// Note might need to pass in Config but will see
 // CreateTestService is a new helper function to clean up orchestration for testing the txnPool
 func CreateTestService(t *testing.T, genesisFilePath string,
 	pubKey []byte, accInfo types.AccountInfo, ctrl *gomock.Controller) (s *Service, encExtrinsic []byte) {
@@ -63,13 +62,13 @@ func CreateTestService(t *testing.T, genesisFilePath string,
 	}
 
 	cfg := &Config{}
-	if cfg.Keystore == nil {
-		cfg.Keystore = keystore.NewGlobalKeystore()
-		kp, err := sr25519.GenerateKeypair()
-		require.NoError(t, err)
-		err = cfg.Keystore.Acco.Insert(kp)
-		require.NoError(t, err)
-	}
+
+	//Construct keystore
+	cfg.Keystore = keystore.NewGlobalKeystore()
+	kp, err := sr25519.GenerateKeypair()
+	require.NoError(t, err)
+	err = cfg.Keystore.Acco.Insert(kp)
+	require.NoError(t, err)
 
 	cfg.LogLvl = log.Critical
 
@@ -78,77 +77,56 @@ func CreateTestService(t *testing.T, genesisFilePath string,
 	testDatadirPath := t.TempDir()
 
 	// Set up block and storage state
-	if cfg.BlockState == nil || cfg.StorageState == nil ||
-		cfg.TransactionState == nil || cfg.EpochState == nil ||
-		cfg.CodeSubstitutedState == nil {
-		telemetryMock := NewMockClient(ctrl)
-		telemetryMock.EXPECT().SendMessage(gomock.Any()).AnyTimes()
+	telemetryMock := NewMockClient(ctrl)
+	telemetryMock.EXPECT().SendMessage(gomock.Any()).AnyTimes()
 
-		config := state.Config{
-			Path:      testDatadirPath,
-			LogLevel:  log.Info,
-			Telemetry: telemetryMock,
-		}
-
-		stateSrvc = state.NewService(config)
-		stateSrvc.UseMemDB()
-
-		err := stateSrvc.Initialise(gen, genesisHeader, &genesisTrie)
-		require.NoError(t, err)
-
-		// Start state service
-		err = stateSrvc.Start()
-		require.NoError(t, err)
+	config := state.Config{
+		Path:      testDatadirPath,
+		LogLevel:  log.Info,
+		Telemetry: telemetryMock,
 	}
 
-	if cfg.BlockState == nil {
-		cfg.BlockState = stateSrvc.Block
-	}
+	stateSrvc = state.NewService(config)
+	stateSrvc.UseMemDB()
 
-	if cfg.StorageState == nil {
-		cfg.StorageState = stateSrvc.Storage
-	}
+	err = stateSrvc.Initialise(gen, genesisHeader, &genesisTrie)
+	require.NoError(t, err)
 
-	if cfg.TransactionState == nil {
-		cfg.TransactionState = stateSrvc.Transaction
-	}
+	// Start state service
+	err = stateSrvc.Start()
+	require.NoError(t, err)
 
-	if cfg.EpochState == nil {
-		cfg.EpochState = stateSrvc.Epoch
-	}
-
-	if cfg.CodeSubstitutedState == nil {
-		cfg.CodeSubstitutedState = stateSrvc.Base
-	}
+	cfg.BlockState = stateSrvc.Block
+	cfg.StorageState = stateSrvc.Storage
+	cfg.TransactionState = stateSrvc.Transaction
+	cfg.EpochState = stateSrvc.Epoch
+	cfg.CodeSubstitutedState = stateSrvc.Base
 
 	// Runtime stuff
-	if cfg.Runtime == nil {
-		// Okay no errors, but test doesn't pass
-		var rtCfg wasmer.Config
-		rtCfg.Storage = rtstorage.NewTrieState(&genesisTrie)
+	var rtCfg wasmer.Config
+	rtCfg.Storage = rtstorage.NewTrieState(&genesisTrie)
 
-		var err error
-		rtCfg.CodeHash, err = cfg.StorageState.LoadCodeHash(nil)
+	rtCfg.CodeHash, err = cfg.StorageState.LoadCodeHash(nil)
+	require.NoError(t, err)
+
+	nodeStorage := runtime.NodeStorage{}
+
+	if stateSrvc != nil {
+		nodeStorage.BaseDB = stateSrvc.Base
+	} else {
+		nodeStorage.BaseDB, err = utils.SetupDatabase(filepath.Join(testDatadirPath, "offline_storage"), false)
 		require.NoError(t, err)
-
-		nodeStorage := runtime.NodeStorage{}
-
-		if stateSrvc != nil {
-			nodeStorage.BaseDB = stateSrvc.Base
-		} else {
-			nodeStorage.BaseDB, err = utils.SetupDatabase(filepath.Join(testDatadirPath, "offline_storage"), false)
-			require.NoError(t, err)
-		}
-
-		rtCfg.NodeStorage = nodeStorage
-
-		cfg.Runtime, err = wasmer.NewRuntimeFromGenesis(rtCfg)
-		require.NoError(t, err)
-
-		cfg.Runtime.(*wasmer.Instance).GetContext().Storage.Set(aliceBalanceKey, encodedAccountInfo)
-		// this key is System.UpgradedToDualRefCount -> set to true since all accounts have been upgraded to v0.9 format
-		cfg.Runtime.(*wasmer.Instance).GetContext().Storage.Set(common.UpgradedToDualRefKey, []byte{1})
 	}
+
+	rtCfg.NodeStorage = nodeStorage
+
+	cfg.Runtime, err = wasmer.NewRuntimeFromGenesis(rtCfg)
+	require.NoError(t, err)
+
+	cfg.Runtime.(*wasmer.Instance).GetContext().Storage.Set(aliceBalanceKey, encodedAccountInfo)
+	// this key is System.UpgradedToDualRefCount -> set to true since all accounts have been upgraded to v0.9 format
+	cfg.Runtime.(*wasmer.Instance).GetContext().Storage.Set(common.UpgradedToDualRefKey, []byte{1})
+
 	cfg.BlockState.StoreRuntime(cfg.BlockState.BestBlockHash(), cfg.Runtime)
 
 	// Hash of encrypted centrifuge extrinsic
@@ -157,24 +135,18 @@ func CreateTestService(t *testing.T, genesisFilePath string,
 		0, "System.remark", testCallArguments)
 	encExtrinsic = common.MustHexToBytes(extHex)
 
-	if cfg.Network == nil {
-		cfg.Network = new(network.Service) // only for nil check in NewService
+	cfg.Network = new(network.Service) // only for nil check in NewService
+
+	cfg.CodeSubstitutes = make(map[common.Hash]string)
+
+	genesisData, err := cfg.CodeSubstitutedState.(*state.BaseState).LoadGenesisData()
+	require.NoError(t, err)
+
+	for k, v := range genesisData.CodeSubstitutes {
+		cfg.CodeSubstitutes[common.MustHexToHash(k)] = v
 	}
 
-	if cfg.CodeSubstitutes == nil {
-		cfg.CodeSubstitutes = make(map[common.Hash]string)
-
-		genesisData, err := cfg.CodeSubstitutedState.(*state.BaseState).LoadGenesisData()
-		require.NoError(t, err)
-
-		for k, v := range genesisData.CodeSubstitutes {
-			cfg.CodeSubstitutes[common.MustHexToHash(k)] = v
-		}
-	}
-
-	if cfg.CodeSubstitutedState == nil {
-		cfg.CodeSubstitutedState = stateSrvc.Base
-	}
+	cfg.CodeSubstitutedState = stateSrvc.Base
 
 	s, err = NewService(cfg)
 	require.NoError(t, err)
