784ce07f12737f660dcc53fd079ca30cc4aac90f
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2022-10-12 12:21:01 -0400

    chore: clear comment

--- lib/grandpa/grandpa.go
+++ lib/grandpa/grandpa.go
@@ -76,9 +76,7 @@ type Service struct {
 	bestFinalCandidate map[uint64]*Vote // map of round number -> best final candidate
 
 	// channels for communication with other services
-	//in             chan *networkVoteMessage // only used to receive *VoteMessage
 	finalisedCh chan *types.FinalisationInfo
-	//receivedCommit chan *CommitMessage
 
 	telemetry telemetry.Client
 }
