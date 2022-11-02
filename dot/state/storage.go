6ec6f5c49a3a32731b7c89619662b234b1c28dc8
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-11-02 11:24:58 +0000

    fix(state): node hashes vs merkle values - Pruners only care about node hashes - Rename variables, functions and methods only dealing with node hashes - Do not write or read inlined nodes with a non-hash Merkle value - Clarify error wrappings and comments

--- dot/state/storage.go
+++ dot/state/storage.go
@@ -64,12 +64,12 @@ func (s *StorageState) StoreTrie(ts *rtstorage.TrieState, header *types.Header) 
 	s.tries.softSet(root, ts.Trie())
 
 	if header != nil {
-		insertedMerkleValues, deletedMerkleValues, err := ts.GetChangedNodeHashes()
+		insertedNodeHashes, deletedNodeHashes, err := ts.GetChangedNodeHashes()
 		if err != nil {
-			return fmt.Errorf("failed to get state trie inserted keys: block %s %w", header.Hash(), err)
+			return fmt.Errorf("getting trie changed node hashes for block hash %s: %w", header.Hash(), err)
 		}
 
-		err = s.pruner.StoreJournalRecord(deletedMerkleValues, insertedMerkleValues, header.Hash(), int64(header.Number))
+		err = s.pruner.StoreJournalRecord(deletedNodeHashes, insertedNodeHashes, header.Hash(), int64(header.Number))
 		if err != nil {
 			return err
 		}
