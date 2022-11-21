374e900fda6f3e109e47def129c1866cf48b3c96
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-11-21 07:24:32 +0000

    dot/sync: use named mutex field for `disjoingBlockSet`

--- dot/sync/disjoint_block_set.go
+++ dot/sync/disjoint_block_set.go
@@ -92,7 +92,7 @@ func (b *pendingBlock) toBlockData() *types.BlockData {
 //
 // if the block is complete, we may not know of its parent.
 type disjointBlockSet struct {
-	sync.RWMutex
+	mutex sync.RWMutex
 	limit int
 
 	// map of block hash -> block data
@@ -128,8 +128,8 @@ func (s *disjointBlockSet) run(done <-chan struct{}) {
 }
 
 func (s *disjointBlockSet) clearBlocks() {
-	s.Lock()
-	defer s.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	for _, block := range s.blocks {
 		if s.timeNow().Sub(block.clearAt) > 0 {
@@ -149,8 +149,8 @@ func (s *disjointBlockSet) addToParentMap(parent, child common.Hash) {
 }
 
 func (s *disjointBlockSet) addHashAndNumber(hash common.Hash, number uint) error {
-	s.Lock()
-	defer s.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	if b, has := s.blocks[hash]; has {
 		b.clearAt = s.timeNow().Add(ttl)
@@ -166,8 +166,8 @@ func (s *disjointBlockSet) addHashAndNumber(hash common.Hash, number uint) error
 }
 
 func (s *disjointBlockSet) addHeader(header *types.Header) error {
-	s.Lock()
-	defer s.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	hash := header.Hash()
 	if b, has := s.blocks[hash]; has {
@@ -186,8 +186,8 @@ func (s *disjointBlockSet) addHeader(header *types.Header) error {
 }
 
 func (s *disjointBlockSet) addBlock(block *types.Block) error {
-	s.Lock()
-	defer s.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	hash := block.Header.Hash()
 	if b, has := s.blocks[hash]; has {
@@ -207,8 +207,8 @@ func (s *disjointBlockSet) addBlock(block *types.Block) error {
 }
 
 func (s *disjointBlockSet) addJustification(hash common.Hash, just []byte) error {
-	s.Lock()
-	defer s.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	b, has := s.blocks[hash]
 	if has {
@@ -222,8 +222,8 @@ func (s *disjointBlockSet) addJustification(hash common.Hash, just []byte) error
 }
 
 func (s *disjointBlockSet) removeBlock(hash common.Hash) {
-	s.Lock()
-	defer s.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.removeBlockInner(hash)
 }
 
@@ -258,33 +258,33 @@ func (s *disjointBlockSet) removeLowerBlocks(num uint) {
 }
 
 func (s *disjointBlockSet) hasBlock(hash common.Hash) bool {
-	s.RLock()
-	defer s.RUnlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	_, has := s.blocks[hash]
 	return has
 }
 
 func (s *disjointBlockSet) size() int {
-	s.RLock()
-	defer s.RUnlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return len(s.blocks)
 }
 
 func (s *disjointBlockSet) getChildren(hash common.Hash) map[common.Hash]struct{} {
-	s.RLock()
-	defer s.RUnlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.parentToChildren[hash]
 }
 
 func (s *disjointBlockSet) getBlock(hash common.Hash) *pendingBlock {
-	s.RLock()
-	defer s.RUnlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.blocks[hash]
 }
 
 func (s *disjointBlockSet) getBlocks() []*pendingBlock {
-	s.RLock()
-	defer s.RUnlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 
 	blocks := make([]*pendingBlock, len(s.blocks))
 	i := 0
