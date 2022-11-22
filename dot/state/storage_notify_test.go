6da3d8f2b299d2694516625bb3253b2c08b1a451
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-11-22 18:39:11 +0000

    Remove some TODOs

--- dot/state/storage_notify_test.go
+++ dot/state/storage_notify_test.go
@@ -24,7 +24,7 @@ func TestStorageState_RegisterStorageObserver(t *testing.T) {
 
 	ts, err := ss.TrieState(nil)
 	require.NoError(t, err)
-	ts = ts.Snapshot() // TODO remove?
+	ts = ts.Snapshot()
 
 	mockfilter := map[string][]byte{}
 	mockobs := NewMockObserver(ctrl)
@@ -65,7 +65,7 @@ func TestStorageState_RegisterStorageObserver_Multi(t *testing.T) {
 	ss := newTestStorageState(t)
 	ts, err := ss.TrieState(nil)
 	require.NoError(t, err)
-	ts = ts.Snapshot() // TODO remove?
+	ts = ts.Snapshot()
 
 	num := 5
 
@@ -106,7 +106,7 @@ func TestStorageState_RegisterStorageObserver_Multi_Filter(t *testing.T) {
 	ss := newTestStorageState(t)
 	ts, err := ss.TrieState(nil)
 	require.NoError(t, err)
-	ts = ts.Snapshot() // TODO remove?
+	ts = ts.Snapshot()
 
 	key1 := []byte("key1")
 	value1 := []byte("value1")
