b098c6d6d7752d7de0bfa2b5e763082792370fc8
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-12-15 16:47:26 +0000

    Add badger race test

---
+++ internal/database/badger/race_test.go
@@ -0,0 +1,78 @@
+// Copyright 2022 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package badger
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Database_threadSafety(t *testing.T) {
+	// This test consists in checking for concurrent access
+	// using the -race detector.
+	t.Parallel()
+
+	var startWg, endWg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+
+	const operations = 4
+	const parallelism = 3
+	const goroutines = parallelism * operations
+	startWg.Add(goroutines)
+	endWg.Add(goroutines)
+
+	const testDuration = 50 * time.Millisecond
+	go func() {
+		timer := time.NewTimer(time.Hour)
+		startWg.Wait()
+		_ = timer.Reset(testDuration)
+		<-timer.C
+		cancel()
+	}()
+
+	runInLoop := func(f func()) {
+		defer endWg.Done()
+		startWg.Done()
+		startWg.Wait()
+		for ctx.Err() == nil {
+			f()
+		}
+	}
+
+	settings := Settings{
+		Path: t.TempDir(),
+	}
+	database, err := New(settings)
+	require.NoError(t, err)
+	key := []byte{1}
+	value := []byte{2}
+
+	for i := 0; i < parallelism; i++ {
+		go runInLoop(func() {
+			_, _ = database.Get(key)
+		})
+
+		go runInLoop(func() {
+			_ = database.Set(key, value)
+		})
+
+		go runInLoop(func() {
+			_ = database.Delete(key)
+		})
+
+		go runInLoop(func() {
+			batch := database.NewWriteBatch()
+			_ = batch.Set(key, value)
+			_ = batch.Delete(key)
+			_ = batch.Set(key, value)
+			_ = batch.Flush()
+		})
+	}
+
+	endWg.Wait()
+}
