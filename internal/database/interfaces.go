d1d840f8761e79930082996af0f661bf27dc1d91
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-01-02 02:54:51 -0800

    Fix typo in package description for `internal/database`
    Co-authored-by: Kishan Sagathiya <kishansagathiya@gmail.com>

--- internal/database/interfaces.go
+++ internal/database/interfaces.go
@@ -1,7 +1,7 @@
 // Copyright 2022 ChainSafe Systems (ON)
 // SPDX-License-Identifier: LGPL-3.0-only
 
-// Package databse contains common interfaces and errors for all database implementations.
+// Package database contains common interfaces and errors for all database implementations.
 package database
 
 // WriteBatch is a batch of write operations that can be
