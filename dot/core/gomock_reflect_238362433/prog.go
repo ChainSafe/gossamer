f094fa347ad01a42fc01028448039be205216855
jimboj <jamesdjohnson218@gmail.com>
2023-01-04 16:35:59 -0700

    regenerated mocks

---
+++ dot/core/gomock_reflect_238362433/prog.go
@@ -0,0 +1,78 @@
+
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"reflect"
+
+	"github.com/golang/mock/mockgen/model"
+
+	pkg_ "github.com/ChainSafe/gossamer/dot/core"
+)
+
+var output = flag.String("output", "", "The output file name, or empty to use stdout.")
+
+func main() {
+	flag.Parse()
+
+	its := []struct{
+		sym string
+		typ reflect.Type
+	}{
+		
+		{ "BlockState", reflect.TypeOf((*pkg_.BlockState)(nil)).Elem()},
+		
+		{ "StorageState", reflect.TypeOf((*pkg_.StorageState)(nil)).Elem()},
+		
+		{ "TransactionState", reflect.TypeOf((*pkg_.TransactionState)(nil)).Elem()},
+		
+		{ "Network", reflect.TypeOf((*pkg_.Network)(nil)).Elem()},
+		
+		{ "CodeSubstitutedState", reflect.TypeOf((*pkg_.CodeSubstitutedState)(nil)).Elem()},
+		
+		{ "RuntimeInstance", reflect.TypeOf((*pkg_.RuntimeInstance)(nil)).Elem()},
+		
+		{ "Telemetry", reflect.TypeOf((*pkg_.Telemetry)(nil)).Elem()},
+		
+	}
+	pkg := &model.Package{
+		// NOTE: This behaves contrary to documented behaviour if the
+		// package name is not the final component of the import path.
+		// The reflect package doesn't expose the package name, though.
+		Name: path.Base("github.com/ChainSafe/gossamer/dot/core"),
+	}
+
+	for _, it := range its {
+		intf, err := model.InterfaceFromInterfaceType(it.typ)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Reflection: %v\n", err)
+			os.Exit(1)
+		}
+		intf.Name = it.sym
+		pkg.Interfaces = append(pkg.Interfaces, intf)
+	}
+
+	outfile := os.Stdout
+	if len(*output) != 0 {
+		var err error
+		outfile, err = os.Create(*output)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open output file %q", *output)
+		}
+		defer func() {
+			if err := outfile.Close(); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to close output file %q", *output)
+				os.Exit(1)
+			}
+		}()
+	}
+
+	if err := gob.NewEncoder(outfile).Encode(pkg); err != nil {
+		fmt.Fprintf(os.Stderr, "gob encode: %v\n", err)
+		os.Exit(1)
+	}
+}
