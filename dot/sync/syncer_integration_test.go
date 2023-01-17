e6da01b2f323d377dbcb2e053a4b2bc2daa34e3f
Eclésio Junior <eclesiomelo.1@gmail.com>
2023-01-17 16:11:32 -0400

    fix(dot/sync): fix `Timestamp slot must match 'CurrentSlot'` while using `westend` spec file (#3040)
    Co-authored-by: Quentin McGaw <quentin.mcgaw@gmail.com>

--- dot/sync/syncer_integration_test.go
+++ dot/sync/syncer_integration_test.go
@@ -125,7 +125,7 @@ func newTestGenesisWithTrieAndHeader(t *testing.T) (
 	gen genesis.Genesis, genesisTrie trie.Trie, genesisHeader types.Header) {
 	t.Helper()
 
-	genesisPath := utils.GetGssmrV3SubstrateGenesisRawPathTest(t)
+	genesisPath := utils.GetWestendDevRawGenesisPath(t)
 	genesisPtr, err := genesis.NewGenesisFromJSONRaw(genesisPath)
 	require.NoError(t, err)
 	gen = *genesisPtr
