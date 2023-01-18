a9497e484bfcb7ba16a542605ee039c0189937ad
Kishan Mohanbhai Sagathiya <kishansagathiya@gmail.com>
2023-01-18 23:12:59 +0530

    addressing review comments

--- lib/babe/mocks_generate_test.go
+++ lib/babe/mocks_generate_test.go
@@ -4,3 +4,5 @@
 package babe
 
 //go:generate mockgen -destination=mock_telemetry_test.go -package $GOPACKAGE . Telemetry
+//go:generate mockgen -destination=mocks/runtime.go -package mocks github.com/ChainSafe/gossamer/dot/core RuntimeInstance
+//go:generate mockgen -destination=mocks/network.go -package mocks github.com/ChainSafe/gossamer/dot/core Network
