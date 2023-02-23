bc369f46b7ebef6302b43af82a7fa6045610cec8
Kishan Mohanbhai Sagathiya <kishansagathiya@gmail.com>
2023-02-23 11:33:27 +0530

    just ignore chaindb.KeyNotFound

--- dot/state/epoch.go
+++ dot/state/epoch.go
@@ -12,9 +12,7 @@ import (
 
 	"github.com/ChainSafe/chaindb"
 	"github.com/ChainSafe/gossamer/dot/types"
-	"github.com/ChainSafe/gossamer/lib/blocktree"
 	"github.com/ChainSafe/gossamer/lib/common"
-	trieproof "github.com/ChainSafe/gossamer/lib/trie/proof"
 	"github.com/ChainSafe/gossamer/pkg/scale"
 )
 
@@ -400,8 +398,7 @@ func (nem nextEpochMap[T]) Retrieve(blockState *BlockState, epoch uint64, header
 		// sometimes while moving to the next epoch is possible the header
 		// is not fully imported by the blocktree, in this case we will use
 		// its parent header which migth be already imported.
-		if errors.Is(err, blocktree.ErrEndNodeNotFound) || errors.Is(err, trieproof.ErrKeyNotFound) ||
-			errors.Is(err, blocktree.ErrKeyNotFound) || errors.Is(err, chaindb.ErrKeyNotFound) {
+		if errors.Is(err, chaindb.ErrKeyNotFound) {
 			parentHeader, err := blockState.GetHeader(header.ParentHash)
 			if err != nil {
 				return nil, fmt.Errorf("cannot get parent header: %w", err)
