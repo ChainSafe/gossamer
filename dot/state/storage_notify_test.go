9b734b17818097faacc98cab247b02635203b8ce
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2023-02-28 12:34:34 -0400

    chore: fix type mismatch problems

--- dot/state/storage_notify_test.go
+++ dot/state/storage_notify_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/ChainSafe/chaindb"
 	"github.com/ChainSafe/gossamer/lib/common"
-	"github.com/dgraph-io/badger/v3/pb"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
@@ -139,9 +138,7 @@ func TestStorageState_RegisterStorageObserver_Multi_Filter(t *testing.T) {
 func Test_Example(t *testing.T) {
 	// this is a working example of how to use db.Subscribe taken from
 	// https://github.com/dgraph-io/badger/blob/f50343ff404d8198df6dc83755ec2eab863d5ff2/db_test.go#L1939-L1948
-	prefix := []pb.Match{
-		{Prefix: []byte{'a'}},
-	}
+	prefix := []byte{'a'}
 
 	// This key should be printed, since it matches the prefix.
 	aKey := []byte("a-key")
