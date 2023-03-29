bad36bc4704b77634cc3fae0d94384b490462969
edwardmack <ed@edwardmack.com>
2023-03-29 10:07:16 -0400

    add js tests

--- zombienet_tests/functional/01-custom.js
+++ zombienet_tests/functional/01-custom.js
@@ -1,8 +1,21 @@
 async function run(nodeName, networkInfo, args) {
     const {wsUri, userDefinedTypes} = networkInfo.nodesByName[nodeName];
     const api = await zombie.connect(wsUri, userDefinedTypes);
-    const validator = await api.query.session.validators();
-    return validator.length;
+    console.log("user types " + JSON.stringify(userDefinedTypes))
+    console.log("babe " + api.consts.babe.epochDuration.toNumber());
+    const now = await api.query.timestamp.now();
+    console.log("now " + now)
+    console.log("wsUri " + wsUri)
+
+    console.log("node metadata " + JSON.stringify(api.rpc.state.getMetadata()))
+    const chain = await api.rpc.system.chain();
+    console.log("chain " + chain);
+    const lastHeader = await api.rpc.chain.getHeader();
+    console.log("header " + lastHeader)
+    // console.log("node api " + JSON.stringify(api))
+    const version = await api.rpc.system.version();
+    console.log("node version " + version)
+    return 2;
 }
 
 module.exports = { run }
