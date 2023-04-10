a8f1128e34987f427558ed0f4a9b6975fc900901
edwardmack <ed@edwardmack.com>
2023-04-10 10:33:07 -0400

    remove un-used line

--- zombienet_tests/functional/scripts/free-balance.js
+++ zombienet_tests/functional/scripts/free-balance.js
@@ -1,7 +1,7 @@
 async function run(nodeName, networkInfo, args) {
     const {wsUri, userDefinedTypes} = networkInfo.nodesByName[nodeName];
     const api = await zombie.connect(wsUri, userDefinedTypes);
-    const ADDR = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+
     const {nonce, data: balance} = await api.query.system.account(args[0]);
 
     return balance.free;
