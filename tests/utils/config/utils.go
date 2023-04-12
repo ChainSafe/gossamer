f8dfa89cec59a5ce841be9c468388e3a0d745d24
kanishka <kanishkatn@gmail.com>
2023-04-12 17:36:05 +0530

    fix lint

--- tests/utils/config/utils.go
+++ tests/utils/config/utils.go
@@ -1,3 +1,6 @@
+// Copyright 2023 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package config
 
 import (
@@ -5,6 +8,7 @@ import (
 	"github.com/ChainSafe/gossamer/lib/common"
 )
 
+// ParseNetworkRole converts a common.NetworkRole to a string representation.
 func ParseNetworkRole(r common.NetworkRole) string {
 	switch r {
 	case common.NoNetworkRole:
