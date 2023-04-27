4fe38a02e6481ab1c55baa709b20f2858cbe6b5d
kanishka <kanishkatn@gmail.com>
2023-04-27 23:45:17 +0530

    tidy config

--- chain/westend-local/default.go
+++ chain/westend-local/default.go
@@ -23,142 +23,49 @@ const (
 
 // DefaultAliceConfig returns a westend local node configuration
 func DefaultAliceConfig() *cfg.Config {
-	return &cfg.Config{
-		BaseConfig: cfg.BaseConfig{
-			Name:           "Westend",
-			ID:             "westend_local",
-			BasePath:       DefaultBasePathAlice,
-			ChainSpec:      "./chain/westend-local/westend-local-spec-raw.json",
-			LogLevel:       "info",
-			PrometheusPort: uint32(9876),
-			RetainBlocks:   512,
-			Pruning:        pruner.Archive,
-		},
-		Log: &cfg.LogConfig{
-			Core:    "info",
-			Digest:  "info",
-			Sync:    "info",
-			Network: "info",
-			RPC:     "info",
-			State:   "info",
-			Runtime: "info",
-			Babe:    "info",
-			Grandpa: "info",
-			Wasmer:  "info",
-		},
-		Account: &cfg.AccountConfig{
-			Key:    "alice",
-			Unlock: "",
-		},
-		Core: &cfg.CoreConfig{
-			Role:             common.AuthorityRole,
-			WasmInterpreter:  wasmer.Name,
-			BabeAuthority:    true,
-			GrandpaAuthority: true,
-			GrandpaInterval:  time.Second,
-		},
-		State: &cfg.StateConfig{
-			Rewind: 0,
-		},
-		Network: &cfg.NetworkConfig{
-			Port:              7001,
-			NoBootstrap:       false,
-			NoMDNS:            false,
-			DiscoveryInterval: 10 * time.Second,
-			ProtocolID:        "dot",
-		},
-		RPC: &cfg.RPCConfig{
-			Port: 8545,
-			Host: "localhost",
-			Modules: []string{
-				"system", "author", "chain", "state", "rpc",
-				"grandpa", "offchain", "childstate", "syncstate", "payment"},
-			WSPort: 8546,
-		},
-		Pprof: &cfg.PprofConfig{
-			ListeningAddress: "localhost:6060",
-			BlockProfileRate: 0,
-			MutexProfileRate: 0,
-		},
-		System: &cfg.SystemConfig{
-			SystemName:    "gossamer",
-			SystemVersion: "0.1.0",
-		},
-	}
+	config := cfg.Copy(DefaultConfig())
+	config.BasePath = DefaultBasePathBob
+	config.PrometheusPort = uint32(9856)
+	config.Network.Port = 7001
+	config.RPC.Port = 8545
+	config.RPC.WSPort = 8546
+	config.Pprof.ListeningAddress = "localhost:6060"
+
+	return &config
 }
 
 // DefaultBobConfig returns a westend local node configuration with bob as the authority
 func DefaultBobConfig() *cfg.Config {
-	return &cfg.Config{
-		BaseConfig: cfg.BaseConfig{
-			Name:           "Westend",
-			ID:             "westend_local",
-			BasePath:       DefaultBasePathBob,
-			ChainSpec:      "./chain/westend-local/westend-local-spec-raw.json",
-			LogLevel:       "info",
-			PrometheusPort: uint32(9876),
-			RetainBlocks:   512,
-			Pruning:        pruner.Archive,
-		},
-		Log: &cfg.LogConfig{
-			Core:    "info",
-			Digest:  "info",
-			Sync:    "info",
-			Network: "info",
-			RPC:     "info",
-			State:   "info",
-			Runtime: "info",
-			Babe:    "info",
-			Grandpa: "info",
-			Wasmer:  "info",
-		},
-		Account: &cfg.AccountConfig{
-			Key:    "bob",
-			Unlock: "",
-		},
-		Core: &cfg.CoreConfig{
-			Role:             common.AuthorityRole,
-			WasmInterpreter:  wasmer.Name,
-			BabeAuthority:    true,
-			GrandpaAuthority: true,
-			GrandpaInterval:  time.Second,
-		},
-		State: &cfg.StateConfig{
-			Rewind: 0,
-		},
-		Network: &cfg.NetworkConfig{
-			Port:              7011,
-			NoBootstrap:       false,
-			NoMDNS:            false,
-			DiscoveryInterval: 10 * time.Second,
-		},
-		RPC: &cfg.RPCConfig{
-			Port: 8555,
-			Host: "localhost",
-			Modules: []string{
-				"system", "author", "chain", "state", "rpc",
-				"grandpa", "offchain", "childstate", "syncstate", "payment"},
-			WSPort: 8546,
-		},
-		Pprof: &cfg.PprofConfig{
-			ListeningAddress: "localhost:6070",
-			BlockProfileRate: 0,
-			MutexProfileRate: 0,
-		},
-		System: &cfg.SystemConfig{
-			SystemName:    "gossamer",
-			SystemVersion: "0.1.0",
-		},
-	}
+	config := cfg.Copy(DefaultConfig())
+	config.BasePath = DefaultBasePathBob
+	config.PrometheusPort = uint32(9866)
+	config.Network.Port = 7011
+	config.RPC.Port = 8555
+	config.RPC.WSPort = 8556
+	config.Pprof.ListeningAddress = "localhost:6070"
+
+	return &config
 }
 
 // DefaultCharlieConfig returns a westend local node configuration with charlie as the authority
 func DefaultCharlieConfig() *cfg.Config {
+	config := cfg.Copy(DefaultConfig())
+	config.BasePath = DefaultBasePathCharlie
+	config.PrometheusPort = uint32(9876)
+	config.Network.Port = 7021
+	config.RPC.Port = 8565
+	config.RPC.WSPort = 8566
+	config.Pprof.ListeningAddress = "localhost:6080"
+
+	return &config
+}
+
+// DefaultConfig returns a westend local node configuration
+func DefaultConfig() *cfg.Config {
 	return &cfg.Config{
 		BaseConfig: cfg.BaseConfig{
 			Name:           "Westend",
 			ID:             "westend_local",
-			BasePath:       DefaultBasePathCharlie,
 			ChainSpec:      "./chain/westend-local/westend-local-spec-raw.json",
 			LogLevel:       "info",
 			PrometheusPort: uint32(9876),
@@ -178,7 +85,7 @@ func DefaultCharlieConfig() *cfg.Config {
 			Wasmer:  "info",
 		},
 		Account: &cfg.AccountConfig{
-			Key:    "charlie",
+			Key:    "",
 			Unlock: "",
 		},
 		Core: &cfg.CoreConfig{
@@ -192,7 +99,6 @@ func DefaultCharlieConfig() *cfg.Config {
 			Rewind: 0,
 		},
 		Network: &cfg.NetworkConfig{
-			Port:              7021,
 			NoBootstrap:       false,
 			NoMDNS:            false,
 			DiscoveryInterval: 10 * time.Second,
@@ -200,7 +106,6 @@ func DefaultCharlieConfig() *cfg.Config {
 			MaxPeers:          cfg.DefaultMaxPeers,
 		},
 		RPC: &cfg.RPCConfig{
-			Port: 8565,
 			Host: "localhost",
 			Modules: []string{
 				"system", "author", "chain", "state", "rpc",
@@ -208,18 +113,12 @@ func DefaultCharlieConfig() *cfg.Config {
 			WSPort: 8546,
 		},
 		Pprof: &cfg.PprofConfig{
-			ListeningAddress: "localhost:6080",
 			BlockProfileRate: 0,
 			MutexProfileRate: 0,
 		},
 		System: &cfg.SystemConfig{
-			SystemName:    "gossamer",
-			SystemVersion: "0.1.0",
+			SystemName:    cfg.DefaultSystemName,
+			SystemVersion: cfg.DefaultSystemVersion,
 		},
 	}
-}
-
-// DefaultConfig returns a westend local node configuration
-func DefaultConfig() *cfg.Config {
-	return DefaultAliceConfig()
 }
