4c7fb4f77ba313d254e20a8986f7d6ae075dc77d
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2023-06-02 08:29:02 -0400

    chore: update golangci lint to 1.53v and fixes some ci warns

--- cmd/gossamer/commands/utils_test.go
+++ cmd/gossamer/commands/utils_test.go
@@ -260,6 +260,7 @@ func TestBindToViper(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			require.Equal(t, tt.expected, tt.result)
