b1278c37b82d99098662b3e580aea64e86c7bed1
Timothy Wu <tim.wu@chainsafe.io>
2023-06-20 13:54:17 -0400

    fulfill Instance interface

--- lib/runtime/wazero/instance.go
+++ lib/runtime/wazero/instance.go
@@ -1,17 +1,22 @@
 package wazero_runtime
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"sync"
 
+	"github.com/ChainSafe/gossamer/dot/types"
 	"github.com/ChainSafe/gossamer/internal/log"
 	"github.com/ChainSafe/gossamer/lib/common"
 	"github.com/ChainSafe/gossamer/lib/crypto"
+	"github.com/ChainSafe/gossamer/lib/crypto/ed25519"
 	"github.com/ChainSafe/gossamer/lib/keystore"
 	"github.com/ChainSafe/gossamer/lib/runtime"
 	"github.com/ChainSafe/gossamer/lib/runtime/offchain"
+	"github.com/ChainSafe/gossamer/lib/transaction"
+	"github.com/ChainSafe/gossamer/pkg/scale"
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
 )
@@ -22,6 +27,8 @@ const Name = "wazero"
 type contextKey string
 
 const runtimeContextKey = contextKey("runtime.Context")
+
+var _ runtime.Instance = &Instance{}
 
 // Instance backed by wazero.Runtime
 type Instance struct {
@@ -56,47 +63,47 @@ func NewInstance(code []byte, cfg Config) (instance *Instance, err error) {
 		Export("ext_logging_log_version_1").
 		NewFunctionBuilder().
 		WithFunc(func() int32 {
-			return 0
+			return 4
 		}).
 		Export("ext_logging_max_level_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32, b int32, c int32) {
-			return
+			panic("unimplemented")
 		}).
 		Export("ext_transaction_index_index_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32, b int32) {
-			return
+			panic("unimplemented")
 		}).
 		Export("ext_transaction_index_renew_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32) {
-			return
+			panic("unimplemented")
 		}).
 		Export("ext_sandbox_instance_teardown_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32, b int64, c int64, d int32) int32 {
-			return 0
+			panic("unimplemented")
 		}).
 		Export("ext_sandbox_instantiate_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32, b int64, c int64, d int32, e int32, f int32) int32 {
-			return 0
+			panic("unimplemented")
 		}).
 		Export("ext_sandbox_invoke_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32, b int32, c int32, d int32) int32 {
-			return 0
+			panic("unimplemented")
 		}).
 		Export("ext_sandbox_memory_get_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32, b int32, c int32, d int32) int32 {
-			return 0
+			panic("unimplemented")
 		}).
 		Export("ext_sandbox_memory_set_version_1").
 		NewFunctionBuilder().
 		WithFunc(func(a int32) {
-			return
+			panic("unimplemented")
 		}).
 		Export("ext_sandbox_memory_teardown_version_1").
 		NewFunctionBuilder().
@@ -431,4 +438,350 @@ func (in *Instance) version() error {
 	in.Context.Version = &version
 
 	return nil
+}
+
+// ValidateTransaction runs the extrinsic through the runtime function
+// TaggedTransactionQueue_validate_transaction and returns *transaction.Validity. The error can
+// be a VDT of either transaction.InvalidTransaction or transaction.UnknownTransaction, or can represent
+// a normal error i.e. unmarshalling error
+func (in *Instance) ValidateTransaction(e types.Extrinsic) (*transaction.Validity, error) {
+	ret, err := in.Exec(runtime.TaggedTransactionQueueValidateTransaction, e)
+	if err != nil {
+		return nil, err
+	}
+
+	return runtime.UnmarshalTransactionValidity(ret)
+}
+
+// Metadata calls runtime function Metadata_metadata
+func (in *Instance) Metadata() ([]byte, error) {
+	return in.Exec(runtime.Metadata, []byte{})
+}
+
+// BabeConfiguration gets the configuration data for BABE from the runtime
+func (in *Instance) BabeConfiguration() (*types.BabeConfiguration, error) {
+	data, err := in.Exec(runtime.BabeAPIConfiguration, []byte{})
+	if err != nil {
+		return nil, err
+	}
+
+	bc := new(types.BabeConfiguration)
+	err = scale.Unmarshal(data, bc)
+	if err != nil {
+		return nil, err
+	}
+
+	return bc, nil
+}
+
+// GrandpaAuthorities returns the genesis authorities from the runtime
+func (in *Instance) GrandpaAuthorities() ([]types.Authority, error) {
+	ret, err := in.Exec(runtime.GrandpaAuthorities, []byte{})
+	if err != nil {
+		return nil, err
+	}
+
+	var gar []types.GrandpaAuthoritiesRaw
+	err = scale.Unmarshal(ret, &gar)
+	if err != nil {
+		return nil, err
+	}
+
+	return types.GrandpaAuthoritiesRawToAuthorities(gar)
+}
+
+// BabeGenerateKeyOwnershipProof returns the babe key ownership proof from the runtime.
+func (in *Instance) BabeGenerateKeyOwnershipProof(slot uint64, authorityID [32]byte) (
+	types.OpaqueKeyOwnershipProof, error) {
+
+	// scale encoded slot uint64 + scale encoded array of 32 bytes
+	const maxBufferLength = 8 + 33
+	buffer := bytes.NewBuffer(make([]byte, 0, maxBufferLength))
+	encoder := scale.NewEncoder(buffer)
+	err := encoder.Encode(slot)
+	if err != nil {
+		return nil, fmt.Errorf("encoding slot: %w", err)
+	}
+	err = encoder.Encode(authorityID)
+	if err != nil {
+		return nil, fmt.Errorf("encoding authority id: %w", err)
+	}
+
+	encodedKeyOwnershipProof, err := in.Exec(runtime.BabeAPIGenerateKeyOwnershipProof, buffer.Bytes())
+	if err != nil {
+		return nil, fmt.Errorf("executing %s: %w", runtime.BabeAPIGenerateKeyOwnershipProof, err)
+	}
+
+	var keyOwnershipProof *types.OpaqueKeyOwnershipProof
+	err = scale.Unmarshal(encodedKeyOwnershipProof, &keyOwnershipProof)
+	if err != nil {
+		return nil, fmt.Errorf("scale decoding key ownership proof: %w", err)
+	}
+
+	if keyOwnershipProof == nil {
+		return nil, nil
+	}
+
+	return *keyOwnershipProof, nil
+}
+
+// BabeSubmitReportEquivocationUnsignedExtrinsic reports equivocation report to the runtime.
+func (in *Instance) BabeSubmitReportEquivocationUnsignedExtrinsic(
+	equivocationProof types.BabeEquivocationProof, keyOwnershipProof types.OpaqueKeyOwnershipProof,
+) error {
+	buffer := bytes.NewBuffer(nil)
+	encoder := scale.NewEncoder(buffer)
+	err := encoder.Encode(equivocationProof)
+	if err != nil {
+		return fmt.Errorf("encoding equivocation proof: %w", err)
+	}
+	err = encoder.Encode(keyOwnershipProof)
+	if err != nil {
+		return fmt.Errorf("encoding key ownership proof: %w", err)
+	}
+	_, err = in.Exec(runtime.BabeAPISubmitReportEquivocationUnsignedExtrinsic, buffer.Bytes())
+	return err
+}
+
+// InitializeBlock calls runtime API function Core_initialise_block
+func (in *Instance) InitializeBlock(header *types.Header) error {
+	encodedHeader, err := scale.Marshal(*header)
+	if err != nil {
+		return fmt.Errorf("cannot encode header: %w", err)
+	}
+
+	_, err = in.Exec(runtime.CoreInitializeBlock, encodedHeader)
+	return err
+}
+
+// InherentExtrinsics calls runtime API function BlockBuilder_inherent_extrinsics
+func (in *Instance) InherentExtrinsics(data []byte) ([]byte, error) {
+	return in.Exec(runtime.BlockBuilderInherentExtrinsics, data)
+}
+
+// ApplyExtrinsic calls runtime API function BlockBuilder_apply_extrinsic
+func (in *Instance) ApplyExtrinsic(data types.Extrinsic) ([]byte, error) {
+	return in.Exec(runtime.BlockBuilderApplyExtrinsic, data)
+}
+
+// FinalizeBlock calls runtime API function BlockBuilder_finalize_block
+func (in *Instance) FinalizeBlock() (*types.Header, error) {
+	data, err := in.Exec(runtime.BlockBuilderFinalizeBlock, []byte{})
+	if err != nil {
+		return nil, err
+	}
+
+	bh := types.NewEmptyHeader()
+	err = scale.Unmarshal(data, bh)
+	if err != nil {
+		return nil, err
+	}
+
+	return bh, nil
+}
+
+// ExecuteBlock calls runtime function Core_execute_block
+func (in *Instance) ExecuteBlock(block *types.Block) ([]byte, error) {
+	// copy block since we're going to modify it
+	b, err := block.DeepCopy()
+	if err != nil {
+		return nil, err
+	}
+
+	b.Header.Digest = types.NewDigest()
+
+	// remove seal digest only
+	for _, d := range block.Header.Digest.Types {
+		digestValue, err := d.Value()
+		if err != nil {
+			return nil, fmt.Errorf("getting digest type value: %w", err)
+		}
+		switch digestValue.(type) {
+		case types.SealDigest:
+			continue
+		default:
+			err = b.Header.Digest.Add(digestValue)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	bdEnc, err := b.Encode()
+	if err != nil {
+		return nil, err
+	}
+
+	return in.Exec(runtime.CoreExecuteBlock, bdEnc)
+}
+
+// DecodeSessionKeys decodes the given public session keys. Returns a list of raw public keys including their key type.
+func (in *Instance) DecodeSessionKeys(enc []byte) ([]byte, error) {
+	return in.Exec(runtime.DecodeSessionKeys, enc)
+}
+
+// PaymentQueryInfo returns information of a given extrinsic
+func (in *Instance) PaymentQueryInfo(ext []byte) (*types.RuntimeDispatchInfo, error) {
+	encLen, err := scale.Marshal(uint32(len(ext)))
+	if err != nil {
+		return nil, err
+	}
+
+	resBytes, err := in.Exec(runtime.TransactionPaymentAPIQueryInfo, append(ext, encLen...))
+	if err != nil {
+		return nil, err
+	}
+
+	dispatchInfo := new(types.RuntimeDispatchInfo)
+	if err = scale.Unmarshal(resBytes, dispatchInfo); err != nil {
+		return nil, err
+	}
+
+	return dispatchInfo, nil
+}
+
+// QueryCallInfo returns information of a given extrinsic
+func (in *Instance) QueryCallInfo(ext []byte) (*types.RuntimeDispatchInfo, error) {
+	encLen, err := scale.Marshal(uint32(len(ext)))
+	if err != nil {
+		return nil, err
+	}
+
+	resBytes, err := in.Exec(runtime.TransactionPaymentCallAPIQueryCallInfo, append(ext, encLen...))
+	if err != nil {
+		return nil, err
+	}
+
+	dispatchInfo := new(types.RuntimeDispatchInfo)
+	if err = scale.Unmarshal(resBytes, dispatchInfo); err != nil {
+		return nil, err
+	}
+
+	return dispatchInfo, nil
+}
+
+// QueryCallFeeDetails returns call fee details for given call
+func (in *Instance) QueryCallFeeDetails(ext []byte) (*types.FeeDetails, error) {
+	encLen, err := scale.Marshal(uint32(len(ext)))
+	if err != nil {
+		return nil, err
+	}
+
+	resBytes, err := in.Exec(runtime.TransactionPaymentCallAPIQueryCallFeeDetails, append(ext, encLen...))
+	if err != nil {
+		return nil, err
+	}
+
+	dispatchInfo := new(types.FeeDetails)
+	if err = scale.Unmarshal(resBytes, dispatchInfo); err != nil {
+		return nil, err
+	}
+
+	return dispatchInfo, nil
+}
+
+// CheckInherents checks inherents in the block verification process.
+// TODO: use this in block verification process (#1873)
+func (in *Instance) CheckInherents() {}
+
+// GrandpaGenerateKeyOwnershipProof returns grandpa key ownership proof from the runtime.
+func (in *Instance) GrandpaGenerateKeyOwnershipProof(authSetID uint64, authorityID ed25519.PublicKeyBytes) (
+	types.GrandpaOpaqueKeyOwnershipProof, error) {
+	const bufferSize = 8 + 32 // authSetID uint64 + ed25519.PublicKeyBytes
+	buffer := bytes.NewBuffer(make([]byte, 0, bufferSize))
+	encoder := scale.NewEncoder(buffer)
+	err := encoder.Encode(authSetID)
+	if err != nil {
+		return nil, fmt.Errorf("encoding auth set id: %w", err)
+	}
+	err = encoder.Encode(authorityID)
+	if err != nil {
+		return nil, fmt.Errorf("encoding authority id: %w", err)
+	}
+	encodedOpaqueKeyOwnershipProof, err := in.Exec(runtime.GrandpaGenerateKeyOwnershipProof, buffer.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	var keyOwnershipProof *types.GrandpaOpaqueKeyOwnershipProof
+	err = scale.Unmarshal(encodedOpaqueKeyOwnershipProof, &keyOwnershipProof)
+	if err != nil {
+		return nil, fmt.Errorf("scale decoding: %w", err)
+	}
+
+	if keyOwnershipProof == nil {
+		return nil, nil
+	}
+
+	return *keyOwnershipProof, nil
+}
+
+// GrandpaSubmitReportEquivocationUnsignedExtrinsic reports an equivocation report to the runtime.
+func (in *Instance) GrandpaSubmitReportEquivocationUnsignedExtrinsic(
+	equivocationProof types.GrandpaEquivocationProof, keyOwnershipProof types.GrandpaOpaqueKeyOwnershipProof,
+) error {
+	buffer := bytes.NewBuffer(nil)
+	encoder := scale.NewEncoder(buffer)
+	err := encoder.Encode(equivocationProof)
+	if err != nil {
+		return fmt.Errorf("encoding equivocation proof: %w", err)
+	}
+	err = encoder.Encode(keyOwnershipProof)
+	if err != nil {
+		return fmt.Errorf("encoding key ownership proof: %w", err)
+	}
+	_, err = in.Exec(runtime.GrandpaSubmitReportEquivocation, buffer.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (in *Instance) RandomSeed()          {}
+func (in *Instance) OffchainWorker()      {}
+func (in *Instance) GenerateSessionKeys() {}
+
+// GetCodeHash returns the code of the instance
+func (in *Instance) GetCodeHash() common.Hash {
+	panic("huh?")
+	// return in.codeHash
+}
+
+// NodeStorage to get reference to runtime node service
+func (in *Instance) NodeStorage() runtime.NodeStorage {
+	return in.Context.NodeStorage
+}
+
+// NetworkService to get referernce to runtime network service
+func (in *Instance) NetworkService() runtime.BasicNetwork {
+	return in.Context.Network
+}
+
+// Keystore to get reference to runtime keystore
+func (in *Instance) Keystore() *keystore.GlobalKeystore {
+	return in.Context.Keystore
+}
+
+// Validator returns the context's Validator
+func (in *Instance) Validator() bool {
+	return in.Context.Validator
+}
+
+// SetContextStorage sets the runtime's storage.
+func (in *Instance) SetContextStorage(s runtime.Storage) {
+	in.Lock()
+	defer in.Unlock()
+	in.Context.Storage = s
+}
+
+// Stop closes the WASM instance, its imports and clears
+// the context allocator in a thread-safe way.
+func (in *Instance) Stop() {
+	in.Lock()
+	defer in.Unlock()
+	in.Context.Allocator.Clear()
+	err := in.Runtime.Close(context.Background())
+	if err != nil {
+		log.Errorf("runtime failed to close: %v", err)
+	}
 }
