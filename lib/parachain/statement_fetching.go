49c375b3f9262de67fce2a965c98b8ac5edb0f17
axaysagathiya <axaysagathiya@gmail.com>
2023-06-22 21:46:01 +0530

    combine request and response file and add tests

--- lib/parachain/statement_fetching.go
+++ lib/parachain/statement_fetching.go
@@ -3,8 +3,23 @@ package parachain
 import (
 	"fmt"
 
+	"github.com/ChainSafe/gossamer/lib/common"
 	"github.com/ChainSafe/gossamer/pkg/scale"
 )
+
+// Request for fetching a large statement via request/response.
+type StatementFetchingRequest struct {
+	// Data needed to locate and identify the needed statement.
+	RelayParent common.Hash `scale:"1"`
+
+	// Hash of candidate that was used create the `CommitedCandidateRecept`.
+	CandidateHash CandidateHash `scale:"2"`
+}
+
+// Encode returns the SCALE encoding of the StatementFetchingRequest.
+func (s *StatementFetchingRequest) Encode() ([]byte, error) {
+	return scale.Marshal(*s)
+}
 
 // StatementFetchingResponse represents the statement fetching response is
 // sent by nodes to the clients who issued a collation fetching request.
