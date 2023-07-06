9d9a21c8440af6f535710994f86764e50f74a2c6
Axay Sagathiya <axaysagathiya@gmail.com>
2023-07-06 12:24:45 +0530

    feat(parachain): Implement request and response message for /req_statement/1 protocol (#3354)
    - Added `StatementFetchingRequest` and `StatementFetchingResponse`  varying data types.
    
    - implemented 'network.Message` interface in `StatementFetchingRequest` and 'network.ResponseMessage` interface in `StatementFetchingResponse` as they will be passed into `func (rrp *RequestResponseProtocol) Do(to peer.ID, req Message, res ResponseMessage) error` function as `req` and `res`.
    
    - I didn't want to create a new YAML file here. so I decided to rename the YAML file name and variable(in which data of the YAML file getting unmarshalled) name so that I can use them in this PR.

---
+++ lib/parachain/statement_fetching.go
@@ -0,0 +1,81 @@
+package parachain
+
+import (
+	"fmt"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/pkg/scale"
+)
+
+// StatementFetchingRequest represents a request for fetching a large statement via request/response.
+type StatementFetchingRequest struct {
+	// Data needed to locate and identify the needed statement.
+	RelayParent common.Hash `scale:"1"`
+
+	// Hash of candidate that was used create the `CommitedCandidateRecept`.
+	CandidateHash CandidateHash `scale:"2"`
+}
+
+// Encode returns the SCALE encoding of the StatementFetchingRequest.
+func (s *StatementFetchingRequest) Encode() ([]byte, error) {
+	return scale.Marshal(*s)
+}
+
+// StatementFetchingResponse represents the statement fetching response is
+// sent by nodes to the clients who issued a collation fetching request.
+//
+// Respond with found full statement.
+type StatementFetchingResponse scale.VaryingDataType
+
+// MissingDataInStatement represents the data missing to reconstruct the full signed statement.
+type MissingDataInStatement CommittedCandidateReceipt
+
+// Index returns the index of varying data type
+func (MissingDataInStatement) Index() uint {
+	return 0
+}
+
+// NewStatementFetchingResponse returns a new statement fetching response varying data type
+func NewStatementFetchingResponse() StatementFetchingResponse {
+	vdt := scale.MustNewVaryingDataType(MissingDataInStatement{})
+	return StatementFetchingResponse(vdt)
+}
+
+// Set will set a value using the underlying  varying data type
+func (s *StatementFetchingResponse) Set(val scale.VaryingDataTypeValue) (err error) {
+	vdt := scale.VaryingDataType(*s)
+	err = vdt.Set(val)
+	if err != nil {
+		return fmt.Errorf("setting value to varying data type: %w", err)
+	}
+
+	*s = StatementFetchingResponse(vdt)
+	return nil
+}
+
+// Value returns the value from the underlying varying data type
+func (s *StatementFetchingResponse) Value() (scale.VaryingDataTypeValue, error) {
+	vdt := scale.VaryingDataType(*s)
+	return vdt.Value()
+}
+
+// Encode returns the SCALE encoding of the StatementFetchingResponse.
+func (s *StatementFetchingResponse) Encode() ([]byte, error) {
+	return scale.Marshal(*s)
+}
+
+// Decode returns the SCALE decoding of the StatementFetchingResponse.
+func (s *StatementFetchingResponse) Decode(in []byte) (err error) {
+	return scale.Unmarshal(in, s)
+}
+
+// String formats a StatementFetchingResponse as a string
+func (s *StatementFetchingResponse) String() string {
+	if s == nil {
+		return "StatementFetchingResponse=nil"
+	}
+
+	v, _ := s.Value()
+	missingData := v.(MissingDataInStatement)
+	return fmt.Sprintf("StatementFetchingResponse MissingDataInStatement=%+v", missingData)
+}
