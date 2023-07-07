72d13c84620d77784460333bc0c4a3d6e9172c88
Axay Sagathiya <axaysagathiya@gmail.com>
2023-07-07 17:06:35 +0530

    feat(lib/parachain): Implement request and response message for /req_available_data/1 protocol (#3368)
    - Added AvailableDataFetchingRequest and AvailableDataFetchingResponse types.
    - Implemented 'network.Message' interface in AvailableDataFetchingRequest and 'network.ResponseMessage' interface in AvailableDataFetchingResponse as they will be passed into this function as req and res.

---
+++ lib/parachain/available_data_fetching_test.go
@@ -0,0 +1,89 @@
+package parachain
+
+import (
+	"testing"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncodeAvailableDataFetchingRequest(t *testing.T) {
+	availableDataFetchingRequest := AvailableDataFetchingRequest{
+		CandidateHash: CandidateHash{
+			common.MustHexToHash("0x677811d2f3ded2489685468dbdb2e4fa280a249fba9356acceb2e823820e2c19"),
+		},
+	}
+
+	actualEncode, err := availableDataFetchingRequest.Encode()
+	require.NoError(t, err)
+
+	expextedEncode := common.MustHexToBytes("0x677811d2f3ded2489685468dbdb2e4fa280a249fba9356acceb2e823820e2c19")
+	require.Equal(t, expextedEncode, actualEncode)
+}
+
+func TestAvailableDataFetchingResponse(t *testing.T) {
+	t.Parallel()
+
+	testHash := common.MustHexToHash("0x677811d2f3ded2489685468dbdb2e4fa280a249fba9356acceb2e823820e2c19")
+	testBytes := testHash.ToBytes()
+	availableData := AvailableData{
+		PoV: Pov{BlockData: testBytes},
+		ValidationData: PersistedValidationData{
+			ParentHead:             testBytes,
+			RelayParentNumber:      BlockNumber(4),
+			RelayParentStorageRoot: testHash,
+			MaxPovSize:             6,
+		},
+	}
+
+	testCases := []struct {
+		name        string
+		value       any
+		encodeValue []byte
+	}{
+		{
+			name:        "AvailableData",
+			value:       availableData,
+			encodeValue: common.MustHexToBytes("0x0080677811d2f3ded2489685468dbdb2e4fa280a249fba9356acceb2e823820e2c1980677811d2f3ded2489685468dbdb2e4fa280a249fba9356acceb2e823820e2c1904000000677811d2f3ded2489685468dbdb2e4fa280a249fba9356acceb2e823820e2c1906000000"), //nolint:lll
+		},
+		{
+			name:        "NoSuchData",
+			value:       NoSuchData{},
+			encodeValue: []byte{1},
+		},
+	}
+
+	for _, c := range testCases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			t.Run("encode", func(t *testing.T) {
+				t.Parallel()
+
+				availableDataFetchingResponse := NewAvailableDataFetchingResponse()
+				err := availableDataFetchingResponse.SetValue(c.value)
+				require.NoError(t, err)
+
+				actualEncode, err := availableDataFetchingResponse.Encode()
+				require.NoError(t, err)
+
+				require.Equal(t, c.encodeValue, actualEncode)
+			})
+
+			t.Run("decode", func(t *testing.T) {
+				t.Parallel()
+
+				availableDataFetchingResponse := NewAvailableDataFetchingResponse()
+				err := availableDataFetchingResponse.Decode(c.encodeValue)
+				require.NoError(t, err)
+
+				actualData, err := availableDataFetchingResponse.Value()
+				require.NoError(t, err)
+
+				require.EqualValues(t, c.value, actualData)
+			})
+
+		})
+	}
+}
