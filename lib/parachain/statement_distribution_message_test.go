a254f5b9ef55ecd1b663b2aabe2d77bfe300f009
Axay Sagathiya <axaysagathiya@gmail.com>
2023-07-07 20:40:42 +0530

    chore(lib/parachain): add tests to decode varying data types regarding statement and collation (#3374)
    added New() method and decode test for below varying data types.
    - statement
    - statement distribution message
    - collation protocol
    - collator protocol message

--- lib/parachain/statement_distribution_message_test.go
+++ lib/parachain/statement_distribution_message_test.go
@@ -58,7 +58,6 @@ func TestStatementDistributionMessage(t *testing.T) {
 		},
 		Commitments: CandidateCommitments{
 			UpwardMessages:            []UpwardMessage{{1, 2, 3}},
-			HorizontalMessages:        []OutboundHrmpMessage{},
 			NewValidationCode:         &ValidationCode{1, 2, 3},
 			HeadData:                  headData{1, 2, 3},
 			ProcessedDownwardMessages: uint32(5),
@@ -184,16 +183,31 @@ func TestStatementDistributionMessage(t *testing.T) {
 		c := c
 		t.Run(c.name, func(t *testing.T) {
 			t.Parallel()
+			t.Run("marshal", func(t *testing.T) {
+				t.Parallel()
 
-			vtd := NewStatementDistributionMessage()
+				vdt := NewStatementDistributionMessage()
+				err := vdt.SetValue(c.enumValue)
+				require.NoError(t, err)
 
-			err := vtd.SetValue(c.enumValue)
-			require.NoError(t, err)
+				bytes, err := scale.Marshal(vdt)
+				require.NoError(t, err)
 
-			bytes, err := scale.Marshal(vtd)
-			require.NoError(t, err)
+				require.Equal(t, c.encodingValue, bytes)
+			})
 
-			require.Equal(t, c.encodingValue, bytes)
+			t.Run("unmarshal", func(t *testing.T) {
+				t.Parallel()
+
+				vdt := NewStatementDistributionMessage()
+				err := scale.Unmarshal(c.encodingValue, &vdt)
+				require.NoError(t, err)
+
+				actualData, err := vdt.Value()
+				require.NoError(t, err)
+
+				require.EqualValues(t, c.enumValue, actualData)
+			})
 		})
 	}
 }
