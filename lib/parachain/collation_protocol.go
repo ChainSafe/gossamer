916144d83665018337ccc0fe7a044f647ef2445b
Kishan Sagathiya <kishansagathiya@gmail.com>
2023-07-11 13:40:38 +0530

    feat(lib/parachain): added parachain service and registering protocols (#3277)
    - Added parachain service
    - Registered collation and validation protocol
    - Confirmed that we can communicate with collators by talking to them in `run()` function

--- lib/parachain/collation_protocol.go
+++ lib/parachain/collation_protocol.go
@@ -1,8 +1,13 @@
 package parachain
 
 import (
+	"fmt"
+
+	"github.com/ChainSafe/gossamer/dot/network"
 	"github.com/ChainSafe/gossamer/lib/common"
+	parachaintypes "github.com/ChainSafe/gossamer/lib/parachain/types"
 	"github.com/ChainSafe/gossamer/pkg/scale"
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 // CollationProtocol represents all network messages on the collation peer-set.
@@ -75,9 +80,9 @@ func (c *CollatorProtocolMessage) Value() (val scale.VaryingDataTypeValue, err e
 // Declare the intent to advertise collations under a collator ID, attaching a
 // signature of the `PeerId` of the node using the given collator ID key.
 type Declare struct {
-	CollatorId        CollatorID        `scale:"1"`
-	ParaID            uint32            `scale:"2"`
-	CollatorSignature CollatorSignature `scale:"3"`
+	CollatorId        parachaintypes.CollatorID        `scale:"1"`
+	ParaID            uint32                           `scale:"2"`
+	CollatorSignature parachaintypes.CollatorSignature `scale:"3"`
 }
 
 // Index returns the index of varying data type
@@ -105,4 +110,84 @@ type CollationSeconded struct {
 // Index returns the index of varying data type
 func (CollationSeconded) Index() uint {
 	return 4
+}
+
+const MaxCollationMessageSize uint64 = 100 * 1024
+
+type CollationProtocolV1 struct{}
+
+// Type returns CollationMsgType
+func (*CollationProtocolV1) Type() network.MessageType {
+	return network.CollationMsgType
+}
+
+// Hash returns the hash of the CollationProtocolV1
+func (cp *CollationProtocolV1) Hash() (common.Hash, error) {
+	// scale encode each extrinsic
+	encMsg, err := cp.Encode()
+	if err != nil {
+		return common.Hash{}, fmt.Errorf("cannot encode message: %w", err)
+	}
+
+	return common.Blake2bHash(encMsg)
+}
+
+// Encode a collator protocol message using scale encode
+func (cp *CollationProtocolV1) Encode() ([]byte, error) {
+	enc, err := scale.Marshal(*cp)
+	if err != nil {
+		return nil, err
+	}
+	return enc, nil
+}
+
+func decodeCollationMessage(in []byte) (network.NotificationsMessage, error) {
+	collationMessage := CollationProtocolV1{}
+
+	err := scale.Unmarshal(in, &collationMessage)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode message: %w", err)
+	}
+
+	return &collationMessage, nil
+}
+
+func handleCollationMessage(_ peer.ID, msg network.NotificationsMessage) (bool, error) {
+	// TODO: Add things
+	fmt.Println("We got a collation message", msg)
+	return false, nil
+}
+
+func getCollatorHandshake() (network.Handshake, error) {
+	return &collatorHandshake{}, nil
+}
+
+func decodeCollatorHandshake(_ []byte) (network.Handshake, error) {
+	return &collatorHandshake{}, nil
+}
+
+func validateCollatorHandshake(_ peer.ID, _ network.Handshake) error {
+	return nil
+}
+
+type collatorHandshake struct{}
+
+// String formats a collatorHandshake as a string
+func (*collatorHandshake) String() string {
+	return "collatorHandshake"
+}
+
+// Encode encodes a collatorHandshake message using SCALE
+func (*collatorHandshake) Encode() ([]byte, error) {
+	return []byte{}, nil
+}
+
+// Decode the message into a collatorHandshake
+func (*collatorHandshake) Decode(_ []byte) error {
+	return nil
+}
+
+// IsValid returns true
+func (*collatorHandshake) IsValid() bool {
+	return true
 }
