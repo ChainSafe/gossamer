0390ade1cf1fdaef2e76d4445c62af7f3f5b6588
Diego <diego2737@gmail.com>
2023-07-20 12:57:37 -0300

    Change constant

--- lib/trie/trie_layout.go
+++ lib/trie/trie_layout.go
@@ -1,6 +1,6 @@
 package trie
 
-const TrieValueNodeThreshold uint32 = 32
+var TrieValueNodeThreshold uint32 = 32
 
 type TrieLayout interface {
 	maxInlineValue() *uint32
@@ -15,6 +15,5 @@ func (tl TrieLayoutV0) maxInlineValue() *uint32 {
 type TrieLayoutV1 struct{}
 
 func (tl TrieLayoutV1) maxInlineValue() *uint32 {
-	value := uint32(TrieValueNodeThreshold)
-	return &value
+	return &TrieValueNodeThreshold
 }
