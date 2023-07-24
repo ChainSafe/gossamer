694450e86432593f036a461f02bbb85e3e4218da
axaysagathiya <axaysagathiya@gmail.com>
2023-07-24 17:21:23 +0530

    handle deepsource error

--- dot/rpc/subscription/helpers_test.go
+++ dot/rpc/subscription/helpers_test.go
@@ -43,7 +43,7 @@ func setupWSConn(t *testing.T) (*WSConn, *websocket.Conn, func()) {
 
 	wsURL := "ws" + strings.TrimPrefix(server.URL, "http")
 	ws, r, err := websocket.DefaultDialer.Dial(wsURL, nil)
-	defer r.Body.Close()
+	r.Body.Close()
 
 	require.NoError(t, err)
 
