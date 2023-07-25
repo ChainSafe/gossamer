5f12fe0a24a7d763e1480e2d72a492ec00ebe261
Axay Sagathiya <axaysagathiya@gmail.com>
2023-07-25 22:41:16 +0530

    Chore(lib/runtime/storage): Implement transactional deletion of keys in trie (#3395)

--- lib/runtime/storage/trie.go
+++ lib/runtime/storage/trie.go
@@ -185,7 +185,10 @@ func (s *TrieState) DeleteChildLimit(key []byte, limit *[]byte) (
 	deleted uint32, allDeleted bool, err error) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	tr, err := s.t.GetChild(key)
+
+	trieSnapshot := s.t.Snapshot()
+
+	tr, err := trieSnapshot.GetChild(key)
 	if err != nil {
 		return 0, false, err
 	}
@@ -193,11 +196,12 @@ func (s *TrieState) DeleteChildLimit(key []byte, limit *[]byte) (
 	childTrieEntries := tr.Entries()
 	qtyEntries := uint32(len(childTrieEntries))
 	if limit == nil {
-		err = s.t.DeleteChild(key)
+		err = trieSnapshot.DeleteChild(key)
 		if err != nil {
 			return 0, false, fmt.Errorf("deleting child trie: %w", err)
 		}
 
+		s.t = trieSnapshot
 		return qtyEntries, true, nil
 	}
 	limitUint := binary.LittleEndian.Uint32(*limit)
@@ -220,6 +224,8 @@ func (s *TrieState) DeleteChildLimit(key []byte, limit *[]byte) (
 			break
 		}
 	}
+
+	s.t = trieSnapshot
 
 	allDeleted = deleted == qtyEntries
 	return deleted, allDeleted, nil
