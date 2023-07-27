36251ef1a27b33ad0b263fb760cf33731a5609cd
Diego <diego2737@gmail.com>
2023-07-27 12:07:26 -0300

    fix(trie): Add missing copyrights

--- lib/trie/trie_db.go
+++ lib/trie/trie_db.go
@@ -1,3 +1,6 @@
+// Copyright 2022 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package trie
 
 import "github.com/ChainSafe/gossamer/lib/common"
--- lib/trie/trie_layout.go
+++ lib/trie/trie_layout.go
@@ -1,3 +1,6 @@
+// Copyright 2022 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package trie
 
 var TrieValueNodeThreshold uint32 = 32
