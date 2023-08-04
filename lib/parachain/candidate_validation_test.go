4370567d7ba71f76c1ae3014dbc20bf1d7a879e8
Kishan Sagathiya <kishansagathiya@gmail.com>
2023-08-04 14:09:28 +0530

    feat(lib/parachain): introduced parachain candidate validation (#3249)
    This commit adds all the functions required for parachain candidate validations.
    
    With this commit, we are able to take candidate receipts and
    - get validation data for it,
    - perform basic checks on it,
    - run respective parachain's validate_block on parachain's runtime,
    - get validate results from parachain's runtime and verify those validation results again relaychain runtime,
    - and declare candidate as valid or invalid in the end.
    
    This commit also includes tests for candidate validation.

---
+++ lib/parachain/candidate_validation_test.go
@@ -0,0 +1,134 @@
+// Copyright 2023 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package parachain
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
+	parachaintypes "github.com/ChainSafe/gossamer/lib/parachain/types"
+	"github.com/ChainSafe/gossamer/pkg/scale"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func createTestCandidateReceiptAndValidationCode(t *testing.T) (
+	parachaintypes.CandidateReceipt, parachaintypes.ValidationCode) {
+	// this wasm was achieved by building polkadot's adder test parachain
+	runtimeFilePath := "./testdata/test_parachain_adder.wasm"
+	validationCodeBytes, err := os.ReadFile(runtimeFilePath)
+	require.NoError(t, err)
+
+	validationCode := parachaintypes.ValidationCode(validationCodeBytes)
+
+	validationCodeHashV := common.MustBlake2bHash(validationCodeBytes)
+
+	collatorKeypair, err := sr25519.GenerateKeypair()
+	require.NoError(t, err)
+	collatorID, err := sr25519.NewPublicKey(collatorKeypair.Public().Encode())
+	require.NoError(t, err)
+
+	candidateReceipt := parachaintypes.CandidateReceipt{
+		Descriptor: parachaintypes.CandidateDescriptor{
+			ParaID:                      uint32(1000),
+			RelayParent:                 common.MustHexToHash("0xded542bacb3ca6c033a57676f94ae7c8f36834511deb44e3164256fd3b1c0de0"), //nolint:lll
+			Collator:                    collatorID.AsBytes(),
+			PersistedValidationDataHash: common.MustHexToHash("0x690d8f252ef66ab0f969c3f518f90012b849aa5ac94e1752c5e5ae5a8996de37"), //nolint:lll
+			PovHash:                     common.MustHexToHash("0xe7df1126ac4b4f0fb1bc00367a12ec26ca7c51256735a5e11beecdc1e3eca274"), //nolint:lll
+			ErasureRoot:                 common.MustHexToHash("0xc07f658163e93c45a6f0288d229698f09c1252e41076f4caa71c8cbc12f118a1"), //nolint:lll
+			ParaHead:                    common.MustHexToHash("0x9a8a7107426ef873ab89fc8af390ec36bdb2f744a9ff71ad7f18a12d55a7f4f5"), //nolint:lll
+			ValidationCodeHash:          parachaintypes.ValidationCodeHash(validationCodeHashV),
+		},
+
+		CommitmentsHash: common.MustHexToHash("0xa54a8dce5fd2a27e3715f99e4241f674a48f4529f77949a4474f5b283b823535"),
+	}
+
+	payload, err := candidateReceipt.Descriptor.CreateSignaturePayload()
+	require.NoError(t, err)
+
+	signatureBytes, err := collatorKeypair.Sign(payload)
+	require.NoError(t, err)
+
+	signature := [sr25519.SignatureLength]byte{}
+	copy(signature[:], signatureBytes)
+
+	candidateReceipt.Descriptor.Signature = parachaintypes.CollatorSignature(signature)
+
+	return candidateReceipt, validationCode
+}
+
+func TestValidateFromChainState(t *testing.T) {
+
+	candidateReceipt, validationCode := createTestCandidateReceiptAndValidationCode(t)
+
+	bd, err := scale.Marshal(BlockDataInAdderParachain{
+		State: uint64(1),
+		Add:   uint64(1),
+	})
+	require.NoError(t, err)
+
+	pov := PoV{
+		BlockData: bd,
+	}
+
+	// NOTE: adder parachain internally compares postState with bd.State in it's validate_block,
+	// so following is necessary.
+	encodedState, err := scale.Marshal(uint64(1))
+	require.NoError(t, err)
+	postState, err := common.Keccak256(encodedState)
+	require.NoError(t, err)
+
+	hd, err := scale.Marshal(HeadDataInAdderParachain{
+		Number:     uint64(1),
+		ParentHash: common.MustHexToHash("0x0102030405060708090001020304050607080900010203040506070809000102"),
+		PostState:  postState,
+	})
+	require.NoError(t, err)
+
+	expectedPersistedValidationData := parachaintypes.PersistedValidationData{
+		ParentHead:             parachaintypes.HeadData{Data: hd},
+		RelayParentNumber:      uint32(1),
+		RelayParentStorageRoot: common.MustHexToHash("0x50c969706800c0e9c3c4565dc2babb25e4a73d1db0dee1bcf7745535a32e7ca1"),
+		MaxPovSize:             uint32(2048),
+	}
+
+	ctrl := gomock.NewController(t)
+
+	mockInstance := NewMockRuntimeInstance(ctrl)
+	mockInstance.EXPECT().
+		ParachainHostPersistedValidationData(
+			uint32(1000),
+			gomock.AssignableToTypeOf(parachaintypes.OccupiedCoreAssumption{})).
+		Return(&expectedPersistedValidationData, nil)
+	mockInstance.EXPECT().
+		ParachainHostValidationCode(uint32(1000), gomock.AssignableToTypeOf(parachaintypes.OccupiedCoreAssumption{})).
+		Return(&validationCode, nil)
+	mockInstance.EXPECT().
+		ParachainHostCheckValidationOutputs(uint32(1000), gomock.AssignableToTypeOf(parachaintypes.CandidateCommitments{})).
+		Return(true, nil)
+
+	mockPoVRequestor := NewMockPoVRequestor(ctrl)
+	mockPoVRequestor.EXPECT().
+		RequestPoV(common.MustHexToHash("0xe7df1126ac4b4f0fb1bc00367a12ec26ca7c51256735a5e11beecdc1e3eca274")).Return(pov)
+
+	candidateCommitments, persistedValidationData, isValid, err := ValidateFromChainState(
+		mockInstance, mockPoVRequestor, candidateReceipt)
+	require.NoError(t, err)
+	require.True(t, isValid)
+	require.NotNil(t, candidateCommitments)
+	require.Equal(t, expectedPersistedValidationData, *persistedValidationData)
+}
+
+type HeadDataInAdderParachain struct {
+	Number     uint64
+	ParentHash [32]byte
+	PostState  [32]byte
+}
+
+type BlockDataInAdderParachain struct {
+	State uint64
+	Add   uint64
+}
