71f598a0a7496c7874ae3fc1b12818b02f4b7a36
edwardmack <ed@edwardmack.com>
2023-08-04 10:58:11 -0400

    add to test objects

--- lib/parachain/network/bridge/rx/bridge-rx.go
+++ lib/parachain/network/bridge/rx/bridge-rx.go
@@ -1,1 +1,36 @@
 package rx
+
+import (
+	"github.com/ChainSafe/gossamer/dot/peerset"
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/libp2p/go-libp2p/core"
+)
+
+// LeafStatus is a simple type representing a leaf status
+type LeafStatus string
+
+// ActivatedLeaf represents an activated leaf
+type ActivatedLeaf struct {
+	hash   common.Hash
+	number int
+	status LeafStatus
+}
+
+// ActiveLeavesUpdate represents an active leaves update
+type ActiveLeavesUpdate struct {
+	ActivatedLeaf
+}
+
+// OverseerSignal represents an overseer signal
+type OverseerSignal struct {
+	ActiveLeaves ActiveLeavesUpdate
+}
+
+// NetworkAction represents a network action
+//
+//	TODO: this is a place holder, replace with variable data type
+type NetworkAction struct {
+	Peer    core.PeerID
+	PeerSet peerset.PeerSet
+	WireMsg string
+}
--- lib/parachain/network/bridge/rx/bridge-rx_test.go
+++ lib/parachain/network/bridge/rx/bridge-rx_test.go
@@ -2,72 +2,115 @@ package rx
 
 import (
 	"fmt"
+	"sync"
+	"testing"
+
 	"github.com/ChainSafe/gossamer/dot/peerset"
 	"github.com/ChainSafe/gossamer/lib/common"
-	"github.com/ChainSafe/gossamer/lib/parachain"
 	"github.com/libp2p/go-libp2p/core"
-	"testing"
 )
 
-//type NetworkAction scale.VaryingDataType
+// Oracle is a simple type representing an oracle
+type Oracle struct{}
 
-//// NewNetworkActionVDT constructor for NetworkAction
-//func NewNetworkActionVDT() NetworkAction {
-//	vdt, err := scale.NewVaryingDataType(ReputationChange{}, DisconnectPeer{}, WriteNotification{})
-//	if err != nil {
-//		panic(err)
-//	}
-//	return NetworkAction(vdt)
-//}
-
-type ReputationChange struct {
-	PeerId           core.PeerID
-	ReputationChange peerset.ReputationChange
+func makeSyncOracle() (*Oracle, *sync.WaitGroup) {
+	return &Oracle{}, &sync.WaitGroup{}
 }
 
-// Index returns varying data type index
-func (rc ReputationChange) Index() uint {
-	return 0
+func (o *Oracle) awaitModeSwitch() {}
+
+// NetworkHandle is a simple type representing a network handle
+type NetworkHandle struct{}
+
+func (nh *NetworkHandle) connectPeer(peer core.PeerID, peerSet, observedRole string) {}
+
+func (nh *NetworkHandle) nextNetworkActions(count int) []string {
+	return nil
 }
 
-type DisconnectPeer struct {
-	PeerId  core.PeerID
-	PeerSet peerset.PeerSet
+// VirtualOverseer is a simple type representing a virtual overseer
+type VirtualOverseer struct{}
+
+func (vo *VirtualOverseer) send(signal OverseerSignal) {}
+
+// TestHarness represents a test harness
+type TestHarness struct {
+	networkHandle   NetworkHandle
+	virtualOverseer VirtualOverseer
+	shared          interface{} // TODO: determine actual shared data type
 }
 
-// Index returns varying data type index
-func (dp DisconnectPeer) Index() uint {
-	return 1
-}
-
-type WriteNotification struct {
-	PeerId  core.PeerID
-	PeerSet peerset.PeerSet
-	Value   []byte
-}
-
-// Index returns varying data type index
-func (wn WriteNotification) Index() uint {
-	return 2
-}
-
-type TestNetworkHandle struct {
-	actionRx      NetworkAction
-	netTx         NetworkEvent
-	protocolNames PeerSetProtocolNames
-}
-
-type NetworkEvent struct{}
-
-type PeerSetProtocolNames struct {
-	Protocols map[string]struct{}
-	Names     map[struct{}]string
+func testHarness(oracle *Oracle, testFunc func(*TestHarness)) {
+	var wg sync.WaitGroup
+	testFunc(&TestHarness{
+		networkHandle:   NetworkHandle{},
+		virtualOverseer: VirtualOverseer{},
+		shared:          nil, // TODO: Replace with the actual shared data
+	})
+	wg.Wait()
 }
 
 func TestSendOurViewUponConnection(t *testing.T) {
-	view := parachain.View{
-		Heads: []common.Hash{{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-			1, 1, 1}},
-	}
-	fmt.Printf("View %v\n", view)
+	oracle, handle := makeSyncOracle()
+
+	testHarness(oracle, func(testHarness *TestHarness) {
+		networkHandle := testHarness.networkHandle
+		virtualOverseer := testHarness.virtualOverseer
+
+		peer := core.PeerID("random-peer-id")
+		head := common.Hash{1, 2, 3}
+
+		virtualOverseer.send(OverseerSignal{
+			ActiveLeaves: ActiveLeavesUpdate{
+				ActivatedLeaf: ActivatedLeaf{
+					hash:   head,
+					number: 1,
+					status: LeafStatus("Fresh"),
+				},
+			},
+		})
+
+		handle.Wait()
+
+		networkHandle.connectPeer(peer, "Validation", "Full")
+		networkHandle.connectPeer(peer, "Collation", "Full")
+
+		// TODO:  implementation of awaitPeerConnections
+		//awaitPeerConnections(&testHarness.shared, 1, 1)
+
+		view := []common.Hash{head}
+		actions := networkHandle.nextNetworkActions(2)
+
+		assertNetworkActionsContains(
+			&actions,
+			&NetworkAction{
+				Peer:    peer,
+				PeerSet: peerset.PeerSet{},
+				WireMsg: EncodeViewUpdate(view),
+			},
+		)
+
+		assertNetworkActionsContains(
+			&actions,
+			&NetworkAction{
+				Peer:    peer,
+				PeerSet: peerset.PeerSet{},
+				WireMsg: EncodeViewUpdate(view),
+			},
+		)
+
+		virtualOverseer.send(OverseerSignal{})
+	})
+}
+
+// EncodeViewUpdate encodes the view update
+func EncodeViewUpdate(view []common.Hash) string {
+	// TODO: add actual encoding
+	return fmt.Sprintf("%v", view)
+}
+
+// assertNetworkActionsContains is a helper function to assert that a network action is present
+func assertNetworkActionsContains(actions *[]string, action *NetworkAction) {
+	// TODO: implement this
+	fmt.Printf("Actions %v, action: %v\n", actions, action)
 }
