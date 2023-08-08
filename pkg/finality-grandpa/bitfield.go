1c9770c35f113748835c581c0b3b770c31406322
Timothy Wu <tim.wu@chainsafe.io>
2023-08-08 15:58:55 -0400

    make bit1 private

--- pkg/finality-grandpa/bitfield.go
+++ pkg/finality-grandpa/bitfield.go
@@ -55,17 +55,17 @@ func (b *bitfield) SetBit(position uint) { //skipcq: GO-W1029
 // iter1s will get an iterator over all bits that are set (i.e. 1) in the bitfield,
 // starting at bit position `start` and moving in steps of size `2^step`
 // per word.
-func (b *bitfield) iter1s(start, step uint) (bit1s []Bit1) { //skipcq: GO-W1029
+func (b *bitfield) iter1s(start, step uint) (bit1s []bit1) { //skipcq: GO-W1029
 	return iter1s(b.bits, start, step)
 }
 
 // Iter1sEven will get an iterator over all bits that are set (i.e. 1) at even bit positions.
-func (b *bitfield) Iter1sEven() []Bit1 { //skipcq: GO-W1029
+func (b *bitfield) Iter1sEven() []bit1 { //skipcq: GO-W1029
 	return b.iter1s(0, 1)
 }
 
 // Iter1sOdd will get an iterator over all bits that are set (i.e. 1) at odd bit positions.
-func (b *bitfield) Iter1sOdd() []Bit1 { //skipcq: GO-W1029
+func (b *bitfield) Iter1sOdd() []bit1 { //skipcq: GO-W1029
 	return b.iter1s(1, 1)
 }
 
@@ -73,7 +73,7 @@ func (b *bitfield) Iter1sOdd() []Bit1 { //skipcq: GO-W1029
 // this bitfield with another bitfield, without modifying either
 // bitfield, starting at bit position `start` and moving in steps
 // of size `2^step` per word.
-func (b *bitfield) iter1sMerged(other bitfield, start, step uint) []Bit1 { //skipcq: GO-W1029
+func (b *bitfield) iter1sMerged(other bitfield, start, step uint) []bit1 { //skipcq: GO-W1029
 	switch {
 	case len(b.bits) == len(other.bits):
 		zipped := make([]uint64, len(b.bits))
@@ -110,21 +110,21 @@ func (b *bitfield) iter1sMerged(other bitfield, start, step uint) []Bit1 { //ski
 // Iter1sMergedEven will get an iterator over all bits that are set (i.e. 1) at even bit positions
 // when merging this bitfield with another bitfield, without modifying
 // either bitfield.
-func (b *bitfield) Iter1sMergedEven(other bitfield) []Bit1 { //skipcq: GO-W1029
+func (b *bitfield) Iter1sMergedEven(other bitfield) []bit1 { //skipcq: GO-W1029
 	return b.iter1sMerged(other, 0, 1)
 }
 
 // Iter1sMergedOdd will get an iterator over all bits that are set (i.e. 1) at odd bit positions
 // when merging this bitfield with another bitfield, without modifying
 // either bitfield.
-func (b *bitfield) Iter1sMergedOdd(other bitfield) []Bit1 { //skipcq: GO-W1029
+func (b *bitfield) Iter1sMergedOdd(other bitfield) []bit1 { //skipcq: GO-W1029
 	return b.iter1sMerged(other, 1, 1)
 }
 
 // Turn an iterator over u64 words into an iterator over bits that
 // are set (i.e. `1`) in these words, starting at bit position `start`
 // and moving in steps of size `2^step` per word.
-func iter1s(iter []uint64, start, step uint) (bit1s []Bit1) {
+func iter1s(iter []uint64, start, step uint) (bit1s []bit1) {
 	if !(start < 64 && step < 7) {
 		panic("wtf?")
 	}
@@ -137,7 +137,7 @@ func iter1s(iter []uint64, start, step uint) (bit1s []Bit1) {
 		for j := uint(0); j < n; j++ {
 			bitPos := start + (j << step)
 			if testBit(word, bitPos) {
-				bit1s = append(bit1s, Bit1{uint(i)*64 + bitPos})
+				bit1s = append(bit1s, bit1{uint(i)*64 + bitPos})
 			}
 		}
 	}
@@ -150,7 +150,7 @@ func testBit(word uint64, position uint) bool {
 }
 
 // A bit that is set (i.e. 1) in a `bitfield`.
-type Bit1 struct {
+type bit1 struct {
 	// The position of the bit in the bitfield.
-	Position uint
+	position uint
 }
