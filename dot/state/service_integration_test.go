5d256447a892e76d1adcf6dcf83d51ea08b22839
Diego <diego2737@gmail.com>
2023-08-15 17:28:34 -0300

    Fix linter

--- dot/state/service_integration_test.go
+++ dot/state/service_integration_test.go
@@ -437,7 +437,7 @@ func generateBlockWithRandomTrie(t *testing.T, serv *Service,
 	rand := time.Now().UnixNano()
 	key := []byte("testKey" + fmt.Sprint(rand))
 	value := []byte("testValue" + fmt.Sprint(rand))
-	err = trieState.Put(key, value)
+	err = trieState.Put(key, value, trie.V0)
 	require.NoError(t, err)
 
 	trieStateRoot, err := trieState.Root()
