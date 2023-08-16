93356218da472a38ed3b553d571d5a69d7a37ed2
edwardmack <ed@edwardmack.com>
2023-08-16 17:38:47 -0400

    save commit, added to orchestra

--- dot/parachain/orchestra.go
+++ dot/parachain/orchestra.go
@@ -4,27 +4,30 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
-type FromOrchestra struct{}
+type FromOrchestra struct {
+	msg string
+}
 
 type SubsystemSender interface{}
 
 type Subsystem interface {
-	Start(ctx context.Context, out chan<- string)
+	Start(subCtx SubsystemContext)
 	Stop()
 }
 
 // SubsystemContext defines the methods that should be implemented by a SubsystemContext
 type SubsystemContext interface {
-	TryRecv() (FromOrchestra, error)
+	//TryRecv() (FromOrchestra, error)
 	Recv() (FromOrchestra, error)
-	Spawn(name string, f func(ctx context.Context)) error
-	SpawnBlocking(name string, f func(context.Context)) error
+	//Spawn(name string, f func(ctx context.Context)) error
+	//SpawnBlocking(name string, f func(context.Context)) error
 	SendMessage(msg interface{})
-	SendMessages(msgs []interface{})
-	SendUnboundedMessage(msg interface{})
-	Sender() SubsystemSender
+	//SendMessages(msgs []interface{})
+	//SendUnboundedMessage(msg interface{})
+	//Sender() SubsystemSender
 }
 
 type SpawnedSubsystem struct {
@@ -33,35 +36,46 @@ type SpawnedSubsystem struct {
 }
 
 type Orchestra struct {
-	subsystems []Subsystem
-	wg         sync.WaitGroup
-	msgChan    chan string
+	subsystems       []Subsystem
+	wg               sync.WaitGroup
+	recMsgChan       <-chan interface{}
+	sendMsgChan      chan interface{}
+	subsystemContext SubsystemContext
 }
 
-func NewOrchestra() *Orchestra {
-	return &Orchestra{}
+func NewOrchestra(recMsgChan <-chan interface{}, sendMsgChan chan interface{}) *Orchestra {
+	return &Orchestra{
+		recMsgChan:  recMsgChan,
+		sendMsgChan: sendMsgChan,
+	}
 }
 
 func (o *Orchestra) AddSubsystem(subsystem Subsystem) {
 	o.subsystems = append(o.subsystems, subsystem)
 }
 
-func (o *Orchestra) Start(ctx context.Context) {
-	o.msgChan = make(chan string)
+func (o *Orchestra) Start(ctx SubsystemContext) {
+	o.subsystemContext = ctx
 	for _, subsystem := range o.subsystems {
 		o.wg.Add(1)
 		go func(subsystem Subsystem) {
 			defer o.wg.Done()
-			subsystem.Start(ctx, o.msgChan)
+			subsystem.Start(o.subsystemContext)
 		}(subsystem)
 	}
 	go func() {
-		fmt.Printf("In chan check")
 		for {
 			select {
-			case msg := <-o.msgChan:
-				fmt.Printf("Msg %v\n", msg)
+			case msg := <-o.recMsgChan:
+				fmt.Printf("Orchesta received msg: %v\n", msg)
 			}
+		}
+	}()
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 1200)
+			fmt.Printf("sending message\n")
+			o.sendMsgChan <- fmt.Sprintf("Msg From Orchastra")
 		}
 	}()
 }
--- dot/parachain/orchestra_test.go
+++ dot/parachain/orchestra_test.go
@@ -1,7 +1,6 @@
 package parachain
 
 import (
-	"context"
 	"fmt"
 	"testing"
 	"time"
@@ -19,18 +18,27 @@ func NewExampleSubsystem(name string) *ExampleSubsystem {
 	}
 }
 
-func (s *ExampleSubsystem) Start(ctx context.Context, out chan<- string) {
+func (s *ExampleSubsystem) Start(subCtx SubsystemContext) {
 	fmt.Printf("Subsystem %s started\n", s.name)
+	go func() {
+		for {
+			fmt.Printf("sub system %v start recieve\n", s.name)
+			msg, err := subCtx.Recv()
+			if err != nil {
+				fmt.Printf("ERROR %v\n", err)
+			}
+			fmt.Printf("%v rec msg %v\n", s.name, msg)
+		}
+
+	}()
+	// main loop for subsystem
 	for {
 		select {
 		case <-s.done:
 			fmt.Printf("Subsystem %s stopped\n", s.name)
 			return
-		case <-ctx.Done():
-			fmt.Printf("Subsystem %s received cancel signal\n", s.name)
-			return
 		default:
-			out <- fmt.Sprintf("Subsystem %s working...", s.name)
+			subCtx.SendMessage(fmt.Sprintf("Subsystem %v working...", s.name))
 			time.Sleep(time.Second)
 		}
 	}
@@ -40,15 +48,43 @@ func (s *ExampleSubsystem) Stop() {
 	close(s.done)
 }
 
-func TestOrchestra(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+type ExampleSubsystemContext struct {
+	msgChan    chan interface{}
+	recMsgChan chan interface{}
+}
 
-	orchestra := NewOrchestra()
-	orchestra.AddSubsystem(NewExampleSubsystem("Subsystem A"))
+func (e *ExampleSubsystemContext) SendMessage(msg interface{}) {
+	e.msgChan <- fmt.Sprintf("%v", msg)
+}
+
+func (e *ExampleSubsystemContext) Recv() (FromOrchestra, error) {
+	for {
+		select {
+		case msg := <-e.recMsgChan:
+			return FromOrchestra{msg: fmt.Sprintf("%v", msg)}, nil
+		}
+	}
+	return FromOrchestra{}, nil
+}
+
+func NewExampleSubsystemContext(msgChan chan interface{}, recMsgChan chan interface{}) SubsystemContext {
+	return &ExampleSubsystemContext{
+		msgChan:    msgChan,
+		recMsgChan: recMsgChan,
+	}
+}
+
+func TestOrchestra(t *testing.T) {
+	toOrchMsgChan := make(chan interface{})
+	fromOrchMsgChan := make(chan interface{})
+	subsystemContext := NewExampleSubsystemContext(toOrchMsgChan, fromOrchMsgChan)
+	orchestra := NewOrchestra(toOrchMsgChan, fromOrchMsgChan)
+
+	subSystemA := NewExampleSubsystem("Subsystem A")
+	orchestra.AddSubsystem(subSystemA)
 	orchestra.AddSubsystem(NewExampleSubsystem("Subsystem B"))
 
-	orchestra.Start(ctx)
+	orchestra.Start(subsystemContext)
 
 	time.Sleep(5 * time.Second)
 	fmt.Println("Stopping orchestra...")
