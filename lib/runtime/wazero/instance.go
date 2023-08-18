2ebae3c8c2f4a840a8390d59950c0c5f06a24e80
Edward Mack <ed@edwardmack.com>
2023-08-18 10:11:45 -0400

    feat(lib/parachain): Create and integrate Validation Protocol struct (#3382)
    Co-authored-by: Kanishka <kanishkatn@gmail.com>
    Co-authored-by: Kishan Mohanbhai Sagathiya <kishansagathiya@gmail.com>
    Co-authored-by: Axay Sagathiya <axaysagathiya@gmail.com>

--- lib/runtime/wazero/instance.go
+++ lib/runtime/wazero/instance.go
@@ -1041,7 +1041,7 @@ func (in *Instance) ParachainHostSessionInfo(sessionIndex parachaintypes.Session
 	return sessionInfo, nil
 }
 
-func (in *Instance) RandomSeed() {
+func (*Instance) RandomSeed() {
 	panic("unimplemented")
 }
 func (*Instance) OffchainWorker() {
