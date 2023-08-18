abe9a90a633f69d9098f1ae5dab2c8ed09d7368a
Edward Mack <ed@edwardmack.com>
2023-08-18 10:11:45 -0400

    feat(lib/parachain): Create and integrate Validation Protocol struct (#3382)
    Co-authored-by: Kanishka <kanishkatn@gmail.com>
    Co-authored-by: Kishan Mohanbhai Sagathiya <kishansagathiya@gmail.com>
    Co-authored-by: Axay Sagathiya <axaysagathiya@gmail.com>

--- dot/parachain/validation_protocol.go
+++ dot/parachain/validation_protocol.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/ChainSafe/gossamer/dot/network"
+	parachaintypes "github.com/ChainSafe/gossamer/dot/parachain/types"
 	"github.com/ChainSafe/gossamer/lib/common"
 	"github.com/ChainSafe/gossamer/pkg/scale"
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -14,17 +15,171 @@ import (
 
 const MaxValidationMessageSize uint64 = 100 * 1024
 
-type ValidationProtocolV1 struct {
-	//	TODO: Implement this struct https://github.com/ChainSafe/gossamer/issues/3318
+// UncheckedSignedAvailabilityBitfield a signed bitfield with signature not yet checked
+type UncheckedSignedAvailabilityBitfield struct {
+	// The payload is part of the signed data. The rest is the signing context,
+	// which is known both at signing and at validation.
+	Payload scale.BitVec `scale:"1"`
+
+	// The index of the validator signing this statement.
+	ValidatorIndex parachaintypes.ValidatorIndex `scale:"2"`
+
+	// The signature by the validator of the signed payload.
+	Signature ValidatorSignature `scale:"3"`
+}
+
+// Bitfield avalibility bitfield for given relay-parent hash
+type Bitfield struct {
+	Hash                                common.Hash                         `scale:"1"`
+	UncheckedSignedAvailabilityBitfield UncheckedSignedAvailabilityBitfield `scale:"2"`
+}
+
+type BitfieldDistributionMessageValues interface {
+	Bitfield
+}
+
+// BitfieldDistributionMessage Network messages used by bitfield distribution subsystem
+type BitfieldDistributionMessage struct {
+	inner any
+}
+
+func setBitfieldDistributionMessage[Value BitfieldDistributionMessageValues](mvdt *BitfieldDistributionMessage, value Value) {
+	mvdt.inner = value
+}
+
+func (mvdt *BitfieldDistributionMessage) SetValue(value any) (err error) {
+	switch value := value.(type) {
+	case Bitfield:
+		setBitfieldDistributionMessage(mvdt, value)
+		return
+
+	default:
+		return fmt.Errorf("unsupported type")
+	}
+}
+
+func (mvdt BitfieldDistributionMessage) IndexValue() (index uint, value any, err error) {
+	switch mvdt.inner.(type) {
+	case Bitfield:
+		return 0, mvdt.inner, nil
+
+	}
+	return 0, nil, scale.ErrUnsupportedVaryingDataTypeValue
+}
+
+func (mvdt BitfieldDistributionMessage) Value() (value any, err error) {
+	_, value, err = mvdt.IndexValue()
+	return
+}
+
+func (mvdt BitfieldDistributionMessage) ValueAt(index uint) (value any, err error) {
+	switch index {
+	case 0:
+		return *new(Bitfield), nil
+
+	}
+	return nil, scale.ErrUnknownVaryingDataTypeValue
+}
+
+// NewBitfieldDistributionMessageVDT returns a new BitfieldDistributionMessage VaryingDataType
+func NewBitfieldDistributionMessageVDT() BitfieldDistributionMessage {
+	return BitfieldDistributionMessage{}
+}
+
+// BitfieldDistribution struct holding BitfieldDistributionMessage
+type BitfieldDistribution struct {
+	BitfieldDistributionMessage
+}
+
+// ApprovalDistribution struct holding ApprovalDistributionMessage
+type ApprovalDistribution struct {
+	ApprovalDistributionMessage
+}
+
+// StatementDistribution struct holding StatementDistributionMessage
+type StatementDistribution struct {
+	StatementDistributionMessage
+}
+
+type ValidationProtocolValues interface {
+	BitfieldDistribution | StatementDistribution | ApprovalDistribution
+}
+
+// ValidationProtocol VaryingDataType for ValidationProtocol
+type ValidationProtocol struct {
+	inner any
+}
+
+func setValidationProtocol[Value ValidationProtocolValues](mvdt *ValidationProtocol, value Value) {
+	mvdt.inner = value
+}
+
+func (mvdt *ValidationProtocol) SetValue(value any) (err error) {
+	switch value := value.(type) {
+	case BitfieldDistribution:
+		setValidationProtocol(mvdt, value)
+		return
+
+	case StatementDistribution:
+		setValidationProtocol(mvdt, value)
+		return
+
+	case ApprovalDistribution:
+		setValidationProtocol(mvdt, value)
+		return
+
+	default:
+		return fmt.Errorf("unsupported type")
+	}
+}
+
+func (mvdt ValidationProtocol) IndexValue() (index uint, value any, err error) {
+	switch mvdt.inner.(type) {
+	case BitfieldDistribution:
+		return 1, mvdt.inner, nil
+
+	case StatementDistribution:
+		return 3, mvdt.inner, nil
+
+	case ApprovalDistribution:
+		return 4, mvdt.inner, nil
+
+	}
+	return 0, nil, scale.ErrUnsupportedVaryingDataTypeValue
+}
+
+func (mvdt ValidationProtocol) Value() (value any, err error) {
+	_, value, err = mvdt.IndexValue()
+	return
+}
+
+func (mvdt ValidationProtocol) ValueAt(index uint) (value any, err error) {
+	switch index {
+	case 1:
+		return *new(BitfieldDistribution), nil
+
+	case 3:
+		return *new(StatementDistribution), nil
+
+	case 4:
+		return *new(ApprovalDistribution), nil
+
+	}
+	return nil, scale.ErrUnknownVaryingDataTypeValue
+}
+
+// NewValidationProtocolVDT constructor or ValidationProtocol VaryingDataType
+func NewValidationProtocolVDT() ValidationProtocol {
+	return ValidationProtocol{}
 }
 
 // Type returns ValidationMsgType
-func (*ValidationProtocolV1) Type() network.MessageType {
+func (*ValidationProtocol) Type() network.MessageType {
 	return network.ValidationMsgType
 }
 
 // Hash returns the hash of the CollationProtocolV1
-func (vp *ValidationProtocolV1) Hash() (common.Hash, error) {
+func (vp *ValidationProtocol) Hash() (common.Hash, error) {
 	encMsg, err := vp.Encode()
 	if err != nil {
 		return common.Hash{}, fmt.Errorf("cannot encode message: %w", err)
@@ -34,7 +189,7 @@ func (vp *ValidationProtocolV1) Hash() (common.Hash, error) {
 }
 
 // Encode a collator protocol message using scale encode
-func (vp *ValidationProtocolV1) Encode() ([]byte, error) {
+func (vp *ValidationProtocol) Encode() ([]byte, error) {
 	enc, err := scale.Marshal(*vp)
 	if err != nil {
 		return enc, err
@@ -43,7 +198,7 @@ func (vp *ValidationProtocolV1) Encode() ([]byte, error) {
 }
 
 func decodeValidationMessage(in []byte) (network.NotificationsMessage, error) {
-	validationMessage := ValidationProtocolV1{}
+	validationMessage := ValidationProtocol{}
 
 	err := scale.Unmarshal(in, &validationMessage)
 	if err != nil {
