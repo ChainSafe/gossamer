bb1e6fb51e9f2a7dc792f6d9653f38cd53cb12ce
edwardmack <ed@edwardmack.com>
2023-08-21 09:56:07 -0400

    add to overseer struct

--- dot/parachain/overseer.go
+++ dot/parachain/overseer.go
@@ -1,9 +1,38 @@
 package parachain
 
+import "fmt"
+
 type Overseer struct {
-	//subsystems map[string]Subsystem
-	channels map[Subsystem]chan SubsystemMessage
+	subsystems []Subsystem
+	channels   map[Subsystem]chan SubsystemMessage
 }
+
+func NewOverseer() *Overseer {
+	overseer := &Overseer{
+		subsystems: nil,
+		channels:   make(map[Subsystem]chan SubsystemMessage),
+	}
+	return overseer
+}
+
+func (o *Overseer) start() {
+	fmt.Printf("Overseer started\n")
+}
+func (o *Overseer) stop() {
+	fmt.Printf("Overseer stopped\n")
+}
+func (o *Overseer) startSubsystems() {
+	for _, subsystem := range o.subsystems {
+		subsystem.Start()
+	}
+}
+
+func (o *Overseer) stopSubsystems() {
+	for _, subsystem := range o.subsystems {
+		subsystem.Stop()
+	}
+}
+
 type messageType int
 
 const (
@@ -19,12 +48,15 @@ type SubsystemMessage struct {
 }
 
 type Subsystem interface {
+	Start()
+	Stop()
 	SendMessage(message SubsystemMessage)
 	ProcessMessage()
 	ReceiveMessage() SubsystemMessage // consider chan here for receiving
 }
 
 func (o *Overseer) RegisterSubSystem(subsystem Subsystem) {
+	o.subsystems = append(o.subsystems, subsystem)
 	//todo: create chan for subsystems here (test)
 	// store map of channels
 	channel := make(chan SubsystemMessage)
@@ -38,5 +70,4 @@ func (o *Overseer) RegisterSubSystem(subsystem Subsystem) {
 			}
 		}
 	}()
-
 }
--- dot/parachain/overseer_test.go
+++ dot/parachain/overseer_test.go
@@ -1,1 +1,42 @@
 package parachain
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type ExampleSubsystem1 struct {
+	name string
+}
+
+func (e *ExampleSubsystem1) Start() {
+	fmt.Printf("%v started\n", e.name)
+}
+func (e *ExampleSubsystem1) Stop() {
+	fmt.Printf("%v stopped\n", e.name)
+}
+func (e *ExampleSubsystem1) SendMessage(message SubsystemMessage) {
+}
+
+func (e *ExampleSubsystem1) ProcessMessage() {
+}
+
+func (e *ExampleSubsystem1) ReceiveMessage() SubsystemMessage {
+	return SubsystemMessage{}
+}
+
+func TestStartSubsystems(t *testing.T) {
+	overseer := NewOverseer()
+
+	ss1 := &ExampleSubsystem1{
+		name: "subSystem 1",
+	}
+	overseer.RegisterSubSystem(ss1)
+	overseer.start()
+	overseer.startSubsystems()
+
+	time.Sleep(5 * time.Second)
+	overseer.stopSubsystems()
+	overseer.stop()
+}
