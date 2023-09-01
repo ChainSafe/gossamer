9d458d5f0738c92c0fe21f76d54f5e645a08efb1
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2023-09-01 10:57:19 -0400

    chore: remove duplicated imports

--- dot/network/discovery.go
+++ dot/network/discovery.go
@@ -18,7 +18,6 @@ import (
 	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/libp2p/go-libp2p/p2p/discovery/routing"
 	"github.com/multiformats/go-multiaddr"
-	ma "github.com/multiformats/go-multiaddr"
 )
 
 const (
@@ -111,7 +110,7 @@ func (d *discovery) start() error {
 		dual.DHTOption(kaddht.V1ProtocolOverride(d.pid + "/kad")),
 		dual.DHTOption(kaddht.Mode(kaddht.ModeAutoServer)),
 		dual.DHTOption(kaddht.AddressFilter(func(as []multiaddr.Multiaddr) []multiaddr.Multiaddr {
-			var addrs []ma.Multiaddr
+			var addrs []multiaddr.Multiaddr
 			for _, addr := range as {
 				if !privateIPs.AddrBlocked(addr) {
 					addrs = append(addrs, addr)
