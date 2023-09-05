ab13de12f1a4c4fa22bcfe2718a4ca99b2f1e696
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2023-09-05 08:52:02 -0400

    chore: fix mocks

--- dot/sync/interfaces.go
+++ dot/sync/interfaces.go
@@ -40,7 +40,6 @@ type BlockState interface {
 	GetHeaderByNumber(num uint) (*types.Header, error)
 	GetAllBlocksAtNumber(num uint) ([]common.Hash, error)
 	IsDescendantOf(parent, child common.Hash) (bool, error)
-	Checkpoint(header *types.Header)
 }
 
 // StorageState is the interface for the storage state
