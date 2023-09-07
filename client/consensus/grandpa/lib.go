ec8bb449bfd25edab0482b95b42febcd8715abbb
Timothy Wu <tim.wu@chainsafe.io>
2023-09-07 17:54:18 -0400

    wip

--- client/consensus/grandpa/lib.go
+++ client/consensus/grandpa/lib.go
@@ -6,7 +6,9 @@ package grandpa
 import (
 	"time"
 
+	"github.com/ChainSafe/gossamer/client/network/config"
 	"github.com/ChainSafe/gossamer/internal/log"
+	"github.com/ChainSafe/gossamer/keystore"
 	"github.com/ChainSafe/gossamer/lib/crypto/ed25519"
 	grandpa "github.com/ChainSafe/gossamer/pkg/finality-grandpa"
 	"golang.org/x/exp/constraints"
@@ -33,7 +35,27 @@ type ClientForGrandpa interface{}
 type Backend interface{}
 
 type Config struct {
+	/// The expected duration for a message to be gossiped across the network.
 	GossipDuration time.Duration
+	/// Justification generation period (in blocks). GRANDPA will try to generate justifications
+	/// at least every justification_period blocks. There are some other events which might cause
+	/// justification generation.
+	JustificationPeriod uint32
+	/// Whether the GRANDPA observer protocol is live on the network and thereby
+	/// a full-node not running as a validator is running the GRANDPA observer
+	/// protocol (we will only issue catch-up requests to authorities when the
+	/// observer protocol is enabled).
+	ObserverEnabled bool
+	/// The role of the local node (i.e. authority, full-node or light).
+	LocalRole config.Role
+	/// Some local identifier of the voter.
+	Name *string
+	/// The keystore that manages the keys of this node.
+	KeyStore *keystore.KeyStore
+	/// TelemetryHandle instance.
+	Telemetry *TelemetryHandle
+	/// Chain specific GRANDPA protocol name. See [`crate::protocol_standard_name`].
+	ProtocolName ProtocolName
 }
 
 type VoterWork[Hash constraints.Ordered, Number constraints.Unsigned, Signature comparable, ID constraints.Ordered] struct {
@@ -49,7 +71,15 @@ type VoterWork[Hash constraints.Ordered, Number constraints.Unsigned, Signature 
 func NewVoterWork[Hash constraints.Ordered, Number constraints.Unsigned, Signature comparable, ID constraints.Ordered](
 	client ClientForGrandpa,
 	config Config,
-
+	network NetworkBridge,
+	selectChain SelectChain,
+	votingRule VotingRule,
+	persistendData persistentData,
+	voterCommandsRX any,
+	prometheusRegistry any,
+	sharedVoterState SharedVoterState,
+	JustificationSender GrandpaJustificationSender,
+	telemetry TelemetryHandle,
 ) *VoterWork[Hash, Number, Signature, ID] {
 	// grandpa.NewVoter[]()
 	return nil
