a888b6627ef7c7d921b28db3832b17160108aac6
Diego <diego2737@gmail.com>
2023-09-28 11:43:16 +0200

    Add triedb tests

--- dot/state/triedb.go
+++ dot/state/triedb.go
@@ -9,13 +9,14 @@ import (
 	"github.com/ChainSafe/gossamer/lib/trie"
 )
 
+// TrieDB is a wrapper around a database.Table that stores tries and keep a cache of them in memory
+// db is a database.Table that stores the tries to prevent colissions with keys in the same DB
 type TrieDB struct {
 	db    database.Table
 	tries *tries
 }
 
 // NewTrieDB creates a new TrieDB
-// db is expected to be a table of the database, see `database.NewTable` for more details
 func NewTrieDB(db database.Table) *TrieDB {
 	return &TrieDB{
 		db:    db,
@@ -23,19 +24,18 @@ func NewTrieDB(db database.Table) *TrieDB {
 	}
 }
 
-func (tdb *TrieDB) deleteCached(root common.Hash) {
-	tdb.tries.delete(root)
-}
-
+// Delete deletes a trie from the database
 func (tdb *TrieDB) Delete(root common.Hash) error {
 	tdb.tries.delete(root)
 	return tdb.db.Del(root.ToBytes())
 }
 
+// Put stores a dirty trie in the database
 func (tdb *TrieDB) Put(t *trie.Trie) error {
 	return t.WriteDirty(tdb.db)
 }
 
+// Get returns the trie with the given root
 func (tdb *TrieDB) Get(root common.Hash) (*trie.Trie, error) {
 	// Get trie from memory
 	t := tdb.tries.get(root)
@@ -54,6 +54,22 @@ func (tdb *TrieDB) Get(root common.Hash) (*trie.Trie, error) {
 	return t, nil
 }
 
+// GetKey returns the value for the given key in the trie with the given root
+// TODO: I add this function to keep the compatibility but I think the caller should be responsible for getting
+// the key from the trie.
+func (tdb *TrieDB) GetKey(root common.Hash, key []byte) ([]byte, error) {
+	t, err := tdb.Get(root)
+	if err != nil {
+		return nil, err
+	}
+
+	return t.Get(key), nil
+}
+
+func (tdb *TrieDB) deleteCached(root common.Hash) {
+	tdb.tries.delete(root)
+}
+
 func (tdb *TrieDB) getFromDB(root common.Hash) (*trie.Trie, error) {
 	t := trie.NewEmptyTrie()
 	err := t.Load(tdb.db, root)
@@ -62,13 +78,4 @@ func (tdb *TrieDB) getFromDB(root common.Hash) (*trie.Trie, error) {
 	}
 
 	return t, nil
-}
-
-func (tdb *TrieDB) GetKey(root common.Hash, key []byte) ([]byte, error) {
-	t, err := tdb.Get(root)
-	if err != nil {
-		return nil, err
-	}
-
-	return t.Get(key), nil
 }
---
+++ dot/state/triedb_test.go
@@ -0,0 +1,213 @@
+package state
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/ChainSafe/gossamer/internal/database"
+	"github.com/ChainSafe/gossamer/internal/trie/node"
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/trie"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewTrieDB(t *testing.T) {
+	t.Parallel()
+
+	db := NewInMemoryDB(t)
+	trieDB := NewTrieDB(db)
+
+	expected := &TrieDB{
+		db: db,
+		tries: &tries{
+			rootToTrie:    map[common.Hash]*trie.Trie{},
+			triesGauge:    triesGauge,
+			setCounter:    setCounter,
+			deleteCounter: deleteCounter,
+		},
+	}
+
+	assert.Equal(t, expected, trieDB)
+}
+
+func Test_Put(t *testing.T) {
+	t.Parallel()
+
+	testNode := &trie.Node{
+		PartialKey:   []byte{0},
+		StorageValue: []byte{17},
+		Dirty:        true,
+	}
+
+	testCases := map[string]struct {
+		trie    *trie.Trie
+		encoded []byte
+		success bool
+		err     string
+	}{
+		"dirty_trie_should_be_stored_encoded": {
+			trie: trie.NewTrie(testNode, nil),
+			encoded: func() []byte {
+				encoded := bytes.NewBuffer(nil)
+				err := testNode.Encode(encoded)
+				assert.NoError(t, err)
+
+				return encoded.Bytes()
+			}(),
+			success: true,
+			err:     "",
+		},
+		"do_not_store_not_dirty_nodes": {
+			trie: func() *trie.Trie {
+				notDirty := testNode.Copy(node.DeepCopySettings)
+				notDirty.Dirty = false
+				return trie.NewTrie(notDirty, nil)
+			}(),
+			encoded: func() []byte {
+				encoded := bytes.NewBuffer(nil)
+				err := testNode.Encode(encoded)
+				assert.NoError(t, err)
+
+				return encoded.Bytes()
+			}(),
+			success: false,
+			err:     "not found",
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCase := testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			db := NewInMemoryDB(t)
+			table := database.NewTable(db, "storage")
+			trieDB := NewTrieDB(table)
+
+			err := trieDB.Put(testCase.trie)
+			assert.NoError(t, err)
+
+			trieFromDB, err := table.Get(testCase.trie.MustHash().ToBytes())
+			if testCase.success {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.encoded, trieFromDB)
+			} else {
+				assert.ErrorContains(t, err, testCase.err)
+			}
+		})
+	}
+}
+
+func Test_GetDecodeTrieAndRefreshCache(t *testing.T) {
+	t.Parallel()
+
+	// Create trie DB using in memory db table
+	db := NewInMemoryDB(t)
+	table := database.NewTable(db, "storage")
+	trieDB := NewTrieDB(table)
+
+	// Build trie with test root node
+	root := &trie.Node{
+		PartialKey:   []byte{0},
+		StorageValue: []byte{17},
+		Dirty:        true,
+	}
+	testTrie := trie.NewTrie(root, table)
+
+	// Encode trie to check later
+	encoded := bytes.NewBuffer(nil)
+	err := root.Encode(encoded)
+	assert.NoError(t, err)
+
+	// Store trie in trieDB
+	err = trieDB.Put(testTrie)
+	assert.NoError(t, err)
+
+	// Cache should be empty
+	assert.Len(t, trieDB.tries.rootToTrie, 0)
+
+	// Get trie from trieDB table and check if it matches the encoded trie
+	trieFromTrieDB, err := trieDB.Get(testTrie.MustHash())
+	assert.NoError(t, err)
+	assert.Equal(t, testTrie.String(), trieFromTrieDB.String())
+
+	// Trie should be added to cache
+	assert.Len(t, trieDB.tries.rootToTrie, 1)
+
+	// Get from cache
+	fromCache := trieDB.tries.get(testTrie.MustHash())
+	assert.Equal(t, testTrie.String(), fromCache.String())
+}
+
+func Test_GetDeletedTrieFromDBShouldReturnError(t *testing.T) {
+	t.Parallel()
+
+	// Create trie DB using in memory db table
+	db := NewInMemoryDB(t)
+	table := database.NewTable(db, "storage")
+	trieDB := NewTrieDB(table)
+
+	// Build trie with test root node
+	root := &trie.Node{
+		PartialKey:   []byte{0},
+		StorageValue: []byte{17},
+		Dirty:        true,
+	}
+	testTrie := trie.NewTrie(root, table)
+
+	// Encode trie to check later
+	encoded := bytes.NewBuffer(nil)
+	err := root.Encode(encoded)
+	assert.NoError(t, err)
+
+	// Store trie in trieDB
+	err = trieDB.Put(testTrie)
+	assert.NoError(t, err)
+
+	// Get trie from trieDB table and check if it matches the encoded trie
+	trieFromTrieDB, err := trieDB.Get(testTrie.MustHash())
+	assert.NoError(t, err)
+	assert.Equal(t, testTrie.String(), trieFromTrieDB.String())
+
+	// Delete trie and try to get it again should return an error
+	err = trieDB.Delete(testTrie.MustHash())
+	assert.NoError(t, err)
+
+	_, err = trieDB.Get(testTrie.MustHash())
+	assert.ErrorContains(t, err, "not found")
+}
+
+func Test_GetKeyFromTrie(t *testing.T) {
+	t.Parallel()
+
+	testKey := []byte("testKey")
+	testValue := []byte("testValue")
+
+	// Create trie DB using in memory db table
+	db := NewInMemoryDB(t)
+	table := database.NewTable(db, "storage")
+	trieDB := NewTrieDB(table)
+
+	// Build trie with test root node
+	root := &trie.Node{
+		PartialKey:   []byte{0},
+		StorageValue: []byte{17},
+		Dirty:        true,
+	}
+	testTrie := trie.NewTrie(root, table)
+	testTrie.Put(testKey, testValue)
+
+	// Encode trie to check later
+	encoded := bytes.NewBuffer(nil)
+	err := root.Encode(encoded)
+	assert.NoError(t, err)
+
+	// Store trie in trieDB
+	err = trieDB.Put(testTrie)
+	assert.NoError(t, err)
+
+	// Get trie from trieDB table and check if it matches the encoded trie
+	valueFromTrie, err := trieDB.GetKey(testTrie.MustHash(), testKey)
+	assert.NoError(t, err)
+	assert.Equal(t, testValue, valueFromTrie)
+}
