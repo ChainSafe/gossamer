cad06b4d0b8255d12cb7f6e22dd510581a41eee3
Timothy Wu <tim.wu@chainsafe.io>
2023-09-29 15:35:36 -0400

    introduce Logger and SetLogger(), update logging TODOs

---
+++ pkg/finality-grandpa/logger.go
@@ -0,0 +1,58 @@
+package grandpa
+
+type logLevel int
+
+const (
+	debug logLevel = iota
+	trace
+)
+
+type Logger interface {
+	Warn(l string)
+	Warnf(format string, values ...any)
+
+	Info(l string)
+	Infof(format string, values ...any)
+
+	Debug(l string)
+	Debugf(format string, values ...any)
+
+	Trace(l string)
+	Tracef(format string, values ...any)
+}
+
+type noopLogger struct{}
+
+func (nl noopLogger) Warn(l string) {
+}
+
+func (nl noopLogger) Warnf(format string, values ...any) {
+}
+
+func (nl noopLogger) Info(l string) {
+}
+
+func (nl noopLogger) Infof(format string, values ...any) {
+}
+
+func (nl noopLogger) Debug(l string) {
+}
+
+func (nl noopLogger) Debugf(format string, values ...any) {
+}
+
+func (nl noopLogger) Trace(l string) {
+}
+
+func (nl noopLogger) Tracef(format string, values ...any) {
+}
+
+var log Logger
+
+func init() {
+	log = noopLogger{}
+}
+
+func SetLogger(l Logger) {
+	log = l
+}
