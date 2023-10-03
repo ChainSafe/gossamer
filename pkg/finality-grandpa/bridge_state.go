a377763f07f5f7bea043a1011fcc98a786c91b18
jimboj <jamesdjohnson218@gmail.com>
2023-10-03 15:50:14 -0600

    clean up changes

--- pkg/finality-grandpa/bridge_state.go
+++ pkg/finality-grandpa/bridge_state.go
@@ -67,7 +67,7 @@ type priorView[Hash, Number any] struct {
 	bridged *bridged[Hash, Number]
 }
 
-// push an update to the latter view.
+// Push an update to the latter view.
 func (pv *priorView[H, N]) update(new RoundState[H, N]) { //skipcq: RVV-B0001
 	pv.bridged.update(new)
 }
