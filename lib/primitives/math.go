044afbe7f354faeb3fa5ca9b73f5fe8cb2d29fc9
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2023-10-04 16:27:58 -0400

    chore: avoid code repetition

--- lib/primitives/math.go
+++ lib/primitives/math.go
@@ -10,8 +10,12 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-// SaturatingAdd computes a + b saturating at the numeric bounds instead of overflowing
-func SaturatingAdd[T constraints.Integer](a, b T) T {
+// saturatingOperations applies the correct operation
+// given the input types
+func saturatingOperations[T constraints.Integer](a, b T,
+	signedSaturatingOperation func(T, T, T, T) T,
+	unsignedSaturatingOperation func(T, T) T,
+) T {
 	switch any(a).(type) {
 	case int, int8, int16, int32, int64:
 		sizeOf := (unsafe.Sizeof(a) * 8) - 1
@@ -21,14 +25,19 @@ func SaturatingAdd[T constraints.Integer](a, b T) T {
 			minValueOfSignedType T = ^maxValueOfSignedType
 		)
 
-		return saturatingAddSigned(a, b, maxValueOfSignedType, minValueOfSignedType)
+		return signedSaturatingOperation(a, b, maxValueOfSignedType, minValueOfSignedType)
 	case uint, uint8, uint16, uint32, uint64, uintptr:
 		// the operation ^T(0) gives us the max value of type T
 		// eg. if T is uint8 then it gives us 255
-		return saturatingAddUnsigned(a, b, ^T(0))
+		return unsignedSaturatingOperation(a, b)
 	}
 
 	panic(fmt.Sprintf("type %T not supported while performing SaturatingAdd", a))
+}
+
+// SaturatingAdd computes a + b saturating at the numeric bounds instead of overflowing
+func SaturatingAdd[T constraints.Integer](a, b T) T {
+	return saturatingOperations(a, b, saturatingAddSigned, saturatingAddUnsigned)
 }
 
 func saturatingAddSigned[T constraints.Integer](a, b, max, min T) T {
@@ -43,7 +52,11 @@ func saturatingAddSigned[T constraints.Integer](a, b, max, min T) T {
 	return a + b
 }
 
-func saturatingAddUnsigned[T constraints.Integer](a, b, max T) T {
+func saturatingAddUnsigned[T constraints.Integer](a, b T) T {
+	// the operation ^T(0) gives us the max value of type T
+	// eg. if T is uint8 then it gives us 255
+	max := ^T(0)
+
 	if a > max-b {
 		return max
 	}
@@ -52,23 +65,7 @@ func saturatingAddUnsigned[T constraints.Integer](a, b, max T) T {
 
 // SaturatingSub computes a - b saturating at the numeric bounds instead of overflowing
 func SaturatingSub[T constraints.Integer](a, b T) T {
-	switch any(a).(type) {
-	case int, int8, int16, int32, int64:
-		sizeOf := (unsafe.Sizeof(a) * 8) - 1
-
-		var (
-			maxValueOfSignedType T = 1<<sizeOf - 1
-			minValueOfSignedType T = ^maxValueOfSignedType
-		)
-
-		return saturatingSubSigned(a, b, maxValueOfSignedType, minValueOfSignedType)
-	case uint, uint8, uint16, uint32, uint64, uintptr:
-		// the operation ^T(0) gives us the max value of type T
-		// eg. if T is uint8 then it gives us 255
-		return saturatingSubUnsigned(a, b)
-	}
-
-	panic(fmt.Sprintf("type %T not supported while performing SaturatingSub", a))
+	return saturatingOperations(a, b, saturatingSubSigned, saturatingSubUnsigned)
 }
 
 func saturatingSubSigned[T constraints.Integer](a, b, max, min T) T {
