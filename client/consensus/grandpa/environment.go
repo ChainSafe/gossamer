ef183e3737783de7f00a46fa68bf85dda5a414f2
jimboj <jamesdjohnson218@gmail.com>
2023-10-12 13:19:27 -0600

    fix loadDecoded

--- client/consensus/grandpa/environment.go
+++ client/consensus/grandpa/environment.go
@@ -249,7 +249,6 @@ func (tve *voterSetState[H, N, ID, Sig]) Value() (val scale.VaryingDataTypeValue
 
 // New Creates a new voterSetState
 func (tve voterSetState[H, N, ID, Sig]) New() voterSetState[H, N, ID, Sig] {
-	fmt.Println("--- New VoterSetState ---------")
 	vdt, err := scale.NewVaryingDataType(voterSetStateLive[H, N, ID, Sig]{
 		CompletedRounds: completedRounds[H, N, ID, Sig]{
 			Rounds: make([]completedRound[H, N, ID, Sig], 0, numLastCompletedRounds),
