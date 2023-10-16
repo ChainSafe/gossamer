f6f7371f4e064de4bd33b6b1b664951489a7b82c
edwardmack <ed@edwardmack.com>
2023-10-16 17:24:24 -0400

    add db availableData store

--- dot/parachain/availability-store/availabilitystore_test.go
+++ dot/parachain/availability-store/availabilitystore_test.go
@@ -1,54 +1,32 @@
 package availability_store
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
 
+	parachaintypes "github.com/ChainSafe/gossamer/dot/parachain/types"
 	"github.com/ChainSafe/gossamer/lib/common"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAvailabilityStore_LoadAvailableData(t *testing.T) {
-	basePath := t.TempDir()
-	type args struct {
-		candidate common.Hash
-	}
-	tests := map[string]struct {
-		args    args
-		want    AvailableData
-		wantErr bool
-	}{
-		"base": {
-			args:    args{candidate: common.Hash{0x01}},
-			want:    AvailableData{},
-			wantErr: false,
-		},
-	}
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			as, err := NewAvailabilityStore(Config{basepath: basePath})
-			require.NoError(t, err)
-
-			got, err := as.LoadAvailableData(tt.args.candidate)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("LoadAvailableData() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("LoadAvailableData() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestAvailabilityStore_LoadAvailableData2(t *testing.T) {
+func TestAvailabilityStore_StoreLoadAvailableData(t *testing.T) {
 	basePath := t.TempDir()
 	as, err := NewAvailabilityStore(Config{basepath: basePath})
-	as.db.Put(common.Hash{0x01}.ToBytes(), []byte("test"))
+	require.NoError(t, err)
+
+	availabeData := AvailableData{
+		PoV: parachaintypes.PoV{BlockData: []byte("blockdata")},
+		ValidationData: parachaintypes.PersistedValidationData{
+			ParentHead: parachaintypes.HeadData{Data: []byte("parentHead")},
+		},
+	}
+	err = as.StoreAvailableData(common.Hash{0x01}, availabeData)
 	require.NoError(t, err)
 
 	got, err := as.LoadAvailableData(common.Hash{0x01})
 	require.NoError(t, err)
-	fmt.Printf("got: %v\n", got)
+	require.Equal(t, availabeData, got)
+
+	got, err = as.LoadAvailableData(common.Hash{0x02})
+	require.EqualError(t, err, "pebble: not found")
+	require.Equal(t, AvailableData{}, got)
 }
