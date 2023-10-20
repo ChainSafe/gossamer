4fb70f3ebee6721fa00745edd7ef0dc912bd8cf9
Kishan Sagathiya <kishansagathiya@gmail.com>
2023-10-20 22:49:40 +0530

    feat(parachain/overseer): added message forwarding in overseer (#3546)
    - Made changes to overseer to support processing messages coming from subsystems
    - Added logic to process those messages

---
+++ dot/parachain/types/subsystem_names.go
@@ -0,0 +1,9 @@
+package parachaintypes
+
+type SubSystemName string
+
+const (
+	CandidateBacking  SubSystemName = "CandidateBacking"
+	CollationProtocol SubSystemName = "CollationProtocol"
+	AvailabilityStore SubSystemName = "AvailabilityStore"
+)
