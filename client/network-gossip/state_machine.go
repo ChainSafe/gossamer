d7f6898c1bacd1a2beaf4325c8960ccb05cf0677
Timothy Wu <tim.wu@chainsafe.io>
2023-10-21 01:43:02 -0400

    more wip

--- client/network-gossip/state_machine.go
+++ client/network-gossip/state_machine.go
@@ -1,15 +1,54 @@
 package gossip
 
-/// Consensus network protocol handler. Manages statements and candidate requests.
-// pub struct ConsensusGossip<B: BlockT> {
-// 	peers: HashMap<PeerId, PeerConsensus<B::Hash>>,
-// 	messages: Vec<MessageEntry<B>>,
-// 	known_messages: LruCache<B::Hash, ()>,
-// 	protocol: ProtocolName,
-// 	validator: Arc<dyn Validator<B>>,
-// 	next_broadcast: Instant,
-// 	metrics: Option<Metrics>,
-// }
+import (
+	"time"
 
-type ConsensusGossip struct {
+	"github.com/ChainSafe/gossamer/client/network"
+	"github.com/ChainSafe/gossamer/primitives/runtime"
+	"github.com/libp2p/go-libp2p/core"
+	libp2p "github.com/libp2p/go-libp2p/core"
+)
+
+type PeerConsensus[H comparable] struct {
+	knownMessages map[H]any
 }
+
+// / Topic stream message with sender.
+type TopicNotification struct {
+	/// Message data.
+	Message []byte
+	/// Sender if available.
+	Sender *libp2p.PeerID
+}
+
+//	struct MessageEntry<B: BlockT> {
+//		message_hash: B::Hash,
+//		topic: B::Hash,
+//		message: Vec<u8>,
+//		sender: Option<PeerId>,
+//	}
+type MessageEntry[H runtime.Hash] struct {
+	messageHash H
+	topic       H
+	message     []byte
+	sender      *libp2p.PeerID
+}
+
+// / Consensus network protocol handler. Manages statements and candidate requests.
+type ConsensusGossip[H runtime.Hash] struct {
+	peers    map[core.PeerID]PeerConsensus[H]
+	messages []MessageEntry[H]
+	// TODO: known_messages: LruCache<B::Hash, ()>,
+	knownMessages map[H]any
+	protocol      network.ProtocolName
+	validator     Validator[H]
+	// next_broadcast: Instant,
+	nextBroadcast time.Time
+	metrics       metrics
+}
+
+//	struct Metrics {
+//		registered_messages: Counter<U64>,
+//		expired_messages: Counter<U64>,
+//	}
+type metrics struct{}
