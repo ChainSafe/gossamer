5549e9f77cbfcf11889738ef1612970cfb925766
axaysagathiya <axaysagathiya@gmail.com>
2023-11-29 21:42:52 +0530

    implement misbehaviour enum using interface

--- dot/parachain/types/subsystems.go
+++ dot/parachain/types/subsystems.go
@@ -29,6 +29,14 @@ var (
 	_ RuntimeApiRequest            = (*RARValidationCodeByHash)(nil)
 	_ CandidateValidationMessage   = (*CVMValidateFromExhaustive)(nil)
 	_ AvailabilityStoreMessage     = (*ASMStoreAvailableData)(nil)
+	_ Misbehaviour                 = (*IssuedAndValidity)(nil)
+	_ ValidityDoubleVote           = (*IssuedAndValidity)(nil)
+	_ Misbehaviour                 = (*MultipleCandidates)(nil)
+	_ Misbehaviour                 = (*UnauthorizedStatement)(nil)
+	_ Misbehaviour                 = (*OnSeconded)(nil)
+	_ DoubleSign                   = (*OnSeconded)(nil)
+	_ Misbehaviour                 = (*OnValidity)(nil)
+	_ DoubleSign                   = (*OnValidity)(nil)
 )
 
 // OverseerFuncRes is a result of an overseer function
@@ -71,9 +79,84 @@ type PDMisbehaviorReport struct {
 func (PDMisbehaviorReport) IsProvisionableData() {}
 
 // Misbehaviour is intended to represent different kinds of misbehaviour along with supporting proofs.
-//
-// TODO: implement this #3601
-type Misbehaviour interface{}
+type Misbehaviour interface {
+	IsMisbehaviour()
+}
+
+// ValidityDoubleVote misbehavior: voting more than one way on candidate validity.
+// Since there are three possible ways to vote, a double vote is possible in
+// three possible combinations (unordered)
+type ValidityDoubleVote interface {
+	Misbehaviour
+	IsValidityDoubleVote()
+}
+
+// IssuedAndValidity represents an implicit vote by issuing and explicit voting for validity.
+type IssuedAndValidity struct {
+	CommittedCandidateReceiptAndSign CommittedCandidateReceiptAndSign
+	CandidateHashAndSign             struct {
+		CandidateHash CandidateHash
+		Signature     ValidatorSignature
+	}
+}
+
+func (IssuedAndValidity) IsMisbehaviour()       {}
+func (IssuedAndValidity) IsValidityDoubleVote() {}
+
+// CommittedCandidateReceiptAndSign combines a committed candidate receipt and its associated signature.
+type CommittedCandidateReceiptAndSign struct {
+	CommittedCandidateReceipt CommittedCandidateReceipt
+	Signature                 ValidatorSignature
+}
+
+// MultipleCandidates misbehavior: declaring multiple candidates.
+type MultipleCandidates struct {
+	First  CommittedCandidateReceiptAndSign
+	Second CommittedCandidateReceiptAndSign
+}
+
+func (MultipleCandidates) IsMisbehaviour() {}
+
+// SignedStatement represents signed statements about candidates.
+type SignedStatement struct {
+	Statement StatementVDT       `scale:"1"`
+	Signature ValidatorSignature `scale:"2"`
+	Sender    ValidatorIndex     `scale:"3"`
+}
+
+// UnauthorizedStatement misbehavior: submitted statement for wrong group.
+type UnauthorizedStatement struct {
+	// A signed statement which was submitted without proper authority.
+	Statement SignedStatement
+}
+
+func (UnauthorizedStatement) IsMisbehaviour() {}
+
+// DoubleSign misbehavior: multiple signatures on same statement.
+type DoubleSign interface {
+	Misbehaviour
+	IsDoubleSign()
+}
+
+// OnSeconded represents a double sign on a candidate.
+type OnSeconded struct {
+	Candidate CommittedCandidateReceipt
+	Sign1     ValidatorSignature
+	Sign2     ValidatorSignature
+}
+
+func (OnSeconded) IsMisbehaviour() {}
+func (OnSeconded) IsDoubleSign()   {}
+
+// OnValidity represents a double sign on validity.
+type OnValidity struct {
+	CandidateHash CandidateHash
+	Sign1         ValidatorSignature
+	Sign2         ValidatorSignature
+}
+
+func (OnValidity) IsMisbehaviour() {}
+func (OnValidity) IsDoubleSign()   {}
 
 // StatementDistributionMessage is a message to the Statement Distribution subsystem.
 type StatementDistributionMessage interface {
