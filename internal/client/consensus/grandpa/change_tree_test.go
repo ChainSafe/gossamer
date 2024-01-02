653eb18d3c78f1619958f0e7f087edcc05978250
Timothy Wu <tim.wu@chainsafe.io>
2024-01-02 13:31:57 -0500

    fix aux_schema change_tree tests wip

--- internal/client/consensus/grandpa/change_tree_test.go
+++ internal/client/consensus/grandpa/change_tree_test.go
@@ -3,77 +3,83 @@
 
 package grandpa
 
-// func TestSwapRemove(t *testing.T) {
-// 	change1 := &PendingChange[string, uint, dummyAuthID]{
-// 		CanonHash: "a",
-// 	}
+import (
+	"testing"
 
-// 	change2 := &PendingChange[string, uint, dummyAuthID]{
-// 		CanonHash: "b",
-// 	}
+	"github.com/stretchr/testify/require"
+)
 
-// 	change3 := &PendingChange[string, uint, dummyAuthID]{
-// 		CanonHash: "c",
-// 	}
+func TestSwapRemove(t *testing.T) {
+	change1 := &PendingChange[string, uint]{
+		CanonHash: "a",
+	}
 
-// 	pendingChangeNode1 := &PendingChangeNode[string, uint, dummyAuthID]{
-// 		Change: change1,
-// 	}
+	change2 := &PendingChange[string, uint]{
+		CanonHash: "b",
+	}
 
-// 	pendingChangeNode2 := &PendingChangeNode[string, uint, dummyAuthID]{
-// 		Change: change2,
-// 	}
+	change3 := &PendingChange[string, uint]{
+		CanonHash: "c",
+	}
 
-// 	pendingChangeNode3 := &PendingChangeNode[string, uint, dummyAuthID]{
-// 		Change: change3,
-// 	}
+	pendingChangeNode1 := &PendingChangeNode[string, uint]{
+		Change: change1,
+	}
 
-// 	changeNodes1 := []*PendingChangeNode[string, uint, dummyAuthID]{
-// 		pendingChangeNode1,
-// 		pendingChangeNode2,
-// 	}
+	pendingChangeNode2 := &PendingChangeNode[string, uint]{
+		Change: change2,
+	}
 
-// 	changeNodes2 := []*PendingChangeNode[string, uint, dummyAuthID]{
-// 		pendingChangeNode1,
-// 		pendingChangeNode2,
-// 		pendingChangeNode3,
-// 	}
-// 	type args struct {
-// 		ct    ChangeTree[string, uint, dummyAuthID]
-// 		index uint
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		exp  PendingChangeNode[string, uint, dummyAuthID]
-// 	}{
-// 		{
-// 			name: "2ElemSliceDeletingLastElement",
-// 			args: args{
-// 				ct: ChangeTree[string, uint, dummyAuthID]{
-// 					TreeRoots: changeNodes1,
-// 				},
-// 				index: 1,
-// 			},
-// 			exp: *pendingChangeNode2,
-// 		},
-// 		{
-// 			name: "3ElemSliceDeletingFirstElement",
-// 			args: args{
-// 				ct: ChangeTree[string, uint, dummyAuthID]{
-// 					TreeRoots: changeNodes2,
-// 				},
-// 				index: 0,
-// 			},
-// 			exp: *pendingChangeNode1,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			oldLength := len(tt.args.ct.Roots())
-// 			removedVal := tt.args.ct.swapRemove(tt.args.ct.Roots(), tt.args.index)
-// 			require.Equal(t, tt.exp, removedVal)
-// 			require.Equal(t, oldLength-1, len(tt.args.ct.Roots()))
-// 		})
-// 	}
-// }
+	pendingChangeNode3 := &PendingChangeNode[string, uint]{
+		Change: change3,
+	}
+
+	changeNodes1 := []*PendingChangeNode[string, uint]{
+		pendingChangeNode1,
+		pendingChangeNode2,
+	}
+
+	changeNodes2 := []*PendingChangeNode[string, uint]{
+		pendingChangeNode1,
+		pendingChangeNode2,
+		pendingChangeNode3,
+	}
+	type args struct {
+		ct    ChangeTree[string, uint]
+		index uint
+	}
+	tests := []struct {
+		name string
+		args args
+		exp  PendingChangeNode[string, uint]
+	}{
+		{
+			name: "2ElemSliceDeletingLastElement",
+			args: args{
+				ct: ChangeTree[string, uint]{
+					TreeRoots: changeNodes1,
+				},
+				index: 1,
+			},
+			exp: *pendingChangeNode2,
+		},
+		{
+			name: "3ElemSliceDeletingFirstElement",
+			args: args{
+				ct: ChangeTree[string, uint]{
+					TreeRoots: changeNodes2,
+				},
+				index: 0,
+			},
+			exp: *pendingChangeNode1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			oldLength := len(tt.args.ct.Roots())
+			removedVal := tt.args.ct.swapRemove(tt.args.ct.Roots(), tt.args.index)
+			require.Equal(t, tt.exp, removedVal)
+			require.Equal(t, oldLength-1, len(tt.args.ct.Roots()))
+		})
+	}
+}
