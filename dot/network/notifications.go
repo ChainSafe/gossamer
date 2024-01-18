4c0145b04cd5a392dc3d7fb31c9b5245918c7a5d
Eclésio Junior <eclesiomelo.1@gmail.com>
2024-01-18 08:06:20 -0400

    fix(dot/sync): execute p2p handshake when there is no target (#3695)

--- dot/network/notifications.go
+++ dot/network/notifications.go
@@ -242,8 +242,9 @@ func closeOutboundStream(info *notificationsProtocol, peerID peer.ID, stream net
 	)
 
 	info.peersData.deleteOutboundHandshakeData(peerID)
+
 	err := stream.Close()
-	if err != nil {
+	if err != nil && err.Error() != ErrStreamReset.Error() {
 		logger.Warnf("failed to close outbound stream: %s", err)
 	}
 }
