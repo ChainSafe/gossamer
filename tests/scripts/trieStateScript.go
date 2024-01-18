343ffb8f3210963b0bbf34b7e23dadcc7edc95df
jimboj <jamesdjohnson218@gmail.com>
2024-01-18 15:09:13 -0700

    clean script and relocate

--- tests/scripts/trieStateScript.go
+++ tests/scripts/trieStateScript.go
@@ -30,8 +30,6 @@ func fetchWithTimeout(ctx context.Context,
 func getResponse(ctx context.Context, method, params string, target interface{}) (err error) {
 	const rpcPort = "8545"
 	endpoint := rpc.NewEndpoint(rpcPort)
-	//"http://18.222.22.42:8545"
-	//endpoint := "http://18.222.22.42:8545"
 	respBody, err := rpc.Post(ctx, endpoint, method, params)
 	if err != nil {
 		return fmt.Errorf("cannot RPC post: %w", err)
@@ -46,30 +44,28 @@ func getResponse(ctx context.Context, method, params string, target interface{})
 }
 
 /*
-1)  ./bin/gossamer init --chain westend-dev --key alice
-1)  ./bin/gossamer --chain westend-dev --key alice --rpc-external=true --unsafe-rpc=true
-finalised block number 512 with hash 0x5895897f12e1a670609929433ac7a69dcae90e0cc2d9c32c0dce0e2a5e5e614e
+This is a script to query the trie state from a specific block height from a running node.
+
+Example commands to run a node:
+ 1. ./bin/gossamer init --chain westend-dev --key alice
+ 2. ./bin/gossamer --chain westend-dev --key alice --rpc-external=true --unsafe-rpc=true
+
+Once the node has started and processed the block whose state you need, can execute the script like so:
+ 1. go run trieStateScript.go <block hash>
 */
 func main() {
-	// Get block hash from cli
+	// Get block hash from cli (i.e. 0x276bfa91f70859348285599321ea96afd3ae681f0be47d36196bac8075ea32e8)
 	blockHash := os.Args[1]
 
-	// Goal, take hash as input and then write to file
 	ctx, _ := context.WithCancel(context.Background())
 
-	// Starting with just genesis info to get working
-	//const westendDevGenesisHash = "0x276bfa91f70859348285599321ea96afd3ae681f0be47d36196bac8075ea32e8"
-	//const westendDevStateRoot = "0x953044ba4386a72ae434d2a2fbdfca77640a28ac3841a924674cbfe7a8b9a81c"
-	//	const westend512StateRoot = "0xe6e996b91f9ec0c2900099754777c13644bb742dd512530dc6230aeaee2c19f9"
-	//const westend5MillionStateRoot = "0x26640792719e73a00872616a3006041ccc2ecbb2d60f5dfa15afbbeee495fc34"
-	const westend7MillionStateRoot = "0x08eae817e3f4fbd43157193e97f6dd51d7b4f3be3c7a584317bf68a8367654dd"
-	//const westend11MilStateRoot = "0xd3d88852c309d948a5689c5860d483817a9c26670cde6030091377bc973669cb"
+	// Plug in the expected state root if you wish to assert the calculated state root
+	const expectedStateRoot = ""
 	params := fmt.Sprintf(`["%s"]`, blockHash)
 
 	var response modules.StateTrieResponse
 	fetchWithTimeout(ctx, "state_trie", params, &response)
 
-	// THis is needed stuff
 	encResponse, err := json.Marshal(response)
 	if err != nil {
 		panic(fmt.Sprintf("json marshalling response %v", err))
@@ -79,8 +75,8 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("writing to file %v", err))
 	}
-	// Below is for testing correctness
 
+	// Below is for testing correctness, can be commented out if this is not desired
 	entries := make(map[string]string, len(response))
 	for _, encodedEntry := range response {
 		bytesEncodedEntry := common.MustHexToBytes(encodedEntry)
@@ -99,7 +95,7 @@ func main() {
 	}
 
 	trieHash := newTrie.MustHash(trie.V0.MaxInlineValue())
-	if westend7MillionStateRoot != trieHash.String() {
+	if expectedStateRoot != trieHash.String() {
 		panic(fmt.Sprintf("westendDevStateRoot does not match trieHash"))
 	}
 }
