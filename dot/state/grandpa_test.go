cfb653bedc30c63690e6a12b383811f14ec4172e
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2024-03-19 15:21:58 -0400

    chore: remove `t.Parallel()`

--- dot/state/grandpa_test.go
+++ dot/state/grandpa_test.go
@@ -220,8 +220,6 @@ func TestAddScheduledChangesKeepTheRightForkTree(t *testing.T) {
 	for tname, tt := range tests {
 		tt := tt
 		t.Run(tname, func(t *testing.T) {
-			t.Parallel()
-
 			// clear the scheduledChangeRoots after the test ends
 			// this does not cause race condition because t.Run without
 			// t.Parallel() blocks until this function returns
