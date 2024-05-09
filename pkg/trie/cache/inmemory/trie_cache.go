d0c380e81f7cc6519d398445bf27396c4a0c80b4
Diego <diego2737@gmail.com>
2024-05-09 15:25:00 -0300

    refactor(pkg/trie): Expose TrieInMemoryCache

--- pkg/trie/cache/inmemory/trie_cache.go
+++ pkg/trie/cache/inmemory/trie_cache.go
@@ -12,37 +12,37 @@ const defaultNodeCacheMaxSize = 8 * 1024 * 1024  // 8MB
 const defaultValueCacheMaxSize = 2 * 1024 * 1024 // 2MB
 
 // TrieInMemoryCache is an in-memory cache for trie nodes
-type trieInMemoryCache struct {
+type TrieInMemoryCache struct {
 	nodeCache  *lruCache
 	valueCache *lruCache
 }
 
 // NewTrieInMemoryCache creates a new TrieInMemoryCache
-func NewTrieInMemoryCache() *trieInMemoryCache {
-	return &trieInMemoryCache{
+func NewTrieInMemoryCache() *TrieInMemoryCache {
+	return &TrieInMemoryCache{
 		nodeCache:  newLruCache(defaultNodeCacheMaxSize),
 		valueCache: newLruCache(defaultValueCacheMaxSize),
 	}
 }
 
 // GetValue returns the value for the given key
-func (tc *trieInMemoryCache) GetValue(key []byte) []byte {
+func (tc *TrieInMemoryCache) GetValue(key []byte) []byte {
 	return tc.valueCache.get(string(key))
 }
 
 // SetValue sets the value for the given key
-func (tc *trieInMemoryCache) SetValue(key []byte, value []byte) {
+func (tc *TrieInMemoryCache) SetValue(key []byte, value []byte) {
 	tc.valueCache.set(string(key), value)
 }
 
 // GetNode returns the node for the given key
-func (tc *trieInMemoryCache) GetNode(key []byte) []byte {
+func (tc *TrieInMemoryCache) GetNode(key []byte) []byte {
 	return tc.nodeCache.get(string(key))
 }
 
 // SetNode sets the node for the given key
-func (tc *trieInMemoryCache) SetNode(key, value []byte) {
+func (tc *TrieInMemoryCache) SetNode(key, value []byte) {
 	tc.nodeCache.set(string(key), value)
 }
 
-var _ cache.TrieCache = (*trieInMemoryCache)(nil)
+var _ cache.TrieCache = (*TrieInMemoryCache)(nil)
