62c2b38690e7847cab2d62185cd55be6d4fc84ce
Diego <diego2737@gmail.com>
2024-05-15 02:09:06 -0300

    refactor(pkg/trie): Use cache with items limit for nodes

--- pkg/trie/cache/inmemory/trie_cache.go
+++ pkg/trie/cache/inmemory/trie_cache.go
@@ -4,23 +4,24 @@
 package inmemory
 
 import (
+	lrucache "github.com/ChainSafe/gossamer/lib/utils/lru-cache"
 	"github.com/ChainSafe/gossamer/pkg/trie/cache"
 )
 
 // https://github.com/paritytech/polkadot-sdk/blob/a8f4f4f00f8fc0da512a09e1450bf4cda954d70d/substrate/primitives/trie/src/cache/mod.rs#L98
-const defaultNodeCacheMaxSize = 8 * 1024 * 1024  // 8MB
+const defaultNodeCacheMaxElements = 10000
 const defaultValueCacheMaxSize = 2 * 1024 * 1024 // 2MB
 
 // TrieInMemoryCache is an in-memory cache for trie nodes
 type TrieInMemoryCache struct {
-	nodeCache  *lruCache
+	nodeCache  *lrucache.LRUCache[string, []byte]
 	valueCache *lruCache
 }
 
 // NewTrieInMemoryCache creates a new TrieInMemoryCache
 func NewTrieInMemoryCache() *TrieInMemoryCache {
 	return &TrieInMemoryCache{
-		nodeCache:  newLruCache(defaultNodeCacheMaxSize),
+		nodeCache:  lrucache.NewLRUCache[string, []byte](defaultNodeCacheMaxElements),
 		valueCache: newLruCache(defaultValueCacheMaxSize),
 	}
 }
@@ -37,12 +38,12 @@ func (tc *TrieInMemoryCache) SetValue(key []byte, value []byte) {
 
 // GetNode returns the node for the given key
 func (tc *TrieInMemoryCache) GetNode(key []byte) []byte {
-	return tc.nodeCache.get(string(key))
+	return tc.nodeCache.Get(string(key))
 }
 
 // SetNode sets the node for the given key
 func (tc *TrieInMemoryCache) SetNode(key, value []byte) {
-	tc.nodeCache.set(string(key), value)
+	tc.nodeCache.Put(string(key), value)
 }
 
 var _ cache.TrieCache = (*TrieInMemoryCache)(nil)
