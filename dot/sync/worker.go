77ed567f92592e7e6efc7c016243b8deeb14dc00
Emil Georgiev <emil.georgiev@mail.schwarz>
2024-06-09 22:52:41 +0300

    docs(dot/sync): add comments to worker

--- dot/sync/worker.go
+++ dot/sync/worker.go
@@ -11,15 +11,29 @@ import (
 	"github.com/libp2p/go-libp2p/core/peer"
 )
 
+// ErrStopTimeout is an error indicating that the worker stop operation timed out.
 var ErrStopTimeout = errors.New("stop timeout")
 
+// worker represents a worker that processes sync tasks by making network requests to peers.
+// It manages the synchronization tasks between nodes in the Polkadot's peer-to-peer network.
+// The primary goal of the worker is to handle and coordinate tasks related to network requests,
+// ensuring that nodes stay synchronized with the blockchain state
 type worker struct {
-	status       byte
-	peerID       peer.ID
-	sharedGuard  chan struct{}
+	// Status of the worker (e.g., available, busy, etc.)
+	status byte
+
+	// ID of the peer this worker is associated with
+	peerID peer.ID
+
+	// Channel used as a semaphore to limit concurrent tasks. By making the channel buffered with some size,
+	// the creator of the channel can control how many workers can work concurrently and send requests.
+	sharedGuard chan struct{}
+
+	// Interface to make network requests
 	requestMaker network.RequestMaker
 }
 
+// newWorker creates and returns a new worker instance.
 func newWorker(pID peer.ID, sharedGuard chan struct{}, network network.RequestMaker) *worker {
 	return &worker{
 		peerID:       pID,
@@ -29,6 +43,9 @@ func newWorker(pID peer.ID, sharedGuard chan struct{}, network network.RequestMa
 	}
 }
 
+// run starts the worker to process tasks from the queue.
+// queue: Channel from which the worker receives tasks
+// wg: WaitGroup to signal when the worker has finished processing
 func (w *worker) run(queue chan *syncTask, wg *sync.WaitGroup) {
 	defer func() {
 		logger.Debugf("[STOPPED] worker %s", w.peerID)
@@ -40,13 +57,18 @@ func (w *worker) run(queue chan *syncTask, wg *sync.WaitGroup) {
 	}
 }
 
+// executeRequest processes a sync task by making a network request to a peer.
+// who: ID of the peer making the request
+// requestMaker: Interface to make the network request
+// task: Sync task to be processed
+// sharedGuard: Channel used for concurrency control
 func executeRequest(who peer.ID, requestMaker network.RequestMaker,
 	task *syncTask, sharedGuard chan struct{}) {
 	defer func() {
-		<-sharedGuard
+		<-sharedGuard // Release the semaphore slot after the request is processed
 	}()
 
-	sharedGuard <- struct{}{}
+	sharedGuard <- struct{}{} // Acquire a semaphore slot before starting the request
 
 	request := task.request
 	logger.Debugf("[EXECUTING] worker %s, block request: %s", who, request)
