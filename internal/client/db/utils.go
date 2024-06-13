5755c2e3f06f0acd2bf999766280565f4317e985
Timothy Wu <tim.wu@chainsafe.io>
2024-06-12 21:35:53 -0400

    wip

--- internal/client/db/utils.go
+++ internal/client/db/utils.go
@@ -12,10 +12,6 @@ import (
 	"github.com/ChainSafe/gossamer/pkg/scale"
 )
 
-// / Number of columns in the db. Must be the same for both full && light dbs.
-// / Otherwise RocksDb will fail to open database && check its type.
-const numColumns uint32 = 13
-
 // / Meta column. The set of keys in the column is shared by full && light storages.
 const columnMeta database.ColumnID = 0
 
