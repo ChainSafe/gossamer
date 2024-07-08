7fd4e0566421c95019418dd5ae3bd7796866791c
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2024-07-08 16:12:19 -0400

    fix: check `network.ErrNilBlockInResponse` in the correct position

--- dot/sync/chain_sync.go
+++ dot/sync/chain_sync.go
@@ -652,17 +652,19 @@ taskResultLoop:
 					logger.Errorf("task result: peer(%s) error: %s",
 						taskResult.who, taskResult.err)
 
+					if errors.Is(taskResult.err, network.ErrNilBlockInResponse) {
+						cs.network.ReportPeer(peerset.ReputationChange{
+							Value:  peerset.BadMessageValue,
+							Reason: peerset.BadMessageReason,
+						}, who)
+					}
+
 					if strings.Contains(taskResult.err.Error(), "protocols not supported") {
 						cs.network.ReportPeer(peerset.ReputationChange{
 							Value:  peerset.BadProtocolValue,
 							Reason: peerset.BadProtocolReason,
 						}, who)
 					}
-				} else if errors.Is(taskResult.err, network.ErrNilBlockInResponse) {
-					cs.network.ReportPeer(peerset.ReputationChange{
-						Value:  peerset.BadMessageValue,
-						Reason: peerset.BadMessageReason,
-					}, who)
 				}
 
 				// TODO: avoid the same peer to get the same task
