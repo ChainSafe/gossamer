2adb7f4197d79c5b3e649467981b97a33e50913b
Timothy Wu <tim.wu@chainsafe.io>
2024-07-29 18:17:06 -0400

    move bitvec to parachaintypes

---
+++ dot/parachain/types/bitvec.go
@@ -0,0 +1,109 @@
+// Copyright 2024 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package parachaintypes
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/ChainSafe/gossamer/pkg/scale"
+)
+
+// BitVec is the implementation of a bit vector
+type BitVec struct {
+	bits []bool
+}
+
+// NewBitVec returns a new BitVec with the given bits
+// This isn't a complete implementation of the bit vector
+// It is only used for ParachainHost runtime exports
+// TODO: Implement the full bit vector
+// https://github.com/ChainSafe/gossamer/issues/3248
+func NewBitVec(bits []bool) BitVec {
+	return BitVec{
+		bits: bits,
+	}
+}
+
+// bitsToBytes converts a slice of bits to a slice of bytes
+// Uses lsb ordering
+// TODO: Implement msb ordering
+// https://github.com/ChainSafe/gossamer/issues/3248
+func (bv BitVec) bitsToBytes() []byte {
+	bits := bv.bits
+	const byteSize = 8
+	bitLength := len(bits)
+	numOfBytes := (bitLength + (byteSize - 1)) / byteSize
+	bytes := make([]byte, numOfBytes)
+
+	if len(bits)%byteSize != 0 {
+		// Pad with zeros to make the number of bits a multiple of byteSize
+		pad := make([]bool, byteSize-len(bits)%byteSize)
+		bits = append(bits, pad...)
+	}
+
+	for i := 0; i < bitLength; i++ {
+		if bits[i] {
+			byteIndex := i / byteSize
+			bitIndex := i % byteSize
+			bytes[byteIndex] |= 1 << bitIndex
+		}
+	}
+
+	return bytes
+}
+
+// bytesToBits converts a slice of bytes to a slice of bits
+func (bv *BitVec) bytesToBits(b []byte) {
+	var bits []bool
+	const byteSize = 8
+	var size = uint(len(b))
+	for _, uint8val := range b {
+		end := size
+		if end > byteSize {
+			end = byteSize
+		}
+		size -= end
+
+		for j := uint(0); j < end; j++ {
+			bit := (uint8val>>j)&1 == 1
+			bits = append(bits, bit)
+		}
+	}
+	bv.bits = bits
+}
+
+// MarshalSCALE fulfils the SCALE interface for encoding
+func (bv BitVec) MarshalSCALE() ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	encoder := scale.NewEncoder(buf)
+	if len(bv.bits) > 268435455 {
+		return nil, fmt.Errorf("bitvec too long")
+	}
+	size := uint(len(bv.bits))
+	err := encoder.Encode(size)
+	if err != nil {
+		return nil, err
+	}
+
+	bytes := bv.bitsToBytes()
+	_, err = buf.Write(bytes)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// UnmarshalSCALE fulfils the SCALE interface for decoding
+func (bv *BitVec) UnmarshalSCALE(r io.Reader) error {
+	decoder := scale.NewDecoder(r)
+	var bytes []byte
+	err := decoder.Decode(&bytes)
+	if err != nil {
+		return err
+	}
+	bv.bytesToBits(bytes)
+	return nil
+}
---
+++ dot/parachain/types/bitvec_test.go
@@ -0,0 +1,148 @@
+// Copyright 2024 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package parachaintypes
+
+import (
+	"testing"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/pkg/scale"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBitVec(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name       string
+		in         string
+		wantBitVec BitVec
+		wantErr    bool
+	}{
+		{
+			name:       "empty_bitvec",
+			in:         "0x00",
+			wantBitVec: NewBitVec(nil),
+			wantErr:    false,
+		},
+		{
+			name:       "1_byte",
+			in:         "0x2055",
+			wantBitVec: NewBitVec([]bool{true, false, true, false, true, false, true, false}),
+			wantErr:    false,
+		},
+		{
+			name: "4_bytes",
+			in:   "0x645536aa01",
+			wantBitVec: NewBitVec([]bool{
+				true, false, true, false, true, false, true, false,
+				false, true, true, false, true, true, false, false,
+				false, true, false, true, false, true, false, true,
+				true,
+			}),
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		test := tt
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			resultBytes, err := common.HexToBytes(test.in)
+			require.NoError(t, err)
+
+			bv := NewBitVec(nil)
+			err = scale.Unmarshal(resultBytes, &bv)
+			require.NoError(t, err)
+			require.Equal(t, test.wantBitVec.bits, bv.bits)
+
+			b, err := scale.Marshal(bv)
+			require.NoError(t, err)
+			require.Equal(t, resultBytes, b)
+		})
+	}
+}
+
+func TestBitVecBytesToBits(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		in      []byte
+		want    []bool
+		wantErr bool
+	}{
+		{
+			name:    "empty",
+			in:      []byte(nil),
+			want:    []bool(nil),
+			wantErr: false,
+		},
+		{
+			name:    "1_byte",
+			in:      []byte{0x55},
+			want:    []bool{true, false, true, false, true, false, true, false},
+			wantErr: false,
+		},
+		{
+			name: "4_bytes",
+			in:   []byte{0x55, 0x36, 0xaa, 0x1},
+			want: []bool{
+				true, false, true, false, true, false, true, false,
+				false, true, true, false, true, true, false, false,
+				false, true, false, true, false, true, false, true,
+				true, false, false, false, false, false, false, false,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, tt.want, bytesToBits(tt.in, uint(len(tt.in)*byteSize)))
+		})
+	}
+}
+
+func TestBitVecBitsToBytes(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		in      []bool
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name:    "empty",
+			in:      []bool(nil),
+			want:    []byte{},
+			wantErr: false,
+		},
+		{
+			name:    "1_byte",
+			in:      []bool{true, false, true, false, true, false, true, false},
+			want:    []byte{0x55},
+			wantErr: false,
+		},
+		{
+			name: "4_bytes",
+			in: []bool{
+				true, false, true, false, true, false, true, false,
+				false, true, true, false, true, true, false, false,
+				false, true, false, true, false, true, false, true,
+				true,
+			},
+			want: []byte{0x55, 0x36, 0xaa, 0x1},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			bv := BitVec{tt.in}
+			bytes := bv.bitsToBytes()
+			require.Equal(t, tt.want, bytes)
+		})
+	}
+}
