84cef6470c1754f014c932759567271f8f9385b8
edwardmack <ed@edwardmack.com>
2024-08-29 12:03:47 -0400

    remove redundant structs for simplicty, removed go routines to simplify code

--- dot/parachain/pvf/worker.go
+++ dot/parachain/pvf/worker.go
@@ -11,8 +11,6 @@ type worker struct {
 	instance *parachainruntime.Instance
 	// TODO(ed): determine if wasProcessed is stored here or in host
 	isProcessed map[parachaintypes.CandidateHash]struct{}
-	// TODO make this a buffered channel, and determine the buffer size
-	workerTasksChan chan *workerTask
 }
 
 type workerTask struct {
@@ -21,117 +19,75 @@ type workerTask struct {
 	candidateReceipt *parachaintypes.CandidateReceipt
 }
 
-func newWorker(validationCode parachaintypes.ValidationCode, queue chan *workerTask) (*worker, error) {
+func newWorker(validationCode parachaintypes.ValidationCode) (*worker, error) {
 	validationRuntime, err := parachainruntime.SetupVM(validationCode)
 
 	if err != nil {
 		return nil, err
 	}
 	return &worker{
-		workerID:        validationCode.Hash(),
-		instance:        validationRuntime,
-		workerTasksChan: queue,
+		workerID: validationCode.Hash(),
+		instance: validationRuntime,
 	}, nil
 }
 
-func (w *worker) executeRequest(task *workerTask) chan *ValidationTaskResult {
+func (w *worker) executeRequest(task *workerTask) (*ValidationResult, error) {
 	logger.Debugf("[EXECUTING] worker %x task %v", w.workerID, task.work)
-	resultCh := make(chan *ValidationTaskResult)
+	candidateHash, err := parachaintypes.GetCandidateHash(task.candidateReceipt)
+	if err != nil {
+		return nil, err
+	}
 
-	go func() {
-		defer close(resultCh)
-		candidateHash, err := parachaintypes.GetCandidateHash(task.candidateReceipt)
-		if err != nil {
-			// TODO: handle error
-			logger.Errorf("getting candidate hash: %w", err)
-		}
+	// do isProcessed check here
+	if _, ok := w.isProcessed[candidateHash]; ok {
+		// TODO: determine what the isPreccessed check should return, and if re-trying is allowed
+		//  get a better understanding of what the isProcessed check should be checking for
+		logger.Debugf("candidate %x already processed", candidateHash)
+	}
+	validationResult, err := w.instance.ValidateBlock(task.work)
 
-		// do isProcessed check here
-		if _, ok := w.isProcessed[candidateHash]; ok {
-			// TODO: determine what the isPreccessed check should return, and if re-trying is allowed
-			//  get a better understanding of what the isProcessed check should be checking for
-			logger.Debugf("candidate %x already processed", candidateHash)
-		}
-		validationResult, err := w.instance.ValidateBlock(task.work)
+	if err != nil {
+		logger.Errorf("executing validate_block: %w", err)
+		reasonForInvalidity := ExecutionError
+		return &ValidationResult{InvalidResult: &reasonForInvalidity}, nil
+	}
 
-		if err != nil {
-			logger.Errorf("executing validate_block: %w", err)
-			reasonForInvalidity := ExecutionError
-			errorResult := &ValidationResult{
-				InvalidResult: &reasonForInvalidity,
-			}
-			resultCh <- &ValidationTaskResult{
-				who:    w.workerID,
-				Result: errorResult,
-			}
-			return
-		}
+	headDataHash, err := validationResult.HeadData.Hash()
+	if err != nil {
+		logger.Errorf("hashing head data: %w", err)
+		reasonForInvalidity := ExecutionError
+		return &ValidationResult{InvalidResult: &reasonForInvalidity}, nil
+	}
 
-		headDataHash, err := validationResult.HeadData.Hash()
-		if err != nil {
-			logger.Errorf("hashing head data: %w", err)
-			reasonForInvalidity := ExecutionError
-			errorResult := &ValidationResult{
-				InvalidResult: &reasonForInvalidity,
-			}
-			resultCh <- &ValidationTaskResult{
-				who:    w.workerID,
-				Result: errorResult,
-			}
-			return
-		}
+	if headDataHash != task.candidateReceipt.Descriptor.ParaHead {
+		reasonForInvalidity := ParaHeadHashMismatch
+		return &ValidationResult{InvalidResult: &reasonForInvalidity}, nil
+	}
+	candidateCommitments := parachaintypes.CandidateCommitments{
+		UpwardMessages:            validationResult.UpwardMessages,
+		HorizontalMessages:        validationResult.HorizontalMessages,
+		NewValidationCode:         validationResult.NewValidationCode,
+		HeadData:                  validationResult.HeadData,
+		ProcessedDownwardMessages: validationResult.ProcessedDownwardMessages,
+		HrmpWatermark:             validationResult.HrmpWatermark,
+	}
 
-		if headDataHash != task.candidateReceipt.Descriptor.ParaHead {
-			reasonForInvalidity := ParaHeadHashMismatch
-			errorResult := &ValidationResult{
-				InvalidResult: &reasonForInvalidity,
-			}
-			resultCh <- &ValidationTaskResult{
-				who:    w.workerID,
-				Result: errorResult,
-			}
-			return
-		}
-		candidateCommitments := parachaintypes.CandidateCommitments{
-			UpwardMessages:            validationResult.UpwardMessages,
-			HorizontalMessages:        validationResult.HorizontalMessages,
-			NewValidationCode:         validationResult.NewValidationCode,
-			HeadData:                  validationResult.HeadData,
-			ProcessedDownwardMessages: validationResult.ProcessedDownwardMessages,
-			HrmpWatermark:             validationResult.HrmpWatermark,
-		}
+	// if validation produced a new set of commitments, we treat the candidate as invalid
+	if task.candidateReceipt.CommitmentsHash != candidateCommitments.Hash() {
+		reasonForInvalidity := CommitmentsHashMismatch
+		return &ValidationResult{InvalidResult: &reasonForInvalidity}, nil
+	}
+	pvd := parachaintypes.PersistedValidationData{
+		ParentHead:             task.work.ParentHeadData,
+		RelayParentNumber:      task.work.RelayParentNumber,
+		RelayParentStorageRoot: task.work.RelayParentStorageRoot,
+		MaxPovSize:             task.maxPoVSize,
+	}
+	return &ValidationResult{
+		ValidResult: &ValidValidationResult{
+			CandidateCommitments:    candidateCommitments,
+			PersistedValidationData: pvd,
+		},
+	}, nil
 
-		// if validation produced a new set of commitments, we treat the candidate as invalid
-		if task.candidateReceipt.CommitmentsHash != candidateCommitments.Hash() {
-			reasonForInvalidity := CommitmentsHashMismatch
-			errorResult := &ValidationResult{
-				InvalidResult: &reasonForInvalidity,
-			}
-			resultCh <- &ValidationTaskResult{
-				who:    w.workerID,
-				Result: errorResult,
-			}
-			return
-		}
-		pvd := parachaintypes.PersistedValidationData{
-			ParentHead:             task.work.ParentHeadData,
-			RelayParentNumber:      task.work.RelayParentNumber,
-			RelayParentStorageRoot: task.work.RelayParentStorageRoot,
-			MaxPovSize:             task.maxPoVSize,
-		}
-		validResult := &ValidationResult{
-			ValidResult: &ValidValidationResult{
-				CandidateCommitments:    candidateCommitments,
-				PersistedValidationData: pvd,
-			},
-		}
-
-		logger.Debugf("[RESULT] worker %x, result: %v, error: %s", w.workerID, validResult, err)
-
-		resultCh <- &ValidationTaskResult{
-			who:    w.workerID,
-			Result: validResult,
-		}
-	}()
-	return resultCh
 }
