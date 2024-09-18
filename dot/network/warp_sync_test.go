3220e598dd5e201005c2ae7587cded3490ae1910
Diego <diego2737@gmail.com>
2024-09-18 14:31:56 -0300

    Add message decoding test

---
+++ dot/network/warp_sync_test.go
@@ -0,0 +1,28 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/ChainSafe/gossamer/dot/network/messages"
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecodeWarpSyncMessage(t *testing.T) {
+	t.Parallel()
+	testWarpReqMessage := &messages.WarpProofRequest{
+		Begin: common.EmptyHash,
+	}
+
+	testPeer := peer.ID("me")
+	reqEnc, err := testWarpReqMessage.Encode()
+	require.NoError(t, err)
+
+	msg, err := decodeWarpSyncMessage(reqEnc, testPeer, true)
+	require.NoError(t, err)
+
+	req, ok := msg.(*messages.WarpProofRequest)
+	require.True(t, ok)
+	require.Equal(t, testWarpReqMessage, req)
+}
