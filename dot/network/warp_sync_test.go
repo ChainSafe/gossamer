c20454159a4812d0066fe8459024d62b2ff1f306
Diego <diego2737@gmail.com>
2024-09-18 17:18:52 -0300

    Add more tests

--- dot/network/warp_sync_test.go
+++ dot/network/warp_sync_test.go
@@ -4,16 +4,19 @@
 package network
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/ChainSafe/gossamer/dot/network/messages"
 	"github.com/ChainSafe/gossamer/lib/common"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/stretchr/testify/require"
+	gomock "go.uber.org/mock/gomock"
 )
 
 func TestDecodeWarpSyncMessage(t *testing.T) {
 	t.Parallel()
+
 	testWarpReqMessage := &messages.WarpProofRequest{
 		Begin: common.EmptyHash,
 	}
@@ -28,4 +31,64 @@ func TestDecodeWarpSyncMessage(t *testing.T) {
 	req, ok := msg.(*messages.WarpProofRequest)
 	require.True(t, ok)
 	require.Equal(t, testWarpReqMessage, req)
+}
+
+func createServiceWithWarpSyncHelper(t *testing.T, warpSyncProvider WarpSyncProvider) *Service {
+	t.Helper()
+
+	config := &Config{
+		BasePath:         t.TempDir(),
+		Port:             availablePort(t),
+		NoBootstrap:      true,
+		NoMDNS:           true,
+		warpSyncProvider: warpSyncProvider,
+	}
+
+	srvc := createTestService(t, config)
+	srvc.noGossip = true
+	handler := newTestStreamHandler(decodeSyncMessage)
+	srvc.host.registerStreamHandler(srvc.host.protocolID, handler.handleStream)
+
+	return srvc
+}
+
+func TestHandleWarpSyncRequestOk(t *testing.T) {
+	t.Parallel()
+
+	expectedProof := []byte{0x01}
+
+	ctrl := gomock.NewController(t)
+
+	warpSyncProvider := NewMockWarpSyncProvider(ctrl)
+	warpSyncProvider.EXPECT().generate(common.EmptyHash).Return(expectedProof, nil).Times(1)
+
+	srvc := createServiceWithWarpSyncHelper(t, warpSyncProvider)
+
+	req := messages.WarpProofRequest{
+		Begin: common.EmptyHash,
+	}
+
+	resp, err := srvc.handleWarpSyncRequest(req)
+	require.NoError(t, err)
+	require.Equal(t, expectedProof, resp)
+}
+
+func TestHandleWarpSyncRequestError(t *testing.T) {
+	t.Parallel()
+
+	expectedError := fmt.Errorf("error generating proof")
+	ctrl := gomock.NewController(t)
+
+	warpSyncProvider := NewMockWarpSyncProvider(ctrl)
+	warpSyncProvider.EXPECT().generate(common.EmptyHash).Return(nil, expectedError).Times(1)
+
+	srvc := createServiceWithWarpSyncHelper(t, warpSyncProvider)
+
+	req := messages.WarpProofRequest{
+		Begin: common.EmptyHash,
+	}
+
+	resp, err := srvc.handleWarpSyncRequest(req)
+	require.Nil(t, resp)
+	require.ErrorIs(t, err, expectedError)
 }
