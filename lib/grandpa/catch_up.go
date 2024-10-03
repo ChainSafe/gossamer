8f8c06600836b8885639145e1fb17c7abbbdf085
jimboj <jamesdjohnson218@gmail.com>
2024-10-03 21:57:11 +0900

    wip/impl neighbor tracking

--- lib/grandpa/catch_up.go
+++ lib/grandpa/catch_up.go
@@ -3,54 +3,69 @@ package grandpa
 import (
 	"fmt"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"sync"
-	"sync/atomic"
 	"time"
 )
 
-type catchUp struct {
-	lock sync.Mutex
-
-	grandpa         *Service
-	readyToCatchUp  *atomic.Bool
-	shutdownCatchup chan struct{}
+type neighborState struct {
+	setID uint64
+	round uint64
+	//highestFinalized uint32 not sure if i need this or not
 }
 
-func newCatchUp(grandpa *Service) *catchUp {
-	c := &catchUp{
-		readyToCatchUp:  &atomic.Bool{},
-		grandpa:         grandpa,
-		shutdownCatchup: make(chan struct{}),
-	}
-	c.readyToCatchUp.Store(true)
-	return c
+type NeighborTracker struct {
+	//grandpa *Service
+	network Network
+
+	peerview     map[peer.ID]neighborState
+	currentSetID uint64
+	currentRound uint64
+	highestFinalized uint32
 }
 
-func (c *catchUp) tryCatchUp(round uint64, setID uint64, peer peer.ID) error {
-	logger.Warnf("Trying to catch up")
-	//c.lock.Lock()
-	if !c.readyToCatchUp.Load() {
-		// Fine we just skip
-		return nil
+func NewNeighborTracker(network Network) *NeighborTracker {
+	return &NeighborTracker{
+		network: network,
+		peerview: make(map[peer.ID]neighborState),
 	}
-	//c.lock.Lock()
-	catchUpRequest := newCatchUpRequest(round, setID)
-	cm, err := catchUpRequest.ToConsensusMessage()
-	if err != nil {
-		return fmt.Errorf("converting catchUpRequest to network message: %w", err)
-	}
+}
 
-	logger.Warnf("sending catchup request message: %v", catchUpRequest)
-	err = c.grandpa.network.SendMessage(peer, cm)
-	if err != nil {
-		return fmt.Errorf("sending catchUpRequest to network message: %w", err)
+func (nt *NeighborTracker) UpdateState(setID uint64, round uint64, highestFinalized uint32) {
+	nt.currentSetID = setID
+	nt.currentRound = round
+	nt.highestFinalized = highestFinalized
+}
+
+
+func (nt *NeighborTracker) UpdatePeer(p peer.ID, setID uint64, round uint64) {
+	peerState := neighborState{setID, round}
+	nt.peerview[p] = peerState
+}
+
+func (nt *NeighborTracker) BroadcastNeighborMsg() error {
+	for id, peerState := range nt.peerview {
+		if !(peerState.round < nt.currentRound || peerState.setID < nt.currentSetID) {
+			// Send msg
+			packet := NeighbourPacketV1{
+				Round:  nt.currentRound,
+				SetID:  nt.currentSetID,
+				Number: nt.highestFinalized,
+			}
+
+			cm, err := packet.ToConsensusMessage()
+			if err != nil {
+				return fmt.Errorf("converting NeighbourPacketV1 to network message: %w", err)
+			}
+
+			err = nt.network.SendMessage(id, cm)
+			if err != nil {
+				return fmt.Errorf("sending message to peer: %v", id)
+			}
+		}
 	}
-	c.readyToCatchUp.Store(false)
-	logger.Warnf("successfully tryed catch up")
 	return nil
 }
 
-func (c *catchUp) initCatchUp() {
+func (nt *NeighborTracker) initCatchUp() {
 	logger.Warnf("Initializing catch up")
 	const duration = time.Second * 30
 	ticker := time.NewTicker(duration)
@@ -60,10 +75,52 @@ func (c *catchUp) initCatchUp() {
 		select {
 		case <-ticker.C:
 			logger.Warnf("Ready to catch up again")
-			c.readyToCatchUp.Store(true)
-		case <-c.shutdownCatchup:
-			logger.Warnf("Closing catch up")
-			return
-		}
+
+		//case <-c.shutdownCatchup:
+		//	logger.Warnf("Closing catch up")
+		//	return
+		//}
 	}
 }
+
+//type catchUp struct {
+//	lock sync.Mutex
+//
+//	grandpa         *Service
+//	readyToCatchUp  *atomic.Bool
+//	shutdownCatchup chan struct{}
+//}
+//
+//func newCatchUp(grandpa *Service) *catchUp {
+//	c := &catchUp{
+//		readyToCatchUp:  &atomic.Bool{},
+//		grandpa:         grandpa,
+//		shutdownCatchup: make(chan struct{}),
+//	}
+//	c.readyToCatchUp.Store(true)
+//	return c
+//}
+
+//func (c *catchUp) tryCatchUp(round uint64, setID uint64, peer peer.ID) error {
+//	logger.Warnf("Trying to catch up")
+//	//c.lock.Lock()
+//	if !c.readyToCatchUp.Load() {
+//		// Fine we just skip
+//		return nil
+//	}
+//	//c.lock.Lock()
+//	catchUpRequest := newCatchUpRequest(round, setID)
+//	cm, err := catchUpRequest.ToConsensusMessage()
+//	if err != nil {
+//		return fmt.Errorf("converting catchUpRequest to network message: %w", err)
+//	}
+//
+//	logger.Warnf("sending catchup request message: %v", catchUpRequest)
+//	err = c.grandpa.network.SendMessage(peer, cm)
+//	if err != nil {
+//		return fmt.Errorf("sending catchUpRequest to network message: %w", err)
+//	}
+//	c.readyToCatchUp.Store(false)
+//	logger.Warnf("successfully tryed catch up")
+//	return nil
+//}
