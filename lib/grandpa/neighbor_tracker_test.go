3c0d6c67e13c6b777e92660889e404796e7f84c3
jimboj <jamesdjohnson218@gmail.com>
2024-10-15 19:53:49 +0900

    feedback and lint

--- lib/grandpa/neighbor_tracker_test.go
+++ lib/grandpa/neighbor_tracker_test.go
@@ -2,9 +2,10 @@ package grandpa
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestNeighborTracker_UpdatePeer(t *testing.T) {
@@ -72,25 +73,13 @@ func TestNeighborTracker_UpdatePeer(t *testing.T) {
 				highestFinalized: 6,
 			},
 		},
-		{
-			name: "nil tracker",
-			args: args{
-				p:                "testPeer",
-				setID:            1,
-				round:            2,
-				highestFinalized: 3,
-			},
-			expectedErr: fmt.Errorf("neighbor tracker is nil"),
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			nt := tt.tracker
 			err := nt.UpdatePeer(tt.args.p, tt.args.setID, tt.args.round, tt.args.highestFinalized)
 			require.Equal(t, err, tt.expectedErr)
-			if nt != nil {
-				require.Equal(t, tt.expectedState, nt.peerview[tt.args.p])
-			}
+			require.Equal(t, tt.expectedState, nt.peerview[tt.args.p])
 		})
 	}
 }
@@ -102,20 +91,10 @@ func TestNeighborTracker_UpdateState(t *testing.T) {
 		highestFinalized uint32
 	}
 	tests := []struct {
-		name        string
-		tracker     *NeighborTracker
-		args        args
-		expectedErr error
+		name    string
+		tracker *NeighborTracker
+		args    args
 	}{
-		{
-			name: "nil tracker",
-			args: args{
-				setID:            1,
-				round:            2,
-				highestFinalized: 3,
-			},
-			expectedErr: fmt.Errorf("neighbor tracker is nil"),
-		},
 		{
 			name:    "happy path",
 			tracker: &NeighborTracker{},
@@ -129,13 +108,10 @@ func TestNeighborTracker_UpdateState(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			nt := tt.tracker
-			err := nt.UpdateState(tt.args.setID, tt.args.round, tt.args.highestFinalized)
-			require.Equal(t, err, tt.expectedErr)
-			if nt != nil {
-				require.Equal(t, nt.currentSetID, tt.args.setID)
-				require.Equal(t, nt.currentRound, tt.args.round)
-				require.Equal(t, nt.highestFinalized, tt.args.highestFinalized)
-			}
+			nt.UpdateState(tt.args.setID, tt.args.round, tt.args.highestFinalized)
+			require.Equal(t, nt.currentSetID, tt.args.setID)
+			require.Equal(t, nt.currentRound, tt.args.round)
+			require.Equal(t, nt.highestFinalized, tt.args.highestFinalized)
 		})
 	}
 }
