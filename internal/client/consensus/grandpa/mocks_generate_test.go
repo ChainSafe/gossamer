6f224a43926aaeb97ebe89bbdadc971a540b7a4b
Diego <diego2737@gmail.com>
2024-10-17 11:16:38 -0300

    Remove unnecesary mocks

--- internal/client/consensus/grandpa/mocks_generate_test.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 ChainSafe Systems (ON)
-// SPDX-License-Identifier: LGPL-3.0-only
-
-package grandpa
-
-//go:generate mockgen -destination=mocks_test.go -package $GOPACKAGE . BlockState,GrandpaState
