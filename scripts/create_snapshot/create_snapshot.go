c197d878137c6b963c623075835464328bc08eaa
Haiko Schol <hs@haikoschol.com>
2025-01-20 13:31:52 +0700

    feat(scripts): add script for creating snapshots

---
+++ scripts/create_snapshot/create_snapshot.go
@@ -0,0 +1,248 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+const checkInterval = 2 * time.Second
+
+func main() {
+	threshold, dbLocation, snapshotDestination := parseArgs(os.Args)
+
+	for {
+		containerID, err := findContainerID("gossamer")
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(checkInterval)
+			continue
+		}
+
+		blockHeight, err := fetchMetric("gossamer_network_syncer_blocks_synced_total")
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(checkInterval)
+			continue
+		}
+
+		if blockHeight >= threshold {
+			if err := stopContainer(containerID); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			dstDir := filepath.Join(snapshotDestination, fmt.Sprintf("block-%d", blockHeight), "db")
+			if err := copyDirectory(dbLocation, dstDir); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			if err := startContainer(containerID); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			break
+		}
+
+		time.Sleep(checkInterval)
+	}
+}
+
+func findContainerID(name string) (string, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return "", fmt.Errorf("creating Docker client: %v", err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		return "", fmt.Errorf("fetching containers: %v", err)
+	}
+
+	for _, c := range containers {
+		for _, containerName := range c.Names {
+			if strings.Contains(containerName, name) {
+				return c.ID, nil
+			}
+		}
+	}
+
+	return "", errors.New("container not found")
+}
+
+func fetchMetric(metricName string) (uint32, error) {
+	httpClient := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := httpClient.Get("http://localhost:9876/metrics")
+	if err != nil {
+		return 0, fmt.Errorf("fetching '%s' metric: %w", metricName, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("failed to fetch metric '%s'. status code: %d", metricName, resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, fmt.Errorf("reading metrics response: %w", err)
+	}
+
+	metricsText := string(body)
+	valueStr := ""
+	scanner := bufio.NewScanner(strings.NewReader(metricsText))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, metricName) {
+			parts := strings.Fields(line)
+			if len(parts) == 2 {
+				valueStr = parts[1]
+				break
+			}
+		}
+	}
+
+	if valueStr == "" {
+		return 0, fmt.Errorf("metric '%s' not found", metricName)
+	}
+
+	blockHeight, err := strconv.ParseFloat(valueStr, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid metric value: %s", valueStr)
+	}
+
+	return uint32(blockHeight), nil
+}
+
+func stopContainer(containerID string) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("creating Docker client: %w", err)
+	}
+
+	timeout := int(10 * time.Second)
+	if err := cli.ContainerStop(context.Background(), containerID, container.StopOptions{Timeout: &timeout}); err != nil {
+		return fmt.Errorf("stopping container: %w", err)
+	}
+
+	fmt.Printf("Stopped gossamer container (ID %s)\n", containerID)
+	return nil
+}
+
+func startContainer(containerID string) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("creating Docker client: %w", err)
+	}
+
+	if err := cli.ContainerStart(context.Background(), containerID, container.StartOptions{}); err != nil {
+		return fmt.Errorf("starting container: %w", err)
+	}
+
+	fmt.Printf("Started gossamer container (ID %s)\n", containerID)
+	return nil
+}
+
+func copyDirectory(srcDir, dstDir string) error {
+	srcDir = filepath.Clean(srcDir)
+	dstDir = filepath.Clean(dstDir)
+
+	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			return err
+		}
+		dstPath := filepath.Join(dstDir, relPath)
+		if info.IsDir() {
+			return os.MkdirAll(dstPath, info.Mode())
+		}
+		return copyFile(path, dstPath)
+	})
+}
+
+func copyFile(srcFile, dstFile string) error {
+	src, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstFile)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	srcInfo, err := src.Stat()
+	if err != nil {
+		return err
+	}
+	return os.Chmod(dstFile, srcInfo.Mode())
+}
+
+func parseArgs(args []string) (threshold uint32, dbLocation string, snapshotDestination string) {
+	if len(args) < 4 {
+		fmt.Printf("usage: %s <approximate block height> <db location> <snapshot destination>\n", path.Base(args[0]))
+		os.Exit(1)
+	}
+
+	thresholdStr := args[1]
+	t, err := strconv.ParseUint(thresholdStr, 10, 32)
+	if err != nil {
+		fmt.Println("invalid threshold value")
+		os.Exit(1)
+	}
+	threshold = uint32(t)
+
+	dbLocation = args[2]
+	snapshotDestination = args[3]
+
+	if !isDirectory(dbLocation) {
+		fmt.Printf("db location is not a directory or not readable: %s\n", dbLocation)
+		os.Exit(1)
+	}
+
+	if !isWritableDirectory(snapshotDestination) {
+		fmt.Printf("snapshot destination is not a writable directory: %s\n", snapshotDestination)
+		os.Exit(1)
+	}
+
+	return
+}
+
+func isDirectory(path string) bool {
+	info, err := os.Stat(path)
+	return err == nil && info.IsDir()
+}
+
+func isWritableDirectory(path string) bool {
+	file, err := os.CreateTemp(path, "test")
+	if err != nil {
+		return false
+	}
+	file.Close()
+	os.Remove(file.Name())
+	return true
+}
---
+++ scripts/create_snapshot/go.mod
@@ -0,0 +1,25 @@
+module haikoschol.com/mksnapshot
+
+go 1.23.4
+
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v27.4.1+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
+	go.opentelemetry.io/otel v1.33.0 // indirect
+	go.opentelemetry.io/otel/metric v1.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.33.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
