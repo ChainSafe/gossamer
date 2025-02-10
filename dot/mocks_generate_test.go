0fb3d34cf6c6faff8f99e46fd38e93968fe004d1
Diego <diego2737@gmail.com>
2025-02-10 14:05:11 -0300

    Remove BlockState interface from network

--- dot/mocks_generate_test.go
+++ dot/mocks_generate_test.go
@@ -4,6 +4,6 @@
 package dot
 
 //go:generate mockgen -destination=mocks_test.go -package=$GOPACKAGE . ServiceRegisterer
-//go:generate mockgen -destination=mock_block_state_test.go -package $GOPACKAGE github.com/ChainSafe/gossamer/dot/network BlockState
 //go:generate mockgen -source=node.go -destination=mock_node_builder_test.go -package=$GOPACKAGE
 //go:generate mockgen -destination=mock_service_builder_test.go -package $GOPACKAGE . ServiceBuilder
+//go:generate mockgen -destination=mock_block_state_test.go -package $GOPACKAGE github.com/ChainSafe/gossamer/dot/state BlockState
