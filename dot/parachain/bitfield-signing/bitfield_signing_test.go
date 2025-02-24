141e20489edf7ebddaef02d53a61fed9e2334517
Freddy Li <tobeacookie@gmail.com>
2025-02-24 16:39:42 -0500

    config the CLA email

--- dot/parachain/bitfield-signing/bitfield_signing_test.go
+++ dot/parachain/bitfield-signing/bitfield_signing_test.go
@@ -364,6 +364,7 @@ func TestProcessActiveLeavesUpdateSignalSuccess(t *testing.T) {
 	}()
 
 	err = handleActiveLeavesUpdate(context.Background(), testBitfieldSigningSubsystem, testActiveLeaves)
+
 	assert.Nil(t, err)
 
 	// wait for the DistributeBitfield content checks
