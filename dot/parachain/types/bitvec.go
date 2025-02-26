d1d10ca204a17faf34a85260d54f67cb597780ae
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2025-02-25 20:51:26 -0400

    chore: replace host functions

--- dot/parachain/types/bitvec.go
+++ dot/parachain/types/bitvec.go
@@ -42,13 +42,6 @@ func NewBitVec(bits []bool) BitVec {
 	}
 }
 
-func (bv *BitVec) Get(idx int) bool {
-	if idx < 0 || idx >= len(bv.bits) {
-		return false
-	}
-	return bv.bits[idx]
-}
-
 // bitsToBytes converts a slice of bits to a slice of bytes
 // Uses lsb ordering
 // TODO: Implement msb ordering
