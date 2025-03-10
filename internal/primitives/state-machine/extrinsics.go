a381b87ef9bc1b91e161819442ece92fc5ca2b9d
Diego <diego2737@gmail.com>
2025-03-10 18:20:30 -0300

    Implement changeset, overlayedMap and extrinsics

---
+++ internal/primitives/state-machine/extrinsics.go
@@ -0,0 +1,23 @@
+package statemachine
+
+import "github.com/tidwall/btree"
+
+type Extrinsics []uint32
+
+func (e *Extrinsics) CopyExtrinsicsInto(dest *btree.Set[uint32]) {
+	for _, ex := range *e {
+		dest.Insert(ex)
+	}
+}
+
+func (e *Extrinsics) Insert(ext uint32) {
+	if len(*e) == 0 || ext != (*e)[len(*e)-1] {
+		*e = append(*e, ext)
+	}
+}
+
+func (e *Extrinsics) Extend(other Extrinsics) {
+	for _, ext := range other {
+		*e = append(*e, ext)
+	}
+}
