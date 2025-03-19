7fd4a15b9323d481e9597a8261e305401d8bb248
Diego <diego2737@gmail.com>
2025-03-19 18:04:56 -0300

    Remove prints

--- internal/primitives/state-machine/storage_entry.go
+++ internal/primitives/state-machine/storage_entry.go
@@ -3,8 +3,6 @@
 
 package statemachine
 
-import "fmt"
-
 // Content in an overlay for a given transactional depth.
 type StorageEntry interface {
 	value() StorageValue
@@ -50,37 +48,12 @@ func (se *AppendStorageEntry) optionalValue() StorageValue {
 	return se.data
 }
 
+// Materialize the internal state and cache the resulting materialized value.
 func (se *AppendStorageEntry) materializedInPlace() {
-	fmt.Printf("data1 %v \n", *se)
-
 	currentLength := se.currentLength
 	if se.materializedLength != nil && *se.materializedLength == currentLength {
 		return
 	}
 	NewStorageAppend(&se.data).ReplaceLength(se.materializedLength, currentLength)
 	se.materializedLength = &currentLength
-
-	fmt.Println("data2", *se)
-	fmt.Println("============================================")
-}
-
-func (a AppendStorageEntry) String() string {
-	// Para materializedLength
-	materializedLengthStr := "None"
-	if a.materializedLength != nil {
-		materializedLengthStr = fmt.Sprintf("Some(%d)", *a.materializedLength)
-	}
-
-	// Para parentSize
-	parentSizeStr := "None"
-	if a.parentSize != nil {
-		parentSizeStr = fmt.Sprintf("Some(%d)", *a.parentSize)
-	}
-
-	return fmt.Sprintf("Append{"+
-		" data: %v,"+
-		" currentLength: %d,"+
-		" materializedLength: %s,"+
-		" parentSize: %s"+
-		"}", a.data, a.currentLength, materializedLengthStr, parentSizeStr)
 }
