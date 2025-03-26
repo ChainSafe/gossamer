61ed110b2df1fb5f0925aebe85e16675c1930aa3
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2025-03-26 12:04:34 -0400

    chore: revert to Explicit

--- dot/parachain/backing/statement_table_test.go
+++ dot/parachain/backing/statement_table_test.go
@@ -219,7 +219,9 @@ func TestStatementTable_importStatement(t *testing.T) {
 		{
 			description: "unauthorised_seconded_statement",
 			table: statementTable{
-				detectedMisbehaviour: make(map[parachaintypes.ValidatorIndex][]parachaintypes.Misbehaviour),
+				detectedMisbehaviour: make(
+					map[parachaintypes.ValidatorIndex][]parachaintypes.Misbehaviour,
+				),
 			},
 			groupID: 1,
 			signedStatement: func() parachaintypes.SignedFullStatement {
@@ -240,7 +242,9 @@ func TestStatementTable_importStatement(t *testing.T) {
 		{
 			description: "unauthorised_valid_statement",
 			table: statementTable{
-				detectedMisbehaviour: make(map[parachaintypes.ValidatorIndex][]parachaintypes.Misbehaviour),
+				detectedMisbehaviour: make(
+					map[parachaintypes.ValidatorIndex][]parachaintypes.Misbehaviour,
+				),
 				candidateVotes: map[parachaintypes.CandidateHash]*candidateData{
 					candidateHash: {
 						groupID:       1,
@@ -296,7 +300,7 @@ func TestStatementTable_importCandidate(t *testing.T) {
 	candidate := getDummyCommittedCandidateReceipt(t)
 	var signature parachaintypes.ValidatorSignature
 
-	var tempSignature = common.MustHexToBytes("0xc67cb93bf0a36fcee3d29de8a6a69a759659680acf486475e0a2552a5fbed87e45adce5f290698d8596095722b33599227f7461f51af8617c8be74b894cf1b86") //nolint:lll
+	tempSignature := common.MustHexToBytes("0xc67cb93bf0a36fcee3d29de8a6a69a759659680acf486475e0a2552a5fbed87e45adce5f290698d8596095722b33599227f7461f51af8617c8be74b894cf1b86") //nolint:lll
 	copy(signature[:], tempSignature)
 
 	statementSeconded := parachaintypes.NewStatementVDT()
@@ -417,7 +421,13 @@ func TestStatementTable_importCandidate(t *testing.T) {
 		t.Run(tc.description, func(t *testing.T) {
 			t.Parallel()
 
-			summary, misehaviour, err := tc.table.importCandidate(authority, candidate, signature, tc.tableCtx, tc.group)
+			summary, misehaviour, err := tc.table.importCandidate(
+				authority,
+				candidate,
+				signature,
+				tc.tableCtx,
+				tc.group,
+			)
 			require.Equal(t, tc.expectedError, err)
 			require.Equal(t, tc.expectedMisehaviour, misehaviour)
 			require.Equal(t, tc.expectedSummary, summary)
@@ -439,7 +449,7 @@ func TestStatementTable_validityVote(t *testing.T) {
 	require.NoError(t, err)
 
 	var validatorSign parachaintypes.ValidatorSignature
-	var tempSignature = common.MustHexToBytes("0xc67cb93bf0a36fcee3d29de8a6a69a759659680acf486475e0a2552a5fbed87e45adce5f290698d8596095722b33599227f7461f51af8617c8be74b894cf1b86") //nolint:lll
+	tempSignature := common.MustHexToBytes("0xc67cb93bf0a36fcee3d29de8a6a69a759659680acf486475e0a2552a5fbed87e45adce5f290698d8596095722b33599227f7461f51af8617c8be74b894cf1b86") //nolint:lll
 	copy(validatorSign[:], tempSignature)
 
 	oldSign := parachaintypes.ValidatorSignature{}
@@ -660,7 +670,7 @@ func TestStatementTable_drainMisbehaviors(t *testing.T) {
 	require.NoError(t, err)
 
 	var validatorSign parachaintypes.ValidatorSignature
-	var tempSignature = common.MustHexToBytes("0xc67cb93bf0a36fcee3d29de8a6a69a759659680acf486475e0a2552a5fbed87e45adce5f290698d8596095722b33599227f7461f51af8617c8be74b894cf1b86") //nolint:lll
+	tempSignature := common.MustHexToBytes("0xc67cb93bf0a36fcee3d29de8a6a69a759659680acf486475e0a2552a5fbed87e45adce5f290698d8596095722b33599227f7461f51af8617c8be74b894cf1b86") //nolint:lll
 	copy(validatorSign[:], tempSignature)
 
 	oldSign := parachaintypes.ValidatorSignature{}
