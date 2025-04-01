fbb412026faa36a5694f5ce761f830cb34c94c9e
Diego <diego2737@gmail.com>
2025-04-01 12:43:54 -0300

    Add missing license

--- internal/primitives/state-machine/lib.go
+++ internal/primitives/state-machine/lib.go
@@ -1,3 +1,6 @@
+// Copyright 2025 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package state_machine
 
 import "github.com/ChainSafe/gossamer/internal/primitives/state-machine/backend"
