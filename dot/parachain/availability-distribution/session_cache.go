141b3f9fbc758d14bf84b740e4c51ec2d6ffc9aa
Haiko Schol <hs@haikoschol.com>
2025-04-01 14:50:47 +0200

    WIP

--- dot/parachain/availability-distribution/session_cache.go
+++ dot/parachain/availability-distribution/session_cache.go
@@ -2,11 +2,15 @@ package availabilitydistribution
 
 import (
 	parachaintypes "github.com/ChainSafe/gossamer/dot/parachain/types"
+	parachainutil "github.com/ChainSafe/gossamer/dot/parachain/util"
 	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/keystore"
 	"github.com/ChainSafe/gossamer/lib/runtime"
+	lrucache "github.com/ChainSafe/gossamer/lib/utils/lru-cache"
+	"math/rand/v2"
 )
 
-// Localised session information, tailored for the needs of availability distribution.
+// SessionInfo is localised session information, tailored for the needs of availability distribution.
 type SessionInfo struct {
 	/// The index of this session.
 	SessionIndex parachaintypes.SessionIndex
@@ -42,6 +46,8 @@ func (s *SessionInfo) NumberOfValidators() uint {
 }
 
 type SessionCache interface {
+	GetSessionIndexForChild(parent common.Hash, rt runtime.Instance) (parachaintypes.SessionIndex, error)
+
 	GetSessionInfo(
 		sessionIndex parachaintypes.SessionIndex,
 		parent common.Hash,
@@ -55,4 +61,148 @@ type SessionCache interface {
 	)
 }
 
-// TODO implement the interface (#4494)
+type LRUSessionCache struct {
+	keystore keystore.Keystore
+
+	// session index by relay parent
+	sessionIndexCache *lrucache.LRUCache[common.Hash, *parachaintypes.SessionIndex]
+	sessionInfoCache  *lrucache.LRUCache[parachaintypes.SessionIndex, *SessionInfo]
+}
+
+func NewLRUSessionCache(keystore keystore.Keystore) LRUSessionCache {
+	return LRUSessionCache{
+		keystore: keystore,
+
+		sessionIndexCache: lrucache.NewLRUCache[common.Hash, *parachaintypes.SessionIndex](10),
+		// We need the current and previous session.
+		sessionInfoCache: lrucache.NewLRUCache[parachaintypes.SessionIndex, *SessionInfo](2),
+	}
+}
+
+// GetSessionIndexForChild returns the session index expected at any child of the `parent` block.
+// This does not return the session index for the `parent` block.
+func (c *LRUSessionCache) GetSessionIndexForChild(
+	parent common.Hash,
+	rt runtime.Instance,
+) (parachaintypes.SessionIndex, error) {
+	c.sessionIndexCache.Lock()
+	cachedIndex := c.sessionIndexCache.Get(parent)
+	if cachedIndex != nil {
+		return *cachedIndex, nil
+	}
+
+	sessionIndex, err := rt.ParachainHostSessionIndexForChild()
+	if err != nil {
+		return 0, err
+	}
+
+	c.sessionIndexCache.Put(parent, &sessionIndex)
+	return sessionIndex, nil
+}
+
+// GetSessionInfo returns the session info for the given session index either from cache or from runtime information.
+// If this node is not a validator, it returns nil.
+func (c *LRUSessionCache) GetSessionInfo(
+	sessionIndex parachaintypes.SessionIndex,
+	parent common.Hash,
+	rt runtime.Instance,
+) (*SessionInfo, error) {
+	if cachedSessionInfo := c.sessionInfoCache.Get(sessionIndex); cachedSessionInfo != nil {
+		return cachedSessionInfo, nil
+	}
+
+	sessionInfo, err := rt.ParachainHostSessionInfo(sessionIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	validatorID, ourIndex := parachainutil.SigningKeyAndIndex(sessionInfo.Validators, c.keystore)
+	if validatorID == nil {
+		// This node is not a validator.
+		return nil, nil
+	}
+
+	nodeFeatures, err := rt.ParachainHostNodeFeatures()
+	if err != nil {
+		return nil, err
+	}
+
+	// Shuffle validator groups to load balance requests that will be sent to them later. If some of the validators
+	// fail to respond correctly, they will be moved inside their group by ReportBadValidators() to ensure they are
+	// tried last on subsequent requests.
+	validatorGroups := c.convertAndShuffleValidatorGroups(
+		sessionInfo.ValidatorGroups,
+		sessionInfo.DiscoveryKeys,
+		sessionIndex,
+	)
+
+	cachedSessionInfo := &SessionInfo{
+		SessionIndex:    sessionIndex,
+		ValidatorGroups: validatorGroups,
+		OurIndex:        ourIndex,
+		OurGroup:        c.getOurGroup(ourIndex, sessionInfo.ValidatorGroups),
+		NodeFeatures:    nodeFeatures,
+	}
+	c.sessionInfoCache.Put(sessionIndex, cachedSessionInfo)
+	return cachedSessionInfo, nil
+}
+
+func (c *LRUSessionCache) getOurGroup(
+	ourIndex parachaintypes.ValidatorIndex,
+	groups [][]parachaintypes.ValidatorIndex,
+) *parachaintypes.GroupIndex {
+	for groupIndex, group := range groups {
+		for _, validatorIndex := range group {
+			if validatorIndex == ourIndex {
+				g := parachaintypes.GroupIndex(groupIndex)
+				return &g
+			}
+		}
+	}
+	return nil
+}
+
+// convertAndShuffleValidatorGroups converts validator indices to authority discovery IDs and shuffles each group.
+func (c *LRUSessionCache) convertAndShuffleValidatorGroups(
+	validatorGroups [][]parachaintypes.ValidatorIndex,
+	discoveryKeys []parachaintypes.AuthorityDiscoveryID,
+	sessionIndex parachaintypes.SessionIndex,
+) [][]parachaintypes.AuthorityDiscoveryID {
+	shuffledGroups := make([][]parachaintypes.AuthorityDiscoveryID, len(validatorGroups))
+
+	for groupIndex, group := range validatorGroups {
+		// Convert indices to authority IDs for this group.
+		authIDs := make([]parachaintypes.AuthorityDiscoveryID, len(group))
+		for j, validatorIndex := range group {
+			authIDs[j] = discoveryKeys[int(validatorIndex)]
+		}
+
+		// Create a new random source using the session index and group index as seed.
+		// This ensures different nodes get different shuffles.
+		r := rand.New(rand.NewPCG(uint64(sessionIndex)*1000, uint64(groupIndex)))
+
+		// Shuffle the group
+		r.Shuffle(len(authIDs), func(i, j int) {
+			authIDs[i], authIDs[j] = authIDs[j], authIDs[i]
+		})
+
+		shuffledGroups[groupIndex] = authIDs
+	}
+	return shuffledGroups
+}
+
+// ReportBadValidators ensures we try unresponsive or misbehaving validators last.
+//
+// We assume validators in a group are tried in reverse order, so the reported bad validators
+// will be put at the beginning of the group.
+func (c *LRUSessionCache) ReportBadValidators(
+	sessionIndex parachaintypes.SessionIndex,
+	groupIndex parachaintypes.GroupIndex,
+	validators []parachaintypes.AuthorityDiscoveryID,
+) {
+	session := c.sessionInfoCache.Get(sessionIndex)
+	if session == nil {
+		return // TODO return error (change SessionCache interface in other branch)
+	}
+	// TODO move bad validators to the beginning of their group
+}
