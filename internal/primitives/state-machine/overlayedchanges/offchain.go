fb8cea17eb33ce3971041e3e7b94a3a7faeefdb1
Diego <diego2737@gmail.com>
2025-04-06 20:27:33 -0300

    Move OffchainOverlayedChanges to its own file

---
+++ internal/primitives/state-machine/overlayedchanges/offchain.go
@@ -0,0 +1,44 @@
+// Copyright 2025 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package overlayedchanges
+
+type OffchainOverlayedChange interface {
+	isOffchainOverlayedChange()
+}
+
+type (
+	OffchainOverlayedChangeRemove   struct{}
+	OffchainOverlayedChangeSetValue []byte
+)
+
+func (OffchainOverlayedChangeRemove) isOffchainOverlayedChange()   {}
+func (OffchainOverlayedChangeSetValue) isOffchainOverlayedChange() {}
+
+type OffchainOverlayedChanges struct {
+	OverlayedMap[string, OffchainOverlayedChange, *GenericOverlayedEntry[OffchainOverlayedChange]]
+}
+
+func NewOffchainOverlayedChanges() OffchainOverlayedChanges {
+	return OffchainOverlayedChanges{
+		NewOverlayedMap[string, OffchainOverlayedChange, *GenericOverlayedEntry[OffchainOverlayedChange]](),
+	}
+}
+
+func (oc OffchainOverlayedChanges) Clone() OffchainOverlayedChanges {
+	return OffchainOverlayedChanges{
+		oc.OverlayedMap.Clone(),
+	}
+}
+
+// Remove a key and its associated value from the offchain database.
+func (oc *OffchainOverlayedChanges) Set(prefix []byte, key []byte, value []byte) {
+	prefixedKey := string(append(prefix, key...))
+	oc.SetOffchain(prefixedKey, OffchainOverlayedChangeSetValue(value), nil)
+}
+
+// Remove a key and its associated value from the offchain database.
+func (oc *OffchainOverlayedChanges) Remove(prefix []byte, key []byte) {
+	prefixedKey := string(append(prefix, key...))
+	oc.SetOffchain(prefixedKey, OffchainOverlayedChangeRemove{}, nil)
+}
