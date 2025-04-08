6dbd38587f894f3843622e01ed0122951054689a
Eclésio Junior <eclesiomelo.1@gmail.com>
2025-04-08 15:46:49 -0400

    Update mock_transaction_handler_test.go

--- dot/rpc/modules/mock_transaction_handler_test.go
+++ dot/rpc/modules/mock_transaction_handler_test.go
@@ -8,8 +8,8 @@ import (
 	reflect "reflect"
 
 	network "github.com/ChainSafe/gossamer/dot/network"
-	peer "github.com/libp2p/go-libp2p/core/peer"
 	gomock "go.uber.org/mock/gomock"
+	peer "github.com/libp2p/go-libp2p/core/peer"
 )
 
 // MockTransactionHandler is a mock of TransactionHandler interface.
