f39c8952e642901a9a99baa22a7341a20030b61c
Timothy Wu <tim.wu@chainsafe.io>
2025-04-11 12:07:10 -0400

    unexport aux_schema method

--- internal/client/consensus/grandpa/aux_schema.go
+++ internal/client/consensus/grandpa/aux_schema.go
@@ -132,12 +132,12 @@ type writeAux func(insertions []api.KeyValue) error
 // 	}, nil
 // }
 
-// UpdateAuthoritySet Update the authority set on disk after a change.
+// updateAuthoritySet Update the authority set on disk after a change.
 //
 // If there has just been a handoff, pass a `new_set` parameter that describes the
 // handoff. `set` in all cases should reflect the current authority set, with all
 // changes and handoffs applied.
-func UpdateAuthoritySet[H runtime.Hash, N runtime.Number](
+func updateAuthoritySet[H runtime.Hash, N runtime.Number](
 	set AuthoritySet[H, N],
 	newSet *newAuthoritySet[H, N],
 	write writeAux,
