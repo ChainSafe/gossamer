3f41a86b36f53ae7685bd2a6bb24a1e962b2e650
Diego <diego2737@gmail.com>
2025-04-21 23:23:56 -0300

    Modify import alias

--- internal/primitives/api/api.go
+++ internal/primitives/api/api.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ChainSafe/gossamer/internal/primitives/runtime"
 	statemachine "github.com/ChainSafe/gossamer/internal/primitives/state-machine"
 	"github.com/ChainSafe/gossamer/internal/primitives/state-machine/overlayedchanges"
-	primitives_trie "github.com/ChainSafe/gossamer/internal/primitives/trie"
+	ptrie "github.com/ChainSafe/gossamer/internal/primitives/trie"
 	"github.com/ChainSafe/gossamer/internal/primitives/trie/recorder"
 )
 
@@ -68,7 +68,7 @@ type ApiExt[
 	// Extract the recorded proof.
 	// This stops the proof recording.
 	// If record_proof was not called before, this will return nil.
-	ExtractProof() *primitives_trie.StorageProof
+	ExtractProof() *ptrie.StorageProof
 	// Returns the current active proof recorder.
 	ProofRecorder() *ProofRecorder[H]
 	/// Convert the api object into the storage changes that were done while executing runtime
