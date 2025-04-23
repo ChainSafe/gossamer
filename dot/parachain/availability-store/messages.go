ca6db9dffad4ac689b064df902ab7288624465fe
Haiko Schol <hs@haikoschol.com>
2025-04-23 18:37:58 +0100

    fix(parachain): use correct type for ErasureChunk.Proof (#4670)
    Co-authored-by: Diego <diego2737@gmail.com>

--- dot/parachain/availability-store/messages.go
+++ dot/parachain/availability-store/messages.go
@@ -35,7 +35,10 @@ type QueryDataAvailability struct {
 type ErasureChunk struct {
 	Chunk []byte
 	Index uint32
-	Proof []byte
+
+	// TODO: ensure constraints on the size
+	// https://github.com/paritytech/polkadot-sdk/blob/017cd2a0687438cfd802af9e3703d9f69bcc03c6/polkadot/node/primitives/src/lib.rs#L585
+	Proof [][]byte
 }
 
 // QueryChunk query an `ErasureChunk` from the AV store by candidate hash and validator index
