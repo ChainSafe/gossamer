afbd0ab69c21bf6726983fad77a81b58327be4b8
Diego <diego2737@gmail.com>
2025-04-23 22:26:16 -0300

    Fix comments

--- internal/primitives/core/interfaces.go
+++ internal/primitives/core/interfaces.go
@@ -6,9 +6,9 @@ package core
 import "github.com/ChainSafe/gossamer/internal/primitives/externalities"
 
 type CodeExecutor interface {
-	/// Call a given method in the runtime.
-	/// Returns a tuple of the result (either the output data or an execution error) together with a
-	/// `bool`, which is true if native execution was used.
+	// Call a given method in the runtime.
+	// Returns a tuple of the result (either the output data or an execution error) together with a
+	// bool, which is true if native execution was used.
 	Call(
 		ext externalities.Externalities,
 		runtimeCode RuntimeCode,
