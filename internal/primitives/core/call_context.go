13b69a64a79b10bf46e9b15a06a3306a596702aa
Diego <diego2737@gmail.com>
2025-04-26 18:34:28 -0300

    Add missing comments

--- internal/primitives/core/call_context.go
+++ internal/primitives/core/call_context.go
@@ -3,9 +3,14 @@
 
 package core
 
+// The context in which a call is done.
+// Depending on the context the executor may chooses different kind of heap sizes for the runtime
+// instance.
 type CallContext uint8
 
 const (
+	// The call is happening in some offchain context
 	CallContextOffchain CallContext = iota
+	// The call is happening in some on-chain context like building or importing a block.
 	CallContextOnchain
 )
