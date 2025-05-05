49cec7d434a4479f43cd2bbd4fd8dd99ddf15d2d
Diego <diego2737@gmail.com>
2025-05-05 18:29:01 -0300

    Add some tests for guard

---
+++ internal/primitives/state-machine/abort_guard_test.go
@@ -0,0 +1,24 @@
+package statemachine
+
+import (
+	"testing"
+)
+
+func TestNeverAbort(t *testing.T) {
+	func() {
+		guard := NewGuard(NeverAbort)
+		defer guard.Done()
+		panic("test-panic")
+	}()
+}
+
+func TestUnwind(t *testing.T) {
+	defer func() {
+		if r := recover(); r != "test-unwind" {
+			t.Fatalf("expected panic 'test-unwind', got %v", r)
+		}
+	}()
+	guard := NewGuard(Unwind)
+	defer guard.Done()
+	panic("test-unwind")
+}
