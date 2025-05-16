7aa956493a17f87aeaefaf506e60736bfd72a0d7
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2025-05-16 16:12:18 -0400

    chore: define `v2::State` for statement-distribution

---
+++ dot/parachain/network/protocol.go
@@ -0,0 +1,9 @@
+package network
+
+type ValidationVersion byte
+
+const (
+	ValitionVersionV1 ValidationVersion = 1
+	ValitionVersionV2 ValidationVersion = 2
+	ValitionVersionV3 ValidationVersion = 3
+)
