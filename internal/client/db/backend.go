efb3bffee3158bebcd6d6b1ca01afcf3f16a3d9c
Diego <diego2737@gmail.com>
2025-05-18 21:54:25 -0300

    Fix wrong function call

--- internal/client/db/backend.go
+++ internal/client/db/backend.go
@@ -192,7 +192,7 @@ func (bio *BlockImportOperation[H, Hasher, N, Header, E]) applyNewState(
 	storage storage.Storage, stateVersion storage.StateVersion,
 ) (root H, err error) {
 	contains := slices.ContainsFunc(storage.Top.Keys(), func(key string) bool {
-		return keys.IsDefaultChildStorageKey([]byte(key))
+		return keys.IsChildStorageKey([]byte(key))
 	})
 	if contains {
 		return root, blockchain.ErrInvalidState
