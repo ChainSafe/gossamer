d04898f238bd7c70b919d056674562b0ed38bda7
Freddy Li <tobeacookie@gmail.com>
2025-06-03 13:24:50 -0400

    lint fix

--- dot/parachain/network-bridge/sender.go
+++ dot/parachain/network-bridge/sender.go
@@ -44,21 +44,21 @@ func (nbs *NetworkBridgeSender) Run(ctx context.Context, overseerToSubSystem <-c
 	}
 }
 
-func (nbs *NetworkBridgeSender) Name() parachaintypes.SubSystemName {
+func (*NetworkBridgeSender) Name() parachaintypes.SubSystemName {
 	return parachaintypes.NetworkBridgeSender
 }
 
-func (nbs *NetworkBridgeSender) ProcessActiveLeavesUpdateSignal(signal parachaintypes.ActiveLeavesUpdateSignal) error {
+func (*NetworkBridgeSender) ProcessActiveLeavesUpdateSignal(signal parachaintypes.ActiveLeavesUpdateSignal) error {
 	// nothing to do here
 	return nil
 }
 
-func (nbs *NetworkBridgeSender) ProcessBlockFinalizedSignal(signal parachaintypes.BlockFinalizedSignal) error {
+func (*NetworkBridgeSender) ProcessBlockFinalizedSignal(signal parachaintypes.BlockFinalizedSignal) error {
 	// nothing to do here
 	return nil
 }
 
-func (nbs *NetworkBridgeSender) Stop() {}
+func (*NetworkBridgeSender) Stop() {}
 
 func (nbs *NetworkBridgeSender) processMessage(msg any) error {
 	// run this function as a goroutine, ideally
