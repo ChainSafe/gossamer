460c7c281dd69a083a31bb21b968ed826a08d68b
Freddy Li <tobeacookie@gmail.com>
2025-06-04 12:08:06 -0400

    feat(dot/parachain): gossip support active leaves signal (#4731)

--- dot/parachain/network-bridge/messages/tx_overseer_messages.go
+++ dot/parachain/network-bridge/messages/tx_overseer_messages.go
@@ -7,11 +7,14 @@ import (
 	collatorprotocolmessages "github.com/ChainSafe/gossamer/dot/parachain/collator-protocol/messages"
 	parachaintypes "github.com/ChainSafe/gossamer/dot/parachain/types"
 	validationprotocol "github.com/ChainSafe/gossamer/dot/parachain/validation-protocol"
+	"github.com/multiformats/go-multiaddr"
 
 	"github.com/ChainSafe/gossamer/dot/peerset"
 	"github.com/libp2p/go-libp2p/core/peer"
 )
 
+// ReportPeer is type of peer reporting
+// Single peer report about malicious actions which should be sent right away
 // TODO: If need be, add ability to report multiple peers in batches
 type ReportPeer struct {
 	PeerID           peer.ID
@@ -62,6 +65,25 @@ type ConnectToValidators struct {
 	// Sends back the number of `AuthorityDiscoveryId`s which
 	// authority discovery has Failed to resolve.
 	Failed chan<- uint
+}
+
+// ConnectToResolvedValidators is alternative to `ConnectToValidators` in case you already know the `Multiaddrs`
+// you want to be connected to.
+type ConnectToResolvedValidators struct {
+	// Each entry corresponds to the addresses of an already resolved validator.
+	ValidatorAddrs map[multiaddr.Multiaddr]struct{}
+	// The peer set we want the connection on.
+	PeerSet PeerSetType
+}
+
+// AddToResolvedValidators extends the known validators set with new peers we already know the `Multiaddrs`, this is
+// usually needed for validators that change their address mid-session. It is usually called
+// after a ConnectToResolvedValidators at the beginning of the session.
+type AddToResolvedValidators struct {
+	// Each entry corresponds to the addresses of an already resolved validator.
+	ValidatorAddrs map[multiaddr.Multiaddr]struct{}
+	// The peer set we want the connection on.
+	PeerSet PeerSetType
 }
 
 type IfDisconnectedBehavior int
--- dot/parachain/service.go
+++ dot/parachain/service.go
@@ -7,10 +7,9 @@ import (
 	"fmt"
 	"time"
 
+	bitfielddistribution "github.com/ChainSafe/gossamer/dot/parachain/bitfield-distribution"
 	gossipsupport "github.com/ChainSafe/gossamer/dot/parachain/gossip-support"
 	"github.com/ChainSafe/gossamer/dot/parachain/provisioner"
-
-	bitfielddistribution "github.com/ChainSafe/gossamer/dot/parachain/bitfield-distribution"
 
 	availabilitydistribution "github.com/ChainSafe/gossamer/dot/parachain/availability-distribution"
 
@@ -127,7 +126,7 @@ func NewService(net Network, forkID string, st *state.Service, ks keystore.Keyst
 	overseer.RegisterSubsystem(disputesCoordinatorSubsystem)
 
 	// register gossip support subsystem
-	gossipSupportSubsystem := gossipsupport.NewGossipSupport(ks, overseer.SubsystemsToOverseer)
+	gossipSupportSubsystem := gossipsupport.NewGossipSupport(ks, overseer.SubsystemsToOverseer, st.Block)
 	overseer.RegisterSubsystem(gossipSupportSubsystem)
 
 	parachainService := &Service{
